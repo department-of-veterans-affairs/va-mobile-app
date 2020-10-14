ce246be04fc6f120ff3e98d0f3843e6e85d5f009
k_li <k_li@digitalfoundry.com>
2020-10-14 10:51:20 -0700

    Adding common components for text area and phone links (#31)
    * Adding common components for text area and phone links
    
    * Code revew feedback
    
    * Removed duplicate types and shorthand props
    
    * Updating themes file
    
    * Reverting color change
    
    * Fixing unit tests
    
    * Removing bg color from phonelink

---
+++ VAMobile/src/components/PhoneLink.tsx
@@ -0,0 +1,47 @@
+import { AccessibilityProps, Linking, TouchableWithoutFeedback } from 'react-native'
+import Box from './Box'
+import React, { FC } from 'react'
+
+import { testIdProps } from 'utils/accessibility'
+import TextView, { TextViewProps } from './TextView'
+import VAIcon from './VAIcon'
+
+/**
+ *  Signifies the props that need to be passed in to {@link PhoneLink}
+ */
+export type LinkButtonProps = AccessibilityProps & {
+	/** phone number used for link */
+	text: string
+}
+
+/**
+ * Reusable component used for opening native phone app
+ */
+const PhoneLink: FC<LinkButtonProps> = ({ text, ...props }) => {
+	const _onPress = (): void => {
+		// 888-123-1231 -> 8881231231
+		const _url = text.replace(/-/g, '')
+
+		//ex. tel:${phoneNumber}
+		Linking.openURL(`tel:${_url}`)
+	}
+
+	const textViewProps: TextViewProps = {
+		color: 'link',
+		variant: 'MobileBody',
+		ml: 4,
+		textDecoration: 'underline',
+		textDecorationColor: 'link',
+	}
+
+	return (
+		<TouchableWithoutFeedback onPress={_onPress} {...testIdProps(text)} accessibilityRole="link" accessible={true} {...props}>
+			<Box flexDirection={'row'} mt={8} mb={8} alignItems={'center'}>
+				<VAIcon name={'Phone'} fill={'link'} />
+				<TextView {...textViewProps}>{text}</TextView>
+			</Box>
+		</TouchableWithoutFeedback>
+	)
+}
+
+export default PhoneLink
