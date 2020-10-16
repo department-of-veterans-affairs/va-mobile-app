b9dbaac4e1532ad7c919f8e82ca3b412067a0193
Komal Yaseen <komal@digitalfoundry.com>
2020-10-16 15:57:15 -0700

    updates

--- VAMobile/src/screens/HomeScreen/VeteransCrisisLineScreen/VeteransCrisisLineScreen.tsx
+++ VAMobile/src/screens/HomeScreen/VeteransCrisisLineScreen/VeteransCrisisLineScreen.tsx
@@ -1,7 +1,9 @@
+import { ScrollView } from 'react-native'
 import React, { FC } from 'react'
 
-import { Box } from 'components'
+import { PhoneLink, TextArea, TextView } from 'components'
 import { testIdProps } from 'utils/accessibility'
+import { useTranslation } from 'utils/hooks'
 
 /**
  * View for Veterans Crisis Line screen
@@ -9,7 +11,20 @@ import { testIdProps } from 'utils/accessibility'
  * Returns VeteransCrisisLineScreen component
  */
 const VeteransCrisisLineScreen: FC = () => {
-	return <Box {...testIdProps('Veterans-Crisis-Line-screen')} />
+	const t = useTranslation('home')
+
+	return (
+		<ScrollView {...testIdProps('Veterans-Crisis-Line-screen')}>
+			<TextArea>
+				<TextView variant="MobileBodyBold">{t('veteransCrisisLine.weAreHereForYou')}</TextView>
+				<TextView variant="MobileBody">{t('veteransCrisisLine.connectWithResponders')}</TextView>
+				<PhoneLink text={t('veteransCrisisLine.crisisCallNumber')} />
+				<TextView variant="MobileBody">{t('veteransCrisisLine.callTTY')}</TextView>
+				<PhoneLink text={t('veteransCrisisLine.hearingLossNumber')} />
+				<TextView variant="MobileBodyBold">{t('veteransCrisisLine.getMoreResources')}</TextView>
+			</TextArea>
+		</ScrollView>
+	)
 }
 
 export default VeteransCrisisLineScreen
