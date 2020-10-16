c7b7143556050ab49a98a4b61781c9cb3de8e800
Komal Yaseen <komal@digitalfoundry.com>
2020-10-16 10:53:14 -0700

    fixed tests

--- VAMobile/src/screens/HomeScreen/ContactVAScreen/ContactVAScreen.test.tsx
+++ VAMobile/src/screens/HomeScreen/ContactVAScreen/ContactVAScreen.test.tsx
@@ -2,17 +2,27 @@ import 'react-native'
 import React from 'react'
 // Note: test renderer must be required after react-native.
 import { act } from 'react-test-renderer'
+import { RouteProp } from '@react-navigation/native'
+import { StackNavigationProp } from '@react-navigation/stack/lib/typescript/src/types'
 import 'jest-styled-components'
+import Mock = jest.Mock
 
 import ContactVAScreen from './ContactVAScreen'
 import { context, renderWithProviders } from 'testUtils'
+import { HomeStackParamList } from '../HomeScreen'
+
 
 context('ContactVAScreen', () => {
 	let component:any
+    let navigationSpy: Mock
+    let routeSpy: Mock
 
 	beforeEach(() => {
+	    navigationSpy = jest.fn(() => {})
+        routeSpy = jest.fn(() => {})
+
 		act(() => {
-			component = renderWithProviders(<ContactVAScreen />)
+			component = renderWithProviders(<ContactVAScreen navigation={navigationSpy as unknown as StackNavigationProp<HomeStackParamList, 'ContactVA'>} route={routeSpy as unknown as RouteProp<HomeStackParamList, 'ContactVA'>} />)
 		})
 	})
 
