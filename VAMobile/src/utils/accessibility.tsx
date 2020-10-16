2135ab9ce9a22fafd34f9d697d6faca38cc5a2cf
Ken Li <k_li@digitalfoundry.com>
2020-10-16 08:47:13 -0700

    Code review feedback

--- VAMobile/src/utils/accessibility.tsx
+++ VAMobile/src/utils/accessibility.tsx
@@ -8,11 +8,6 @@ interface AccessabilityProps {
 	accessibilityLabel?: string
 }
 export const testIdProps = (id: string, disableAccessible?: boolean): AccessabilityProps => {
-	// ignore test attributes set in this function for dev or prod as it exposes accessibilityLabel to users on android
-	if (!IS_TEST) {
-		return {}
-	}
-
 	const disableAccessibility = disableAccessible ? { accessible: false } : { accessible: undefined }
 
 	if (isIOS()) {
@@ -24,11 +19,5 @@ export const testIdProps = (id: string, disableAccessible?: boolean): Accessabil
 
 export const a11yHintProp = (hint: string): { accessibilityHint?: string } => {
 	// Remove a11yHints from tests as it can cause querying issues for android integration tests
-	if (IS_TEST) {
-		return {}
-	}
-
-	return {
-		accessibilityHint: hint,
-	}
+	return IS_TEST ? {} : { accessibilityHint: hint }
 }
