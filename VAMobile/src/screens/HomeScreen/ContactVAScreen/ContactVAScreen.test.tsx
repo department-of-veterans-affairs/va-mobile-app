f55649f9c4b31ca3b553b8a03cfd9abd6179f44a
Komal Yaseen <komal@digitalfoundry.com>
2020-10-16 11:38:09 -0700

    removed unnecessary spys

--- VAMobile/src/screens/HomeScreen/ContactVAScreen/ContactVAScreen.test.tsx
+++ VAMobile/src/screens/HomeScreen/ContactVAScreen/ContactVAScreen.test.tsx
@@ -3,21 +3,15 @@ import React from 'react'
 // Note: test renderer must be required after react-native.
 import { act } from 'react-test-renderer'
 import 'jest-styled-components'
-import Mock = jest.Mock
 
 import ContactVAScreen from './ContactVAScreen'
 import { context, renderWithProviders } from 'testUtils'
 
 context('ContactVAScreen', () => {
 	let component:any
-    let navigationSpy: Mock
-    let routeSpy: Mock
     const createTestProps = (props?: any) => ({ navigation: { navigate: jest.fn() }, route: {}, ...props });
 
 	beforeEach(() => {
-	    navigationSpy = jest.fn(() => {})
-        routeSpy = jest.fn(() => {})
-
         const props = createTestProps()
 
 		act(() => {
