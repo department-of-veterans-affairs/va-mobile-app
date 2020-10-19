bdbbea2abfc80014a6b3f87b0f26204de2ffa543
komaly15 <71414663+komaly15@users.noreply.github.com>
2020-10-19 09:33:31 -0700

    Generalized phone link component to be "click to call action" component (#47)
    * added svgs
    
    * updated svgs
    
    * updated fill
    
    * generalized phone link component to be click for action link
    
    * updated comment
    
    * linter
    
    * updated component so that text displayed may be different than url to redirect to
    
    * fixed test

---
+++ VAMobile/src/components/ClickForActionLink.tsx
@@ -0,0 +1,74 @@
+import { AccessibilityProps, Linking, TouchableWithoutFeedback } from 'react-native'
+import Box from './Box'
+import React, { FC } from 'react'
+
+import { testIdProps } from 'utils/accessibility'
+import TextView, { TextViewProps } from './TextView'
+import VAIcon, { VA_ICON_MAP } from './VAIcon'
+
+type LinkTypeOptions = 'text' | 'call' | 'url'
+
+/**
+ *  Signifies the props that need to be passed in to {@link ClickForActionLink}
+ */
+export type LinkButtonProps = AccessibilityProps & {
+	/** phone number or text for url, used for link */
+	text: string
+
+	/** string signifying the type of link it is (click to call/text/go to website) */
+	linkType: LinkTypeOptions
+
+	/** signifies actual link for url, may be different than text displayed */
+	urlLink?: string
+}
+
+/**
+ * Reusable component used for opening native calling app, texting app, or opening a url in the browser
+ */
+const ClickForActionLink: FC<LinkButtonProps> = ({ text, linkType, urlLink, ...props }) => {
+	const _onPress = (): void => {
+		// 888-123-1231 -> 8881231231
+		const number = text.replace(/-/g, '')
+
+		let openUrlText = urlLink ? urlLink : ''
+		if (linkType === 'call') {
+			openUrlText = `tel:${number}`
+		} else if (linkType === 'text') {
+			openUrlText = `sms:${number}`
+		}
+
+		// ex. numbers: tel:${phoneNumber}, sms:${phoneNumber}
+		// ex. url: https://google.com (need https for url)
+		Linking.openURL(openUrlText)
+	}
+
+	const getIconName = (): keyof typeof VA_ICON_MAP => {
+		switch (linkType) {
+			case 'call':
+				return 'Phone'
+			case 'text':
+				return 'Text'
+			case 'url':
+				return 'Chat'
+		}
+	}
+
+	const textViewProps: TextViewProps = {
+		color: 'link',
+		variant: 'MobileBody',
+		ml: 4,
+		textDecoration: 'underline',
+		textDecorationColor: 'link',
+	}
+
+	return (
+		<TouchableWithoutFeedback onPress={_onPress} {...testIdProps(text)} accessibilityRole="link" accessible={true} {...props}>
+			<Box flexDirection={'row'} mt={8} mb={8} alignItems={'center'}>
+				<VAIcon name={getIconName()} fill={'link'} />
+				<TextView {...textViewProps}>{text}</TextView>
+			</Box>
+		</TouchableWithoutFeedback>
+	)
+}
+
+export default ClickForActionLink
