6be0a13745729de2bb9895602321a86f2746d919
k_li <k_li@digitalfoundry.com>
2020-10-21 08:09:07 -0700

    Adding onPress support for textArea (#64)

--- VAMobile/src/components/TextArea.tsx
+++ VAMobile/src/components/TextArea.tsx
@@ -1,13 +1,22 @@
+import { TouchableWithoutFeedback } from 'react-native'
 import React, { FC } from 'react'
 
 import Box, { BoxProps } from './Box'
+
+/**
+ *  Signifies the props that need to be passed in to {@link TextArea}
+ */
+export type TextAreaProps = {
+  /** onPress callback */
+  onPress?: () => void
+}
 
 /**
  * Text area block for content
  *
  * @returns TextView component
  */
-const TextArea: FC = (props) => {
+const TextArea: FC<TextAreaProps> = ({ onPress, children }) => {
   const boxProps: BoxProps = {
     backgroundColor: 'textBox',
     p: 16,
@@ -20,7 +29,17 @@ const TextArea: FC = (props) => {
     borderTopColor: 'primary',
   }
 
-  return <Box {...boxProps}>{props.children}</Box>
+  const _onPress = (): void => {
+    if (onPress) {
+      onPress()
+    }
+  }
+
+  return (
+    <TouchableWithoutFeedback onPress={_onPress}>
+      <Box {...boxProps}>{children}</Box>
+    </TouchableWithoutFeedback>
+  )
 }
 
 export default TextArea
