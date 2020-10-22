4c9fb92ecc36e887b41a18f61dbfad67e470837d
John Marchi <j_marchi@digitalfoundry.com>
2020-10-22 14:59:26 -0700

    interim

--- VAMobile/src/screens/ProfileScreen/PersonalInformationScreen/EditEmailScreen.tsx
+++ VAMobile/src/screens/ProfileScreen/PersonalInformationScreen/EditEmailScreen.tsx
@@ -1,15 +1,28 @@
 import { StackScreenProps } from '@react-navigation/stack/lib/typescript/src/types'
 import React, { FC } from 'react'
 
-import { Box } from 'components'
+import { Box, SaveButton } from 'components'
 import { ProfileStackParamList } from '../ProfileStackParamList'
+
+// type SaveButtonProps = {
+//   onSave: () => void
+//   disabled: boolean
+// }
+//
+// const getSaveButton: FC<SaveButtonProps> = ({ onSave, disabled }) => {
+//   return (<SaveButton onSave={onSave} disabled={disabled} />)
+// }
 
 type EditEmailScreenProps = StackScreenProps<ProfileStackParamList, 'EditEmail'>
 
 const EditEmailScreen: FC<EditEmailScreenProps> = ({ navigation }) => {
-  // navigation.setOptions({
-  //   headerRight: () =>
-  // })
+  const saveEmail = (): void => {
+    console.log('saving email')
+  }
+
+  navigation.setOptions({
+    headerRight: () => <SaveButton onSave={saveEmail} disabled={false} />,
+  })
 
   return <Box />
 }
