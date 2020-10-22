99dd7a3a200db3b383b48895439b2480054a265b
k_li <k_li@digitalfoundry.com>
2020-10-22 08:08:45 -0700

    Military Information backend integration with mock (#69)

--- VAMobile/src/store/reducers/index.ts
+++ VAMobile/src/store/reducers/index.ts
@@ -2,28 +2,33 @@ import { combineReducers } from 'redux'
 
 import auth, { AuthState, initialAuthState } from './auth'
 import directDeposit, { DirectDepositState, initialDirectDepositState } from './directDeposit'
+import militaryService, { MilitaryServiceState, initialMilitaryServiceState } from './militaryService'
 import tabBar, { TabBarState, initialTabBarState } from './tabBar'
 
 export * from './auth'
 export * from './tabBar'
 export * from './directDeposit'
+export * from './militaryService'
 
 export interface StoreState {
   auth: AuthState
   tabBar: TabBarState
   directDeposit: DirectDepositState
+  militaryService: MilitaryServiceState
 }
 
 export const InitialState: StoreState = {
   auth: initialAuthState,
   tabBar: initialTabBarState,
   directDeposit: initialDirectDepositState,
+  militaryService: initialMilitaryServiceState,
 }
 
 const allReducers = combineReducers({
   auth,
   tabBar,
   directDeposit,
+  militaryService,
 })
 
 export default allReducers
