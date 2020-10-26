869c90281c10a3d20a53ad45030cb431b0926659
Komal Yaseen <komal@digitalfoundry.com>
2020-10-26 12:48:31 -0700

    test

---
+++ VAMobile/src/screens/ProfileScreen/PersonalInformationScreen/EditPhoneNumberScreen/EditPhoneNumberScreen.test.tsx
@@ -0,0 +1,51 @@
+import 'react-native'
+import React from 'react'
+// Note: test renderer must be required after react-native.
+import { act } from 'react-test-renderer'
+import { context, mockNavProps, mockStore, renderWithProviders } from 'testUtils'
+
+import EditPhoneNumberScreen from './EditPhoneNumberScreen'
+
+jest.mock("../../../../utils/hooks", ()=> {
+  const React = require('React')
+  let theme = jest.requireActual("../../../../styles/themes/standardTheme").default
+  return {
+    useTranslation: () => jest.fn(),
+    useTheme: jest.fn(()=> {
+      return {...theme}
+    })
+  }
+})
+
+context('EditPhoneNumberScreen', () => {
+  let store: any
+  let component: any
+
+  beforeEach(() => {
+    const props = mockNavProps(
+      {},
+      {
+        navigate: jest.fn(),
+        setOptions: jest.fn(),
+      },
+      {
+        params: {
+          displayTitle: 'Home phone',
+          phoneType: 'HOME',
+        },
+      },
+    )
+
+    store = mockStore({
+      auth: { initializing: true, loggedIn: false, loading: false },
+    })
+
+    act(() => {
+      component = renderWithProviders(<EditPhoneNumberScreen {...props} />, store)
+    })
+  })
+
+  it('initializes correctly', async () => {
+    expect(component).toBeTruthy()
+  })
+})
