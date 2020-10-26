dd885b1a3db293828518e72c19ec39fa0bf3f9f2
Brandon Levinger <brandon.levinger@gmail.com>
2020-10-26 09:54:14 -0700

    Fixing tests

--- VAMobile/src/store/types/index.ts
+++ VAMobile/src/store/types/index.ts
@@ -11,7 +11,10 @@ export * from './tabBar'
 export * from './directDeposit'
 export * from './militaryService'
 
-export interface ActionDef<T extends string, P> {
+type ActObjs<T extends keyof AllActionDefs> = AllActionDefs[T]
+type ActObjsPayload<T extends keyof AllActionDefs> = AllActionDefs[T]['payload']
+
+export interface ActionDef<T extends string, P extends ActObjsPayload<AllActionTypes>> {
   type: T
   payload: P
 }
@@ -22,7 +25,6 @@ export type StoreStateFn = () => StoreState
 
 export type AllActionDefs = AuthActions & TabBarActions & DirectDepositActions & MilitaryServiceActions
 
-type ActObjs<T extends keyof AllActionDefs> = AllActionDefs[T]
 type AllActionTypes = keyof AllActionDefs
 
 export type ReduxAction = ActObjs<AllActionTypes>
