5d59004b1e99cfcb6f31ebc61fe76e5f5b139d9a
komaly15 <71414663+komaly15@users.noreply.github.com>
2020-10-28 10:03:50 -0700

    Edit phone number screen testing (#96)
    * Adding VATextInput
    
    * fixing width
    
    * updated android manifest so navigation bar isn't pushed up when a keyboard appears
    
    * changing placeholder to use key
    
    * edit phone number ui updates
    
    * reverted app commenting
    
    * updates
    
    * updates
    
    * accessibility update
    
    * test
    
    * updates
    
    * fixed test
    
    * PR comment update
    
    * moved everything to auth action/reducer
    
    * moved everything to auth action/reducer
    
    * updated condition
    
    * tests
    
    * unit tests
    
    * updates
    
    * addressed PR comments
    
    * fixed lint error
    
    * added empty case
    
    * integration
    
    * updates
    
    * updates
    
    * spacing
    
    * spacing
    
    Co-authored-by: John Marchi <j_marchi@digitalfoundry.com>

--- VAMobile/src/screens/ProfileScreen/PersonalInformationScreen/EditPhoneNumberScreen/EditPhoneNumberScreen.test.tsx
+++ VAMobile/src/screens/ProfileScreen/PersonalInformationScreen/EditPhoneNumberScreen/EditPhoneNumberScreen.test.tsx
@@ -1,8 +1,9 @@
 import 'react-native'
 import React from 'react'
+import {TextInput} from 'react-native'
 // Note: test renderer must be required after react-native.
-import { act } from 'react-test-renderer'
-import { context, mockNavProps, mockStore, renderWithProviders } from 'testUtils'
+import {act, ReactTestInstance} from 'react-test-renderer'
+import {context, mockNavProps, mockStore, renderWithProviders} from 'testUtils'
 
 import EditPhoneNumberScreen from './EditPhoneNumberScreen'
 
@@ -19,6 +20,7 @@ jest.mock("../../../../utils/hooks", ()=> {
 context('EditPhoneNumberScreen', () => {
   let store: any
   let component: any
+  let testInstance: ReactTestInstance
 
   beforeEach(() => {
     const props = mockNavProps(
@@ -42,9 +44,52 @@ context('EditPhoneNumberScreen', () => {
     act(() => {
       component = renderWithProviders(<EditPhoneNumberScreen {...props} />, store)
     })
+
+    testInstance = component.root
   })
 
   it('initializes correctly', async () => {
     expect(component).toBeTruthy()
+  })
+
+  describe('when the text input changes', () => {
+    describe('when the length is less than or equal to 10 digits', () => {
+      it('should display just the numbers in the text input', async() => {
+        const phoneNumTextInput = testInstance.findAllByType(TextInput)[0]
+        phoneNumTextInput.props.onChangeText('12345')
+        expect(phoneNumTextInput.props.value).toEqual('12345')
+      })
+    })
+
+    describe('when the new text is greater than 10 digits', () => {
+      it('will not update phoneNumber to the new value', async() => {
+        const phoneNumTextInput = testInstance.findAllByType(TextInput)[0]
+        phoneNumTextInput.props.onChangeText('12345')
+        expect(phoneNumTextInput.props.value).toEqual('12345')
+
+        phoneNumTextInput.props.onChangeText('123456789011')
+        expect(phoneNumTextInput.props.value).toEqual('12345')
+      })
+    })
+  })
+
+  describe('when the text input has been edited', () => {
+    describe('when there are 10 digits', () => {
+      it('should set the value of the text input to the formatted number', async () => {
+        const phoneNumTextInput = testInstance.findAllByType(TextInput)[0]
+        phoneNumTextInput.props.onChangeText('1234567890')
+        phoneNumTextInput.props.onEndEditing()
+        expect(phoneNumTextInput.props.value).toEqual('(123)-456-7890')
+      })
+    })
+
+    describe('when there are not 10 digits', () => {
+      it('should set the value of the text input to the number', async () => {
+        const phoneNumTextInput = testInstance.findAllByType(TextInput)[0]
+        phoneNumTextInput.props.onChangeText('12345678')
+        phoneNumTextInput.props.onEndEditing()
+        expect(phoneNumTextInput.props.value).toEqual('12345678')
+      })
+    })
   })
 })
