28b94b7cc2ecd45d663170c90a4bfeefbab21892
Komal Yaseen <komal@digitalfoundry.com>
2020-10-29 12:04:41 -0700

    added blue border color

--- VAMobile/src/styles/themes/VAColors.ts
+++ VAMobile/src/styles/themes/VAColors.ts
@@ -7,6 +7,7 @@ export default {
   white: '#ffffff',
   black: '#000000',
   orange: '#eb7f29',
+  blue: '#0072C1',
   linkDefault: '#004795',
   warningMessage: '#fac922',
   gibillAccent: '#fff1d2',
