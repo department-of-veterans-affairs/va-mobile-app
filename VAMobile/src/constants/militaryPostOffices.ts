43f8ef98fc9cf96db9335b276d8e2285ac099232
Komal Yaseen <komal@digitalfoundry.com>
2020-11-03 14:13:15 -0800

    edit address military base updates

---
+++ VAMobile/src/constants/militaryPostOffices.ts
@@ -0,0 +1,14 @@
+export const MilitaryPostOffices = [
+  {
+    value: 'APO',
+    label: 'APO',
+  },
+  {
+    value: 'FPO',
+    label: 'FPO',
+  },
+  {
+    value: 'DPO',
+    label: 'DPO',
+  },
+]
