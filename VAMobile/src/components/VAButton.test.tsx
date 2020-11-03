a072c57394f8698a2b22136aef2bf3b5f08d1d6d
John Marchi <j_marchi@digitalfoundry.com>
2020-11-03 12:07:06 -0800

    Button Common Component (#134)
    * interim
    
    * reusable button
    
    * PR comments
    
    * pr comments
    
    * pr comments
    
    * adding accessibility hint

---
+++ VAMobile/src/components/VAButton.test.tsx
@@ -0,0 +1,33 @@
+import 'react-native'
+import React from 'react'
+// Note: test renderer must be required after react-native.
+import 'jest-styled-components'
+import { ReactTestInstance, act } from 'react-test-renderer'
+import Mock = jest.Mock
+
+import { context, renderWithProviders } from 'testUtils'
+import VAButton from './VAButton'
+
+context('VAButton', () => {
+  let component: any
+  let testInstance: ReactTestInstance
+  let onPressSpy: Mock
+
+  beforeEach(() => {
+    onPressSpy = jest.fn(() => {})
+
+    act(() => {
+      component = renderWithProviders(<VAButton label={'my bytton'} onPress={onPressSpy} textColor="primaryContrast" backgroundColor="button" />)
+    })
+    testInstance = component.root
+  })
+
+  it('initializes correctly', async () => {
+    expect(component).toBeTruthy()
+  })
+
+  it('should call onChange', async () => {
+    testInstance.findByType(VAButton).props.onPress()
+    expect(onPressSpy).toBeCalled()
+  })
+})
