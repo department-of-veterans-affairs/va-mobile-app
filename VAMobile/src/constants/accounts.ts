ed56bcfc8e86f47edf805a80be3cac32857a7d02
k_li <k_li@digitalfoundry.com>
2020-11-11 09:30:03 -0800

    Updating actions and store for direct deposit (#156)
    * Updating actions and store for direct desposit
    
    * Adding missing parameter for getBankInfo error case
    
    * Updating accounts types within constant folder

--- VAMobile/src/constants/accounts.ts
+++ VAMobile/src/constants/accounts.ts
@@ -1,15 +1,25 @@
-export const AccountTypes = {
-  CHECKING: 'checking',
-  SAVINGS: 'savings',
+import { AccountTypes } from 'store/api/types'
+
+export const ACCOUNT_TYPES: {
+  checking: AccountTypes
+  savings: AccountTypes
+} = {
+  checking: 'Checking',
+  savings: 'Savings',
 }
 
-export const AccountOptions = [
+export type AccountOption = {
+  label: string
+  value: AccountTypes
+}
+
+export const AccountOptions: Array<AccountOption> = [
   {
     label: 'common:accountType.checking',
-    value: AccountTypes.CHECKING,
+    value: ACCOUNT_TYPES.checking,
   },
   {
     label: 'common:accountType.savings',
-    value: AccountTypes.SAVINGS,
+    value: ACCOUNT_TYPES.savings,
   },
 ]
