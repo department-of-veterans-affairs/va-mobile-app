b5c0d1b5ab3590566cedfbcfc9bfa42fe4f18381
Komal Yaseen <komal@digitalfoundry.com>
2020-11-12 08:49:39 -0800

    updated some switch styles

--- VAMobile/src/components/Switch.tsx
+++ VAMobile/src/components/Switch.tsx
@@ -1,20 +1,28 @@
-import { AccessibilityProperties, Switch as RNSwitch } from 'react-native'
+import { AccessibilityProps, Switch as RNSwitch } from 'react-native'
 import React, { FC } from 'react'
 
 import { useTheme } from 'utils/hooks'
+import styled from 'styled-components/native'
 
-export type SwitchProps = AccessibilityProperties & {
+export type SwitchProps = AccessibilityProps & {
   onPress: (value: boolean) => void
   on?: boolean
 }
+
+const StyledRNSwitch = styled(RNSwitch)`
+  shadow-opacity: 0.3;
+  shadow-radius: 1px;
+  shadow-offset: 0px 0.5px;
+  min-width: 51px;
+`
 
 const Switch: FC<SwitchProps> = (props) => {
   const { onPress, on } = props
   const theme = useTheme()
   return (
-    <RNSwitch
+    <StyledRNSwitch
       trackColor={{ false: theme.colors.control.switchOffTrack, true: theme.colors.control.switchOnTrack }}
-      thumbColor={on ? theme.colors.control.switchOnThumb : theme.colors.control.switchOffThumb}
+      thumbColor={theme.colors.control.switchOnThumb}
       onValueChange={onPress}
       value={!!on}
     />
