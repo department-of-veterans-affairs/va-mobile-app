bf3b54f6abb2659bf35d3ba10c1a8321d8e3b872
komaly15 <71414663+komaly15@users.noreply.github.com>
2020-11-13 10:11:53 -0800

    Appointments overview screen (#174)
    * added gesture recognizer package
    
    * segmented controller update
    
    * updated test
    
    * removed unused impmorts
    
    * appointments overview screen
    
    * updated function names
    
    * fixed lint errors
    
    * updated for accessibility
    
    * moved props into const
    
    * removed swipe package
    
    * removed gesture recognizer usage
    
    * updates
    
    * updates
    
    * updates
    
    * updated segmented controller usage

---
+++ VAMobile/src/screens/AppointmentsScreen/PastAppointments/PastAppointments.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react'
+
+import { TextView } from 'components'
+
+type PastAppointmentsProps = {}
+
+const PastAppointments: FC<PastAppointmentsProps> = () => {
+  return <TextView>Past appointments</TextView>
+}
+
+export default PastAppointments
