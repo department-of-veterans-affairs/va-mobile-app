02f69717c5cd0c96bf35b41d380d220adc970e2a
komaly15 <71414663+komaly15@users.noreply.github.com>
2020-11-17 10:19:40 -0800

    Benefit Summary & Service Verification letter (#183)
    * updates 1
    
    * added iris url
    
    * updates w/ url
    
    * added enum
    
    * updated wide buttons text width based on onPress
    
    * Created action/reducers for data and formatted data
    
    * updated to use flex w/  margin left
    
    * added icon
    
    * updated names
    
    * updates
    
    * updates
    
    * added accessibility hint to switch
    
    * added accessiibility hints
    
    * updated to const
    
    * updated to const
    
    * moved useEffect
    
    * removed console log and raw text from profile json, used common instead
    
    * standardized margins to use existing values
    
    * updated constant name per PR comment
    
    * lint update

--- VAMobile/src/utils/formattingUtils.ts
+++ VAMobile/src/utils/formattingUtils.ts
@@ -1,3 +1,5 @@
+import { format } from 'date-fns'
+
 /**
  * Returns the formatted phone number
  *
@@ -7,4 +9,31 @@
  */
 export const formatPhoneNumber = (phoneNumber: string): string => {
   return `(${phoneNumber.substring(0, 3)})-${phoneNumber.substring(3, 6)}-${phoneNumber.substring(6)}`
+}
+
+/**
+ * Returns the date formatted in the format MMMM dd, yyyy
+ *
+ * @param date - string signifying the raw date, i.e. 2013-06-06T04:00:00.000+00:00
+ *
+ * @returns date string formatted as MMMM dd, yyyy
+ */
+export const formatDateMMMMDDYYYY = (date: string): string => {
+  if (date) {
+    const newDate = new Date(date)
+    return format(new Date(newDate.getUTCFullYear(), newDate.getUTCMonth(), newDate.getUTCDate()), 'MMMM dd, yyyy')
+  }
+
+  return ''
+}
+
+/**
+ * Returns the word formatted so that the first letter is upper case and the rest is lowercase
+ *
+ * @param word - word to capitalize
+ *
+ * @returns word with capitalized first letter and rest of the word lowercased
+ */
+export const capitalizeWord = (word: string): string => {
+  return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
 }
