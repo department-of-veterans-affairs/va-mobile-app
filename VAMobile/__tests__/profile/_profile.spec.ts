a0b96aabb90e3a97dcff56c24936e9c1d7156787
Komal Yaseen <komal@digitalfoundry.com>
2020-11-19 12:01:55 -0800

    updated commented code

--- VAMobile/__tests__/profile/_profile.spec.ts
+++ VAMobile/__tests__/profile/_profile.spec.ts
@@ -1,7 +1,7 @@
 import { doLogin, logout } from '../utils'
 import ProfilePageFlow from './ProfilePageFlow'
 
-xdescribe('profile', () => {
+describe('profile', () => {
     before(async () => {
         if (driver.isAndroid) {
             console.log("Resetting app")
