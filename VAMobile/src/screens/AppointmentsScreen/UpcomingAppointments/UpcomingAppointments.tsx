a490af33eb496667cd5d1184ace7dd296f450c48
Komal Yaseen <komal@digitalfoundry.com>
2020-11-18 16:08:25 -0800

    test updates

--- VAMobile/src/screens/AppointmentsScreen/UpcomingAppointments/UpcomingAppointments.tsx
+++ VAMobile/src/screens/AppointmentsScreen/UpcomingAppointments/UpcomingAppointments.tsx
@@ -1,15 +1,16 @@
 import { useDispatch, useSelector } from 'react-redux'
-import React, { FC, ReactInstance, useEffect } from 'react'
+import React, { FC, ReactNode, useEffect } from 'react'
 
 import _ from 'underscore'
 
-import { AppointmentType, AppointmentTypeConstants, AppointmentTypeToName } from 'store/api/types'
+import { AppointmentType, AppointmentTypeConstants, AppointmentTypeToName, AppointmentsList } from 'store/api/types'
 import { AppointmentsState, StoreState } from 'store/reducers'
 import { Box, ButtonList, ButtonListItemObj, TextView, textIDObj } from 'components'
 import { DateTime, DateTimeFormatOptions } from 'luxon'
 import { NAMESPACE } from 'constants/namespaces'
 import { getAppointmentsInDateRange } from 'store/actions'
 import { getFormattedDate } from 'utils/formattingUtils'
+import { testIdProps } from 'utils/accessibility'
 import { useTheme, useTranslation } from 'utils/hooks'
 
 type UpcomingAppointmentsProps = {}
@@ -60,7 +61,28 @@ const UpcomingAppointments: FC<UpcomingAppointmentsProps> = () => {
     return yearToSortedMonths
   }
 
-  const getGroupedAppointments = (): ReactInstance => {
+  const getButtonListItems = (listOfAppointments: AppointmentsList): Array<ButtonListItemObj> => {
+    const buttonListItems: Array<ButtonListItemObj> = []
+
+    _.forEach(listOfAppointments, (appointment) => {
+      const { attributes } = appointment
+
+      const textIDs: Array<textIDObj> = [
+        { textID: 'common:text.raw', fieldObj: { text: getFormattedDateTime(attributes.startTime, DateTime.DATE_FULL, attributes.timeZone, { weekday: 'long' }) } },
+        {
+          textID: { id: 'common:text.raw', isBold: true },
+          fieldObj: { text: getFormattedDateTime(attributes.startTime, DateTime.TIME_SIMPLE, attributes.timeZone, { timeZoneName: 'short' }) },
+        },
+        { textID: 'common:text.raw', fieldObj: { text: getLocation(attributes.appointmentType, attributes.location.name) } },
+      ]
+
+      buttonListItems.push({ textIDs, onPress: onAppointmentPress })
+    })
+
+    return buttonListItems
+  }
+
+  const getGroupedAppointments = (): ReactNode => {
     if (!appointmentsByYear) {
       return <></>
     }
@@ -71,25 +93,9 @@ const UpcomingAppointments: FC<UpcomingAppointmentsProps> = () => {
     return _.map(sortedYears, (year) => {
       return _.map(yearsToSortedMonths[year], (month) => {
         const listOfAppointments = appointmentsByYear[year][month]
+        const buttonListItems = getButtonListItems(listOfAppointments)
 
-        const buttonListItems: Array<ButtonListItemObj> = []
-
-        _.forEach(listOfAppointments, (appointment) => {
-          const { attributes } = appointment
-
-          const textIDs: Array<textIDObj> = [
-            { textID: 'common:text.raw', fieldObj: { text: getFormattedDateTime(attributes.startTime, DateTime.DATE_FULL, attributes.timeZone, { weekday: 'long' }) } },
-            {
-              textID: { id: 'common:text.raw', isBold: true },
-              fieldObj: { text: getFormattedDateTime(attributes.startTime, DateTime.TIME_SIMPLE, attributes.timeZone, { timeZoneName: 'short' }) },
-            },
-            { textID: 'common:text.raw', fieldObj: { text: getLocation(attributes.appointmentType, attributes.location.name) } },
-          ]
-
-          buttonListItems.push({ textIDs, onPress: onAppointmentPress })
-        })
-
-        const displayedMonth = getFormattedDate(new Date(parseInt(year), parseInt(month)).toISOString(), 'MMMM')
+        const displayedMonth = getFormattedDate(new Date(parseInt(year, 10), parseInt(month, 10)).toISOString(), 'MMMM')
 
         return (
           <Box key={month} mb={theme.dimensions.marginBetween}>
@@ -104,7 +110,7 @@ const UpcomingAppointments: FC<UpcomingAppointmentsProps> = () => {
   }
 
   return (
-    <Box>
+    <Box {...testIdProps('Upcoming-appointments')}>
       <TextView mx={theme.dimensions.gutter} mb={theme.dimensions.marginBetween}>
         {t('upcomingAppointments.confirmedApptsDisplayed')}
       </TextView>
