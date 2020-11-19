9211c2bda87854a7f25b338f441473eb207a55b8
John Marchi <j_marchi@digitalfoundry.com>
2020-11-19 08:25:08 -0800

    Buttonlist refactor to use translated text (#198)
    * Bumping iOS version for release branch (#155)
    
    * bumping iOS version
    
    * updating version
    
    * updating version for sprint 3 demo
    
    * Adding NSFaceIDUsageDescription to iOS build (#157)
    
    * Adding NSFaceIDUsageDescription
    
    * Update VAMobile/ios/VAMobile/Info.plist
    
    From Bridget: Hi there! I am just looking at his suggested change - I am fine with that change. I think communicating the value is a good idea, but also think it could be confusing to use a modal to do that rather than just focusing on asking for permission. Let's accept for now and I'll ask for a writer's perspective on this.
    
    Co-authored-by: Jonathan Julian <jonathan@adhocteam.us>
    
    Co-authored-by: Jonathan Julian <jonathan@adhocteam.us>
    
    * interim
    
    * refactoring buttonlist to take in translated strings
    
    Co-authored-by: Jonathan Julian <jonathan@adhocteam.us>

---
+++ VAMobile/src/components/types/TextLine.tsx
@@ -0,0 +1,10 @@
+/**
+ * Represents a line of text and associated properties
+ */
+export type TextLine = {
+  /** string to display */
+  text: string
+
+  /** if true makes the line bold */
+  isBold?: boolean
+}
