57e37ec8bfb41da3082d878a37dca1b3fd1310ed
komaly15 <71414663+komaly15@users.noreply.github.com>
2020-11-19 11:07:05 -0800

    Upcoming appointments screen (#201)
    * created action/reducer to get appointments in given date range
    
    * updated to group by month, added scrollview
    
    * added parameter to wide button
    
    * updates
    
    * updated wide button to take in list of objects
    
    * fixed test
    
    * pulled widebutton
    
    * updates
    
    * updates
    
    * test updates
    
    * test updates
    
    * moved function
    
    * fixed errors
    
    * broke apart date functions per PR comment
    
    * pulled develop and updated button list object
    
    * updated const to map to translation IDs
    
    * updated function names

--- VAMobile/src/utils/formattingUtils.ts
+++ VAMobile/src/utils/formattingUtils.ts
@@ -1,3 +1,4 @@
+import { DateTime, DateTimeFormatOptions } from 'luxon'
 import { format } from 'date-fns'
 
 /**
@@ -12,6 +13,74 @@ export const formatPhoneNumber = (phoneNumber: string): string => {
 }
 
 /**
+ * Returns the date formatted utilizing the luxon DateTime functionality
+ *
+ * @param dateTime - string signifying the raw date, i.e. 2013-06-06T04:00:00.000+00:00
+ * @param dateTimeType - type DateTimeFormatOptions, to describe the format of the resulting string i.e. DateTime.DATE_SHORT = mm/dd/yyy
+ * @param timeZone - optional parameter to set the timezone if there is one given
+ * @param dateTimeOptions - optional fields to additionally display onto the date
+ *
+ * @returns date string formatted based on formatBy
+ */
+export const getFormattedDateOrTimeWithFormatOption = (
+  dateTime: string,
+  dateTimeType: DateTimeFormatOptions,
+  timeZone?: string,
+  dateTimeOptions?: { [key: string]: string },
+): string => {
+  const dateObj = DateTime.fromISO(dateTime)
+  if (timeZone) {
+    dateObj.setZone(timeZone)
+  }
+
+  return dateObj.toLocaleString(Object.assign(dateTimeType, dateTimeOptions))
+}
+
+/**
+ * Returns the date formatted in the format DAY OF WEEK, MONTH DAY, YEAR
+ *
+ * @param dateTime - string signifying the raw date, i.e. 2013-06-06T04:00:00.000+00:00
+ * @param timeZone - string signifying the current timeZone i.e. America/Los_Angeles
+ *
+ * @returns the date formatted in the format DAY OF WEEK, MONTH DAY, YEAR
+ */
+export const getFormattedDateWithWeekdayForTimeZone = (dateTime: string, timeZone: string): string => {
+  return getFormattedDateOrTimeWithFormatOption(dateTime, DateTime.DATE_FULL, timeZone, { weekday: 'long' })
+}
+
+/**
+ * Returns the date formatted in the format HH:MM aa TIMEZONE
+ *
+ * @param dateTime - string signifying the raw date, i.e. 2013-06-06T04:00:00.000+00:00
+ * @param timeZone - string signifying the current timeZone i.e. America/Los_Angeles
+ *
+ * @returns  the date formatted in the format HH:MM aa TIMEZONE
+ */
+export const getFormattedTimeForTimeZone = (dateTime: string, timeZone: string): string => {
+  return getFormattedDateOrTimeWithFormatOption(dateTime, DateTime.TIME_SIMPLE, timeZone, { timeZoneName: 'short' })
+}
+
+/**
+ * Returns the date formatted utilizing the formatBy parameter
+ *
+ * @param date - string signifying the raw date, i.e. 2013-06-06T04:00:00.000+00:00
+ * @param formatBy - string signifying how the date should be formatted, i.e. MMMM dd, yyyy
+ *
+ * @returns date string formatted based on formatBy
+ */
+export const getFormattedDate = (date: string, formatBy: string): string => {
+  if (date) {
+    const newDate = new Date(date)
+    return format(
+      new Date(newDate.getUTCFullYear(), newDate.getUTCMonth(), newDate.getUTCDate(), newDate.getUTCHours(), newDate.getUTCMinutes(), newDate.getUTCSeconds()),
+      formatBy,
+    )
+  }
+
+  return ''
+}
+
+/**
  * Returns the date formatted in the format MMMM dd, yyyy
  *
  * @param date - string signifying the raw date, i.e. 2013-06-06T04:00:00.000+00:00
@@ -19,12 +88,7 @@ export const formatPhoneNumber = (phoneNumber: string): string => {
  * @returns date string formatted as MMMM dd, yyyy
  */
 export const formatDateMMMMDDYYYY = (date: string): string => {
-  if (date) {
-    const newDate = new Date(date)
-    return format(new Date(newDate.getUTCFullYear(), newDate.getUTCMonth(), newDate.getUTCDate()), 'MMMM dd, yyyy')
-  }
-
-  return ''
+  return getFormattedDate(date, 'MMMM dd, yyyy')
 }
 
 /**
