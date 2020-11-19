3392ba875b33d3b06af3b792e03fe25d5d769ac7
Komal Yaseen <komal@digitalfoundry.com>
2020-11-18 16:09:50 -0800

    test updates

--- VAMobile/__tests__/screenObjects/appointments.screen.tsx
+++ VAMobile/__tests__/screenObjects/appointments.screen.tsx
@@ -2,8 +2,8 @@ import AppScreen from './app.screen';
 
 const SELECTORS = {
 	APPOINTMENTS_SCREEN: '~Appointments-screen',
-  APPOINTMENTS_UPCOMING_TAB: '~Upcoming',
-  APPOINTMENTS_PAST_TAB: '~Past',
+      APPOINTMENTS_UPCOMING_TAB: '~Upcoming',
+      APPOINTMENTS_PAST_TAB: '~Past',
 };
 
 class AppointmentsScreen extends AppScreen {
@@ -15,9 +15,9 @@ class AppointmentsScreen extends AppScreen {
 	  return $(SELECTORS.APPOINTMENTS_UPCOMING_TAB)
   }
 
-  get appointmentsPastTab() {
-    return $(SELECTORS.APPOINTMENTS_PAST_TAB)
-  }
+      get appointmentsPastTab() {
+        return $(SELECTORS.APPOINTMENTS_PAST_TAB)
+      }
 }
 
 export default new AppointmentsScreen()
