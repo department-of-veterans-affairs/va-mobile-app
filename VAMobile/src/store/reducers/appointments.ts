0b20e3397ee23710b7054eeb28904e0f17a3b69a
Komal Yaseen <komal@digitalfoundry.com>
2020-12-07 09:34:58 -0800

    reverted data removal

--- VAMobile/src/store/reducers/appointments.ts
+++ VAMobile/src/store/reducers/appointments.ts
@@ -46,6 +46,8 @@ export default createReducer<AppointmentsState>(initialAppointmentsState, {
 
     return {
       ...state,
+      appointmentsList,
+      appointmentsByYear,
       error,
       loading: false,
     }
