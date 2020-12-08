01e7223be9daa0be80515594cc62cef1b0f06603
Ken Li <k_li@digitalfoundry.com>
2020-12-08 15:07:56 -0800

    Updated hints and text for accessibility for ContactVA

--- VAMobile/src/screens/HomeScreen/ContactVAScreen/ContactVAScreen.tsx
+++ VAMobile/src/screens/HomeScreen/ContactVAScreen/ContactVAScreen.tsx
@@ -1,8 +1,8 @@
 import { ScrollView } from 'react-native'
-import { StackScreenProps } from '@react-navigation/stack'
-import React, { FC } from 'react'
+import { StackHeaderLeftButtonProps, StackScreenProps } from '@react-navigation/stack'
+import React, { FC, ReactNode, useEffect } from 'react'
 
-import { Box, ClickForActionLink, LinkTypeOptionsConstants, TextArea, TextView } from 'components'
+import { BackButton, Box, ClickForActionLink, LinkTypeOptionsConstants, TextArea, TextView } from 'components'
 import { HomeStackParamList } from '../HomeScreen'
 import { NAMESPACE } from 'constants/namespaces'
 import { a11yHintProp, testIdProps } from 'utils/accessibility'
@@ -16,7 +16,7 @@ type ContactVAScreenProps = StackScreenProps<HomeStackParamList, 'ContactVA'>
  *
  * Returns ContactVAScreen component
  */
-const ContactVAScreen: FC<ContactVAScreenProps> = () => {
+const ContactVAScreen: FC<ContactVAScreenProps> = ({ navigation }) => {
   const theme = useTheme()
   const t = useTranslation(NAMESPACE.HOME)
   const navigateTo = useRouteNavigation()
@@ -24,6 +24,14 @@ const ContactVAScreen: FC<ContactVAScreenProps> = () => {
   const onCrisisLine = navigateTo('VeteransCrisisLine')
 
   const marginBetween = theme.dimensions.marginBetween
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerLeft: (props: StackHeaderLeftButtonProps): ReactNode => (
+        <BackButton onPress={props.onPress} canGoBack={props.canGoBack} i18nId={'back'} showCarat={true} a11yHint={t('contactVA.back.a11yHint')} />
+      ),
+    })
+  })
 
   return (
     <ScrollView>
@@ -40,7 +48,7 @@ const ContactVAScreen: FC<ContactVAScreenProps> = () => {
             displayedText={t('contactVA.va311.numberDisplayed')}
             numberOrUrlLink={t('contactVA.va311.number')}
             linkType={LinkTypeOptionsConstants.call}
-            {...a11yHintProp(t('contactVA.va311.number.a11yHint'))}
+            {...a11yHintProp(t('contactVA.number.a11yHint'))}
           />
           <TextView color="primary" variant="MobileBody" my={marginBetween}>
             {t('contactVA.tty.body')}
@@ -49,7 +57,7 @@ const ContactVAScreen: FC<ContactVAScreenProps> = () => {
             displayedText={t('contactVA.tty.number')}
             numberOrUrlLink={t('contactVA.tty.number')}
             linkType={LinkTypeOptionsConstants.call}
-            {...a11yHintProp(t('contactVA.tty.number.a11yHint'))}
+            {...a11yHintProp(t('contactVA.number.a11yHint'))}
           />
         </TextArea>
       </Box>
