f659b4660ca99510000075361cafe6468362d833
Patrick Saxton <patrick@urban-intellect.com>
2020-12-10 17:04:51 -0600

    add documentation

--- VAMobile/src/components/VADatePicker.tsx
+++ VAMobile/src/components/VADatePicker.tsx
@@ -6,17 +6,29 @@ import RNDateTimePicker from '@react-native-community/datetimepicker'
 import React, { FC, useState } from 'react'
 import theme from '../styles/themes/standardTheme'
 
+/**
+ * Props type for the VADatePicker
+ */
 export type VADatePickerProps = {
   /** What should the picker show if no value is selected? */
   defaultString: string
   /** function to run on date selected */
   onChange: (event: Event, selectedDate: Date | undefined) => void
 }
+/**
+ * Common component for selecting a date. (This can be updated to a date/time picker later if we need it)
+ * @param defaultString - string that represents what the component should display if there is no value selected
+ * @param onChange - function to run in the parent when selection changes
+ */
 const VADatePicker: FC<VADatePickerProps> = ({ defaultString, onChange }) => {
+  // value is the display value in the text area. Is human readable
   const [value, setValue] = useState(defaultString)
+  // boolean to show or hide the picker
   const [show, setShow] = useState(false)
+  // date value that is used by the picker
   const [date, setDate] = useState(new Date())
 
+  // this helps simulate the UI of a VATextInput
   const textColor = value === defaultString ? 'placeholder' : 'primary'
 
   const wrapperProps: BoxProps = {
@@ -31,9 +43,10 @@ const VADatePicker: FC<VADatePickerProps> = ({ defaultString, onChange }) => {
     alignItems: 'center',
     backgroundColor: 'textBox',
   }
+
   return (
     <Box {...wrapperProps}>
-      <TouchableOpacity onPress={() => setShow(true)}>
+      <TouchableOpacity onPress={(): void => setShow(true)}>
         <TextView variant={'MobileBody'} color={textColor}>
           {value}
         </TextView>
