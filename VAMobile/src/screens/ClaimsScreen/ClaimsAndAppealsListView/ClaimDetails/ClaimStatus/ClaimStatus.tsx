e7606b1f57ea3581abe65e84c662f91b14ce4319
Komal Yaseen <komal@digitalfoundry.com>
2020-12-10 10:50:26 -0800

    moved data to active specific component

--- VAMobile/src/screens/ClaimsScreen/ClaimsAndAppealsListView/ClaimDetails/ClaimStatus/ClaimStatus.tsx
+++ VAMobile/src/screens/ClaimsScreen/ClaimsAndAppealsListView/ClaimDetails/ClaimStatus/ClaimStatus.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import React, { FC, ReactElement } from 'react'
 
 import { Box, ClickForActionLink, LinkButtonProps, LinkTypeOptionsConstants, List, ListItemObj, TextArea, TextView } from 'components'
 import { ClaimData } from 'store/api/types'
@@ -16,31 +16,45 @@ const ClaimStatus: FC<ClaimStatusProps> = ({ claim }) => {
   const t = useTranslation(NAMESPACE.CLAIMS)
   const navigateTo = useRouteNavigation()
 
-  const displayDate = claim && claim.attributes && claim.attributes.maxEstDate ? formatDateMMMMDDYYYY(claim.attributes.maxEstDate) : t('claimDetails.noEstimatedDecisionDate')
-
-  const detailsFAQListItems: Array<ListItemObj> = [
-    { textLines: t('claimDetails.whyWeCombine'), onPress: navigateTo('ConsolidatedClaimsNote') },
-    { textLines: t('claimDetails.whatShouldIDoIfDisagree'), onPress: navigateTo('WhatDoIDoIfDisagreement') },
-  ]
-
   const clickToCallProps: LinkButtonProps = {
     displayedText: t('claimDetails.VANumberDisplayed'),
     numberOrUrlLink: t('claimDetails.VANumber'),
     linkType: LinkTypeOptionsConstants.call,
   }
 
+  const ActiveClaimStatusDetails = (): ReactElement => {
+    const isActiveClaim = claim?.attributes?.open || false
+
+    if (isActiveClaim) {
+      const displayDate = claim && claim.attributes && claim.attributes.maxEstDate ? formatDateMMMMDDYYYY(claim.attributes.maxEstDate) : t('claimDetails.noEstimatedDecisionDate')
+
+      const detailsFAQListItems: Array<ListItemObj> = [
+        { textLines: t('claimDetails.whyWeCombine'), onPress: navigateTo('ConsolidatedClaimsNote') },
+        { textLines: t('claimDetails.whatShouldIDoIfDisagree'), onPress: navigateTo('WhatDoIDoIfDisagreement') },
+      ]
+
+      return (
+        <Box>
+          <TextArea>
+            <TextView variant="MobileBody">{t('claimDetails.estimatedDecisionDate')}</TextView>
+            <TextView variant="MobileBodyBold">{displayDate}</TextView>
+            <TextView variant="MobileBody" mt={theme.dimensions.marginBetween}>
+              {t('claimDetails.weBaseThis')}
+            </TextView>
+          </TextArea>
+          <Box mt={theme.dimensions.marginBetweenCards}>
+            <List items={detailsFAQListItems} />
+          </Box>
+        </Box>
+      )
+    }
+
+    return <></>
+  }
+
   return (
     <Box>
-      <TextArea>
-        <TextView variant="MobileBody">{t('claimDetails.estimatedDecisionDate')}</TextView>
-        <TextView variant="MobileBodyBold">{displayDate}</TextView>
-        <TextView variant="MobileBody" mt={theme.dimensions.marginBetween}>
-          {t('claimDetails.weBaseThis')}
-        </TextView>
-      </TextArea>
-      <Box mt={theme.dimensions.marginBetweenCards}>
-        <List items={detailsFAQListItems} />
-      </Box>
+      <ActiveClaimStatusDetails />
       <Box mt={theme.dimensions.marginBetweenCards}>
         <TextArea>
           <TextView variant="MobileBodyBold" accessibilityRole="header">
