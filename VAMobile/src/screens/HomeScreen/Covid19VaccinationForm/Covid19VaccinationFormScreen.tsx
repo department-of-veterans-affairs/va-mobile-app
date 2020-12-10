88a2202052fc83a79c763c1eca46976aeac3cb3c
Patrick Saxton <patrick@urban-intellect.com>
2020-12-10 13:24:32 -0600

    update for home button, revert if needs some other type, else cherry pick and make solo PR

---
+++ VAMobile/src/screens/HomeScreen/Covid19VaccinationForm/Covid19VaccinationFormScreen.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react'
+
+export type Covid19VaccinationFormScreenProps = {
+
+}
+const Covid19VaccinationFormScreen: FC<Covid19VaccinationFormScreenProps> = () => {
+    
+    return ()
+}
+
+export default Covid19VaccinationFormScreen
