d311567a119b3120b71ed8e49ecdc22c1491b512
Komal Yaseen <komal@digitalfoundry.com>
2020-12-14 11:22:44 -0800

    updated check

--- VAMobile/src/screens/ClaimsScreen/ClaimDetails/ClaimStatus/ClaimStatus.tsx
+++ VAMobile/src/screens/ClaimsScreen/ClaimDetails/ClaimStatus/ClaimStatus.tsx
@@ -30,8 +30,7 @@ const ClaimStatus: FC<ClaimStatusProps> = ({ claim, claimType }) => {
     const isActiveClaim = claimType === ClaimTypeConstants.ACTIVE
 
     if (isActiveClaim) {
-      const estimatedDateExists = !!claim && !!claim.attributes && !!claim.attributes.maxEstDate
-      const displayDate = estimatedDateExists ? formatDateMMMMDDYYYY(claim.attributes.maxEstDate) : t('claimDetails.noEstimatedDecisionDate')
+      const displayDate = claim?.attributes?.maxEstDate ? formatDateMMMMDDYYYY(claim.attributes.maxEstDate) : t('claimDetails.noEstimatedDecisionDate')
 
       const detailsFAQListItems: Array<ListItemObj> = [
         { textLines: t('claimDetails.whyWeCombine'), onPress: navigateTo('ConsolidatedClaimsNote') },
@@ -43,7 +42,7 @@ const ClaimStatus: FC<ClaimStatusProps> = ({ claim, claimType }) => {
           <TextArea>
             <TextView variant="MobileBody">{t('claimDetails.estimatedDecisionDate')}</TextView>
             <TextView variant="MobileBodyBold">{displayDate}</TextView>
-            {estimatedDateExists && (
+            {!!claim?.attributes?.maxEstDate && (
               <TextView variant="MobileBody" mt={theme.dimensions.marginBetween}>
                 {t('claimDetails.weBaseThis')}
               </TextView>
