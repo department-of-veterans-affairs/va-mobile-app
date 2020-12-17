fcb7c1da351e01c69684c1cab72b928ae37777c1
k_li <k_li@digitalfoundry.com>
2020-12-17 09:04:01 -0800

    Integrating direct deposit endpoint (#303)
    Co-authored-by: lexicalninja <patrick@urban-intellect.com>

--- VAMobile/__tests__/profile/ProfilePageFlow.ts
+++ VAMobile/__tests__/profile/ProfilePageFlow.ts
@@ -284,7 +284,7 @@ export default () => {
 
       it('should go to the edit direct deposit screen and render its content', async () => {
         // Go to edit direct deposit screen
-        const directDepositBankEdit = await DirectDepositScreen.directDepositBankEdit('bank-of-america-*************1234-savings-account')
+        const directDepositBankEdit = await DirectDepositScreen.directDepositBankEdit()
         await directDepositBankEdit.click()
         await EditDirectDepositScreen.waitForIsShown()
       })
--- VAMobile/src/store/actions/directDeposit.test.ts
+++ VAMobile/src/store/actions/directDeposit.test.ts
@@ -1,14 +1,34 @@
 import { context, realStore } from 'testUtils'
 import { finishEditBankInfo, getBankData, updateBankInfo } from './directDeposit'
 import { find } from 'underscore'
-import { AccountTypes } from '../api/types'
+import { DirectDepositData } from '../api/types'
+import { when } from "jest-when";
+import * as api from "../api";
 
 context('directDeposit', () => {
   describe('getBankData', () => {
-    it('should get the users bank data', () => {
-      const store = realStore()
-      store.dispatch(getBankData())
+    it('should get the users bank data', async () => {
+      const mockBankInfoPayload: DirectDepositData = {
+        data: {
+          type: 'paymentInformation',
+          id: '75fbb2a6-6c7e-5003-95dc-a950697f4937',
+          attributes: {
+            paymentAccount: {
+              accountNumber: '*************1234',
+              accountType: 'Savings',
+              financialInstitutionName: 'Bank',
+              financialInstitutionRoutingNumber: '04120282'
+            }
+          }
+        }
+      }
 
+      when(api.get as jest.Mock)
+          .calledWith('/v0/payment-information/benefits')
+          .mockResolvedValue(mockBankInfoPayload)
+
+      const store = realStore()
+      await store.dispatch(getBankData())
       const actions = store.getActions()
 
       const startAction = find(actions, { type: 'DIRECT_DEPOSIT_START_GET_BANK_DATA' })
@@ -21,17 +41,67 @@ context('directDeposit', () => {
 
       expect(store.getState().directDeposit.paymentAccount).toEqual({
         accountNumber: '*************1234',
-        accountType: 'Savings account',
-        financialInstitutionName: 'Bank of America',
-        financialInstitutionRoutingNumber: '948529982',
+        accountType: 'Savings',
+        financialInstitutionName: 'Bank',
+        financialInstitutionRoutingNumber: '04120282',
       })
+    })
+
+    it('should get error if it cannot get data', async () => {
+      const error = new Error('error from backend')
+
+      when(api.get as jest.Mock)
+          .calledWith('/v0/payment-information/benefits')
+          .mockRejectedValue(error)
+
+      const store = realStore()
+      await store.dispatch(getBankData())
+      const actions = store.getActions()
+
+      const startAction = find(actions, { type: 'DIRECT_DEPOSIT_START_GET_BANK_DATA' })
+      expect(startAction).toBeTruthy()
+      expect(startAction?.state.directDeposit.loading).toBeTruthy()
+
+      const finishAction = find(actions, { type: 'DIRECT_DEPOSIT_FINISH_GET_BANK_DATA' })
+      expect(finishAction).toBeTruthy()
+      expect(finishAction?.state.directDeposit.loading).toBeFalsy()
+
+      const { directDeposit } = store.getState()
+      expect(directDeposit.paymentAccount).toEqual({})
+      expect(directDeposit.error).toEqual(error)
     })
   })
 
   describe('updateBankInfo', () => {
-    it('should update bankInfo', () => {
+    it('should update bankInfo', async () => {
+      const mockBankInfoPayload: DirectDepositData = {
+        data: {
+          type: 'paymentInformation',
+          id: '75fbb2a6-6c7e-5003-95dc-a950697f4937',
+          attributes: {
+            paymentAccount: {
+              accountNumber: '*************5678',
+              accountType: 'Savings',
+              financialInstitutionName: 'Bank',
+              financialInstitutionRoutingNumber: '987654321'
+            }
+          }
+        }
+      }
+
+      const updateBankInfoData: api.PaymentAccountData = {
+        accountNumber: '12345678912345678',
+        accountType: 'Savings',
+        financialInstitutionRoutingNumber: '987654321',
+        financialInstitutionName: 'Bank', // api requires a
+      }
+
+      when(api.put as jest.Mock)
+          .calledWith('/v0/payment-information/benefits', updateBankInfoData)
+          .mockResolvedValue(mockBankInfoPayload)
+
       const store = realStore()
-      store.dispatch(updateBankInfo('12345678912345678', '987654321', 'Savings'))
+      await store.dispatch(updateBankInfo(updateBankInfoData.accountNumber, updateBankInfoData.financialInstitutionRoutingNumber, updateBankInfoData.accountType))
 
       const actions = store.getActions()
 
@@ -44,13 +114,40 @@ context('directDeposit', () => {
       expect(finishAction?.state.directDeposit.loading).toBeFalsy()
 
       const { directDeposit } = store.getState()
-      expect(directDeposit.paymentAccount).toEqual({
-        accountNumber: '12345678912345678',
-        accountType: 'Savings account',
-        financialInstitutionName: undefined,
-        financialInstitutionRoutingNumber: '987654321',
-      })
+      expect(directDeposit.paymentAccount).toEqual(mockBankInfoPayload.data.attributes.paymentAccount)
       expect(directDeposit.error).toBeFalsy()
+    })
+
+    it('should get error if it cannot update data', async () => {
+      const error = new Error('error from backend')
+
+      const updateBankInfoData: api.PaymentAccountData = {
+        accountNumber: '12345678912345678',
+        accountType: 'Savings',
+        financialInstitutionRoutingNumber: '987654321',
+        financialInstitutionName: 'Bank', // api requires a
+      }
+
+      when(api.put as jest.Mock)
+          .calledWith('/v0/payment-information/benefits', updateBankInfoData)
+          .mockRejectedValue(error)
+
+      const store = realStore()
+      await store.dispatch(updateBankInfo(updateBankInfoData.accountNumber, updateBankInfoData.financialInstitutionRoutingNumber, updateBankInfoData.accountType))
+
+      const actions = store.getActions()
+
+      const startAction = find(actions, { type: 'DIRECT_DEPOSIT_START_SAVE_BANK_INFO' })
+      expect(startAction).toBeTruthy()
+      expect(startAction?.state.directDeposit.loading).toBeTruthy()
+
+      const finishAction = find(actions, { type: 'DIRECT_DEPOSIT_FINISH_SAVE_BANK_INFO' })
+      expect(finishAction).toBeTruthy()
+      expect(finishAction?.state.directDeposit.loading).toBeFalsy()
+
+      const { directDeposit } = store.getState()
+      expect(directDeposit.paymentAccount).toEqual({})
+      expect(directDeposit.error).toEqual(error)
     })
   })
 
