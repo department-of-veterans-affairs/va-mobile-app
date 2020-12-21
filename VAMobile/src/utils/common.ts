dead293944e6819e1c61aeda502648e345dfe841
Komal Yaseen <komal@digitalfoundry.com>
2020-12-21 13:46:20 -0800

    fixed lint error

--- VAMobile/src/utils/common.ts
+++ VAMobile/src/utils/common.ts
@@ -1,8 +1,8 @@
 import { PixelRatio, TextInput } from 'react-native'
 import { RefObject } from 'react'
 
-import RNPickerSelect from 'react-native-picker-select'
 import { DateTime } from 'luxon'
+import RNPickerSelect from 'react-native-picker-select'
 
 import { PhoneData } from 'store/api/types/PhoneData'
 import { formatPhoneNumber } from './formattingUtils'
