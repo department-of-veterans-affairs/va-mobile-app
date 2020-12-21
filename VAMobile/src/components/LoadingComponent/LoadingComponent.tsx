31d8c5b802ef5029e56a5aeabb22884be88cde57
Patrick Saxton <patrick@urban-intellect.com>
2020-12-21 11:01:51 -0600

    create loading component

---
+++ VAMobile/src/components/LoadingComponent/LoadingComponent.tsx
@@ -0,0 +1,33 @@
+import { ActivityIndicator, ScrollView, ViewStyle } from 'react-native'
+import { TextView } from 'components'
+import { useTheme } from 'utils/hooks'
+import Box from '../Box'
+import React, { FC } from 'react'
+
+export type LoadingComponentProps = {
+  text?: string
+}
+const LoadingComponent: FC<LoadingComponentProps> = ({ text }) => {
+  const theme = useTheme()
+
+  const scrollStyles: ViewStyle = {
+    flexGrow: 1,
+    justifyContent: 'center',
+    backgroundColor: theme.colors.background.main,
+  }
+
+  return (
+    <ScrollView contentContainerStyle={scrollStyles}>
+      <Box justifyContent="center" mx={theme.dimensions.gutter} mt={theme.dimensions.contentMarginTop} mb={theme.dimensions.contentMarginBottom}>
+        <ActivityIndicator size="large" color={theme.colors.icon.spinner} />
+        <Box mt={theme.dimensions.marginBetween}>
+          <TextView textAlign={'center'} variant="MobileBody">
+            {text}
+          </TextView>
+        </Box>
+      </Box>
+    </ScrollView>
+  )
+}
+
+export default LoadingComponent
