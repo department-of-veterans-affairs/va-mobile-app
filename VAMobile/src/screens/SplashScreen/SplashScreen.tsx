13f12f105d649a186cdd329129eed4263316a599
Patrick Saxton <patrick@urban-intellect.com>
2020-12-21 15:06:42 -0600

    rough out splash ui

--- VAMobile/src/screens/SplashScreen/SplashScreen.tsx
+++ VAMobile/src/screens/SplashScreen/SplashScreen.tsx
@@ -1,12 +1,22 @@
-import { Box, TextView } from 'components'
+import { Box, VAIcon } from 'components'
+import { ScrollView, ViewStyle } from 'react-native'
+import { useTheme } from 'utils/hooks'
 import React, { FC } from 'react'
 
 export type SplashScreenProps = {}
 const SplashScreen: FC<SplashScreenProps> = () => {
+  const theme = useTheme()
+  const splashStyles: ViewStyle = {
+    flexGrow: 1,
+    justifyContent: 'center',
+    backgroundColor: theme.colors.background.splashScreen,
+  }
   return (
-    <Box>
-      <TextView>Splash Screen</TextView>
-    </Box>
+    <ScrollView contentContainerStyle={splashStyles}>
+      <Box justifyContent="center" mx={theme.dimensions.gutter} mt={theme.dimensions.contentMarginTop} mb={theme.dimensions.contentMarginBottom} alignItems={'center'}>
+        <VAIcon name={'Logo'} />
+      </Box>
+    </ScrollView>
   )
 }
 
