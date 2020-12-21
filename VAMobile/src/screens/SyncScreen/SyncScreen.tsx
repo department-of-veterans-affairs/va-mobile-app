aef9229d4f857d478034ecb848b7fc58e4286607
Patrick Saxton <patrick@urban-intellect.com>
2020-12-21 15:06:52 -0600

    rough out sync ui

--- VAMobile/src/screens/SyncScreen/SyncScreen.tsx
+++ VAMobile/src/screens/SyncScreen/SyncScreen.tsx
@@ -1,12 +1,31 @@
-import { Box, TextView } from 'components'
+import { Box, TextView, VAIcon } from 'components'
+import { ScrollView, ViewStyle } from 'react-native'
+import { useTheme } from 'utils/hooks'
 import React, { FC } from 'react'
 
 export type SyncScreenProps = {}
 const SyncScreen: FC<SyncScreenProps> = () => {
+  const theme = useTheme()
+  const splashStyles: ViewStyle = {
+    flexGrow: 1,
+    justifyContent: 'center',
+    backgroundColor: theme.colors.background.splashScreen,
+  }
+
+  // TODO set up store and api calls to show different text as time goes on or fake it
+
   return (
-    <Box>
-      <TextView>Sync Screen</TextView>
-    </Box>
+    <ScrollView contentContainerStyle={splashStyles}>
+      <Box justifyContent="center" mx={theme.dimensions.gutter} mt={theme.dimensions.contentMarginTop} mb={theme.dimensions.contentMarginBottom} alignItems={'center'}>
+        <VAIcon name={'Logo'} />
+        <Box flexDirection={'row'} alignItems={'center'} justifyContent={'center'} mx={theme.dimensions.gutter}>
+          <VAIcon name={'CheckMark'} fill="#fff" />
+          <TextView justifyContent={'flex-start'} ml={10} color={'primaryContrast'}>
+            Connecting...
+          </TextView>
+        </Box>
+      </Box>
+    </ScrollView>
   )
 }
 
