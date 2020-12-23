8380ac9f1a7de8a85d60b08ede311f7c0a8b1881
Komal Yaseen <komal@digitalfoundry.com>
2020-12-23 10:30:20 -0800

    updates

--- VAMobile/src/screens/ClaimsScreen/AppealDetailsScreen/AppealStatus/AppealDecision/AppealDecision.tsx
+++ VAMobile/src/screens/ClaimsScreen/AppealDetailsScreen/AppealStatus/AppealDecision/AppealDecision.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import React, { FC, ReactElement } from 'react'
 
 import _ from 'underscore'
 
@@ -18,14 +18,18 @@ type AppealDecisionProps = {
 const AppealDecision: FC<AppealDecisionProps> = ({ issues, aoj, ama, boardDecision }) => {
   const t = useTranslation(NAMESPACE.CLAIMS)
 
-  const allowedIssues = issues.filter((issue) => issue.disposition === 'allowed')
-  const deniedIssues = issues.filter((issue) => issue.disposition === 'denied')
-  const remandIssues = issues.filter((issue) => issue.disposition === 'remand')
+  const getIssuesByDisposition = (stringToCompare: string): Array<AppealStatusDetailsIssue> => {
+    return issues.filter((issue) => issue.disposition === stringToCompare)
+  }
+
+  const allowedIssues = getIssuesByDisposition('allowed')
+  const deniedIssues = getIssuesByDisposition('denied')
+  const remandIssues = getIssuesByDisposition('remand')
 
   const pluralize = {
-    allowed: allowedIssues.length > 1 ? 'issues' : 'issue',
-    denied: deniedIssues.length > 1 ? 'issues' : 'issue',
-    remand: remandIssues.length > 1 ? 'these issues' : 'this issue',
+    allowed: allowedIssues.length > 1 ? t('appealDetails.issues') : t('appealDetails.issue'),
+    denied: deniedIssues.length > 1 ? t('appealDetails.issues') : t('appealDetails.issue'),
+    remand: remandIssues.length > 1 ? t('appealDetails.theseIssues') : t('appealDetails.thisIssue'),
   }
 
   const aojDescription = getAojDescription(aoj, t)
@@ -36,16 +40,32 @@ const AppealDecision: FC<AppealDecisionProps> = ({ issues, aoj, ama, boardDecisi
     })
   }
 
+  const getSpecificIssuesBlock = (header: string, subText: string, specificIssues: Array<AppealStatusDetailsIssue>): ReactElement => {
+    if (specificIssues.length === 0) {
+      return <></>
+    }
+
+    return (
+      <Box>
+        <TextView variant="MobileBodyBold">{header}</TextView>
+        <TextView variant="MobileBody">{subText}</TextView>
+        <VABulletList listOfText={getIssuesListOfText(specificIssues)} />
+      </Box>
+    )
+  }
+
   let allowedBlock = null
   let deniedBlock = null
   let remandBlock = null
 
+  const judgeOrReviewer = boardDecision ? t('appealDetails.judge') : t('appealDetails.reviewer')
+
   if (allowedIssues.length > 0) {
     allowedBlock = (
       <Box>
-        <TextView variant="MobileBodyBold">Granted</TextView>
+        <TextView variant="MobileBodyBold">{t('appealDetails.granted')}</TextView>
         <TextView variant="MobileBody">
-          The {boardDecision ? 'judge' : 'reviewer'} granted the following {pluralize.allowed}:
+          {t('appealDetails.personGrantedOrDenied', { person: judgeOrReviewer, action: t('appealDetails.granted').toLowerCase(), pluralizedIssue: pluralize.allowed })}
         </TextView>
         <VABulletList listOfText={getIssuesListOfText(allowedIssues)} />
       </Box>
@@ -55,9 +75,9 @@ const AppealDecision: FC<AppealDecisionProps> = ({ issues, aoj, ama, boardDecisi
   if (deniedIssues.length > 0) {
     deniedBlock = (
       <Box>
-        <TextView variant="MobileBodyBold">Denied</TextView>
+        <TextView variant="MobileBodyBold">{t('appealDetails.denied')}</TextView>
         <TextView variant="MobileBody">
-          The {boardDecision ? 'judge' : 'reviewer'} denied the following {pluralize.denied}:
+          {t('appealDetails.personGrantedOrDenied', { person: judgeOrReviewer, action: t('appealDetails.denied').toLowerCase(), pluralizedIssue: pluralize.denied })}
         </TextView>
         <VABulletList listOfText={getIssuesListOfText(deniedIssues)} />
       </Box>
@@ -69,8 +89,11 @@ const AppealDecision: FC<AppealDecisionProps> = ({ issues, aoj, ama, boardDecisi
       <Box>
         <TextView variant="MobileBodyBold">Remand</TextView>
         <TextView variant="MobileBody">
-          The judge is sending {pluralize.remand} back to the {aojDescription} to
-          {ama ? 'correct an error' : 'gather more evidence or to fix a mistake before deciding whether to grant or deny'}
+          {t('appealDetails.judgeSendingBack', {
+            pluralizedIssue: pluralize.remand,
+            aojDesc: aojDescription,
+            action: ama ? t('appealDetails.correctAnError') : t('appealDetails.gatherMoreEvidence'),
+          })}
         </TextView>
         <VABulletList listOfText={getIssuesListOfText(remandIssues)} />
       </Box>
