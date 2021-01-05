32f5bc8d2dd89585b9521404dc8da8ac82b3448a
komaly15 <71414663+komaly15@users.noreply.github.com>
2021-01-05 10:46:01 -0800

    Claims File Upload page | Section 2 (#344)
    * claim file upload section 1
    
    * ui and a11y
    
    * added tests
    
    * updated margin

--- VAMobile/src/screens/ClaimsScreen/ClaimDetailsScreen/ClaimStatus/ClaimFileUpload/ClaimFileUpload.test.tsx
+++ VAMobile/src/screens/ClaimsScreen/ClaimDetailsScreen/ClaimStatus/ClaimFileUpload/ClaimFileUpload.test.tsx
@@ -1,24 +1,73 @@
 import React from 'react'
 
-import { context, renderWithProviders } from "testUtils"
+import {context, mockNavProps, renderWithProviders} from "testUtils"
 import { act, ReactTestInstance } from "react-test-renderer"
 
 import ClaimFileUpload from './ClaimFileUpload'
+import {TextView} from 'components'
+import {ClaimEventData} from 'store/api/types'
 
 context('ClaimFileUpload', () => {
   let component: any
   let testInstance: ReactTestInstance
+  let props: any
 
-  beforeEach(() => {
+  let requests = [
+    {
+      type: 'still_need_from_you_list',
+      date: '2020-07-16',
+      status: 'NEEDED',
+      uploaded: false,
+      uploadsAllowed: true
+    }
+  ]
+
+  const initializeTestInstance = (requests: ClaimEventData[]): void => {
+    props = mockNavProps(undefined, undefined, { params: { requests }})
+
     act(() => {
-      component = renderWithProviders(<ClaimFileUpload />)
+      component = renderWithProviders(<ClaimFileUpload {...props} />)
     })
 
     testInstance = component.root
+  }
+
+  beforeEach(() => {
+    initializeTestInstance(requests)
   })
 
   it('should initialize', async () => {
     expect(component).toBeTruthy()
   })
 
+  describe('when number of requests is greater than 1', () => {
+    it('should display the text "You have {{number}} file requests from VA"', async () => {
+      let updatedRequests = [
+        {
+          type: 'still_need_from_you_list',
+          date: '2020-07-16',
+          status: 'NEEDED',
+          uploaded: false,
+          uploadsAllowed: true
+        },
+        {
+          type: 'still_need_from_you_list',
+          date: '2020-07-16',
+          status: 'NEEDED',
+          uploaded: false,
+          uploadsAllowed: true
+        }
+      ]
+
+      initializeTestInstance(updatedRequests)
+
+      expect(testInstance.findAllByType(TextView)[6].props.children).toEqual('You have 2 file requests from VA')
+    })
+  })
+
+  describe('when number of requests is equal to 1', () => {
+    it('should display the text "You have 1 file request from VA"', async () => {
+      expect(testInstance.findAllByType(TextView)[6].props.children).toEqual('You have 1 file request from VA')
+    })
+  })
 })
