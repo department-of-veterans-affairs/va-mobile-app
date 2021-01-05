e20b18a28e8626bc2a8e7b0dbc710b0b720673ae
Komal Yaseen <komal@digitalfoundry.com>
2021-01-05 14:08:16 -0800

    updated integration

--- VAMobile/__tests__/screenObjects/claimDetailStatus.screen.tsx
+++ VAMobile/__tests__/screenObjects/claimDetailStatus.screen.tsx
@@ -3,7 +3,8 @@ import AppScreen from './app.screen';
 const SELECTORS = {
   CLAIMS_DETAILS_STATUS_SCREEN: '~Claim-status-screen',
   CLAIMS_DETAILS_STATUS_FIND_OUT_BUTTON: '~find-out-why-we-sometimes-combine-claims.',
-  CLAIMS_DETAILS_STATUS_WHAT_SHOULD_I_DO_BUTTON: '~what-should-i-do-if-i-disagree-with-your-decision-on-my-va-disability-claim?'
+  CLAIMS_DETAILS_STATUS_WHAT_SHOULD_I_DO_BUTTON: '~what-should-i-do-if-i-disagree-with-your-decision-on-my-va-disability-claim?',
+  CLAIMS_DETAILS_STATUS_VIEW_FILE_REQUESTS: '~View File Requests'
 };
 
 class ClaimsDetailsStatusScreen extends AppScreen {
@@ -17,6 +18,10 @@ class ClaimsDetailsStatusScreen extends AppScreen {
 
   get whatShouldIDoButton() {
     return $(SELECTORS.CLAIMS_DETAILS_STATUS_WHAT_SHOULD_I_DO_BUTTON)
+  }
+
+  get viewFileRequestsButton() {
+    return $(SELECTORS.CLAIMS_DETAILS_STATUS_VIEW_FILE_REQUESTS)
   }
 }
 
