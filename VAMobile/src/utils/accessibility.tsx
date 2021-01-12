008daa3e514b80e7b691b73f3165a60ad9b6a954
Clayton Matulich <clayton@digitalfoundry.com>
2021-01-12 11:47:08 -0800

    added comments

--- VAMobile/src/utils/accessibility.tsx
+++ VAMobile/src/utils/accessibility.tsx
@@ -10,6 +10,8 @@ interface AccessabilityProps {
 export const testIdProps = (id: string, disableAccessible?: boolean): AccessabilityProps => {
   const disableAccessibility = disableAccessible ? { accessible: false } : { accessible: undefined }
 
+  // only set accessibility label on one platform or the other when integration tests are running to ensure the tests work
+  // if tests are not running, return testID and accessibilityLabel for both platforms
   if (IS_TEST) {
     if (isIOS()) {
       return { ...disableAccessibility, testID: id }
