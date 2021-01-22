10a7b5d7837459c228b3d1ed9d3e0d52e394952c
Komal Yaseen <komal@digitalfoundry.com>
2021-01-21 16:57:42 -0800

    select a file updates

--- VAMobile/src/screens/ClaimsScreen/ClaimDetailsScreen/ClaimStatus/ClaimFileUpload/SelectFile/SelectFile.tsx
+++ VAMobile/src/screens/ClaimsScreen/ClaimDetailsScreen/ClaimStatus/ClaimFileUpload/SelectFile/SelectFile.tsx
@@ -1,20 +1,28 @@
 import { ScrollView } from 'react-native'
 import { StackHeaderLeftButtonProps } from '@react-navigation/stack'
 import { StackScreenProps } from '@react-navigation/stack/lib/typescript/src/types'
-import React, { FC, ReactNode, useEffect } from 'react'
+import React, { FC, ReactNode, useEffect, useState } from 'react'
 
-import { BackButton, Box, TextView, VAButton } from 'components'
+import { ImagePickerResponse, launchImageLibrary } from 'react-native-image-picker'
+import { useActionSheet } from '@expo/react-native-action-sheet'
+import DocumentPicker from 'react-native-document-picker'
+
+import { AlertBox, BackButton, Box, TextView, VAButton } from 'components'
 import { BackButtonLabelConstants } from 'constants/backButtonLabels'
 import { ClaimsStackParamList } from '../../../../ClaimsScreen'
+import { MAX_TOTAL_FILE_SIZE_IN_BYTES, postCameraLaunchCallback } from 'utils/claims'
 import { NAMESPACE } from 'constants/namespaces'
 import { testIdProps } from 'utils/accessibility'
-import { useTheme, useTranslation } from 'utils/hooks'
+import { useRouteNavigation, useTheme, useTranslation } from 'utils/hooks'
 
 type SelectFilesProps = StackScreenProps<ClaimsStackParamList, 'SelectFile'>
 
 const SelectFile: FC<SelectFilesProps> = ({ navigation, route }) => {
   const t = useTranslation(NAMESPACE.CLAIMS)
   const theme = useTheme()
+  const navigateTo = useRouteNavigation()
+  const { showActionSheetWithOptions } = useActionSheet()
+  const [error, setError] = useState('')
   const { request } = route.params
 
   useEffect(() => {
@@ -25,6 +33,55 @@ const SelectFile: FC<SelectFilesProps> = ({ navigation, route }) => {
     })
   })
 
+  const onFileFolder = async (): Promise<void> => {
+    try {
+      const document = await DocumentPicker.pick({
+        type: [DocumentPicker.types.images, DocumentPicker.types.plainText, DocumentPicker.types.pdf],
+      })
+
+      // TODO: Update error message for when the file size is too big
+      if (document.size > MAX_TOTAL_FILE_SIZE_IN_BYTES) {
+        setError(t('fileUpload.fileSizeError'))
+        return
+      }
+
+      navigateTo('UploadFile', { request, fileUploaded: document })()
+    } catch (docError) {
+      if (DocumentPicker.isCancel(docError)) {
+        return
+      }
+
+      setError(docError.code)
+    }
+  }
+
+  const cameraRollCallbackIfUri = (response: ImagePickerResponse): void => {
+    navigateTo('UploadFile', { request, imageUploaded: response })()
+  }
+
+  const onSelectFile = (): void => {
+    const options = [t('fileUpload.cameraRoll'), t('fileUpload.fileFolder'), t('common:cancel')]
+
+    showActionSheetWithOptions(
+      {
+        options,
+        cancelButtonIndex: 2,
+      },
+      (buttonIndex) => {
+        switch (buttonIndex) {
+          case 0:
+            launchImageLibrary({ mediaType: 'photo', quality: 0.9 }, (response: ImagePickerResponse): void => {
+              postCameraLaunchCallback(response, setError, cameraRollCallbackIfUri, 0, t)
+            })
+            break
+          case 1:
+            onFileFolder()
+            break
+        }
+      },
+    )
+  }
+
   return (
     <ScrollView {...testIdProps('File upload: Select a file to upload for the request')}>
       <Box mt={theme.dimensions.contentMarginTop} mb={theme.dimensions.contentMarginBottom} mx={theme.dimensions.gutter}>
@@ -34,9 +91,14 @@ const SelectFile: FC<SelectFilesProps> = ({ navigation, route }) => {
         <TextView variant="MobileBody" mt={theme.dimensions.marginBetween}>
           {t('fileUpload.pleaseRequestFromPhoneFiles')}
         </TextView>
+        {!!error && (
+          <Box mt={theme.dimensions.marginBetween}>
+            <AlertBox title={error} border="error" background="noCardBackground" />
+          </Box>
+        )}
         <Box mt={theme.dimensions.textAndButtonLargeMargin}>
           <VAButton
-            onPress={(): void => {}}
+            onPress={onSelectFile}
             label={t('fileUpload.selectAFile')}
             testID={t('fileUpload.selectAFile')}
             textColor="primaryContrast"
---
+++ VAMobile/src/screens/ClaimsScreen/ClaimDetailsScreen/ClaimStatus/ClaimFileUpload/SelectFile/UploadFile/UploadFile.tsx
@@ -0,0 +1,50 @@
+import { ScrollView } from 'react-native'
+import { StackHeaderLeftButtonProps } from '@react-navigation/stack'
+import { StackScreenProps } from '@react-navigation/stack/lib/typescript/src/types'
+import React, { FC, ReactNode, useEffect } from 'react'
+
+import { BackButton, Box, TextView, VAButton } from 'components'
+import { BackButtonLabelConstants } from 'constants/backButtonLabels'
+import { ClaimsStackParamList } from '../../../../../ClaimsScreen'
+import { NAMESPACE } from 'constants/namespaces'
+import { testIdProps } from 'utils/accessibility'
+import { useTheme, useTranslation } from 'utils/hooks'
+
+type UploadFileProps = StackScreenProps<ClaimsStackParamList, 'UploadFile'>
+
+const UploadFile: FC<UploadFileProps> = ({ navigation, route }) => {
+  const t = useTranslation(NAMESPACE.CLAIMS)
+  const theme = useTheme()
+  const { request, fileUploaded, imageUploaded } = route.params
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerLeft: (props: StackHeaderLeftButtonProps): ReactNode => (
+        <BackButton onPress={props.onPress} canGoBack={props.canGoBack} label={BackButtonLabelConstants.cancel} showCarat={false} />
+      ),
+    })
+  })
+
+  return (
+    <ScrollView {...testIdProps('File upload: upload file')}>
+      <Box mt={theme.dimensions.contentMarginTop} mb={theme.dimensions.contentMarginBottom} mx={theme.dimensions.gutter}>
+        <TextView variant="MobileBodyBold" accessibilityRole="header">
+          {request.displayName}
+        </TextView>
+        <TextView>{fileUploaded.name || imageUploaded.fileName}</TextView>
+        <Box mt={theme.dimensions.textAndButtonLargeMargin}>
+          <VAButton
+            onPress={(): void => {}}
+            label={t('fileUpload.upload')}
+            testID={t('fileUpload.upload')}
+            textColor="primaryContrast"
+            backgroundColor="button"
+            a11yHint={t('fileUpload.uploadFileA11yHint')}
+          />
+        </Box>
+      </Box>
+    </ScrollView>
+  )
+}
+
+export default UploadFile
