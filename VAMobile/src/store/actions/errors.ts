d09d2a9eae630f2e1357a619990bcf9afa695e76
Clayton Matulich <clayton@digitalfoundry.com>
2021-01-22 15:44:36 -0800

    fixed multi line comment

--- VAMobile/src/store/actions/errors.ts
+++ VAMobile/src/store/actions/errors.ts
@@ -18,9 +18,11 @@ const dispatchSetError = (errorType: CommonErrorTypes, bool: boolean, wasError: 
  */
 export const setError = (errorType: CommonErrorTypes, bool: boolean): AsyncReduxAction => {
   return async (dispatch, getState): Promise<void> => {
-    // If setting error to false, get a list of all values of error states while omitting
-    // the master wasError and the error type to be set, if the rest of the errors are false this means all errors are about
-    // to be false, set master wasError to false
+    /*
+     * If setting error to false, get a list of all values of error states while omitting
+     * the master non-specific error variables and the error type to be set. If the rest of the
+     * specific errors are false this means all specific errors are about to be false, set master wasError to false
+     */
     const errors = omit(getState()?.errors, ['wasError', errorType])
     const wasError = bool ? true : some(values(errors))
     dispatch(dispatchSetError(errorType, bool, wasError))
