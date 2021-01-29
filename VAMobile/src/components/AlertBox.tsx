69feeff693d685d141f11c31331046b67592b5ac
Komal Yaseen <komal@digitalfoundry.com>
2021-01-29 15:09:00 -0800

    fixed a11y

--- VAMobile/src/components/AlertBox.tsx
+++ VAMobile/src/components/AlertBox.tsx
@@ -14,12 +14,16 @@ export type AlertBoxProps = {
   text?: string
   /** optional bolded title text */
   title?: string
+  /** optional accessibility label for the text */
+  textA11yLabel?: string
+  /** optional accessibility label for the title */
+  titleA11yLabel?: string
 }
 
 /**
  * Displays content in a box styled as an alert
  */
-const AlertBox: FC<AlertBoxProps> = ({ border, background, children, title, text }) => {
+const AlertBox: FC<AlertBoxProps> = ({ border, background, children, title, text, textA11yLabel, titleA11yLabel }) => {
   const theme = useTheme()
 
   const boxProps: BoxProps = {
@@ -33,14 +37,14 @@ const AlertBox: FC<AlertBoxProps> = ({ border, background, children, title, text
   return (
     <Box {...boxProps}>
       {title && (
-        <Box {...testIdProps(title)} accessibilityRole={text ? 'header' : undefined} accessible={true}>
+        <Box {...testIdProps(textA11yLabel || title)} accessibilityRole={text ? 'header' : undefined} accessible={true}>
           <TextView variant="MobileBodyBold" mb={text ? theme.dimensions.marginBetween : 0}>
             {title}
           </TextView>
         </Box>
       )}
       {text && (
-        <Box {...testIdProps(text)} accessible={true}>
+        <Box {...testIdProps(textA11yLabel || text)} accessible={true}>
           <TextView variant="MobileBody">{text}</TextView>
         </Box>
       )}
