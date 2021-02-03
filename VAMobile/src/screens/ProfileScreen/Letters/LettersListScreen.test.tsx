49e109092bda3663da7ea250a766aa407dc4d107
Komal Yaseen <komal@digitalfoundry.com>
2021-02-03 12:18:13 -0800

    updated to use useRouteNav

--- VAMobile/src/screens/ProfileScreen/Letters/LettersListScreen.test.tsx
+++ VAMobile/src/screens/ProfileScreen/Letters/LettersListScreen.test.tsx
@@ -13,6 +13,19 @@ import { Pressable } from 'react-native'
 import { CommonErrorTypesConstants } from 'constants/errors'
 import { ScreenIDTypesConstants } from 'store/api/types/Screens'
 
+let mockNavigationSpy = jest.fn()
+  jest.mock('../../../utils/hooks', () => {
+  let original = jest.requireActual("../../../utils/hooks")
+  let theme = jest.requireActual("../../../styles/themes/standardTheme").default
+  return {
+    ...original,
+    useTheme: jest.fn(()=> {
+      return {...theme}
+    }),
+    useRouteNavigation: () => { return () => mockNavigationSpy },
+  }
+})
+
 const lettersData: LettersList = [
   {
     name: 'Commissary Letter',
@@ -53,7 +66,6 @@ context('LettersListScreen', () => {
   let component: any
   let testInstance: ReactTestInstance
   let props: any
-  let navigationSpy = jest.fn()
 
   const initializeTestInstance = (lettersList: LettersList | null, loading = false, errorsState: ErrorsState = initialErrorsState) => {
     const storeVals = {
@@ -68,7 +80,7 @@ context('LettersListScreen', () => {
 
     store = mockStore(storeVals)
 
-    props = mockNavProps(undefined, { navigate: navigationSpy })
+    props = mockNavProps()
 
     act(() => {
       component = renderWithProviders(<LettersListScreen {...props} />, store)
@@ -109,62 +121,42 @@ context('LettersListScreen', () => {
   describe('when a link is clicked', () => {
     it('should call navigations navigate for Benefit Summary Service Verification Letter', async () => {
       testInstance.findAllByType(Pressable)[6].props.onPress()
-      expect(navigationSpy).toHaveBeenCalled()
-      expect(navigationSpy).toHaveBeenCalledWith('BenefitSummaryServiceVerificationLetter')
+      expect(mockNavigationSpy).toHaveBeenCalled()
     })
 
     it('should call navigations navigate for Service Verification Letter', async () => {
       testInstance.findAllByType(Pressable)[4].props.onPress()
-      expect(navigationSpy).toHaveBeenCalled()
-      expect(navigationSpy).toHaveBeenCalledWith('ServiceVerificationLetter')
+      expect(mockNavigationSpy).toHaveBeenCalled()
     })
 
     it('should call navigations navigate for Commissary Letter', async () => {
-      const description = 'If you’re a Veteran with a 100% service-connected disability rating take this letter, a copy of your DD214 or other discharge papers, and your DD2765 to a local military ID and pass office. You can schedule an appointment to get a Retiree Military ID card at the office or use the Rapid Appointments Scheduler. The Retiree Military ID card gives you access to your local base facilities, including the commissary and post exchange.'
-
       testInstance.findAllByType(Pressable)[0].props.onPress()
-      expect(navigationSpy).toHaveBeenCalled()
-      expect(navigationSpy).toHaveBeenCalledWith('GenericLetter', { header: 'Commissary Letter', description, letterType: 'commissary', screenID: 'COMMISSARY_LETTER_SCREEN' })
+      expect(mockNavigationSpy).toHaveBeenCalled()
     })
 
     it('should call navigations navigate for Civil Service Letter', async () => {
       testInstance.findAllByType(Pressable)[5].props.onPress()
-      expect(navigationSpy).toHaveBeenCalled()
-      expect(navigationSpy).toHaveBeenCalledWith('GenericLetter', { header: 'Civil Service Preference Letter', description: 'This letter shows that you’re a disabled Veteran and you qualify for preference for civil service jobs.', letterType: 'civil_service', screenID: 'CIVIL_SERVICE_LETTER_SCREEN' })
+      expect(mockNavigationSpy).toHaveBeenCalled()
     })
 
     it('should call navigations navigate for Benefit Verification Letter', async () => {
-      const description = 'This letter shows the benefits you’re receiving from VA. The letter also shows your benefit gross amount (the amount before anything is taken out) and net amount (the amount after deductions are taken out), your benefit effective date, and your disability rating.'
-      const descriptionA11yLabel = 'This letter shows the benefits you’re receiving from V-A . The letter also shows your benefit gross amount (the amount before anything is taken out) and net amount (the amount after deductions are taken out), your benefit effective date, and your disability rating.'
-
       testInstance.findAllByType(Pressable)[7].props.onPress()
-      expect(navigationSpy).toHaveBeenCalled()
-      expect(navigationSpy).toHaveBeenCalledWith('GenericLetter', { header: 'Benefit Verification Letter', description, letterType: 'benefit_verification', screenID: 'BENEFIT_VERIFICATION_LETTER_SCREEN', descriptionA11yLabel })
+      expect(mockNavigationSpy).toHaveBeenCalled()
     })
 
     it('should call navigations navigate for Proof of Service Letter', async () => {
-      const description = 'This card shows that you served honorably in the Armed Forces. This card might be useful as proof of status to receive discounts at certain stores or restaurants.'
-
       testInstance.findAllByType(Pressable)[1].props.onPress()
-      expect(navigationSpy).toHaveBeenCalled()
-      expect(navigationSpy).toHaveBeenCalledWith('GenericLetter', { header: 'Proof of Service Letter', description, letterType: 'proof_of_service', screenID: 'PROOF_OF_SERVICE_LETTER_SCREEN' })
+      expect(mockNavigationSpy).toHaveBeenCalled()
     })
 
     it('should call navigations navigate for Proof of Creditable Prescription Drug Coverage Letter', async () => {
-      const description = 'You will need this letter as proof that you qualify for Medicare Part D prescription drug coverage.'
-
       testInstance.findAllByType(Pressable)[2].props.onPress()
-      expect(navigationSpy).toHaveBeenCalled()
-      expect(navigationSpy).toHaveBeenCalledWith('GenericLetter', { header: 'Proof of Creditable Prescription Drug Coverage Letter', description, letterType: 'medicare_partd', screenID: 'PROOF_OF_CREDIBLE_PRESCRIPTION_LETTER_SCREEN' })
+      expect(mockNavigationSpy).toHaveBeenCalled()
     })
 
     it('should call navigations navigate for Proof of Minimum Essential Coverage Letter', async () => {
-      const description = 'This letter indicates that you have Minimum Essential Coverage (MEC) as provided by VA. MEC means that your health care plan meets the health insurance requirements under the Affordable Care Act (ACA). To prove that you’re enrolled in the VA health care system, you must have IRS Form 1095-B from VA to show what months you were covered by a VA health care plan.'
-      const descriptionA11yLabel = 'This letter indicates that you have Minimum Essential Coverage (M-E-C) as provided by V-A . M-E-C means that your health care plan meets the health insurance requirements under the Affordable Care Act (A-C-A). To prove that you’re enrolled in the V-A health care system, you must have I-R-S Form 1095-B from V-A to show what months you were covered by a V-A health care plan.'
-
       testInstance.findAllByType(Pressable)[3].props.onPress()
-      expect(navigationSpy).toHaveBeenCalled()
-      expect(navigationSpy).toHaveBeenCalledWith('GenericLetter', { header: 'Proof of Minimum Essential Coverage Letter', description, letterType: 'minimum_essential_coverage', screenID: 'PROOF_OF_MINIMUM_ESSENTIAL_COVERAGE_LETTER_SCREEN', descriptionA11yLabel })
+      expect(mockNavigationSpy).toHaveBeenCalled()
     })
   })
 
--- VAMobile/src/screens/ProfileScreen/Letters/LettersListScreen.tsx
+++ VAMobile/src/screens/ProfileScreen/Letters/LettersListScreen.tsx
@@ -9,82 +9,75 @@ import { LetterData, LetterTypeConstants } from 'store/api/types'
 import { LetterTypes } from 'store/api/types'
 import { LettersState, StoreState } from 'store/reducers'
 import { NAMESPACE } from 'constants/namespaces'
+import { OnPressHandler, useError, useRouteNavigation, useTheme, useTranslation } from 'utils/hooks'
 import { ProfileStackParamList } from '../ProfileScreen'
 import { ScreenIDTypesConstants } from 'store/api/types/Screens'
 import { getLetters } from 'store/actions/letters'
 import { testIdProps } from 'utils/accessibility'
-import { useError, useTheme, useTranslation } from 'utils/hooks'
 import NoLettersScreen from './NoLettersScreen'
 
 type LettersListScreenProps = StackScreenProps<ProfileStackParamList, 'LettersList'>
 
-const LettersListScreen: FC<LettersListScreenProps> = ({ navigation }) => {
+const LettersListScreen: FC<LettersListScreenProps> = () => {
   const dispatch = useDispatch()
   const { letters, loading } = useSelector<StoreState, LettersState>((state) => state.letters)
   const theme = useTheme()
+  const navigateTo = useRouteNavigation()
   const t = useTranslation(NAMESPACE.PROFILE)
   const tCommon = useTranslation(NAMESPACE.COMMON)
 
-  const letterPressFn = (letterType: LetterTypes, letterName: string): (() => void) => {
-    return (): void => {
-      switch (letterType) {
-        case LetterTypeConstants.benefitSummary:
-          navigation.navigate('BenefitSummaryServiceVerificationLetter')
-          break
-        case LetterTypeConstants.serviceVerification:
-          navigation.navigate('ServiceVerificationLetter')
-          break
-        case LetterTypeConstants.commissary:
-          navigation.navigate('GenericLetter', {
-            header: letterName,
-            description: t('letters.commissary.description'),
-            letterType,
-            screenID: ScreenIDTypesConstants.COMMISSARY_LETTER_SCREEN_ID,
-          })
-          break
-        case LetterTypeConstants.civilService:
-          navigation.navigate('GenericLetter', {
-            header: letterName,
-            description: t('letters.civilService.description'),
-            letterType,
-            screenID: ScreenIDTypesConstants.CIVIL_SERVICE_LETTER_SCREEN_ID,
-          })
-          break
-        case LetterTypeConstants.benefitVerification:
-          navigation.navigate('GenericLetter', {
-            header: letterName,
-            description: t('letters.benefitVerification.description'),
-            letterType,
-            screenID: ScreenIDTypesConstants.BENEFIT_VERIFICATION_LETTER_SCREEN_ID,
-            descriptionA11yLabel: t('letters.benefitVerificationA11yLabel.description'),
-          })
-          break
-        case LetterTypeConstants.proofOfService:
-          navigation.navigate('GenericLetter', {
-            header: letterName,
-            description: t('letters.proofOfService.description'),
-            letterType,
-            screenID: ScreenIDTypesConstants.PROOF_OF_SERVICE_LETTER_SCREEN_ID,
-          })
-          break
-        case LetterTypeConstants.medicarePartd:
-          navigation.navigate('GenericLetter', {
-            header: letterName,
-            description: t('letters.proofOfCrediblePrescription.description'),
-            letterType,
-            screenID: ScreenIDTypesConstants.PROOF_OF_CREDIBLE_PRESCRIPTION_LETTER_SCREEN_ID,
-          })
-          break
-        case LetterTypeConstants.minimumEssentialCoverage:
-          navigation.navigate('GenericLetter', {
-            header: letterName,
-            description: t('letters.minimumEssentialCoverage.description'),
-            letterType,
-            screenID: ScreenIDTypesConstants.PROOF_OF_MINIMUM_ESSENTIAL_COVERAGE_LETTER_SCREEN_ID,
-            descriptionA11yLabel: t('letters.minimumEssentialCoverageA11yLabel.description'),
-          })
-          break
-      }
+  const letterPressFn = (letterType: LetterTypes, letterName: string): OnPressHandler | undefined => {
+    switch (letterType) {
+      case LetterTypeConstants.benefitSummary:
+        return navigateTo('BenefitSummaryServiceVerificationLetter')
+      case LetterTypeConstants.serviceVerification:
+        return navigateTo('ServiceVerificationLetter')
+      case LetterTypeConstants.commissary:
+        return navigateTo('GenericLetter', {
+          header: letterName,
+          description: t('letters.commissary.description'),
+          letterType,
+          screenID: ScreenIDTypesConstants.COMMISSARY_LETTER_SCREEN_ID,
+        })
+      case LetterTypeConstants.civilService:
+        return navigateTo('GenericLetter', {
+          header: letterName,
+          description: t('letters.civilService.description'),
+          letterType,
+          screenID: ScreenIDTypesConstants.CIVIL_SERVICE_LETTER_SCREEN_ID,
+        })
+      case LetterTypeConstants.benefitVerification:
+        return navigateTo('GenericLetter', {
+          header: letterName,
+          description: t('letters.benefitVerification.description'),
+          letterType,
+          screenID: ScreenIDTypesConstants.BENEFIT_VERIFICATION_LETTER_SCREEN_ID,
+          descriptionA11yLabel: t('letters.benefitVerificationA11yLabel.description'),
+        })
+      case LetterTypeConstants.proofOfService:
+        return navigateTo('GenericLetter', {
+          header: letterName,
+          description: t('letters.proofOfService.description'),
+          letterType,
+          screenID: ScreenIDTypesConstants.PROOF_OF_SERVICE_LETTER_SCREEN_ID,
+        })
+      case LetterTypeConstants.medicarePartd:
+        return navigateTo('GenericLetter', {
+          header: letterName,
+          description: t('letters.proofOfCrediblePrescription.description'),
+          letterType,
+          screenID: ScreenIDTypesConstants.PROOF_OF_CREDIBLE_PRESCRIPTION_LETTER_SCREEN_ID,
+        })
+      case LetterTypeConstants.minimumEssentialCoverage:
+        return navigateTo('GenericLetter', {
+          header: letterName,
+          description: t('letters.minimumEssentialCoverage.description'),
+          letterType,
+          screenID: ScreenIDTypesConstants.PROOF_OF_MINIMUM_ESSENTIAL_COVERAGE_LETTER_SCREEN_ID,
+          descriptionA11yLabel: t('letters.minimumEssentialCoverageA11yLabel.description'),
+        })
+      default:
+        return undefined
     }
   }
 
