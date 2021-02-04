4fca0d1e913df37c4af2e21729455758bf00ee99
Komal Yaseen <komal@digitalfoundry.com>
2021-02-04 13:35:53 -0800

    Added test files

---
+++ VAMobile/src/components/Carousel.test.tsx
@@ -0,0 +1,30 @@
+import 'react-native'
+import React from 'react'
+// Note: test renderer must be required after react-native.
+import 'jest-styled-components'
+import { ReactTestInstance, act } from 'react-test-renderer'
+
+import { context, renderWithProviders } from 'testUtils'
+import Carousel from './Carousel'
+import {TextView} from './index'
+
+context('Carousel', () => {
+  let component: any
+  let testInstance: ReactTestInstance
+  let t = jest.fn(() => {})
+
+  const TestComponent = () => {
+    return <TextView>Test Component</TextView>
+  }
+
+  beforeEach(() => {
+    act(() => {
+      component = renderWithProviders(<Carousel screenList={[ { name: 'TestComponent', component: TestComponent } ]} onCarouselEnd={() => {}} translation={t} />)
+    })
+    testInstance = component.root
+  })
+
+  it('initializes correctly', async () => {
+    expect(component).toBeTruthy()
+  })
+})
---
+++ VAMobile/src/components/CarouselTabBar.test.tsx
@@ -0,0 +1,79 @@
+import 'react-native'
+import React from 'react'
+// Note: test renderer must be required after react-native.
+import 'jest-styled-components'
+import { ReactTestInstance, act } from 'react-test-renderer'
+import {Pressable} from 'react-native'
+
+import { NavigationHelpers, ParamListBase, TabNavigationState } from '@react-navigation/native'
+import {BottomTabNavigationEventMap} from '@react-navigation/bottom-tabs/src/types'
+
+import { context, renderWithProviders } from 'testUtils'
+import CarouselTabBar from './CarouselTabBar'
+import {TextView} from './index'
+
+
+context('CarouselTabBar', () => {
+  let component: any
+  let testInstance: ReactTestInstance
+  let t = jest.fn(() => {})
+  let navigationSpy = jest.fn()
+
+  const TestComponent = () => {
+    return <TextView>Test Component</TextView>
+  }
+
+  const TestComponent2 = () => {
+    return <TextView>Test Component2</TextView>
+  }
+
+  const initializeTestInstance = (onCarouselEndSpy?: () => void) => {
+    const screenList = [
+      { name: 'TestComponent', component: TestComponent },
+      { name: 'TestComponent2', component: TestComponent2 }
+    ]
+
+    act(() => {
+      component = renderWithProviders(<CarouselTabBar screenList={screenList} onCarouselEnd={onCarouselEndSpy} translation={t} navigation={{ navigate: navigationSpy } as unknown as NavigationHelpers<ParamListBase, BottomTabNavigationEventMap>} />)
+    })
+    testInstance = component.root
+  }
+
+  beforeEach(() => {
+    initializeTestInstance()
+  })
+
+  it('initializes correctly', async () => {
+    expect(component).toBeTruthy()
+  })
+
+  describe('on click of continue', () => {
+    describe('when the user is on the last screen', () => {
+      it('should call onCarouselEnd', async () => {
+        let onCarouselEndSpy = jest.fn()
+        initializeTestInstance(onCarouselEndSpy)
+
+        testInstance.findAllByType(Pressable)[1].props.onPress()
+        testInstance.findAllByType(Pressable)[1].props.onPress()
+        testInstance.findAllByType(Pressable)[1].props.onPress()
+        expect(onCarouselEndSpy).toHaveBeenCalled()
+      })
+    })
+
+    it('should call navigation navigate', async () => {
+      testInstance.findAllByType(Pressable)[1].props.onPress()
+      expect(navigationSpy).toHaveBeenCalled()
+      expect(navigationSpy).toHaveBeenCalledWith('TestComponent2')
+    })
+  })
+
+  describe('on click of skip', () => {
+    it('should call onCarouselEnd', async () => {
+      let onCarouselEndSpy = jest.fn()
+      initializeTestInstance(onCarouselEndSpy)
+
+      testInstance.findAllByType(Pressable)[0].props.onPress()
+      expect(onCarouselEndSpy).toHaveBeenCalled()
+    })
+  })
+})
