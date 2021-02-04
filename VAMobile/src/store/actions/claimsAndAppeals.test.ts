df119fd78680e836fd2a59831007746ee95c241a
k_li <k_li@digitalfoundry.com>
2021-02-04 12:52:55 -0800

    Appeals details integration with get endpoint (#386)
    * Appeals details integration with get endpoint
    
    * Update integration tests
    
    * Adding workaround fix for when a user does not have permissions to load in claims and appeal
    
    Co-authored-by: lexicalninja <patrick@urban-intellect.com>

--- VAMobile/src/store/actions/claimsAndAppeals.test.ts
+++ VAMobile/src/store/actions/claimsAndAppeals.test.ts
@@ -1,5 +1,4 @@
 import _ from 'underscore'
-
 import * as api from '../api'
 import { context, realStore, when } from 'testUtils'
 import {
@@ -11,6 +10,7 @@ import {
   submitClaimDecision,
   uploadFileToClaim
 } from './claimsAndAppeals'
+import { appeal as AppealPayload } from 'screens/ClaimsScreen/appealData'
 import { claim as Claim } from 'screens/ClaimsScreen/claimData'
 import {ClaimEventData} from '../api/types'
 
@@ -181,9 +181,13 @@ context('claimsAndAppeals', () => {
 
   describe('getAppeal', () => {
     it('should dispatch the correct actions', async () => {
-      // TODO: add more tests when using the api instead of mocked data
+      const id = '2765759'
+      when(api.get as jest.Mock)
+          .calledWith(`/v0/appeal/2765759`)
+          .mockResolvedValue({ data: AppealPayload})
+
       const store = realStore()
-      await store.dispatch(getAppeal('0'))
+      await store.dispatch(getAppeal(id))
 
       const actions = store.getActions()
 
@@ -196,6 +200,30 @@ context('claimsAndAppeals', () => {
 
       const { claimsAndAppeals } = store.getState()
       expect(claimsAndAppeals.error).toBeFalsy()
+      expect(claimsAndAppeals.appeal).toEqual(AppealPayload)
+    })
+
+    it('should return error if it fails', async () => {
+      const error = new Error('Backend error')
+      const id = '2765759'
+      when(api.get as jest.Mock)
+          .calledWith(`/v0/appeal/2765759`)
+          .mockRejectedValue(error)
+
+      const store = realStore()
+      await store.dispatch(getAppeal(id))
+
+      const actions = store.getActions()
+
+      const startAction = _.find(actions, { type: 'CLAIMS_AND_APPEALS_START_GET_APPEAL' })
+      expect(startAction).toBeTruthy()
+
+      const endAction = _.find(actions, { type: 'CLAIMS_AND_APPEALS_FINISH_GET_APPEAL' })
+      expect(endAction).toBeTruthy()
+      expect(endAction?.state.claimsAndAppeals.loadingAppeal).toBe(false)
+
+      const { claimsAndAppeals } = store.getState()
+      expect(claimsAndAppeals.error).toEqual(error)
     })
   })
 
