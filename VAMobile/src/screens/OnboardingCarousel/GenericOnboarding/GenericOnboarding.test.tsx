a26806884132762e5b3e82d5b56cb53badf111ad
Komal Yaseen <komal@digitalfoundry.com>
2021-02-08 12:14:55 -0800

    added tests

---
+++ VAMobile/src/screens/OnboardingCarousel/GenericOnboarding/GenericOnboarding.test.tsx
@@ -0,0 +1,24 @@
+import 'react-native'
+import React from 'react'
+import 'jest-styled-components'
+import {act, ReactTestInstance} from 'react-test-renderer'
+
+import {context, renderWithProviders} from 'testUtils'
+import GenericOnboarding from './GenericOnboarding'
+
+context('GenericOnboarding', () => {
+  let component: any
+  let testInstance: ReactTestInstance
+
+  beforeEach(() => {
+    act(() => {
+      component = renderWithProviders(<GenericOnboarding header={'header'} text={'text'}/>)
+    })
+
+    testInstance = component.root
+  })
+
+  it('initializes correctly', async () => {
+    expect(component).toBeTruthy()
+  })
+})
