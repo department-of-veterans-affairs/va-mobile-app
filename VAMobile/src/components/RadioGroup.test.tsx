4fdee9196adce98326ad410244a18b435a741c6c
Clayton Matulich <clayton@digitalfoundry.com>
2021-02-08 19:29:34 -0800

    added test

---
+++ VAMobile/src/components/RadioGroup.test.tsx
@@ -0,0 +1,54 @@
+import 'react-native'
+import React from 'react'
+
+// Note: test renderer must be required after react-native.
+import 'jest-styled-components'
+import { ReactTestInstance, act } from 'react-test-renderer'
+
+import { context, renderWithProviders } from 'testUtils'
+import RadioGroup from './RadioGroup'
+import Mock = jest.Mock
+
+const mockOptions = [
+  {
+    value: 1,
+    label: '1'
+  },
+  {
+    value: 2,
+    label: '2'
+  },
+  {
+    value: 3,
+    label: '3'
+  }
+]
+
+type SelectedValueType =  { [key: string]: string | number } | string | number
+
+context('VASelector', () => {
+  let component: any
+  let testInstance: ReactTestInstance
+  let selected: SelectedValueType
+  let setSelected: Mock
+
+  const initializeTestInstance = (selectedValue: SelectedValueType ): void => {
+    selected = selectedValue
+    setSelected = jest.fn((updatedSelected) => selected = updatedSelected)
+
+    act(() => {
+      component = renderWithProviders(<RadioGroup onChange={setSelected} value={selected} options={mockOptions}/>)
+    })
+
+    testInstance = component.root
+  }
+
+  beforeEach(() => {
+    initializeTestInstance(mockOptions[1].value)
+  })
+
+  it('initializes correctly', async () => {
+    expect(component).toBeTruthy()
+  })
+  
+})
