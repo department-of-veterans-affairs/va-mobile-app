3f0aa6b7895a480a2ef29b0c220ba607241f43c7
Patrick Saxton <patrick@urban-intellect.com>
2021-03-04 14:56:51 -0600

    linking?

---
+++ VAMobile/src/constants/linking.tsx
@@ -0,0 +1,26 @@
+import { LinkingOptions } from '@react-navigation/native'
+import { LoadingComponent } from '../components'
+import { NavigationState } from 'react-navigation'
+import React, { ReactElement } from 'react'
+
+export const linking: LinkingOptions = {
+  prefixes: [
+    /* your linking prefixes */
+    'vamobile://',
+  ],
+  config: {
+    /* configuration for matching screens with paths */
+    screens: {
+      Appointments: 'appointments/:id',
+    },
+  },
+}
+
+export const state: NavigationState = {
+  index: 0,
+  isTransitioning: false,
+  key: '',
+  routes: [],
+}
+
+export const fallback: ReactElement = <LoadingComponent />
