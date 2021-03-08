90db43f0ffcd2d17c1f71843fa3385bc437d6d9b
komaly15 <71414663+komaly15@users.noreply.github.com>
2021-03-08 11:16:51 -0800

    Form wrapper common component (#543)
    * text input updates
    
    * updated file structure
    
    * added tests
    
    * tests
    
    * expanded on testID comment
    
    * updated checkbox component
    
    * updated test
    
    * updated field name
    
    * fixed test
    
    * initial wraper
    
    * updated label to labelKey
    
    * moved translation to profile
    
    * wrapper updates
    
    * Fixed bug
    
    * Fixed lint error
    
    * fixed lint error
    
    * clean up
    
    * tests
    
    * updated read me
    
    * import and comparator update
    
    * passed in validationList to check again
    
    * updated tests
    
    * Read me update
    
    * updated to spread array keys for indexes list

--- VAMobile/src/components/FormWrapper/FormFields/VAPicker.tsx
+++ VAMobile/src/components/FormWrapper/FormFields/VAPicker.tsx
@@ -1,7 +1,7 @@
 import RNPickerSelect, { PickerSelectProps } from 'react-native-picker-select'
 import React, { FC, ReactNode, useEffect, useState } from 'react'
 
-import { Box, VAIcon } from '../../index'
+import { Box, VAIcon, ValidationFunctionItems } from '../../index'
 import { generateInputTestID, getInputWrapperProps, renderInputError, renderInputLabelSection, updateInputErrorMessage } from './formFieldUtils'
 import { testIdProps } from 'utils/accessibility'
 import { useTheme } from 'utils/hooks'
@@ -48,9 +48,11 @@ export type VAPickerProps = {
   /** optional key for string to display underneath label */
   helperTextKey?: string
   /** optional callback to update the error message if there is an error */
-  setError?: (error: string) => void
+  setError?: (error?: string) => void
   /** if this exists updated picker styles to error state */
   error?: string
+  /** optional list of validation functions to check against */
+  validationList?: Array<ValidationFunctionItems>
 }
 
 const VAPicker: FC<VAPickerProps> = ({
@@ -69,6 +71,7 @@ const VAPicker: FC<VAPickerProps> = ({
   helperTextKey,
   setError,
   error,
+  validationList,
 }) => {
   const theme = useTheme()
   const t = useTranslation()
@@ -76,8 +79,8 @@ const VAPicker: FC<VAPickerProps> = ({
   const [isFocused, setIsFocused] = useState(false)
 
   useEffect(() => {
-    updateInputErrorMessage(isFocused, isRequiredField, setError, selectedValue, focusUpdated, labelKey, setFocusUpdated, t)
-  }, [isFocused, labelKey, selectedValue, setError, isRequiredField, t, focusUpdated])
+    updateInputErrorMessage(isFocused, isRequiredField, error, setError, selectedValue, focusUpdated, setFocusUpdated, validationList)
+  }, [isFocused, labelKey, selectedValue, error, setError, isRequiredField, t, focusUpdated, validationList])
 
   const onClose = (): void => {
     setIsFocused(false)
--- VAMobile/src/screens/ProfileScreen/DirectDepositScreen/EditDirectDepositScreen/EditDirectDepositScreen.tsx
+++ VAMobile/src/screens/ProfileScreen/DirectDepositScreen/EditDirectDepositScreen/EditDirectDepositScreen.tsx
@@ -1,13 +1,11 @@
 import { KeyboardAvoidingView, ScrollView, TextInput } from 'react-native'
-import { StackHeaderLeftButtonProps } from '@react-navigation/stack'
 import { StackScreenProps } from '@react-navigation/stack/lib/typescript/src/types'
 import { useDispatch, useSelector } from 'react-redux'
-import React, { FC, ReactNode, useEffect, useRef, useState } from 'react'
+import React, { FC, useEffect, useRef, useState } from 'react'
 
 import { AccountOptions } from 'constants/accounts'
 import { AccountTypes } from 'store/api/types'
-import { AlertBox, BackButton, Box, CollapsibleView, ErrorComponent, LoadingComponent, SaveButton, TextView, VAImage, VAPicker, VASelector, VATextInput } from 'components'
-import { BackButtonLabelConstants } from 'constants/backButtonLabels'
+import { AlertBox, Box, CollapsibleView, ErrorComponent, FieldType, FormFieldType, FormWrapper, LoadingComponent, TextView, VAImage } from 'components'
 import { DirectDepositState, StoreState } from 'store/reducers'
 import { NAMESPACE } from 'constants/namespaces'
 import { RootNavStackParamList } from 'App'
@@ -34,11 +32,7 @@ const EditDirectDepositScreen: FC<EditDirectDepositProps> = ({ navigation }) => 
   const accountNumRef = useRef<TextInput>(null)
   const { bankInfoUpdated, saving, invalidRoutingNumberError } = useSelector<StoreState, DirectDepositState>((state) => state.directDeposit)
 
-  const gutter = theme.dimensions.gutter
-  const contentMarginTop = theme.dimensions.contentMarginTop
-  const contentMarginBottom = theme.dimensions.contentMarginBottom
-  const condensedMarginBetween = theme.dimensions.condensedMarginBetween
-  const standardMarginBetween = theme.dimensions.standardMarginBetween
+  const { gutter, contentMarginTop, contentMarginBottom, standardMarginBetween, condensedMarginBetween } = theme.dimensions
 
   const [routingNumber, setRoutingNumber] = useState('')
   const [accountNumber, setAccountNumber] = useState('')
@@ -56,29 +50,10 @@ const EditDirectDepositScreen: FC<EditDirectDepositProps> = ({ navigation }) => 
   const [confirmedDisabled, setConfirmedDisabled] = useState(true)
   const [saveDisabled, setSaveDisabled] = useState(true)
 
-  //TODO #14161
-  const onSave = (): void => {
-    dispatch(updateBankInfo(accountNumber, routingNumber, accountType as AccountTypes, ScreenIDTypesConstants.EDIT_DIRECT_DEPOSIT_SCREEN_ID))
-  }
-
-  const goBack = (): void => {
-    navigation.goBack()
-    dispatch(finishEditBankInfo())
-  }
-
   useEffect(() => {
     if (bankInfoUpdated) {
       goBack()
     }
-  })
-
-  useEffect(() => {
-    navigation.setOptions({
-      headerLeft: (props: StackHeaderLeftButtonProps): ReactNode => (
-        <BackButton onPress={goBack} canGoBack={props.canGoBack} label={BackButtonLabelConstants.cancel} showCarat={false} />
-      ),
-      headerRight: () => <SaveButton onSave={onSave} disabled={saveDisabled} />,
-    })
   })
 
   useEffect(() => {
@@ -95,16 +70,6 @@ const EditDirectDepositScreen: FC<EditDirectDepositProps> = ({ navigation }) => 
     setSaveDisabled(!(isValidContent && confirmed))
   }, [routingNumber, accountNumber, accountType, confirmed, saveDisabled])
 
-  const checkboxProps = {
-    labelKey: 'profile:editDirectDeposit.confirm',
-    selected: confirmed,
-    onSelectionChange: setConfirmed,
-    disabled: confirmedDisabled,
-    a11yHint: t('editDirectDeposit.confirmHint'),
-  }
-
-  const behavior = isIOS() ? 'position' : undefined
-
   if (useError(ScreenIDTypesConstants.EDIT_DIRECT_DEPOSIT_SCREEN_ID)) {
     return <ErrorComponent />
   }
@@ -112,6 +77,70 @@ const EditDirectDepositScreen: FC<EditDirectDepositProps> = ({ navigation }) => 
   if (saving) {
     return <LoadingComponent text={t('directDeposit.savingInformation')} />
   }
+
+  const onSave = (): void => {
+    dispatch(updateBankInfo(accountNumber, routingNumber, accountType as AccountTypes, ScreenIDTypesConstants.EDIT_DIRECT_DEPOSIT_SCREEN_ID))
+  }
+
+  const goBack = (): void => {
+    navigation.goBack()
+    dispatch(finishEditBankInfo())
+  }
+
+  const behavior = isIOS() ? 'position' : undefined
+
+  const formFieldsList: Array<FormFieldType> = [
+    {
+      fieldType: FieldType.TextInput,
+      fieldProps: {
+        labelKey: 'profile:editDirectDeposit.routingNumber',
+        inputType: 'phone',
+        onChange: setRoutingNumber,
+        maxLength: MAX_ROUTING_DIGITS,
+        placeholderKey: 'profile:editDirectDeposit.routingNumberPlaceHolder',
+        value: routingNumber,
+        isRequiredField: true,
+        helperTextKey: 'profile:editDirectDeposit.routingNumberHelperText',
+      },
+    },
+    {
+      fieldType: FieldType.TextInput,
+      fieldProps: {
+        labelKey: 'profile:editDirectDeposit.accountNumber',
+        inputType: 'phone',
+        onChange: setAccountNumber,
+        maxLength: MAX_ACCOUNT_DIGITS,
+        placeholderKey: 'profile:editDirectDeposit.accountNumberPlaceHolder',
+        value: accountNumber,
+        inputRef: accountNumRef,
+        isRequiredField: true,
+        helperTextKey: 'profile:editDirectDeposit.accountNumberHelperText',
+      },
+    },
+    {
+      fieldType: FieldType.Picker,
+      fieldProps: {
+        labelKey: 'profile:editDirectDeposit.accountType',
+        selectedValue: accountType,
+        onSelectionChange: setAccountType,
+        pickerOptions: accountOptions,
+        placeholderKey: 'profile:editDirectDeposit.accountTypePlaceHolder',
+        onUpArrow: (): void => focusTextInputRef(accountNumRef),
+        isRequiredField: true,
+      },
+    },
+    {
+      fieldType: FieldType.Selector,
+      fieldProps: {
+        labelKey: 'profile:editDirectDeposit.confirm',
+        selected: confirmed,
+        onSelectionChange: setConfirmed,
+        disabled: confirmedDisabled,
+        a11yHint: t('editDirectDeposit.confirmHint'),
+        isRequiredField: true,
+      },
+    },
+  ]
 
   return (
     <ScrollView {...testIdProps('Direct-deposit: Edit-direct-deposit-page')}>
@@ -125,50 +154,13 @@ const EditDirectDepositScreen: FC<EditDirectDepositProps> = ({ navigation }) => 
           <Box mx={gutter}>
             <TextView variant="MobileBody">{t('editDirectDeposit.bankInfoTitle')}</TextView>
           </Box>
-          <Box mt={theme.dimensions.condensedMarginBetween}>
+          <Box mt={condensedMarginBetween}>
             <CollapsibleView text={t('editDirectDeposit.findTheseNumbers')}>
               <VAImage name={'PaperCheck'} a11yLabel={t('editDirectDeposit.checkingExample')} marginX={gutter} />
             </CollapsibleView>
           </Box>
-          <Box mt={condensedMarginBetween} mx={gutter}>
-            <TextView>{t('editDirectDeposit.routingNumber')}</TextView>
-          </Box>
-          <Box mt={condensedMarginBetween}>
-            <VATextInput
-              inputType="phone"
-              onChange={setRoutingNumber}
-              maxLength={MAX_ROUTING_DIGITS}
-              placeholderKey={'profile:editDirectDeposit.routingNumberPlaceHolder'}
-              value={routingNumber}
-            />
-          </Box>
           <Box mt={standardMarginBetween} mx={gutter}>
-            <TextView>{t('editDirectDeposit.accountNumber')}</TextView>
-          </Box>
-          <Box mt={condensedMarginBetween}>
-            <VATextInput
-              inputType="phone"
-              onChange={setAccountNumber}
-              maxLength={MAX_ACCOUNT_DIGITS}
-              placeholderKey={'profile:editDirectDeposit.accountNumberPlaceHolder'}
-              value={accountNumber}
-              inputRef={accountNumRef}
-            />
-          </Box>
-          <Box mt={standardMarginBetween} mx={gutter}>
-            <TextView>{t('editDirectDeposit.accountType')}</TextView>
-          </Box>
-          <Box mt={condensedMarginBetween}>
-            <VAPicker
-              selectedValue={accountType}
-              onSelectionChange={setAccountType}
-              pickerOptions={accountOptions}
-              placeholderKey={'profile:editDirectDeposit.accountTypePlaceHolder'}
-              onUpArrow={(): void => focusTextInputRef(accountNumRef)}
-            />
-          </Box>
-          <Box mt={standardMarginBetween} mx={gutter}>
-            <VASelector {...checkboxProps} />
+            <FormWrapper fieldsList={formFieldsList} onSave={onSave} saveDisabled={saveDisabled} goBack={goBack} />
           </Box>
         </Box>
       </KeyboardAvoidingView>
