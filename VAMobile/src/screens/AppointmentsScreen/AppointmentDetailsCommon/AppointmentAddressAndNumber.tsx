423c59c8f48162a3b1adb82dcd5c1e5051189cf1
Patrick Saxton <patrick@urban-intellect.com>
2021-03-12 10:39:56 -0600

    Revert "revert cleanup"
    This reverts commit 736e05cab4dff94db9458b2e1d95c18bf84cadc5.

--- VAMobile/src/screens/AppointmentsScreen/AppointmentDetailsCommon/AppointmentAddressAndNumber.tsx
+++ VAMobile/src/screens/AppointmentsScreen/AppointmentDetailsCommon/AppointmentAddressAndNumber.tsx
@@ -1,10 +1,10 @@
 import React, { FC, ReactElement } from 'react'
 
-import { AppointmentAddress, AppointmentPhone, AppointmentType, AppointmentTypeConstants } from 'store/api/types'
-import { Box, TextView } from 'components'
+import { AppointmentAddress, AppointmentLocation, AppointmentPhone, AppointmentType, AppointmentTypeConstants } from 'store/api/types'
+import { Box, ClickForActionLink, TextView } from 'components'
 import { getAllFieldsThatExist } from 'utils/common'
+import { getDirectionsUrl } from 'utils/location'
 import { testIdProps } from 'utils/accessibility'
-import { useTheme } from 'utils/hooks'
 import ClickToCallClinic from './ClickToCallClinic'
 
 export const isVAOrCCOrVALocation = (appointmentType: AppointmentType): boolean => {
@@ -18,16 +18,14 @@ export const isVAOrCCOrVALocation = (appointmentType: AppointmentType): boolean 
 type AppointmentAddressAndNumberProps = {
   appointmentType: AppointmentType
   healthcareService: string
-  locationName: string
+  location: AppointmentLocation
   address: AppointmentAddress | undefined
   phone: AppointmentPhone | undefined
 }
 
-const AppointmentAddressAndNumber: FC<AppointmentAddressAndNumberProps> = ({ appointmentType, healthcareService, locationName, address, phone }) => {
-  const theme = useTheme()
+const AppointmentAddressAndNumber: FC<AppointmentAddressAndNumberProps> = ({ appointmentType, healthcareService, location, address, phone }) => {
   const appointmentIsAtlas = appointmentType === AppointmentTypeConstants.VA_VIDEO_CONNECT_ATLAS
   const isValidAppointment = isVAOrCCOrVALocation(appointmentType) || appointmentIsAtlas
-
   if (!isValidAppointment) {
     return <></>
   }
@@ -47,22 +45,22 @@ const AppointmentAddressAndNumber: FC<AppointmentAddressAndNumberProps> = ({ app
 
   const cityStateZip = address ? `${address.city}, ${address.state} ${address.zipCode}` : ''
 
-  const testIdFields = !appointmentIsAtlas ? [locationName, address?.street || '', cityStateZip] : [address?.street || '', cityStateZip]
+  const testIdFields = !appointmentIsAtlas ? [location.name, address?.street || '', cityStateZip] : [address?.street || '', cityStateZip]
   const testId = getAllFieldsThatExist(testIdFields).join(' ').trim()
 
   return (
     <Box>
       <VA_VALocation_AppointmentData />
       <Box {...testIdProps(testId)} accessible={true}>
-        {!appointmentIsAtlas && <TextView variant="MobileBody">{locationName}</TextView>}
+        {!appointmentIsAtlas && <TextView variant="MobileBody">{location.name}</TextView>}
         {!!address && <TextView variant="MobileBody">{address.street}</TextView>}
         {!!cityStateZip && <TextView variant="MobileBody">{cityStateZip}</TextView>}
       </Box>
 
       {/*TODO: Replace placeholder with get directions click for action link */}
-      <TextView mt={theme.dimensions.standardMarginBetween} color="link" textDecoration="underline">
-        GET DIRECTIONS
-      </TextView>
+      <Box>
+        <ClickForActionLink displayedText={'GET DIRECTIONS'} linkType={'directions'} numberOrUrlLink={getDirectionsUrl(location)} />
+      </Box>
 
       {!appointmentIsAtlas && <ClickToCallClinic phone={phone} />}
     </Box>
