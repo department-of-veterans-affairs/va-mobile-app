ef18496faeb7ba8181b991185eef02ccba456ed6
Clayton Matulich <clayton@digitalfoundry.com>
2021-03-29 16:12:42 -0700

    fixed incorrect props, added testIdProps and a11y label

--- VAMobile/src/screens/ClaimsScreen/NoClaimsAndAppealsAccess/NoClaimsAndAppealsAccess.tsx
+++ VAMobile/src/screens/ClaimsScreen/NoClaimsAndAppealsAccess/NoClaimsAndAppealsAccess.tsx
@@ -1,20 +1,20 @@
 import { Box, ClickToCallPhoneNumber, TextArea, TextView, VAScrollView } from 'components'
 import { NAMESPACE } from 'constants/namespaces'
+import { testIdProps } from 'utils/accessibility'
 import { useTheme, useTranslation } from 'utils/hooks'
 import React, { FC } from 'react'
 
-export type SplashScreenProps = Record<string, unknown>
-const NoClaimsAndAppealsAccess: FC<SplashScreenProps> = () => {
+const NoClaimsAndAppealsAccess: FC = () => {
   const theme = useTheme()
   const t = useTranslation(NAMESPACE.CLAIMS)
 
   return (
-    <VAScrollView>
+    <VAScrollView {...testIdProps('NoClaimsAndAppealsAccess')}>
       <Box my={theme.dimensions.contentMarginTop} mx={theme.dimensions.gutter}>
         <TextView variant="BitterBoldHeading">{t('claimsAndAppeals.noClaimsAndAppealsAccess.title')}</TextView>
       </Box>
       <TextArea>
-        <TextView>{t('claimsAndAppeals.noClaimsAndAppealsAccess.body')}</TextView>
+        <TextView {...testIdProps(t('claimsAndAppeals.noClaimsAndAppealsAccess.body.a11yLabel'))}>{t('claimsAndAppeals.noClaimsAndAppealsAccess.body')}</TextView>
         <ClickToCallPhoneNumber phone={t('claimDetails.VANumber')} displayedText={t('claimDetails.VANumberDisplayed')} />
       </TextArea>
     </VAScrollView>
