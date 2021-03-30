5a00794aac7d391430d6abdf1222b6c0434851ef
Komal Yaseen <komal@digitalfoundry.com>
2021-03-30 13:33:36 -0700

    health care tab content

--- VAMobile/src/screens/index.ts
+++ VAMobile/src/screens/index.ts
@@ -1,4 +1,4 @@
-export { default as Appointments } from './HealthScreen/Appointments'
+export { default as HealthScreen } from './HealthScreen'
 export { default as ClaimsScreen } from './ClaimsScreen'
 export { default as HomeScreen } from './HomeScreen'
 export { default as ProfileScreen } from './ProfileScreen'
