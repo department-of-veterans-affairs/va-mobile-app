689e925d45f975b7fbabf386d8930e96c2888ed9
Clayton Matulich <clayton@digitalfoundry.com>
2021-03-30 13:40:47 -0700

    reverted commented out tests

--- VAMobile/__tests__/claims/_claims.spec.ts
+++ VAMobile/__tests__/claims/_claims.spec.ts
@@ -1,22 +1,22 @@
-// import { doLogin, logout } from '../utils'
-// import ClaimsPageFlow from './ClaimsPageFlow.test'
-//
-// describe('claims', () => {
-//   before(async () => {
-//     if (driver.isAndroid) {
-//       console.log('Resetting app')
-//       await driver.reset()
-//     }
-//     await doLogin('vets.gov.user+100@gmail.com', '278SsNrLgPv5')
-//   })
-//
-//   after(async () => {
-//     if (driver.isIOS) {
-//       await browser.execute('mobile:clearKeychains')
-//     }
-//
-//     await logout()
-//   })
-//
-//   ClaimsPageFlow()
-// })
+import { doLogin, logout } from '../utils'
+import ClaimsPageFlow from './ClaimsPageFlow.test'
+
+describe('claims', () => {
+  before(async () => {
+    if (driver.isAndroid) {
+      console.log('Resetting app')
+      await driver.reset()
+    }
+    await doLogin('vets.gov.user+100@gmail.com', '278SsNrLgPv5')
+  })
+
+  after(async () => {
+    if (driver.isIOS) {
+      await browser.execute('mobile:clearKeychains')
+    }
+
+    await logout()
+  })
+
+  ClaimsPageFlow()
+})
