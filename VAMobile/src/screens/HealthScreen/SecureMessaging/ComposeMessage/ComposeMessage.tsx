f4e56f565c1fadb075af2eae35c782fd25152481
Komal Yaseen <komal@digitalfoundry.com>
2021-04-07 10:57:07 -0700

    updates

--- VAMobile/src/screens/HealthScreen/SecureMessaging/ComposeMessage/ComposeMessage.tsx
+++ VAMobile/src/screens/HealthScreen/SecureMessaging/ComposeMessage/ComposeMessage.tsx
@@ -1,9 +1,43 @@
 import React, { FC } from 'react'
 
-import { TextView } from 'components'
+import { Box, CollapsibleView, CrisisLineCta, TextArea, TextView, VAScrollView } from 'components'
+import { NAMESPACE } from 'constants/namespaces'
+import { testIdProps } from 'utils/accessibility'
+import { useRouteNavigation, useTheme, useTranslation } from 'utils/hooks'
 
 const ComposeMessage: FC = () => {
-  return <TextView>Compose message screen</TextView>
+  const t = useTranslation(NAMESPACE.HEALTH)
+  const theme = useTheme()
+  const navigateTo = useRouteNavigation()
+
+  const onCrisisLine = navigateTo('VeteransCrisisLine')
+
+  return (
+    <VAScrollView {...testIdProps('Compose-message-page')}>
+      <CrisisLineCta onPress={onCrisisLine} />
+      <Box mb={theme.dimensions.contentMarginBottom}>
+        <Box mb={theme.dimensions.standardMarginBetween} mx={theme.dimensions.gutter}>
+          <CollapsibleView
+            text={t('secureMessaging.composeMessage.whenWillIGetAReply')}
+            showInTextArea={false}
+            a11yHint={t('secureMessaging.composeMessage.whenWillIGetAReplyA11yHint')}>
+            <TextView variant="MobileBody" {...testIdProps(t('secureMessaging.composeMessage.threeDaysToReceiveResponseA11yLabel'))} mt={theme.dimensions.condensedMarginBetween}>
+              {t('secureMessaging.composeMessage.threeDaysToReceiveResponse')}
+            </TextView>
+            <TextView variant="MobileBody" mt={theme.dimensions.standardMarginBetween}>
+              <TextView variant="MobileBodyBold">{t('secureMessaging.composeMessage.important')}</TextView>
+              <TextView {...testIdProps(t('secureMessaging.composeMessage.pleaseCallHealthProviderA11yLabel'))}>
+                {t('secureMessaging.composeMessage.pleaseCallHealthProvider')}
+              </TextView>
+            </TextView>
+          </CollapsibleView>
+        </Box>
+        <TextArea>
+          <TextView>To do: add form</TextView>
+        </TextArea>
+      </Box>
+    </VAScrollView>
+  )
 }
 
 export default ComposeMessage
