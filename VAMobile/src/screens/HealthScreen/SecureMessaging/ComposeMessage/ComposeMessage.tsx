5abf4bb7197a9098966de4c6a39c508e498e63dd
Komal Yaseen <komal@digitalfoundry.com>
2021-04-06 17:09:56 -0700

    initial updates

---
+++ VAMobile/src/screens/HealthScreen/SecureMessaging/ComposeMessage/ComposeMessage.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react'
+
+import { TextView } from 'components'
+
+const ComposeMessage: FC = () => {
+  return <TextView>Compose message screen</TextView>
+}
+
+export default ComposeMessage
