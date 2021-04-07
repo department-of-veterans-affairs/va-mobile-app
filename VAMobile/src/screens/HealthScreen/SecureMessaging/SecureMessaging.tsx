a59ddd4008c47e17f3a26c5656eb4b4ae5306206
komaly15 <71414663+komaly15@users.noreply.github.com>
2021-04-07 08:35:17 -0700

    Created component for compose a message footer (#648)
    * created component for compose message footer
    
    * added test

--- VAMobile/src/screens/HealthScreen/SecureMessaging/SecureMessaging.tsx
+++ VAMobile/src/screens/HealthScreen/SecureMessaging/SecureMessaging.tsx
@@ -5,13 +5,14 @@ import React, { FC, ReactElement, useEffect, useState } from 'react'
 
 import { getInbox, prefetchInboxMessages } from 'store/actions'
 
-import { Box, ErrorComponent, FooterButton, SegmentedControl } from 'components'
+import { Box, ErrorComponent, SegmentedControl } from 'components'
 import { HealthStackParamList } from '../HealthStackScreens'
 import { NAMESPACE } from 'constants/namespaces'
 import { ScreenIDTypesConstants } from 'store/api/types/Screens'
 import { StoreState } from 'store/reducers'
 import { testIdProps } from 'utils/accessibility'
 import { useError, useTheme, useTranslation } from 'utils/hooks'
+import ComposeMessageFooter from './ComposeMessageFooter/ComposeMessageFooter'
 import Folders from './Folders/Folders'
 import Inbox from './Inbox/Inbox'
 
@@ -75,7 +76,7 @@ const SecureMessaging: FC<SecureMessagingScreen> = ({}) => {
           </Box>
         </Box>
       </ScrollView>
-      <FooterButton text={t('secureMessaging.composeMessage')} iconProps={{ name: 'Compose' }} a11yHint={t('secureMessaging.composeMessage.a11yHint')} />
+      <ComposeMessageFooter />
     </>
   )
 }
