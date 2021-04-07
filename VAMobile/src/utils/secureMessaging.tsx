29a9c724ade30c19df67d5bad73637c01fdb6a23
Patrick Saxton <patrick@urban-intellect.com>
2021-04-07 13:59:35 -0500

    update util function
    # Conflicts:
    #	VAMobile/src/utils/secureMessaging.tsx

--- VAMobile/src/utils/secureMessaging.tsx
+++ VAMobile/src/utils/secureMessaging.tsx
@@ -1,29 +1,36 @@
-import React, { ReactNode } from 'react'
-
 import { DateTime } from 'luxon'
 import { TFunction } from 'i18next'
 
-import { DefaultList, TextLine } from 'components'
+import { DefaultListItemObj, TextLine } from 'components'
 import { SecureMessagingMessageList } from 'store/api/types'
 import { getTestIDFromTextLines } from './accessibility'
 
-export const renderMessages = (messages: SecureMessagingMessageList, t: TFunction, onMessagePress: (messageID: number) => void, folderName?: string): ReactNode => {
+export const getMessagesListItems = (
+  messages: SecureMessagingMessageList,
+  t: TFunction,
+  onMessagePress: (messageID: number) => void,
+  folderName?: string,
+): Array<DefaultListItemObj> => {
   if (!messages || !messages.length) {
-    return null
+    return []
   }
 
-  const listItems = messages.map((message) => {
+  return messages.map((message, index) => {
     const { attributes } = message
     const { recipientName, senderName, subject, sentDate } = attributes
 
     const textLines: Array<TextLine> = [
       { text: t('common:text.raw', { text: folderName === 'Sent' ? recipientName : senderName }), variant: 'MobileBodyBold' },
-      { text: t('common:text.raw', { text: subject }) },
-      { text: t('common:text.raw', { text: DateTime.fromISO(sentDate).toFormat("dd MMM '@' HHmm ZZZZ") }) },
+      { text: t('common:text.raw', { text: t('secureMessaging.viewMessage.subject', { subject: subject }) }) },
+      { text: t('common:text.raw', { text: DateTime.fromISO(sentDate).toFormat("dd MMM yyyy '@' HHmm ZZZZ") }) },
     ]
 
-    return { textLines, onPress: () => onMessagePress(message.id), a11yHintText: t('secureMessaging.viewMessage.a11yHint'), testId: getTestIDFromTextLines(textLines) }
+    return {
+      textLines,
+      onPress: () => onMessagePress(message.id),
+      a11yHintText: t('secureMessaging.viewMessage.a11yHint'),
+      testId: getTestIDFromTextLines(textLines),
+      a11yValue: t('common:listPosition', { position: index + 1, total: messages.length }),
+    }
   })
-
-  return <DefaultList items={listItems} />
 }
