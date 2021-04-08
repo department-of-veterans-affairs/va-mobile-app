99fc3351c2d3808e90be96ab49b964f78b492c49
“Kelly <kelly@digitalfoundry.com>
2021-04-08 12:58:55 -0700

    removing unnecessary import

--- VAMobile/src/screens/HealthScreen/SecureMessaging/ViewMessage/CollapsibleMessage.tsx
+++ VAMobile/src/screens/HealthScreen/SecureMessaging/ViewMessage/CollapsibleMessage.tsx
@@ -1,5 +1,4 @@
 import { ActivityIndicator } from 'react-native'
-import { DateTime } from 'luxon'
 import { useDispatch, useSelector } from 'react-redux'
 import React, { FC, ReactNode } from 'react'
 
