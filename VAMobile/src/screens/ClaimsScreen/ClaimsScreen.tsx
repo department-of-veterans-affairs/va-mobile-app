faca2b4d6e5e96aed9a7630b8300e74630d8936c
Komal Yaseen <komal@digitalfoundry.com>
2021-04-08 13:07:55 -0700

    Revert "added check to claims"
    This reverts commit 2f3c45464e386c601bd2bd776fd3a8fe0656a38e.

--- VAMobile/src/screens/ClaimsScreen/ClaimsScreen.tsx
+++ VAMobile/src/screens/ClaimsScreen/ClaimsScreen.tsx
@@ -3,8 +3,6 @@ import { ViewStyle } from 'react-native'
 import { useDispatch, useSelector } from 'react-redux'
 import React, { FC, ReactElement, useEffect, useState } from 'react'
 
-import { useIsFocused } from '@react-navigation/native'
-
 import { AlertBox, Box, ErrorComponent, LoadingComponent, SegmentedControl, VAScrollView } from 'components'
 import { AuthorizedServicesState, ClaimsAndAppealsState, StoreState } from 'store/reducers'
 import { ClaimsStackParamList } from './ClaimsStackScreens'
@@ -22,7 +20,6 @@ const ClaimsScreen: FC<IClaimsScreen> = ({}) => {
   const t = useTranslation(NAMESPACE.CLAIMS)
   const theme = useTheme()
   const dispatch = useDispatch()
-  const isClaimsFocused = useIsFocused()
   const { loadingAllClaimsAndAppeals, claimsServiceError, appealsServiceError } = useSelector<StoreState, ClaimsAndAppealsState>((state) => state.claimsAndAppeals)
   const { claims: claimsAuthorization, appeals: appealsAuthorization } = useSelector<StoreState, AuthorizedServicesState>((state) => state.authorizedServices)
   const claimsAndAppealsAccess = claimsAuthorization || appealsAuthorization
@@ -37,7 +34,7 @@ const ClaimsScreen: FC<IClaimsScreen> = ({}) => {
   // let ClaimsAndAppealsListView handle subsequent filtering to avoid reloading all claims and appeals
   useEffect(() => {
     dispatch(getAllClaimsAndAppeals(ScreenIDTypesConstants.CLAIMS_SCREEN_ID))
-  }, [dispatch, isClaimsFocused])
+  }, [dispatch])
 
   const scrollStyles: ViewStyle = {
     flexGrow: 1,
