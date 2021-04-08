8ba2d40ced8d55992201e0e3e5da01e4296d08f4
Patrick Saxton <patrick@urban-intellect.com>
2021-04-08 13:49:38 -0500

    redux work mostly done

--- VAMobile/src/components/NotificationManger/NotificationManger.tsx
+++ VAMobile/src/components/NotificationManger/NotificationManger.tsx
@@ -1,27 +1,37 @@
+import { AuthState, StoreState, registerDevice, updateAppointmentBadge } from 'store'
 import { NotificationBackgroundFetchResult, Notifications } from 'react-native-notifications'
 import { View } from 'react-native'
 import { isIOS } from '../../utils/platform'
-import { updateAppointmentBadge } from 'store'
-import { useDispatch } from 'react-redux'
-import React, { FC } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import React, { FC, useEffect } from 'react'
 
 const NotificationManger: FC = ({ children }) => {
+  const { loggedIn } = useSelector<StoreState, AuthState>((state) => state.auth)
   const dispatch = useDispatch()
-  const registerDevice = () => {
+  const register = () => {
     Notifications.events().registerRemoteNotificationsRegistered((event) => {
-      // TODO: Send the token to my server so it could send back push notifications...
       console.log('Device Token Received', event.deviceToken)
-      // dispatch(updateDeviceToken(event.deviceToken))
+      dispatch(registerDevice(event.deviceToken))
     })
     Notifications.events().registerRemoteNotificationsRegistrationFailed((event) => {
+      //TODO: Log this error in crashlytics?
       console.error(event)
+      dispatch(registerDevice())
     })
 
     Notifications.registerRemoteNotifications()
   }
 
+  useEffect(() => {
+    if (loggedIn) {
+      register()
+    }
+  })
+
   const registerNotificationEvents = () => {
+    // Register callbacks for notifications that happen when the app is in the foreground
     Notifications.events().registerNotificationReceivedForeground((notification, completion) => {
+      //TODO: UX creates foreground notification story/stories
       console.log('Notification Received - Foreground', notification)
       let title, body
       if (isIOS()) {
@@ -33,17 +43,21 @@ const NotificationManger: FC = ({ children }) => {
       }
       // Calling completion on iOS with `alert: true` will present the native iOS inApp notification.
       console.log(`${title}: ${body}`)
-      // Alert.alert(title, body)
       dispatch(updateAppointmentBadge(true))
       completion({ alert: false, sound: false, badge: false })
     })
 
+    // Register callback for opened notifications
     Notifications.events().registerNotificationOpened((notification, completion) => {
+      /** this should be logged in firebase automatically. Anything here should be actions the app takes when it
+       * opens like deep linking, etc
+       */
       console.log('Notification opened by device user', notification)
       console.log(`Notification opened with an action identifier: ${notification.identifier}`)
       completion()
     })
 
+    // Register callbacks for notifications that happen when the app is in the background
     Notifications.events().registerNotificationReceivedBackground((notification, completion) => {
       console.log('Notification Received - Background', notification)
 
@@ -51,6 +65,7 @@ const NotificationManger: FC = ({ children }) => {
       completion(NotificationBackgroundFetchResult.NEW_DATA)
     })
 
+    // Callback in case there is need to do something with initial notification before it goes to system tray
     Notifications.getInitialNotification()
       .then((notification) => {
         console.log('Initial notification was:', notification || 'N/A')
@@ -58,7 +73,6 @@ const NotificationManger: FC = ({ children }) => {
       .catch((err) => console.error('getInitialNotification() failed', err))
   }
 
-  registerDevice()
   registerNotificationEvents()
 
   const s = { flex: 1 }
--- VAMobile/src/store/actions/notifications.ts
+++ VAMobile/src/store/actions/notifications.ts
@@ -15,7 +15,13 @@ const updateDeviceToken = (deviceToken?: string): ReduxAction => {
 export const registerDevice = (deviceToken?: string): AsyncReduxAction => {
   return async (dispatch, _getState): Promise<void> => {
     if (deviceToken) {
-      await AsyncStorage.setItem(DEVICE_TOKEN_KEY, deviceToken)
+      const savedToken = await AsyncStorage.getItem(DEVICE_TOKEN_KEY)
+      // if there is no saved token, we have not registered
+      // if there is a token and it is different, we need to register the change with VETEXT
+      if (!savedToken || savedToken !== deviceToken) {
+        //TODO: here we call the register api for VETEXT
+        await AsyncStorage.setItem(DEVICE_TOKEN_KEY, deviceToken)
+      }
     } else {
       await AsyncStorage.removeItem(DEVICE_TOKEN_KEY)
     }
