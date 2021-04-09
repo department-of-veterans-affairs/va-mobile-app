038e6b340721ed2cb16c6b7e45425493dde2bea4
Patrick Saxton <patrick@urban-intellect.com>
2021-04-09 14:21:50 -0500

    add push register api

--- VAMobile/src/store/actions/notifications.ts
+++ VAMobile/src/store/actions/notifications.ts
@@ -1,5 +1,9 @@
+import * as api from '../api'
 import { AsyncReduxAction, ReduxAction } from '../types'
+import { deviceName } from 'utils/deviceData'
 import AsyncStorage from '@react-native-async-storage/async-storage'
+import { PUSH_APP_NAME, PushOsName } from "../api";
+import { isIOS } from "../../utils/platform";
 
 const DEVICE_TOKEN_KEY = '@store_device_token'
 
@@ -14,16 +18,29 @@ const updateDeviceToken = (deviceToken?: string): ReduxAction => {
 
 export const registerDevice = (deviceToken?: string): AsyncReduxAction => {
   return async (dispatch, _getState): Promise<void> => {
-    if (deviceToken) {
-      const savedToken = await AsyncStorage.getItem(DEVICE_TOKEN_KEY)
-      // if there is no saved token, we have not registered
-      // if there is a token and it is different, we need to register the change with VETEXT
-      if (!savedToken || savedToken !== deviceToken) {
-        //TODO: here we call the register api for VETEXT
-        await AsyncStorage.setItem(DEVICE_TOKEN_KEY, deviceToken)
+    try {
+      if (deviceToken) {
+        const savedToken = await AsyncStorage.getItem(DEVICE_TOKEN_KEY)
+        // if there is no saved token, we have not registered
+        // if there is a token and it is different, we need to register the change with VETEXT
+        if (!savedToken || savedToken !== deviceToken) {
+          const params: api.PushRegistration = {
+            deviceName,
+            deviceToken,
+            appName: PUSH_APP_NAME,
+            osName: isIOS() ? PushOsName.ios : PushOsName.android,
+          }
+          const response = await api.put<api.PushRegistration>('/v0/push/register', params)
+          //TODO: save endpoint sid to AsyncStorage
+          console.log(response)
+          await AsyncStorage.setItem(DEVICE_TOKEN_KEY, deviceToken)
+        }
+      } else {
+        await AsyncStorage.removeItem(DEVICE_TOKEN_KEY)
       }
-    } else {
-      await AsyncStorage.removeItem(DEVICE_TOKEN_KEY)
+    } catch (e) {
+      //TODO: log in crashlytics?
+      console.log(e)
     }
     dispatch(updateDeviceToken(deviceToken))
   }
---
+++ VAMobile/src/store/api/types/Notifications.ts
@@ -0,0 +1,22 @@
+/** constant value for push api os names */
+export const PushOsName: {
+  ios: PushOs
+  android: PushOs
+} = {
+  ios: 'ios',
+  android: 'android',
+}
+
+/** union type for push os name strings */
+export type PushOs = 'android' | 'ios'
+
+/** constant string for push api app name */
+export const PUSH_APP_NAME = 'va_mobile_app'
+
+/** api type for registering device for push notifications api */
+export type PushRegistration = {
+  deviceToken: string
+  osName: PushOs
+  deviceName: string
+  appName: string
+}
