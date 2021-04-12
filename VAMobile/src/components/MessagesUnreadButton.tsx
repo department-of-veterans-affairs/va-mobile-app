96bbec457bf2cf8dbdba21b91ac6fe958fac8c29
“Kelly <kelly@digitalfoundry.com>
2021-04-12 09:33:10 -0700

    adding nav button

--- VAMobile/src/components/MessagesUnreadButton.tsx
+++ VAMobile/src/components/MessagesUnreadButton.tsx
@@ -1,2 +1,27 @@
 import { Pressable, ViewStyle } from 'react-native'
 import React, { FC, useState } from 'react'
+import LargeNavButton from "./LargeNavButton";
+import {NAMESPACE} from "../constants/namespaces";
+import {useTheme, useTranslation} from "../utils/hooks";
+
+type UnreadButtonProps = {
+    unread: number
+}
+
+const MessagesUnreadButton: FC<UnreadButtonProps> = ({}) => {
+    const t = useTranslation(NAMESPACE.HEALTH)
+    const theme = useTheme()
+    return(
+        <LargeNavButton
+            title={t('appointments.title')}
+            subText={t('appointments.subText')}
+            a11yHint={t('appointments.a11yHint')}
+            onPress={onAppointments}
+            borderWidth={theme.dimensions.buttonBorderWidth}
+            borderColor={'secondary'}
+            borderColorActive={'primaryDarkest'}
+            borderStyle={'solid'}
+
+        />
+    )
+}
