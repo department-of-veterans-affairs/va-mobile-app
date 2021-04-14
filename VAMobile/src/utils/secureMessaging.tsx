ac9a69abf9a68b74099ffdf24d034f72224028e9
komaly15 <71414663+komaly15@users.noreply.github.com>
2021-04-14 08:01:30 -0700

    Compose a Message form (UI) + field errors (#673)
    * udpates
    
    * ui updates
    
    * update a11y
    
    * added tests
    
    * added field errors
    
    * updated import and onpress

--- VAMobile/src/utils/secureMessaging.tsx
+++ VAMobile/src/utils/secureMessaging.tsx
@@ -1,6 +1,6 @@
 import { TFunction } from 'i18next'
 
-import { DefaultListItemObj, TextLine } from 'components'
+import { DefaultListItemObj, PickerItem, TextLine } from 'components'
 import { SecureMessagingMessageList } from 'store/api/types'
 import { getFormattedDateTimeYear } from './formattingUtils'
 import { getTestIDFromTextLines } from './accessibility'
@@ -29,4 +29,37 @@ export const getMessagesListItems = (
       a11yValue: t('common:listPosition', { position: index + 1, total: messages.length }),
     }
   })
+}
+
+export const getComposeMessageSubjectPickerOptions = (t: TFunction): Array<PickerItem> => {
+  return [
+    {
+      value: '',
+      label: '',
+    },
+    {
+      value: t('secureMessaging.composeMessage.general'),
+      label: t('secureMessaging.composeMessage.general'),
+    },
+    {
+      value: t('secureMessaging.composeMessage.covid'),
+      label: t('secureMessaging.composeMessage.covid'),
+    },
+    {
+      value: t('appointments.appointment'),
+      label: t('appointments.appointment'),
+    },
+    {
+      value: t('secureMessaging.composeMessage.medication'),
+      label: t('secureMessaging.composeMessage.medication'),
+    },
+    {
+      value: t('secureMessaging.composeMessage.test'),
+      label: t('secureMessaging.composeMessage.test'),
+    },
+    {
+      value: t('secureMessaging.composeMessage.education'),
+      label: t('secureMessaging.composeMessage.education'),
+    },
+  ]
 }
