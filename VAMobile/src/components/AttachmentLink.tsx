44621c674cf173708d802d44a87def9b7adf890e
Kelly Xiao <81595300+kxiao133@users.noreply.github.com>
2021-04-14 14:35:51 -0700

    Common Component: Attachment link UI (#674)
    * changed network connection error and generic error w/ refresh pages to display "Refresh" button instead of "Try Again" button
    
    * created basic file attachment link component, clicking does not open file yet
    
    * removing ImagePickerResponse, sticking to SecureMessageAttachment for props instead
    
    * added test file, added README.md description
    
    * fixing build changes
    
    * changing component to be more generic
    
    * removed unnecessary attachment prop
    
    * updating README.md
    
    * removing unused imports
    
    * changing text display to match conditional size/sizeUnit props
    
    * deleting old commented out part
    
    * added export to index.ts for common component, updated translation label location, updated testIdProps and pressableProps order, removed import accidentally added to FooterButton.test.tsx
    
    * updated import statements, added onPress check to test file, fed onPress prop to pressableProps
    
    * adding key for map function

---
+++ VAMobile/src/components/AttachmentLink.tsx
@@ -0,0 +1,49 @@
+import { Pressable, PressableProps } from 'react-native'
+import { generateTestID } from 'utils/common'
+import { testIdProps } from 'utils/accessibility'
+import { useTheme } from 'utils/hooks'
+import Box from './Box'
+import React, { FC } from 'react'
+import TextView from './TextView'
+import VAIcon from './VAIcon'
+
+export type AttachmentLinkProps = {
+  /** Name of link/attachment */
+  name: string
+  /** Size of file attachment */
+  size?: number
+  /** File size unit: KB, GB, etc. */
+  sizeUnit?: string
+  /** onPress function */
+  onPress?: () => void
+  /** a11y Hint */
+  a11yHint?: string
+}
+
+const AttachmentLink: FC<AttachmentLinkProps> = ({ name, size, sizeUnit, onPress, a11yHint }) => {
+  const theme = useTheme()
+
+  const pressableProps: PressableProps = {
+    onPress: onPress,
+    accessibilityRole: 'button',
+    accessible: true,
+    accessibilityHint: a11yHint || '',
+  }
+  const formattedSize = size && sizeUnit ? `(${size} ${sizeUnit})` : ''
+  const text = [name, formattedSize].join(' ').trim()
+  const testId = generateTestID(text, '')
+
+  return (
+    <Pressable {...testIdProps(testId)} {...pressableProps}>
+      <Box flexDirection={'row'}>
+        <Box mt={theme.dimensions.alertBorderWidth} mr={theme.dimensions.textIconMargin}>
+          <VAIcon name="PaperClip" width={16} height={16} fill={'link'} />
+        </Box>
+        <TextView variant={'MobileBodyLink'} color={'link'} accessible={true}>
+          {text}
+        </TextView>
+      </Box>
+    </Pressable>
+  )
+}
+export default AttachmentLink
