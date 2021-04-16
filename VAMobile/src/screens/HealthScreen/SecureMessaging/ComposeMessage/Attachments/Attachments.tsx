693886771477ac271b90648e678ee845e00c09e8
komaly15 <71414663+komaly15@users.noreply.github.com>
2021-04-16 08:49:32 -0700

    Attach file from action sheet (2 of 6) (#678)
    * attachments screen
    
    * updated integration tests
    
    * added await
    
    * updates
    
    * comments
    
    * clean up
    
    * update
    
    * added test

--- VAMobile/src/screens/HealthScreen/SecureMessaging/ComposeMessage/Attachments/Attachments.tsx
+++ VAMobile/src/screens/HealthScreen/SecureMessaging/ComposeMessage/Attachments/Attachments.tsx
@@ -1,11 +1,15 @@
-import React, { FC, ReactNode, useEffect } from 'react'
+import React, { FC, ReactNode, useEffect, useState } from 'react'
 
 import { StackHeaderLeftButtonProps, StackScreenProps } from '@react-navigation/stack'
+import { useActionSheet } from '@expo/react-native-action-sheet'
 
-import { BackButton, Box, ButtonTypesConstants, TextView, VABulletList, VAButton, VAScrollView } from 'components'
+import { BackButton, Box, ButtonTypesConstants, TextView, VAButton, VAScrollView } from 'components'
 import { BackButtonLabelConstants } from 'constants/backButtonLabels'
+import { DocumentPickerResponse } from 'screens/ClaimsScreen/ClaimsStackScreens'
 import { HealthStackParamList } from 'screens/HealthScreen/HealthStackScreens'
+import { ImagePickerResponse } from 'react-native-image-picker'
 import { NAMESPACE } from 'constants/namespaces'
+import { onAddFileAttachments } from 'utils/secureMessaging'
 import { testIdProps } from 'utils/accessibility'
 import { useTheme, useTranslation } from 'utils/hooks'
 
@@ -14,6 +18,8 @@ type AttachmentsProps = StackScreenProps<HealthStackParamList, 'Attachments'>
 const Attachments: FC<AttachmentsProps> = ({ navigation }) => {
   const t = useTranslation(NAMESPACE.HEALTH)
   const theme = useTheme()
+  const { showActionSheetWithOptions } = useActionSheet()
+  const [error, setError] = useState('')
 
   useEffect(() => {
     navigation.setOptions({
@@ -23,20 +29,24 @@ const Attachments: FC<AttachmentsProps> = ({ navigation }) => {
     })
   })
 
-  const onSelectAFile = (): void => {}
+  const callbackOnSuccessfulFileSelection = (_response: ImagePickerResponse | DocumentPickerResponse): void => {}
+
+  const onSelectAFile = (): void => {
+    onAddFileAttachments(t, showActionSheetWithOptions, setError, callbackOnSuccessfulFileSelection, 0)
+  }
 
   return (
     <VAScrollView {...testIdProps('Attachments-page')}>
       <Box mt={theme.dimensions.contentMarginTop} mb={theme.dimensions.contentMarginBottom} mx={theme.dimensions.gutter}>
+        {/*TODO: Replace error with mobile alert with error*/}
+        {!!error && <TextView>{error}</TextView>}
         <TextView variant="MobileBodyBold" accessibilityRole="header">
           {t('secureMessaging.attachments.fileAttachment')}
         </TextView>
         <TextView variant="MobileBody" my={theme.dimensions.standardMarginBetween}>
           {t('secureMessaging.attachments.youMayAttach')}
         </TextView>
-        <Box ml={theme.dimensions.gutter}>
-          <VABulletList listOfText={[t('secureMessaging.attachments.acceptedFileTypes')]} />
-        </Box>
+        <TextView variant="MobileBody">{t('secureMessaging.attachments.acceptedFileTypes')}</TextView>
         <TextView variant="MobileBody" my={theme.dimensions.standardMarginBetween}>
           {t('secureMessaging.attachments.sizeRequirements')}
         </TextView>
--- VAMobile/src/utils/secureMessaging.tsx
+++ VAMobile/src/utils/secureMessaging.tsx
@@ -1,9 +1,16 @@
 import { TFunction } from 'i18next'
 
+import { ActionSheetOptions } from '@expo/react-native-action-sheet'
+import { ImagePickerResponse } from 'react-native-image-picker/src/types'
+import { launchCamera, launchImageLibrary } from 'react-native-image-picker'
+import DocumentPicker from 'react-native-document-picker'
+
 import { DefaultListItemObj, PickerItem, TextLine } from 'components'
+import { DocumentPickerResponse } from 'screens/ClaimsScreen/ClaimsStackScreens'
+import { MAX_SINGLE_MESSAGE_ATTACHMENT_SIZE_IN_BYTES, MAX_TOTAL_MESSAGE_ATTACHMENTS_SIZE_IN_BYTES } from 'constants/secureMessaging'
 import { SecureMessagingMessageList } from 'store/api/types'
-import { getFormattedDateTimeYear } from './formattingUtils'
-import { getTestIDFromTextLines } from './accessibility'
+import { getFormattedDateTimeYear } from 'utils/formattingUtils'
+import { getTestIDFromTextLines } from 'utils/accessibility'
 
 export const getMessagesListItems = (
   messages: SecureMessagingMessageList,
@@ -62,4 +69,137 @@ export const getComposeMessageSubjectPickerOptions = (t: TFunction): Array<Picke
       label: t('secureMessaging.composeMessage.education'),
     },
   ]
+}
+
+/**
+ * Returns true if the given file type is a doc, docx, jpeg, jpg, gif, txt, pdf, png, rtf, xlx, or xlsx file
+ *
+ * @param fileType - given file type to check if valid
+ */
+const isValidAttachmentsFileType = (fileType: string): boolean => {
+  const validFileTypes = ['doc', 'docx', 'jpeg', 'jpg', 'gif', 'text/plain', 'txt', 'pdf', 'png', 'rtf', 'xls', 'xlsx']
+  return !!validFileTypes.find((type) => fileType.includes(type))
+}
+
+/**
+ * Selects a file from the devices file system, sets the error if an error is handled, otherwise calls callbackIfUri
+ *
+ * @param setError - function setting the error message
+ * @param callbackIfUri - callback function called if there is no error with the file
+ * @param totalBytesUsed - total number of bytes used so far by previously selected images/files
+ */
+export const onFileFolderSelect = async (
+  setError: (error: string) => void,
+  callbackIfUri: (response: ImagePickerResponse | DocumentPickerResponse) => void,
+  totalBytesUsed: number,
+): Promise<void> => {
+  try {
+    const document = await DocumentPicker.pick({
+      type: [DocumentPicker.types.images, DocumentPicker.types.plainText, DocumentPicker.types.pdf],
+    })
+
+    const { size, type } = document
+
+    // TODO: update error messages
+    if (size > MAX_SINGLE_MESSAGE_ATTACHMENT_SIZE_IN_BYTES) {
+      setError('FILE SIZE ERROR')
+    } else if (size + totalBytesUsed > MAX_TOTAL_MESSAGE_ATTACHMENTS_SIZE_IN_BYTES) {
+      setError('SUM OF FILE SIZES ERROR')
+    } else if (!isValidAttachmentsFileType(type)) {
+      setError('INVALID FILE TYPE ERROR')
+    } else {
+      setError('')
+      callbackIfUri(document)
+    }
+  } catch (docError) {
+    if (DocumentPicker.isCancel(docError)) {
+      return
+    }
+
+    setError(docError.code)
+  }
+}
+
+/**
+ * After the camera takes a photo or a photo is selected from the gallery, if an error exists setError is called to display
+ * the error message. If there is no error and the image uri exists, callbackIfUri is called.
+ *
+ * @param response - response with image data given after image is taken or selected
+ * @param setError - function setting the error message
+ * @param callbackIfUri - callback function called if there is no error with the image and the uri exists
+ * @param totalBytesUsed - total number of bytes used so far by previously selected images/files
+ */
+export const postCameraOrImageLaunchOnFileAttachments = (
+  response: ImagePickerResponse,
+  setError: (error: string) => void,
+  callbackIfUri: (response: ImagePickerResponse | DocumentPickerResponse) => void,
+  totalBytesUsed: number,
+): void => {
+  const { fileSize, errorMessage, uri, didCancel } = response
+
+  if (didCancel) {
+    return
+  }
+
+  // TODO: update error messages
+  if (!!fileSize && fileSize > MAX_SINGLE_MESSAGE_ATTACHMENT_SIZE_IN_BYTES) {
+    setError('FILE SIZE ERROR')
+  } else if (!!fileSize && fileSize + totalBytesUsed > MAX_TOTAL_MESSAGE_ATTACHMENTS_SIZE_IN_BYTES) {
+    setError('SUM OF FILE SIZES ERROR')
+  } else if (!!response.type && !isValidAttachmentsFileType(response.type)) {
+    setError('INVALID FILE TYPE ERROR')
+  } else if (errorMessage) {
+    setError(errorMessage)
+  } else {
+    setError('')
+
+    if (uri) {
+      callbackIfUri(response)
+    }
+  }
+}
+
+/**
+ * Opens up an action sheet with the options to open the camera, camera roll, the devices file folders,
+ * or cancel. On click of one of the options, it's corresponding action is implemented (launching the
+ * camera or camera roll or the device's folders).
+ *
+ * @param t - translation function
+ * @param showActionSheetWithOptions - hook to open the action sheet
+ * @param setError - sets error message
+ * @param callbackIfUri - callback when a file is selected or taken with the camera successfully
+ * @param totalBytesUsed - total number of bytes used so far by previously selected images/files
+ */
+export const onAddFileAttachments = (
+  t: TFunction,
+  showActionSheetWithOptions: (options: ActionSheetOptions, callback: (i: number) => void) => void,
+  setError: (error: string) => void,
+  callbackIfUri: (response: ImagePickerResponse | DocumentPickerResponse) => void,
+  totalBytesUsed: number,
+): void => {
+  const options = [t('common:camera'), t('common:photoGallery'), t('common:fileFolder'), t('common:cancel')]
+
+  showActionSheetWithOptions(
+    {
+      options,
+      cancelButtonIndex: 3,
+    },
+    (buttonIndex) => {
+      switch (buttonIndex) {
+        case 0:
+          launchCamera({ mediaType: 'photo', quality: 0.9 }, (response: ImagePickerResponse): void => {
+            postCameraOrImageLaunchOnFileAttachments(response, setError, callbackIfUri, totalBytesUsed)
+          })
+          break
+        case 1:
+          launchImageLibrary({ mediaType: 'photo', quality: 0.9 }, (response: ImagePickerResponse): void => {
+            postCameraOrImageLaunchOnFileAttachments(response, setError, callbackIfUri, totalBytesUsed)
+          })
+          break
+        case 2:
+          onFileFolderSelect(setError, callbackIfUri, totalBytesUsed)
+          break
+      }
+    },
+  )
 }
