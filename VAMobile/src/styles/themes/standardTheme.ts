0706e1bcd87a04b4fb104f75e9a1b29651c66888
John Marchi <j_marchi@digitalfoundry.com>
2021-04-19 10:26:19 -0700

    updating base list item for picker list (#684)
    Co-authored-by: Komal Yaseen <komal@digitalfoundry.com>

--- VAMobile/src/styles/themes/standardTheme.ts
+++ VAMobile/src/styles/themes/standardTheme.ts
@@ -91,6 +91,7 @@ const theme: VATheme = {
       covid19VaccinationsActive: colors.primaryDarkest,
       unreadMessagesTag: colors.grayDark,
       navHeader: colors.primaryDarker,
+      pickerSelectedItem: colors.primaryAltLightest,
     },
     alertBox: {
       cardBackground: colors.grayLightest,
@@ -221,6 +222,8 @@ const theme: VATheme = {
     tagCountTopPadding: 3,
     messagePhotoAttachmentMaxHeight: 300,
     maxNumMessageAttachments: 4,
+    pickerModalSelectedIconWidth: 16,
+    pickerModalSelectedIconHeight: 13,
   },
 
   fontFace: {
