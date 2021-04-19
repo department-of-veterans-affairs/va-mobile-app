f71280d61da5d6e76128f80e5bee20c3b8eee76f
Patrick Saxton <patrick@urban-intellect.com>
2021-04-19 12:12:47 -0500

    update tests

--- VAMobile/src/screens/ProfileScreen/SettingsScreen/NotificationsSettingsScreen/NotificationsSettingsScreen.test.tsx
+++ VAMobile/src/screens/ProfileScreen/SettingsScreen/NotificationsSettingsScreen/NotificationsSettingsScreen.test.tsx
@@ -6,9 +6,8 @@ import {context, mockNavProps, mockStore, renderWithProviders} from 'testUtils'
 import { InitialState } from "../../../../store";
 import { PushPreference } from "../../../../store/api";
 import NotificationsSettingsScreen from "./NotificationsSettingsScreen";
-import { AlertBox, TextView } from "../../../../components";
 
-let mockPushEnabled = true
+let mockPushEnabled = false
 jest.mock('../../../../utils/notifications', () => {
     return {
         notificationsEnabled: jest.fn(() => {
@@ -17,19 +16,10 @@ jest.mock('../../../../utils/notifications', () => {
     }
 })
 
-jest.mock('../../../../utils/deviceData', () => {
-    return {
-        deviceName: jest.fn(() => {
-            return 'Test Device Name'
-        })
-    }
-})
-
 context('NotificationsSettingsScreen', () => {
     let store: any
     let component: any
     let testInstance: ReactTestInstance
-
 
 
     const apptPrefOn: PushPreference = {
@@ -62,21 +52,11 @@ context('NotificationsSettingsScreen', () => {
         testInstance = component.root
     }
     beforeEach(() => {
-        initializeTestInstance(true, [apptPrefOn])
+        initializeTestInstance(false, [apptPrefOn])
     })
 
     it('initializes correctly', async () => {
         expect(component).toBeTruthy()
-    })
-
-    describe('when system notifications are off', () => {
-        it('should render the alert box', async () => {
-            initializeTestInstance(true, [apptPrefOn])
-            const alertBox = testInstance.findAllByType(AlertBox)[0]
-            expect(alertBox).toBeTruthy()
-            expect(alertBox.findAllByType(TextView)[0].props.childer).toEqual('Turn on push notifications')
-            expect(alertBox.findAllByType(TextView)[1].props.childer).toEqual('To get notifications from the VA mobile app, you\'ll need to turn them on in your system settings.')
-        })
     })
 
 })
