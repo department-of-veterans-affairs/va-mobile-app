e9cc845e4f7da8126cab845af136bcb45929b2eb
“Kelly <kelly@digitalfoundry.com>
2021-04-21 15:39:58 -0700

    added viewMessage.tsx test file

---
+++ VAMobile/src/screens/HealthScreen/SecureMessaging/ViewMessage/ViewMessageScreen.test.tsx
@@ -0,0 +1,165 @@
+import 'react-native'
+import React from 'react'
+import {Pressable} from "react-native";
+// Note: test renderer must be required after react-native.
+import 'jest-styled-components'
+import { ReactTestInstance, act } from 'react-test-renderer'
+
+import { context, mockNavProps, mockStore, renderWithProviders } from 'testUtils'
+import {
+    CategoryTypeFields,
+    SecureMessagingMessageMap,
+    SecureMessagingThreads
+} from 'store/api/types'
+import {initialAuthState, initialErrorsState, initialSecureMessagingState} from "store";
+import {AccordionCollapsible, LoadingComponent, TextView} from 'components'
+import ViewMessageScreen from "./ViewMessageScreen";
+import Mock = jest.Mock;
+
+
+let mockNavigationSpy = jest.fn()
+jest.mock('/utils/hooks', () => {
+    let original = jest.requireActual("/utils/hooks")
+    let theme = jest.requireActual("/styles/themes/standardTheme").default
+    return {
+        ...original,
+        useTheme: jest.fn(()=> {
+            return {...theme}
+        }),
+        useRouteNavigation: () => { return () => mockNavigationSpy},
+    }
+})
+
+// Contains message Ids grouped together by thread
+const mockThreads: Array<Array<number>> = [
+    [1,2,3], [45]
+]
+
+// Contains message attributes mapped to their ids
+const mockMessagesById: SecureMessagingMessageMap = {
+1: {
+        messageId: 1,
+        category: CategoryTypeFields.other,
+        subject: 'mock subject 1: The initial message sets the overall thread subject header',
+        body: 'test1',
+        attachment: false,
+        sentDate: '1',
+        senderId: 2,
+        senderName: 'mock sender 1',
+        recipientId: 3,
+        recipientName: 'mock recipient name 1',
+        readReceipt: 'mock read receipt 1'
+    },
+    2 : {
+        messageId: 2,
+        category: CategoryTypeFields.other,
+        subject: '',
+        body: 'test 2',
+        attachment: false,
+        sentDate: '2',
+        senderId: 2,
+        senderName: 'mock sender 2',
+        recipientId: 3,
+        recipientName: 'mock recipient name 2',
+        readReceipt: 'mock read receipt 2'
+    },
+    3 : {
+        messageId: 3,
+        category: CategoryTypeFields.other,
+        subject: '',
+        body: 'Last accordion collapsible should be open, so the body text of this message should display',
+        attachment: false,
+        sentDate: '3',
+        senderId: 2,
+        senderName: 'mock sender 3',
+        recipientId: 3,
+        recipientName: 'mock recipient name 3',
+        readReceipt: 'mock read receipt'
+},
+    45: {
+        messageId: 45,
+        category: CategoryTypeFields.other,
+        subject: 'This message should not display because it has different thread ID',
+        body: 'test',
+        attachment: false,
+        sentDate: '1-1-21',
+        senderId: 2,
+        senderName: 'mock sender 45',
+        recipientId: 3,
+        recipientName: 'mock recipient name',
+        readReceipt: 'mock read receipt'
+    }
+}
+
+
+context('ViewMessageScreen', () => {
+    let component: any
+    let store: any
+    let props: any
+    let testInstance: ReactTestInstance
+    let onPressSpy: Mock
+    onPressSpy = jest.fn(() => {})
+
+    const initializeTestInstance = (mockMessagesById: SecureMessagingMessageMap, threadList: SecureMessagingThreads, loading: boolean = false) => {
+        /** messageID is 3 because inbox/folder previews the last message from a thread, aka the message we clicked on to access the rest of thread
+         * While the renderMessages function can identify the correct thread array from any one of the messageIDs in that particular thread, it also
+         * uses messageID to determine which AccordionCollapsible component should be expanded by default.
+         * So it's important when testing to set this messageID to the last message in the thread to match design specs for ViewMessage.tsx
+         * */
+        props = mockNavProps(undefined, undefined, { params: { messageID: 3 }})
+
+        onPressSpy = jest.fn(() => {})
+
+        store = mockStore({
+            auth: {...initialAuthState},
+            secureMessaging: {
+                ...initialSecureMessagingState,
+                loading: loading,
+                messagesById: mockMessagesById,
+                threads: threadList,
+            },
+            errors: initialErrorsState,
+
+        })
+
+        act(() => {
+            component = renderWithProviders(
+                <ViewMessageScreen {...props} />, store
+            )
+        })
+
+        testInstance = component.root
+    }
+
+    beforeEach(() => {
+        initializeTestInstance(mockMessagesById, mockThreads)
+    })
+
+    it('initializes correctly', async () => {
+        expect(component).toBeTruthy()
+    })
+
+    it('renders only messages in the same thread as the message associated with messageID', async () =>{
+        expect(testInstance.findAllByType(AccordionCollapsible).length).toBe(3)
+    })
+
+    it('should render the correct text content of thread, and all accordions except the last should be closed', async () => {
+        expect(testInstance.findAllByType(TextView)[1].props.children).toBe('mock sender 1')
+        expect(testInstance.findAllByType(TextView)[2].props.children).toBe('Invalid DateTime')
+        expect(testInstance.findAllByType(TextView)[3].props.children).toBe('mock sender 2')
+        expect(testInstance.findAllByType(TextView)[4].props.children).toBe('Invalid DateTime')
+        expect(testInstance.findAllByType(TextView)[5].props.children).toBe('mock sender 3')
+        expect(testInstance.findAllByType(TextView)[6].props.children).toBe('Invalid DateTime')
+    })
+
+    it("should render last accordion's body text since it should be expanded", async () => {
+        expect(testInstance.findAllByType(TextView)[7].props.children).toBe('Last accordion collapsible should be open, so the body text of this message should display')
+    })
+
+    describe('when loading is set to true', () => {
+        it('should show loading screen', async () => {
+            initializeTestInstance({}, [], true)
+            expect(testInstance.findByType(LoadingComponent)).toBeTruthy()
+        })
+    })
+})
