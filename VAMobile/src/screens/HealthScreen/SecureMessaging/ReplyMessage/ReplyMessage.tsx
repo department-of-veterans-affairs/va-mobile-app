dbc27f491e715fd9a4451380a3e569f8b82ed831
“Kelly <kelly@digitalfoundry.com>
2021-04-26 11:04:36 -0700

    added formwrapper text input, send and cancel button

--- VAMobile/src/screens/HealthScreen/SecureMessaging/ReplyMessage/ReplyMessage.tsx
+++ VAMobile/src/screens/HealthScreen/SecureMessaging/ReplyMessage/ReplyMessage.tsx
@@ -1,6 +1,19 @@
-import React, { FC, ReactNode, useEffect } from 'react'
+import React, { FC, ReactNode, useEffect, useState } from 'react'
 
-import { AlertBox, BackButton, Box, CrisisLineCta, LoadingComponent, TextView, VAScrollView } from 'components'
+import {
+  BackButton,
+  Box,
+  ButtonTypesConstants,
+  CrisisLineCta,
+  FieldType,
+  FormFieldType,
+  FormWrapper,
+  LoadingComponent,
+  TextArea,
+  TextView,
+  VAButton,
+  VAScrollView,
+} from 'components'
 import { BackButtonLabelConstants } from 'constants/backButtonLabels'
 import { HealthStackParamList } from 'screens/HealthScreen/HealthStackScreens'
 import { NAMESPACE } from 'constants/namespaces'
@@ -17,6 +30,9 @@ const ReplyMessage: FC<ReplyMessageProps> = ({ navigation, route }) => {
   const t = useTranslation(NAMESPACE.HEALTH)
   const theme = useTheme()
   const navigateTo = useRouteNavigation()
+
+  const [onSaveClicked, setOnSaveClicked] = useState(false)
+  const [messageReply, setMessageReply] = useState('')
 
   const messageID = Number(route.params.messageID)
   const { messagesById, threads, loading } = useSelector<StoreState, SecureMessagingState>((state) => state.secureMessaging)
@@ -38,11 +54,50 @@ const ReplyMessage: FC<ReplyMessageProps> = ({ navigation, route }) => {
     return <LoadingComponent text={t('secureMessaging.viewMessage.loading')} />
   }
 
+  const formFieldsList: Array<FormFieldType<unknown>> = [
+    {
+      fieldType: FieldType.TextInput,
+      fieldProps: {
+        inputType: 'none',
+        value: messageReply,
+        onChange: setMessageReply,
+        labelKey: 'health:secureMessaging.composeMessage.message',
+        isRequiredField: true,
+        isTextArea: true,
+      },
+      fieldErrorMessage: t('secureMessaging.composeMessage.message.fieldError'),
+    },
+  ]
+  const renderForm = (): ReactNode => {
+    return (
+      <TextArea>
+        <FormWrapper fieldsList={formFieldsList} onSave={(): void => {}} onSaveClicked={onSaveClicked} setOnSaveClicked={setOnSaveClicked} />
+        <Box mt={theme.dimensions.standardMarginBetween}>
+          <VAButton
+            label={t('secureMessaging.composeMessage.send')}
+            onPress={() => setOnSaveClicked(true)}
+            a11yHint={t('secureMessaging.composeMessage.send.a11yHint')}
+            buttonType={ButtonTypesConstants.buttonPrimary}
+          />
+        </Box>
+        <Box mt={theme.dimensions.standardMarginBetween}>
+          <VAButton
+            label={t('common:cancel')}
+            onPress={() => navigation.goBack()}
+            a11yHint={t('secureMessaging.composeMessage.cancel.a11yHint')}
+            buttonType={ButtonTypesConstants.buttonSecondary}
+          />
+        </Box>
+      </TextArea>
+    )
+  }
+
   return (
     <VAScrollView {...testIdProps('Reply-message-page')}>
       <CrisisLineCta onPress={onCrisisLine} />
       <Box mb={theme.dimensions.contentMarginBottom}>
-        <AlertBox title="Placeholder for Reply Form" border={'informational'} background={'noCardBackground'} />
+        <Box>{renderForm()}</Box>
+
         <Box accessible={true} accessibilityRole={'header'}>
           <TextView ml={theme.dimensions.gutter} mt={theme.dimensions.standardMarginBetween} variant={'MobileBodyBold'}>
             {t('secureMessaging.reply.messageThread')}
