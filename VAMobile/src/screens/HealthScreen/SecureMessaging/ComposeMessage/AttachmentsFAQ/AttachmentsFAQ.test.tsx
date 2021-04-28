d0694bb80e98117c61044786376868afcecd37a3
“Kelly <kelly@digitalfoundry.com>
2021-04-28 12:49:57 -0700

    created new folder, moved attachmentsFAQ file to folder and  created new test file

---
+++ VAMobile/src/screens/HealthScreen/SecureMessaging/ComposeMessage/AttachmentsFAQ/AttachmentsFAQ.test.tsx
@@ -0,0 +1,61 @@
+import 'react-native'
+import React from 'react'
+// Note: test renderer must be required after react-native.
+import {act, ReactTestInstance} from 'react-test-renderer'
+import {context, mockNavProps, mockStore, renderWithProviders} from 'testUtils'
+
+import {initialAuthState} from 'store/reducers'
+import {TextView} from 'components'
+import AttachmentsFAQ from "./AttachmentsFAQ";
+
+let mockNavigationSpy = jest.fn()
+jest.mock('utils/hooks', () => {
+    let original = jest.requireActual("utils/hooks")
+    let theme = jest.requireActual("styles/themes/standardTheme").default
+    return {
+        ...original,
+        useTheme: jest.fn(()=> {
+            return {...theme}
+        }),
+        useRouteNavigation: () => { return () => mockNavigationSpy},
+    }
+})
+
+jest.mock('@react-navigation/native', () => {
+    const original = jest.requireActual('@react-navigation/native')
+    return {
+        ...original,
+        useFocusEffect: () => jest.fn(),
+    };
+})
+
+context('HowDoIUpdateScreen', () => {
+    let store: any
+    let component: any
+    let testInstance: ReactTestInstance
+
+    beforeEach(() => {
+        const props = mockNavProps({}, { setOptions: jest.fn(), navigate: jest.fn() })
+
+        store = mockStore({
+            auth: {...initialAuthState},
+        })
+
+        act(() => {
+            component = renderWithProviders(<AttachmentsFAQ {...props} />, store)
+        })
+
+        testInstance = component.root
+    })
+
+    it('initializes correctly', async () => {
+        expect(component).toBeTruthy()
+    })
+
+    describe('when the call TTY link is clicked', () => {
+        it('should call useRouteNavigation', async () => {
+            testInstance.findAllByType(TextView)[4].props.onPress()
+            expect(mockNavigationSpy).toBeCalled()
+        })
+    })
+})
