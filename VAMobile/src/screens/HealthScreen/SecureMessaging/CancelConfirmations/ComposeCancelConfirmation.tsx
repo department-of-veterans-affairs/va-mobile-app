b87a24b557f3c45fcd3a8128cd15a3b202ebf0e7
“Kelly <kelly@digitalfoundry.com>
2021-04-29 15:29:29 -0700

    added translation values for cancel confirmation screen, updated screen's ui to match designs and route to inbox

--- VAMobile/src/screens/HealthScreen/SecureMessaging/CancelConfirmations/ComposeCancelConfirmation.tsx
+++ VAMobile/src/screens/HealthScreen/SecureMessaging/CancelConfirmations/ComposeCancelConfirmation.tsx
@@ -1,12 +1,23 @@
-import { BackButton, Box, TextView } from 'components'
+import { AlertBox, BackButton, Box, ButtonTypesConstants, CrisisLineCta, VAButton, VAScrollView } from 'components'
 import { BackButtonLabelConstants } from 'constants/backButtonLabels'
 import { HealthStackParamList } from 'screens/HealthScreen/HealthStackScreens'
+import { NAMESPACE } from 'constants/namespaces'
+import { SecureMessagingTabTypesConstants } from 'store/api'
 import { StackHeaderLeftButtonProps, StackScreenProps } from '@react-navigation/stack'
+import { testIdProps } from 'utils/accessibility'
+import { updateSecureMessagingTab } from 'store'
+import { useDispatch } from 'react-redux'
+import { useRouteNavigation, useTheme, useTranslation } from 'utils/hooks'
 import React, { FC, ReactNode, useEffect } from 'react'
 
 type ComposeCancelConfirmationProps = StackScreenProps<HealthStackParamList, 'ComposeCancelConfirmation'>
 
 const ComposeCancelConfirmation: FC<ComposeCancelConfirmationProps> = ({ navigation }) => {
+  const t = useTranslation(NAMESPACE.HEALTH)
+  const theme = useTheme()
+  const dispatch = useDispatch()
+  const navigateTo = useRouteNavigation()
+
   useEffect(() => {
     navigation.setOptions({
       headerLeft: (props: StackHeaderLeftButtonProps): ReactNode => (
@@ -15,10 +26,41 @@ const ComposeCancelConfirmation: FC<ComposeCancelConfirmationProps> = ({ navigat
     })
   })
 
+  const onCrisisLine = navigateTo('VeteransCrisisLine')
+
+  const onGoToInbox = (): void => {
+    dispatch(updateSecureMessagingTab(SecureMessagingTabTypesConstants.INBOX))
+    navigateTo('SecureMessaging')()
+  }
+
   return (
-    <Box>
-      <TextView>{'PLACEHOLDER FOR CANCEL CONFIRMATION SCREEN'}</TextView>
-    </Box>
+    <VAScrollView {...testIdProps('Compose Message Cancel Confirmation: compose-message-cancel-confirmation-page')}>
+      <CrisisLineCta onPress={onCrisisLine} />
+      <Box mb={theme.dimensions.contentMarginBottom} mx={theme.dimensions.gutter}>
+        <AlertBox
+          title={t('secureMessaging.composeMessage.cancel.cancelQuestion')}
+          text={t('secureMessaging.composeMessage.cancel.ifYouCancel')}
+          background="noCardBackground"
+          border="warning">
+          <Box mt={theme.dimensions.standardMarginBetween}>
+            <VAButton
+              onPress={onGoToInbox}
+              label={t('secureMessaging.composeMessage.cancel.goToInbox')}
+              a11yHint={t('secureMessaging.composeMessage.cancel.goToInboxA11y')}
+              buttonType={ButtonTypesConstants.buttonPrimary}
+            />
+            <Box mt={theme.dimensions.standardMarginBetween}>
+              <VAButton
+                onPress={() => navigation.goBack()}
+                label={t('secureMessaging.sendConfirmation.editingButton')}
+                a11yHint={t('secureMessaging.sendConfirmation.editingButton.a11y')}
+                buttonType={ButtonTypesConstants.buttonSecondary}
+              />
+            </Box>
+          </Box>
+        </AlertBox>
+      </Box>
+    </VAScrollView>
   )
 }
 export default ComposeCancelConfirmation
