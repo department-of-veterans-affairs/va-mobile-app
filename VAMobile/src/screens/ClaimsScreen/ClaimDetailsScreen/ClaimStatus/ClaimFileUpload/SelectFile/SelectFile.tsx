3b97bbdce426b85ff9f0b49ef39a5b53dab4ef4e
John Marchi <j_marchi@digitalfoundry.com>
2021-05-18 15:42:29 -0700

    Claim File Request Integration Single File (#769)
    * merge
    
    * Claims file upload - single file
    
    * adding tests
    
    * code cleanup
    
    * fixing lint error
    
    * cleanup
    
    * updating gitignore
    
    * removing logs

--- VAMobile/src/screens/ClaimsScreen/ClaimDetailsScreen/ClaimStatus/ClaimFileUpload/SelectFile/SelectFile.tsx
+++ VAMobile/src/screens/ClaimsScreen/ClaimDetailsScreen/ClaimStatus/ClaimFileUpload/SelectFile/SelectFile.tsx
@@ -50,8 +50,14 @@ const SelectFile: FC<SelectFilesProps> = ({ navigation, route }) => {
         return
       }
 
+      // TODO: Convert to base64 string for the claims multi file upload API
+      // const base64 = await getBase64ForUri(document.uri)
+      const fileUploaded = {
+        ...document,
+      }
+
       setError('')
-      navigateTo('UploadFile', { request, fileUploaded: document })()
+      navigateTo('UploadFile', { request, fileUploaded })()
     } catch (docError) {
       if (DocumentPicker.isCancel(docError)) {
         return
