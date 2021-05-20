d37f2d282becb21b2d1388fab103d569e5641d76
Narin Ratana <narin@adhocteam.us>
2021-05-20 11:50:18 -0700

    Remove usage of base64 for SM attachments

--- VAMobile/src/utils/secureMessaging.tsx
+++ VAMobile/src/utils/secureMessaging.tsx
@@ -213,18 +213,15 @@ export const postCameraOrImageLaunchOnFileAttachments = (
   setError: (error: string) => void,
   callbackIfUri: (response: ImagePickerResponse | DocumentPickerResponse, isImage: boolean) => void,
   totalBytesUsed: number,
-  imageBase64s: Array<string>,
   t: TFunction,
 ): void => {
-  const { fileSize, errorMessage, uri, didCancel, type, base64 } = response
+  const { fileSize, errorMessage, uri, didCancel, type } = response
 
   if (didCancel) {
     return
   }
 
-  if (!!base64 && imageBase64s.indexOf(base64) !== -1) {
-    setError(t('secureMessaging.attachments.duplicateFileError'))
-  } else if (!!type && !isValidAttachmentsFileType(type)) {
+  if (!!type && !isValidAttachmentsFileType(type)) {
     setError(t('secureMessaging.attachments.fileTypeError'))
   } else if (!!fileSize && fileSize > MAX_SINGLE_MESSAGE_ATTACHMENT_SIZE_IN_BYTES) {
     setError(t('secureMessaging.attachments.fileSizeError'))
@@ -274,20 +271,14 @@ export const onAddFileAttachments = (
     (buttonIndex) => {
       switch (buttonIndex) {
         case 0:
-          launchCamera(
-            { mediaType: 'photo', quality: 1, maxWidth: MAX_IMAGE_DIMENSION, maxHeight: MAX_IMAGE_DIMENSION, includeBase64: true },
-            (response: ImagePickerResponse): void => {
-              postCameraOrImageLaunchOnFileAttachments(response, setError, callbackIfUri, totalBytesUsed, imageBase64s, t)
-            },
-          )
+          launchCamera({ mediaType: 'photo', quality: 1, maxWidth: MAX_IMAGE_DIMENSION, maxHeight: MAX_IMAGE_DIMENSION }, (response: ImagePickerResponse): void => {
+            postCameraOrImageLaunchOnFileAttachments(response, setError, callbackIfUri, totalBytesUsed, t)
+          })
           break
         case 1:
-          launchImageLibrary(
-            { mediaType: 'photo', quality: 1, maxWidth: MAX_IMAGE_DIMENSION, maxHeight: MAX_IMAGE_DIMENSION, includeBase64: true },
-            (response: ImagePickerResponse): void => {
-              postCameraOrImageLaunchOnFileAttachments(response, setError, callbackIfUri, totalBytesUsed, imageBase64s, t)
-            },
-          )
+          launchImageLibrary({ mediaType: 'photo', quality: 1, maxWidth: MAX_IMAGE_DIMENSION, maxHeight: MAX_IMAGE_DIMENSION }, (response: ImagePickerResponse): void => {
+            postCameraOrImageLaunchOnFileAttachments(response, setError, callbackIfUri, totalBytesUsed, t)
+          })
           break
         case 2:
           onFileFolderSelect(setError, callbackIfUri, totalBytesUsed, fileUris, t)
