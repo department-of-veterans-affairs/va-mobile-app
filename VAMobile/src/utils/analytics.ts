51fff76b8111e8c14114f82de59f68b127298cbd
Patrick Saxton <patrick@urban-intellect.com>
2021-05-21 14:45:50 -0500

    ignore

--- VAMobile/src/utils/analytics.ts
+++ VAMobile/src/utils/analytics.ts
@@ -6,16 +6,18 @@ type Event = {
 }
 
 // todo: make a file full of event classes/objects to reference
-
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const logAnalyticsEvent = async (event: Event): Promise<void> => {
   const { name, params } = event
   await analytics().logEvent(name, params)
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const setAnalyticsUserProperty = async (name: string, value: string | null): Promise<void> => {
   await analytics().setUserProperty(name, value)
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const setAnalyticsUserProperties = async (properties: { [key: string]: string | null }): Promise<void> => {
   await analytics().setUserProperties(properties)
 }
