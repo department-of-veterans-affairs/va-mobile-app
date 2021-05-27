49f3ade8ac079b23b8e1b1967b0d36d1fceec9ff
Ken Li <k_li@digitalfoundry.com>
2021-05-27 13:27:24 -0700

    Select & copy text

--- VAMobile/src/screens/HealthScreen/SecureMessaging/SendConfirmation/SendConfirmation.test.tsx
+++ VAMobile/src/screens/HealthScreen/SecureMessaging/SendConfirmation/SendConfirmation.test.tsx
@@ -18,8 +18,12 @@ import {CategoryTypeFields} from "store/api/types";
 let mockNavigationSpy = jest.fn()
 jest.mock('utils/hooks', () => {
     let original = jest.requireActual("utils/hooks")
+    let theme = jest.requireActual("styles/themes/standardTheme").default
     return {
         ...original,
+        useTheme: jest.fn(()=> {
+            return {...theme}
+        }),
         useRouteNavigation: () => { return () => mockNavigationSpy},
     }
 })
