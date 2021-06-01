5e130bf488c97da259add7f9a4d25173e2ecb9d3
Kelly Xiao <81595300+kxiao133@users.noreply.github.com>
2021-06-01 08:17:36 -0700

    Message thread loading bug (#818)
    * changed network connection error and generic error w/ refresh pages to display "Refresh" button instead of "Try Again" button
    
    * added new unread icon svg
    
    * removing build changes
    
    * undoing change meant for other PR
    
    * new messageList item
    
    * adding export to index file for new messageList component
    
    * changed TextLines to take in props for an icon-to-line-number map, and display icons accordingly
    
    * hooking up new messageList component to inbox and folder lists
    
    * adding new theme dimension for icon left margin 16
    
    * passing in readReceipt and attachment boolean props to messageList
    
    * changing '==' to '==='
    
    * added tentative messageList test file, still need to add VAIcon check
    
    * added more generic textlines with icons component, added to messageList
    
    * modified test to check for VAIcons
    
    * removing unnecessary import
    
    * adding 'READ' string constant for message readReceipt attribute
    
    * icon prop fed into textline from secureMessaging.tsx now, read/unread + attachment icon conditionals worked out here instead of in messageList
    
    * TextLinesWithIcon modified to only take in props for single textLine, to add icon directly next to each line
    
    * changed to take in textLinewithIcon, map function transferred to here
    
    * test modified b/c props got modified
    
    * created new generatTestID function to include accessibility labels for icons appearing next to textlines
    
    * translation for unread icon a11y label
    
    * renamed file
    
    * added exports
    
    * updated import statements, added test file for TextLineWithIcon
    
    * updated to only display READ tag for Sent folder, updated import statements for messagesCountTag component
    
    * added read tag component, read tag dependent on folder name, updated some themes for font size
    
    * added test file for new sent read tag component, deleted unused import from count tag component test file
    
    * updated accessibility labels to differ between READ tag for sent folder and READ tag for user's unread messages
    
    * modified test file to check for a11y labels, added new common testid generation function to generate with spaces not dashes
    
    * removed unnecessary testId function, updated test files
    
    * replacing conditional with variable
    
    * fixing merge conflicts, adding READ tag comment, updating format/theme names
    
    * fixing merge conflicts, updated categoryType string to match backend labels - since they didn't align properly before
    
    * adding formatSubject  function so i can use it in reply page layout pr
    
    * adding translation value for appointments, adding tests in inbox.test.tsx for subject format
    
    * updating translation key
    
    * deleting unnnecessary translation key
    
    * updated inbox test file to test all subject categories and make code cleaner
    
    * deleted unused mock data for inbox test file
    
    * removing flex 1 so ui is still correct when accordion toggled from open to closed
    
    * revert back to original until that bug fix is merged in
    
    * setting it so no activity indicator shows for messages with no attachments

--- VAMobile/src/store/reducers/secureMessaging.ts
+++ VAMobile/src/store/reducers/secureMessaging.ts
@@ -227,6 +227,7 @@ export default createReducer<SecureMessagingState>(initialSecureMessagingState, 
       ...state,
       messagesById,
       loading: false,
+      loadingAttachments: false,
       inboxMessages: updatedInboxMessages,
       inbox: {
         ...inbox,
