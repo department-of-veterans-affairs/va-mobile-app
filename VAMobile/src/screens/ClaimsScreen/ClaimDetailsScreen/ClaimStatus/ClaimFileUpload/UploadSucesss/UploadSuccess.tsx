61c9a924c3dcd165c055f491d88c4c5241d2f7e0
Ken Li <k_li@digitalfoundry.com>
2021-06-22 10:39:27 -0700

    Samsung Vah rather than VA talkback issues

--- VAMobile/src/screens/ClaimsScreen/ClaimDetailsScreen/ClaimStatus/ClaimFileUpload/UploadSucesss/UploadSuccess.tsx
+++ VAMobile/src/screens/ClaimsScreen/ClaimDetailsScreen/ClaimStatus/ClaimFileUpload/UploadSucesss/UploadSuccess.tsx
@@ -38,7 +38,12 @@ const UploadSuccess: FC<UploadSuccessProps> = ({ navigation }) => {
   return (
     <VAScrollView {...testIdProps('File-Upload: Upload-success-page')}>
       <Box mt={theme.dimensions.contentMarginTop} mb={theme.dimensions.contentMarginBottom} mx={theme.dimensions.gutter}>
-        <AlertBox title={t('fileUpload.yourFileWasUploaded')} text={t('fileUpload.thankYouForUploading')} border="success" background="noCardBackground">
+        <AlertBox
+          title={t('fileUpload.yourFileWasUploaded')}
+          text={t('fileUpload.thankYouForUploading')}
+          textA11yLabel={t('fileUpload.thankYouForUploading.a11yLabel')}
+          border="success"
+          background="noCardBackground">
           <Box mt={theme.dimensions.standardMarginBetween}>
             <VAButton
               onPress={navigateToFileRequests}
