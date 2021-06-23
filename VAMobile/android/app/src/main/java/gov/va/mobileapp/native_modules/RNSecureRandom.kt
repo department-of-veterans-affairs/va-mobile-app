64ac5e4629ed5f2ba10966b7dc95de49dbd8810f
Patrick Saxton <patrick@urban-intellect.com>
2021-06-22 20:39:36 -0500

    add sha256 fun to android

--- VAMobile/android/app/src/main/java/gov/va/mobileapp/native_modules/RNSecureRandom.kt
+++ VAMobile/android/app/src/main/java/gov/va/mobileapp/native_modules/RNSecureRandom.kt
@@ -7,6 +7,7 @@ import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import java.lang.Exception
+import java.security.MessageDigest
 import java.security.SecureRandom
 
 /**
@@ -28,8 +29,26 @@ class RNSecureRandom (reactContext: ReactApplicationContext) : ReactContextBaseJ
             val random = SecureRandom()
             val values = ByteArray(count)
             random.nextBytes(values)
-            promise.resolve(Base64.encodeToString(values, 0))
+            promise.resolve(Base64.encodeToString(values, Base64.DEFAULT))
         }catch (e: Exception) {
+            promise.reject(e)
+        }
+    }
+
+    /**
+     * Takes a string, hashes it with SHA256 and returns a base64 string of that hash
+     * random array as a base64 string
+     * @param string string to hash to SHA256
+     * @param promise Promise to resolve the base64 string that is generated
+     */
+    @ReactMethod
+    fun generateSHA256String(string: String, promise: Promise) {
+        try{
+            MessageDigest.getInstance("SHA-256").apply {
+                update(string.toByteArray())
+                promise.resolve(Base64.encodeToString(digest(), Base64.DEFAULT))
+            }
+        } catch (e: Exception) {
             promise.reject(e)
         }
     }
