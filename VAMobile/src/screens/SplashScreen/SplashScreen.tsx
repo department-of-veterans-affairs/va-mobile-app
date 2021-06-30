af0de6b6b287733555fdca50fdad3187c2cab942
Patrick Saxton <patrick@urban-intellect.com>
2021-06-30 13:12:24 -0500

    turn off fb for pre-launch and staging

--- VAMobile/src/screens/SplashScreen/SplashScreen.tsx
+++ VAMobile/src/screens/SplashScreen/SplashScreen.tsx
@@ -2,8 +2,13 @@ import { Box, VAIcon, VAScrollView } from 'components'
 import { ViewStyle } from 'react-native'
 import { testIdProps } from 'utils/accessibility'
 import { useTheme } from 'utils/hooks'
-import React, { FC } from 'react'
+import { utils } from '@react-native-firebase/app'
+import React, { FC, useEffect } from 'react'
+import analytics from '@react-native-firebase/analytics'
+import crashlytics from '@react-native-firebase/crashlytics'
+import getEnv from 'utils/env'
 
+const { ENVIRONMENT } = getEnv()
 export type SplashScreenProps = Record<string, unknown>
 const SplashScreen: FC<SplashScreenProps> = () => {
   const theme = useTheme()
@@ -12,6 +17,17 @@ const SplashScreen: FC<SplashScreenProps> = () => {
     justifyContent: 'center',
     backgroundColor: theme.colors.background.splashScreen,
   }
+
+  useEffect(() => {
+    async function checkTestLab() {
+      if (utils().isRunningInTestLab || ENVIRONMENT === 'staging') {
+        await crashlytics().setCrashlyticsCollectionEnabled(false)
+        await analytics().setAnalyticsCollectionEnabled(false)
+      }
+    }
+    checkTestLab()
+  })
+
   return (
     <VAScrollView {...testIdProps('Splash-page')} contentContainerStyle={splashStyles}>
       <Box justifyContent="center" mx={theme.dimensions.gutter} mt={theme.dimensions.contentMarginTop} mb={theme.dimensions.contentMarginBottom} alignItems={'center'}>
