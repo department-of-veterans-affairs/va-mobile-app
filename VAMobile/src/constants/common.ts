ded573592d36039adddc8f754c759c9a5958a3a6
Patrick Saxton <patrick.saxton@adhocteam.us>
2021-07-01 11:21:04 -0500

    Feature/firebase updates (#915)
    * keep android deob files
    
    * add production string for ENVIRONMENT
    
    * turn off fb for pre-launch and staging
    
    * add environment constant type
    
    * remove fb check from splash screen
    
    * add fb check to initialize action
    
    * update when FB is turned off
    
    * add mocks for new functions

--- VAMobile/src/constants/common.ts
+++ VAMobile/src/constants/common.ts
@@ -1,1 +1,11 @@
 export const DEFAULT_PAGE_SIZE = 10
+
+export const EnvironmentTypesConstants: {
+  Staging: EnvironmentTypes
+  Production: EnvironmentTypes
+} = {
+  Staging: 'staging',
+  Production: 'production',
+}
+
+export type EnvironmentTypes = 'staging' | 'production'
