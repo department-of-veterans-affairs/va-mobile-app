cbb4b890169e88694a3b4f0683d3558de9c122e0
Rafael Arroyo <rafael.arroyo@adhocteam.us>
2021-07-22 10:22:09 -0400

    Reverting api/types for error

--- VAMobile/src/store/api/types/Errors.ts
+++ VAMobile/src/store/api/types/Errors.ts
@@ -1,12 +1,10 @@
 // parent level error object that contains the status, text, and json properties from a fetch response stream
 // used to keep full error information in respective stores
 // json property is used to parse through to find error metadata
-export type NetworkErrorType = boolean | undefined
-
 export type APIError = {
   status?: number
   text?: string
-  networkError?: NetworkErrorType
+  networkError?: boolean
   json?: {
     errors: Array<{
       title: string
