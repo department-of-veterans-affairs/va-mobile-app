cab8bc44b49b94d85e244cf6b40b3f63a41677b2
Rafael Arroyo <rafael.arroyo@adhocteam.us>
2021-08-19 19:06:11 -0400

    Update DisabilityRatingsScreen.test.tsx

--- VAMobile/src/screens/ProfileScreen/DisabilityRatingsScreen.test.tsx
+++ VAMobile/src/screens/ProfileScreen/DisabilityRatingsScreen.test.tsx
@@ -89,10 +89,10 @@ context('DisabilityRatingsScreen', () => {
     expect(profileBanner).toBeTruthy()
 
     const headers = testInstance.findAllByProps({ accessibilityRole: 'header' })
-    expect(headers[5].props.children).toBe('Combined Disability Rating')
-    expect(headers[15].props.children).toBe('Individual Ratings')
+    expect(headers[5].props.children).toBe('Combined disability rating')
+    expect(headers[15].props.children).toBe('Individual ratings')
     expect(headers[20].props.children).toBe('Learn about VA disability ratings')
-    expect(headers[30].props.children).toBe('Need help?')
+    expect(headers[30].props.children).toBe('Need Help?')
 
     const texts = testInstance.findAllByType(TextView)
     expect(texts[2].props.children).toBe('70%')
