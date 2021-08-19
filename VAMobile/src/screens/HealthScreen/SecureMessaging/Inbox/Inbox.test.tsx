e6702acac05582007b265f74d0ad0de1cf088442
Teesh <teesh.s93@gmail.com>
2021-08-19 11:55:54 -0500

    Add test

--- VAMobile/src/screens/HealthScreen/SecureMessaging/Inbox/Inbox.test.tsx
+++ VAMobile/src/screens/HealthScreen/SecureMessaging/Inbox/Inbox.test.tsx
@@ -15,7 +15,7 @@ import {
 import {initialAuthState, initialErrorsState, initialSecureMessagingState} from 'store'
 import {LoadingComponent, TextView} from 'components'
 import {fetchInboxMessages} from 'store/actions'
-
+import {SecureMessagingMessageList} from 'store/api/types'
 
 let mockNavigationSpy = jest.fn()
 jest.mock('/utils/hooks', () => {
@@ -43,13 +43,35 @@ jest.mock('../../../../store/actions', () => {
   }
 })
 
+
+
 context('Inbox', () => {
   let component: any
   let store: any
   let props: any
   let testInstance: ReactTestInstance
+  
+  const _ = undefined
+  
+  const testInboxMessage = (category: CategoryTypes = CategoryTypeFields.other , subjectLine: string = 'Default subject line'): SecureMessagingMessageList => [{
+    type: 'test',
+    id: 1,
+    attributes: {
+      messageId: 1,
+      category: category,
+      subject: subjectLine? subjectLine : '',
+      body: 'test',
+      attachment: false,
+      sentDate: '1-1-21',
+      senderId: 2,
+      senderName: 'mock sender',
+      recipientId: 3,
+      recipientName: 'mock recipient name',
+      readReceipt: 'mock read receipt'
+    },
+  }]
 
-  const initializeTestInstance = (category: CategoryTypes = CategoryTypeFields.other , subjectLine: string = 'Default subject line', loading: boolean = false) => {
+  const initializeTestInstance = (inboxMessages: SecureMessagingMessageList | undefined = undefined, loading: boolean = false) => {
     props = mockNavProps()
 
     store = mockStore({
@@ -57,23 +79,7 @@ context('Inbox', () => {
       secureMessaging: {
         ...initialSecureMessagingState,
         loading: loading,
-        inboxMessages: [{
-          type: 'test',
-          id: 1,
-          attributes: {
-            messageId: 1,
-            category: category,
-            subject: subjectLine? subjectLine : '',
-            body: 'test',
-            attachment: false,
-            sentDate: '1-1-21',
-            senderId: 2,
-            senderName: 'mock sender',
-            recipientId: 3,
-            recipientName: 'mock recipient name',
-            readReceipt: 'mock read receipt'
-          },
-        }],
+        inboxMessages: inboxMessages,
         paginationMetaByFolderId: {
           [0]: {
             currentPage: 2,
@@ -106,32 +112,21 @@ context('Inbox', () => {
 
   describe('when there are no inbox messages', () => {
     it('should render NoInboxMessages', async () => {
-      store = mockStore({
-        secureMessaging: {
-          ...initialSecureMessagingState,
-          inboxMessages: []
-        }
-      })
-
-      act(() => {
-        component = renderWithProviders(
-          <Inbox/>, store
-        )
-      })
-
-      testInstance = component.root
+      initializeTestInstance([])
       expect(testInstance.findByType(NoInboxMessages)).toBeTruthy()
     })
   })
 
   describe('when there are inbox messages', () => {
-    it('should render NoInboxMessages', async () => {
+    it('should not render NoInboxMessages', async () => {
+      initializeTestInstance(testInboxMessage())
       expect(testInstance.findAllByType(NoInboxMessages)).toHaveLength(0)
     })
   })
 
   describe('when a message is clicked', () => {
     it('should call useRouteNavigation', async () => {
+      initializeTestInstance(testInboxMessage())
       testInstance.findAllByType(Pressable)[0].props.onPress()
       expect(mockNavigationSpy).toHaveBeenCalled()
     })
@@ -139,63 +134,64 @@ context('Inbox', () => {
 
   describe('when loading is set to true', () => {
     it('should show loading screen', async () => {
-      initializeTestInstance(CategoryTypeFields.other, '', true)
+      initializeTestInstance(testInboxMessage(CategoryTypeFields.other, ''), true)
       expect(testInstance.findByType(LoadingComponent)).toBeTruthy()
+      expect(testInstance.findAllByType(NoInboxMessages)).toHaveLength(0)
     })
   })
 
   describe('when subject line is empty', () => {
     it('should show only category with no colon or space after', async () => {
-      initializeTestInstance(CategoryTypeFields.other, '')
+      initializeTestInstance(testInboxMessage(CategoryTypeFields.other, ''))
       expect(testInstance.findAllByType(TextView)[2].props.children).toBe('General')
     })
   })
 
   describe('when subject category is OTHER', () => {
     it('should show correct text', async () => {
-      initializeTestInstance(CategoryTypeFields.other)
+      initializeTestInstance(testInboxMessage(CategoryTypeFields.other))
       expect(testInstance.findAllByType(TextView)[2].props.children).toBe('General: Default subject line')
     })
   })
 
   describe('when subject category is GENERAL', () => {
     it('should show correct text', async () => {
-      initializeTestInstance(CategoryTypeFields.general)
+      initializeTestInstance(testInboxMessage(CategoryTypeFields.general))
       expect(testInstance.findAllByType(TextView)[2].props.children).toBe('General: Default subject line')
     })
   })
 
   describe('when subject category is APPOINTMENTS', () => {
     it('should show correct text', async () => {
-      initializeTestInstance(CategoryTypeFields.appointment)
+      initializeTestInstance(testInboxMessage(CategoryTypeFields.appointment))
       expect(testInstance.findAllByType(TextView)[2].props.children).toBe('Appointment: Default subject line')
     })
   })
 
   describe('when subject category is MEDICATION', () => {
     it('should show correct text', async () => {
-      initializeTestInstance(CategoryTypeFields.medication)
+      initializeTestInstance(testInboxMessage(CategoryTypeFields.medication))
       expect(testInstance.findAllByType(TextView)[2].props.children).toBe('Medication: Default subject line')
     })
   })
 
   describe('when subject category is TEST_RESULTS', () => {
     it('should show correct text', async () => {
-      initializeTestInstance(CategoryTypeFields.test)
+      initializeTestInstance(testInboxMessage(CategoryTypeFields.test))
       expect(testInstance.findAllByType(TextView)[2].props.children).toBe('Test: Default subject line')
     })
   })
 
   describe('when subject category is EDUCATION', () => {
     it('should show correct text', async () => {
-      initializeTestInstance(CategoryTypeFields.education)
+      initializeTestInstance(testInboxMessage(CategoryTypeFields.education))
       expect(testInstance.findAllByType(TextView)[2].props.children).toBe('Education: Default subject line')
     })
   })
 
   describe('when subject category is COVID', () => {
     it('should show correct text', async () => {
-      initializeTestInstance(CategoryTypeFields.covid)
+      initializeTestInstance(testInboxMessage(CategoryTypeFields.covid))
       expect(testInstance.findAllByType(TextView)[2].props.children).toBe('COVID: Default subject line')
     })
   })
