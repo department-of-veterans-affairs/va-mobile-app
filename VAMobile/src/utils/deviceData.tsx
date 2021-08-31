57e9cc0011f18911e1ea4003a62a1025efd58ecf
Patrick Saxton <patrick.saxton@adhocteam.us>
2021-08-31 16:40:33 -0500

    Feature - In-App review logic (#1477)
    * update Android DeviceData module
    
    * update swift module
    
    * update device data utils
    
    * clean up swift
    
    * create review utils
    
    * add register review action to all redux calls
    
    * add for profile info view
    
    * always have button
    
    * fix tests
    
    * lint error

--- VAMobile/src/utils/deviceData.tsx
+++ VAMobile/src/utils/deviceData.tsx
@@ -7,4 +7,14 @@ const DD = NativeModules.DeviceData
 /**
  * returns the custom name of the device set by the user.
  */
-export const deviceName = isIOS() ? DD.deviceName : DD.getDeviceName()
+export const deviceName: string = isIOS() ? DD.deviceName : DD.getDeviceName()
+
+/**
+ * returns the version name string for the app
+ */
+export const versionName: string = isIOS() ? DD.versionName : DD.getVersionName()
+
+/**
+ * returns the current build number of the app
+ */
+export const buildNumber: number = isIOS() ? DD.buildNumber : DD.getBuildNumber()
