0e6700becb310e81de3d522cf92e6fafd63648df
Rafael Arroyo <rafael.arroyo@adhocteam.us>
2021-08-31 17:49:49 -0400

    Adding a loading to prevent old data being presented before the new w… (#1451)
    * Adding a loading to prevent old data being presented before the new when going to appointment details
    
    * changing transition logic to make it more clearer
    
    * fix test
    
    * Removing setTimeout from test

--- VAMobile/src/screens/HealthScreen/Appointments/UpcomingAppointments/UpcomingAppointmentDetails.tsx
+++ VAMobile/src/screens/HealthScreen/Appointments/UpcomingAppointments/UpcomingAppointmentDetails.tsx
@@ -33,6 +33,7 @@ import {
 import { AppointmentsState, StoreState } from 'store/reducers'
 import { BackButtonLabelConstants } from 'constants/backButtonLabels'
 import { HealthStackParamList } from '../../HealthStackScreens'
+import { InteractionManager } from 'react-native'
 import { NAMESPACE } from 'constants/namespaces'
 import { a11yHintProp, testIdProps } from 'utils/accessibility'
 import { clearAppointmentCancellation, getAppointment } from 'store/actions'
@@ -65,6 +66,7 @@ const UpcomingAppointmentDetails: FC<UpcomingAppointmentDetailsProps> = ({ route
     attributes || ({} as AppointmentAttributes)
   const { name, address, phone, code, url } = location || ({} as AppointmentLocation)
   const isAppointmentCanceled = status === AppointmentStatusConstants.CANCELLED
+  const [isTransitionComplete, setIsTransitionComplete] = React.useState(false)
 
   const whoCanceled =
     statusDetail === AppointmentStatusDetailTypeConsts.CLINIC || statusDetail === AppointmentStatusDetailTypeConsts.CLINIC_REBOOK
@@ -73,6 +75,9 @@ const UpcomingAppointmentDetails: FC<UpcomingAppointmentDetailsProps> = ({ route
 
   useEffect(() => {
     dispatch(getAppointment(appointmentID))
+    InteractionManager.runAfterInteractions(() => {
+      setIsTransitionComplete(true)
+    })
   }, [dispatch, appointmentID])
 
   useEffect(() => {
@@ -279,8 +284,8 @@ const UpcomingAppointmentDetails: FC<UpcomingAppointmentDetailsProps> = ({ route
     return <></>
   }
 
-  if (loadingAppointmentCancellation) {
-    return <LoadingComponent text={t('upcomingAppointmentDetails.loadingAppointmentCancellation')} />
+  if (loadingAppointmentCancellation || !isTransitionComplete) {
+    return <LoadingComponent text={t(!isTransitionComplete ? 'appointmentDetails.loading' : 'upcomingAppointmentDetails.loadingAppointmentCancellation')} />
   }
 
   return (
