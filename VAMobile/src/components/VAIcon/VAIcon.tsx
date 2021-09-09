8ac7a177486813b01d508612f10edf6297c71eb3
Teesh <teesh.s93@gmail.com>
2021-09-09 13:08:34 -0500

    update min target in pods, react and RN in package

--- VAMobile/src/components/VAIcon/VAIcon.tsx
+++ VAMobile/src/components/VAIcon/VAIcon.tsx
@@ -159,7 +159,7 @@ const VAIcon: FC<VAIconProps> = (props: VAIconProps) => {
   useEffect(() => {
     // Listener for the current app state, updates the font scale when app state is active and the font scale has changed
     AppState.addEventListener('change', (newState: AppStateStatus): void => updateFontScale(newState, fontScale, dispatch))
-    return (): void => AppState.removeEventListener('change', (newState: AppStateStatus): void => updateFontScale(newState, fontScale, dispatch))
+    // return (): void => AppState.removeEventListener('change', (newState: AppStateStatus): void => updateFontScale(newState, fontScale, dispatch))
   }, [dispatch, fontScale])
 
   if (fill) {
