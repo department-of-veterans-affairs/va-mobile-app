3de0e5b0752b49aa117bb27c6686f371e0df442b
Rafael Arroyo <rafael.arroyo@adhocteam.us>
2021-09-22 13:32:11 -0400

    Feature/1366 icon for apptoinments (#1599)
    * Initial commit work in process waiting on backend
    
    * finish implementation with backend changes for phone only
    
    * breaking the common logic used in upcoming and past appointments
    
    * fixing test
    
    * Adding JDOC
    
    Co-authored-by: Narin Ratana <narin@users.noreply.github.com>

--- VAMobile/src/components/TextLines.tsx
+++ VAMobile/src/components/TextLines.tsx
@@ -2,24 +2,29 @@ import React, { FC } from 'react'
 
 import { TextLine } from './types'
 import Box from './Box'
+import TextLineWithIcon, { TextLineWithIconProps } from './TextLineWithIcon'
 import TextView from './TextView'
 
 type TextLinesProps = {
   /** List of text for the button */
-  listOfText?: Array<TextLine>
+  listOfText?: Array<TextLine | TextLineWithIconProps>
 }
 
 export const TextLines: FC<TextLinesProps> = ({ listOfText }) => {
   return (
     <Box flex={1}>
       <Box flexDirection="column">
-        {listOfText?.map((textObj: TextLine, index: number) => {
-          const { text, variant = 'MobileBody', color = 'primary', textAlign = 'left' } = textObj
-          return (
-            <TextView variant={variant} textAlign={textAlign} color={color} key={index}>
-              {text}
-            </TextView>
-          )
+        {listOfText?.map((textObj: TextLine | TextLineWithIconProps, index: number) => {
+          if ('iconProps' in textObj && textObj.iconProps !== undefined) {
+            return <TextLineWithIcon key={index} {...textObj} />
+          } else {
+            const { text, variant = 'MobileBody', color = 'primary', textAlign = 'left' } = textObj
+            return (
+              <TextView variant={variant} textAlign={textAlign} color={color} key={index}>
+                {text}
+              </TextView>
+            )
+          }
         })}
       </Box>
     </Box>
