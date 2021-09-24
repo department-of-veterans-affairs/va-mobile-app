e9b6da85146edefcd602ea4bbe04fda530f96faa
Teesh <teesh.s93@gmail.com>
2021-09-24 15:38:12 -0500

    Add error types

---
+++ VAMobile/src/components/CommonErrorComponents/DowntimeError.tsx
@@ -0,0 +1,44 @@
+import { ViewStyle } from 'react-native'
+import React, { FC } from 'react'
+
+import { AlertBox, Box, ButtonTypesConstants, ClickToCallPhoneNumber, TextView, VAButton, VAScrollView } from 'components'
+import { NAMESPACE } from 'constants/namespaces'
+import { useTheme, useTranslation } from 'utils/hooks'
+
+export type DowntimeErrorProps = {
+  /** optional function called when the Try again button is pressed */
+  onTryAgain?: () => void
+  titleText?: string
+  titleA11yHint?: string
+  errorText?: string
+  errorA11y?: string
+  callPhone?: string
+}
+
+const DowntimeError: FC<DowntimeErrorProps> = ({feature, start, end}) => {
+  const t = useTranslation(NAMESPACE.COMMON)
+  const theme = useTheme()
+
+  const scrollStyles: ViewStyle = {
+    justifyContent: 'center',
+    backgroundColor: theme.colors.background.main,
+  }
+
+  const containerStyles = {
+    mx: theme.dimensions.gutter,
+    mt: theme.dimensions.contentMarginTop,
+    mb: theme.dimensions.contentMarginBottom,
+  }
+
+  const standardMarginBetween = theme.dimensions.standardMarginBetween
+
+  return (
+    <VAScrollView contentContainerStyle={scrollStyles}>
+      <Box justifyContent="center" {...containerStyles}>
+        <AlertBox title={t('downtime.title')} titleA11yLabel={t('downtime.title.a11yHint')} text={t('downtime.message')} border="warning" background="noCardBackground" />
+      </Box>
+    </VAScrollView>
+  )
+}
+
+export default DowntimeError
--- VAMobile/src/store/api/types/Errors.ts
+++ VAMobile/src/store/api/types/Errors.ts
@@ -23,3 +23,56 @@ export type ErrorMessage = {
   severity: string
   text: string
 }
+
+export type DowntimeFeatureType =
+  | 'facility_locator'
+  | 'auth_dslogon'
+  | 'auth_idme'
+  | 'auth_mhv'
+  | 'appeals'
+  | 'military_service_history'
+  | 'claims'
+  | 'direct_deposit_benefits'
+  | 'letters_and_documents'
+  | 'secure_messaging'
+  | 'appointments'
+  | 'user_profile_update'
+
+export const DowntimeFeatureTypeConstants: {
+  facilityLocator: DowntimeFeatureType
+  authDSLogon: DowntimeFeatureType
+  authIDMe: DowntimeFeatureType
+  authMHV: DowntimeFeatureType
+  appeals: DowntimeFeatureType
+  militaryServiceHistory: DowntimeFeatureType
+  claims: DowntimeFeatureType
+  directDepositBenefits: DowntimeFeatureType
+  letters: DowntimeFeatureType
+  secureMessaging: DowntimeFeatureType
+  appointments: DowntimeFeatureType
+  userProfileUpdate: DowntimeFeatureType
+} = {
+  facilityLocator: 'facility_locator',
+  authDSLogon: 'auth_dslogon',
+  authIDMe: 'auth_idme',
+  authMHV: 'auth_mhv',
+  appeals: 'appeals',
+  militaryServiceHistory: 'military_service_history',
+  claims: 'claims',
+  directDepositBenefits: 'direct_deposit_benefits',
+  letters: 'letters_and_documents',
+  secureMessaging: 'secure_messaging',
+  appointments: 'appointments',
+  userProfileUpdate: 'user_profile_update',
+}
+
+export const DowntimeFeatureNameTypes = {
+  [DowntimeFeatureTypeConstants.facilityLocator]: 'Facility Locator',
+  [DowntimeFeatureTypeConstants.claims]: 'Claims',
+  [DowntimeFeatureTypeConstants.appointments]: 'Appointments',
+  [DowntimeFeatureTypeConstants.secureMessaging]: 'Secure Messaging',
+  [DowntimeFeatureTypeConstants.letters]: 'Letters',
+  [DowntimeFeatureTypeConstants.directDepositBenefits]: 'Direct Deposit',
+}
+
+// 'Claims' | 'Appointments' | 'Secure Messaging' | 'Letters' | 'Disability Rating' | 'Direct Deposit'
