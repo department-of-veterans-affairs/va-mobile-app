4910e2dee5addf82c309f7269602930168742441
Rafael Arroyo <rafael.arroyo@adhocteam.us>
2021-09-29 18:37:56 -0400

    Covid-19 appointment for the past appointments

--- VAMobile/src/store/actions/appointments.ts
+++ VAMobile/src/store/actions/appointments.ts
@@ -106,19 +106,68 @@ export const prefetchAppointments = (upcoming: AppointmentsDateRange, past: Appo
 
       // use loaded data if we have it
       const loadedPastAppointments = getLoadedAppointments(loadedPastThreeMonths, pastPagination, 1, DEFAULT_PAGE_SIZE)
-      if (loadedPastAppointments) {
-        pastAppointments = loadedPastAppointments
-      } else {
-        pastAppointments = await api.get<AppointmentsGetData>('/v0/appointments', {
-          startDate: past.startDate,
-          endDate: past.endDate,
-          'page[size]': DEFAULT_PAGE_SIZE.toString(),
-          'page[number]': '1', // prefetch assume always first page
-          sort: '-startDateUtc', // reverse sort for past timeRanges so it shows most recent to oldest
-        } as Params)
-      }
+      //All mock data should be removed after backend implementation
       // TODO: delete in story #19175
       const signInEmail = getState()?.personalInformation?.profile?.signinEmail || ''
+      if (signInEmail === 'vets.gov.user+1414@gmail.com') {
+        pastAppointments = {
+          data: [
+            {
+              type: 'appointment',
+              id: '4',
+              attributes: {
+                appointmentType: AppointmentTypeConstants.VA,
+                status: AppointmentStatusConstants.BOOKED,
+                phoneOnly: false,
+                statusDetail: null,
+                covidVaccination: true,
+                startDateLocal: '2021-09-06T19:53:14.000+00:00',
+                startDateUtc: '2021-09-06T19:53:14.000+00:00',
+                minutesDuration: 60,
+                comment: 'Please arrive 20 minutes before the start of your appointment',
+                timeZone: 'America/Los_Angeles' as AppointmentTimeZone,
+                healthcareService: 'Blind Rehabilitation Center',
+                reason: null,
+                location: {
+                  name: 'VA Long Beach Healthcare System',
+                  address: {
+                    street: '5901 East 7th Street',
+                    city: 'Long Beach',
+                    state: 'CA',
+                    zipCode: '90822',
+                  },
+                  phone: {
+                    number: '456-7890',
+                    extension: '',
+                    areaCode: '123',
+                  },
+                  url: '',
+                  code: '',
+                },
+                practitioner: {
+                  prefix: 'Dr.',
+                  firstName: 'Larry',
+                  middleName: '',
+                  lastName: 'TestDoctor',
+                },
+              },
+            },
+          ],
+        }
+      } else {
+        if (loadedPastAppointments) {
+          pastAppointments = loadedPastAppointments
+        } else {
+          pastAppointments = await api.get<AppointmentsGetData>('/v0/appointments', {
+            startDate: past.startDate,
+            endDate: past.endDate,
+            'page[size]': DEFAULT_PAGE_SIZE.toString(),
+            'page[number]': '1', // prefetch assume always first page
+            sort: '-startDateUtc', // reverse sort for past timeRanges so it shows most recent to oldest
+          } as Params)
+        }
+      }
+
       if (signInEmail === 'vets.gov.user+1414@gmail.com') {
         upcomingAppointments = {
           data: [
