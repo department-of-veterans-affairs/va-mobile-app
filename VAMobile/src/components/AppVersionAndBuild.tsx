2f9fd6cca6a9a20156f2ea0639e641ca7f554dde
Rafael Arroyo <rafael.arroyo@adhocteam.us>
2021-09-30 17:09:20 -0400

    Feature/1482 add version and build to  app (#1670)
    * Adding build and version number to app login and settings page
    
    * add test

---
+++ VAMobile/src/components/AppVersionAndBuild.tsx
@@ -0,0 +1,29 @@
+import React, { FC } from 'react'
+
+import { Box, TextView } from 'components'
+import { NAMESPACE } from 'constants/namespaces'
+import { VATextColors, VATypographyThemeVariants } from 'styles/theme'
+import { buildNumber, versionName } from 'utils/deviceData'
+import { useTheme, useTranslation } from 'utils/hooks'
+
+export type AppVersionAndBuildProps = {
+  /** color of the text */
+  textColor?: keyof VATextColors
+  /** font weight of the text */
+  textWeight?: keyof VATypographyThemeVariants
+}
+
+const AppVersionAndBuild: FC<AppVersionAndBuildProps> = ({ textColor = 'primary', textWeight = 'MobileBody' }) => {
+  const t = useTranslation(NAMESPACE.COMMON)
+  const theme = useTheme()
+
+  return (
+    <Box mb={theme.dimensions.contentMarginBottom} justifyContent={'center'} alignItems={'center'}>
+      <TextView variant={textWeight} flexDirection="row" color={textColor}>
+        {t('versionAndBuild', { versionName, buildNumber })}
+      </TextView>
+    </Box>
+  )
+}
+
+export default AppVersionAndBuild
