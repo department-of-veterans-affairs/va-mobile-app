14bfc14ea513d7d8167047eb4a03f57080fa6af7
Patrick Saxton <patrick@urban-intellect.com>
2021-10-05 16:11:11 -0500

    Adding a loading to prevent old data being presented before the new w… (#1451)
    * Adding a loading to prevent old data being presented before the new when going to appointment details
    
    * changing transition logic to make it more clearer
    
    * fix test
    
    * Removing setTimeout from test

--- VAMobile/src/screens/HealthScreen/Appointments/UpcomingAppointments/UpcomingAppointmentDetails.tsx
+++ VAMobile/src/screens/HealthScreen/Appointments/UpcomingAppointments/UpcomingAppointmentDetails.tsx
@@ -73,11 +73,6 @@ const UpcomingAppointmentDetails: FC<UpcomingAppointmentDetailsProps> = ({ route
       ? t('appointments.canceled.whoCanceled.facility')
       : t('appointments.canceled.whoCanceled.you')
 
-  const whoCanceled =
-    statusDetail === AppointmentStatusDetailTypeConsts.CLINIC || statusDetail === AppointmentStatusDetailTypeConsts.CLINIC_REBOOK
-      ? t('appointments.canceled.whoCanceled.facility')
-      : t('appointments.canceled.whoCanceled.you')
-
   useEffect(() => {
     dispatch(getAppointment(appointmentID))
     InteractionManager.runAfterInteractions(() => {
