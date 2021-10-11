d0ca03b4c984e6d95e51b253b33762004b66ab11
Rafael Arroyo <rafael.arroyo@adhocteam.us>
2021-10-11 19:35:28 -0400

    Update NoDisabilityRatings.tsx

--- VAMobile/src/screens/ProfileScreen/DisabilityRatingsScreen/NoDisabilityRatings/NoDisabilityRatings.tsx
+++ VAMobile/src/screens/ProfileScreen/DisabilityRatingsScreen/NoDisabilityRatings/NoDisabilityRatings.tsx
@@ -1,7 +1,8 @@
 import React, { FC } from 'react'
 
-import { Box, TextView } from 'components'
+import { Box, TextView, VAScrollView } from 'components'
 import { NAMESPACE } from 'constants/namespaces'
+import { StyleProp, ViewStyle } from 'react-native'
 import { testIdProps } from 'utils/accessibility'
 import { useTheme, useTranslation } from 'utils/hooks'
 
@@ -10,15 +11,21 @@ const NoDisabilityRatings: FC = () => {
   const t = useTranslation(NAMESPACE.PROFILE)
   const bodyText = t('disabilityRating.noDisabilityRatings.body')
 
+  const mainViewStyle: StyleProp<ViewStyle> = {
+    flexGrow: 1,
+  }
+
   return (
-    <Box flex={1} justifyContent="center" mx={theme.dimensions.gutter} alignItems="center">
-      <TextView variant="MobileBodyBold" textAlign="center" accessibilityRole="header">
-        {t('disabilityRating.noDisabilityRatings.title')}
-      </TextView>
-      <TextView variant="MobileBody" textAlign="center" my={theme.dimensions.standardMarginBetween} {...testIdProps(bodyText)}>
-        {bodyText}
-      </TextView>
-    </Box>
+    <VAScrollView contentContainerStyle={mainViewStyle}>
+      <Box flex={1} justifyContent="center" mx={theme.dimensions.gutter} alignItems="center">
+        <TextView variant="MobileBodyBold" textAlign="center" accessibilityRole="header">
+          {t('disabilityRating.noDisabilityRatings.title')}
+        </TextView>
+        <TextView variant="MobileBody" textAlign="center" my={theme.dimensions.standardMarginBetween} {...testIdProps(bodyText)}>
+          {bodyText}
+        </TextView>
+      </Box>
+    </VAScrollView>
   )
 }
 
