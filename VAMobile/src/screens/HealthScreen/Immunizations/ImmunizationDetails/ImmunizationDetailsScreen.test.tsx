44fed1935989bd81b464f06b24236f5ec259858d
John Marchi <j_marchi@digitalfoundry.com>
2021-10-11 11:32:29 -0700

    details screen test

--- VAMobile/src/screens/HealthScreen/Immunizations/ImmunizationDetails/ImmunizationDetailsScreen.test.tsx
+++ VAMobile/src/screens/HealthScreen/Immunizations/ImmunizationDetails/ImmunizationDetailsScreen.test.tsx
@@ -6,24 +6,24 @@ import { ReactTestInstance, act } from 'react-test-renderer'
 
 import {context, findByTypeWithText, mockNavProps, mockStore, renderWithProviders} from 'testUtils'
 import {initialAuthState, initialErrorsState, initialImmunizationState, initialSecureMessagingState} from 'store'
-import { LoadingComponent, TextView, MessagesCountTag } from 'components'
-import ImmunizationListScreen from './ImmunizationListScreen'
+import { TextView } from 'components'
+import ImmunizationDetailsScreen from './ImmunizationDetailsScreen'
 
-context('ImmunizationListScreen', () => {
+context('ImmunizationDetailsScreen', () => {
   let component: any
   let props: any
   let store: any
   let testInstance: ReactTestInstance
 
   const initializeTestInstance = (loaded: boolean = true) => {
-    props = mockNavProps()
+    props = mockNavProps(undefined, undefined,{ params: { immunizationId: 'abc'}})
 
     store = mockStore({
       auth: { ...initialAuthState },
       immunization: {
         ...initialImmunizationState,
-        immunizations: [
-          {
+        immunizationsById: {
+          abc: {
             id: 'abc',
             vaccineCode:'COVID-19 vaccine',
             recorded: '2021-09-25',
@@ -64,55 +64,14 @@ context('ImmunizationListScreen', () => {
               seriesDoses: '2'
             }
           },
-          {
-            id: 'abc',
-            vaccineCode:'Influenza vaccine',
-            recorded: '2021-09-25',
-            primarySource: true,
-            status: "Completed",
-            lotNumber: '205A21A',
-            expirationDate: '2030-09-25',
-            manufacturer: {
-              active: true,
-              name: 'Pfizer'
-            },
-            location: {
-              id: '1',
-              name: 'Cheyenne VA Medical Center',
-              address: {
-                type: 'both',
-                city: 'Cheyenne',
-                state: 'WY',
-                postalCode: '82001-5356',
-                country: 'US'
-              }
-            },
-            reaction: [{
-              date: '2021-09-25',
-              reported: true,
-              detail: {
-                display: 'Lethargy'
-              }
-            }],
-            doseQuantity: {
-              text: '0.3ML'
-            },
-            notes: [],
-            protocolApplied: {
-              series: 'series x',
-              targetDisease: 'Influenza',
-              doseNumber: '2',
-              seriesDoses: '2'
-            }
-          },
-        ],
+        },
         loading: !loaded,
       },
       errors: initialErrorsState,
     })
 
     act(() => {
-      component = renderWithProviders(<ImmunizationListScreen {...props} />, store)
+      component = renderWithProviders(<ImmunizationDetailsScreen {...props} />, store)
     })
 
     testInstance = component.root
@@ -125,18 +84,12 @@ context('ImmunizationListScreen', () => {
     expect(component).toBeTruthy()
   })
 
-  describe('when loading is set to true', () => {
-    it('should show loading screen', async () => {
-      initializeTestInstance(false)
-      expect(testInstance.findByType(LoadingComponent)).toBeTruthy()
-    })
-  })
-
-  describe('when showing the list', () => {
-    it('should show the correct list items', async () => {
+  describe('when showing the vaccine', () => {
+    it('should show fields from the vaccine data', async () => {
       initializeTestInstance()
       expect(findByTypeWithText(testInstance, TextView, 'COVID-19 vaccine')).toBeTruthy()
-      expect(findByTypeWithText(testInstance, TextView, 'Influenza vaccine')).toBeTruthy()
+      expect(findByTypeWithText(testInstance, TextView, 'Cheyenne VA Medical Center')).toBeTruthy()
+      expect(findByTypeWithText(testInstance, TextView, 'Pfizer 0.3ML')).toBeTruthy()
     })
   })
 })
