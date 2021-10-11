c58ed7074513c2fc0c54a30c3a5dc67ada15f8d2
Rafael Arroyo <rafael.arroyo@adhocteam.us>
2021-10-11 17:51:42 -0400

    Disability Rating Empty state

---
+++ VAMobile/src/screens/ProfileScreen/DisabilityRatingsScreen/NoDisabilityRatings/NoDisabilityRatings.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from 'react'
+
+import { Box, TextView } from 'components'
+import { NAMESPACE } from 'constants/namespaces'
+import { testIdProps } from 'utils/accessibility'
+import { useTheme, useTranslation } from 'utils/hooks'
+
+const NoDisabilityRatings: FC = () => {
+  const theme = useTheme()
+  const t = useTranslation(NAMESPACE.PROFILE)
+  const bodyText = t('disabilityRating.noDisabilityRatings.body')
+
+  return (
+    <Box flex={1} justifyContent="center" mx={theme.dimensions.gutter} alignItems="center">
+      <TextView variant="MobileBodyBold" textAlign="center" accessibilityRole="header">
+        {t('disabilityRating.noDisabilityRatings.title')}
+      </TextView>
+      <TextView variant="MobileBody" textAlign="center" my={theme.dimensions.standardMarginBetween} {...testIdProps(bodyText)}>
+        {bodyText}
+      </TextView>
+    </Box>
+  )
+}
+
+export default NoDisabilityRatings
