c3ecd7f098ee66a6d56d25bf9d70d013ba7032ca
Teesh <teesh.s93@gmail.com>
2021-10-14 22:28:43 -0500

    Downtime Message Feature XL (#1778)
    * Downtime Message PR 1 (#1669)
    
    * Create files
    
    * Add error types
    
    * Add downtime function, translations, error types
    
    * Fix tests
    
    * Fix init error test
    
    * Add test
    
    * fix lint
    
    * Add clear all metadata function
    
    * fix bad merge
    
    * lint fix
    
    * Add downtime error display
    
    * Fix lint
    
    * add disability rating
    
    * translation streamline
    
    * Add JSDoc comments
    
    * Fix lint and comments
    
    * Re-push
    
    * Downtime Messages PR 2 (#1763)
    
    * Create files
    
    * Add error types
    
    * Add downtime function, translations, error types
    
    * Fix tests
    
    * Fix init error test
    
    * Add test
    
    * fix lint
    
    * Add clear all metadata function
    
    * fix bad merge
    
    * lint fix
    
    * Add downtime error display
    
    * Fix lint
    
    * add disability rating
    
    * translation streamline
    
    * Add JSDoc comments
    
    * Fix lint and comments
    
    * add downtime error clear function
    
    * Add actiondef type for downtime
    
    * Re-push
    
    * Allow clearing error type
    
    * Add downtime check to sync
    
    * Update profile downtime screen
    
    * Fix Disability Rating screen error
    
    * fix lint and jest
    
    * fix merge error
    
    * Fix bugs, register component, add tests
    
    * Downtime Messages PR 3 (#1772)
    
    * Fix bugs, register component, add tests
    
    * change continues to filters

--- VAMobile/src/store/actions/errors.ts
+++ VAMobile/src/store/actions/errors.ts
@@ -1,6 +1,8 @@
-import { CommonErrorTypes } from 'constants/errors'
-import { ReduxAction } from '../types'
-import { ScreenIDTypes } from '../api'
+import * as api from '../api'
+import { AsyncReduxAction, ReduxAction } from '../types'
+import { CommonErrorTypes, CommonErrorTypesConstants } from 'constants/errors'
+import { DateTime } from 'luxon'
+import { DowntimeFeatureNameConstants, DowntimeFeatureToScreenID, MaintenanceWindowsGetData, ScreenIDTypes } from '../api/types'
 
 export const dispatchSetError = (errorType?: CommonErrorTypes, screenID?: ScreenIDTypes): ReduxAction => {
   return {
@@ -27,5 +29,85 @@ export const dispatchSetTryAgainFunction = (tryAgain: () => Promise<void>): Redu
     payload: {
       tryAgain,
     },
+  }
+}
+
+/**
+ * Sets the error metadata for a given screen ID. Currently only utilized for downtime messages
+ * @param metadata - Any key value pair of data
+ * @param screenID - ID of the screen with the error
+ */
+export const dispatchSetMetadata = (metadata?: { [key: string]: string }, screenID?: ScreenIDTypes): ReduxAction => {
+  return {
+    type: 'ERRORS_SET_METADATA',
+    payload: {
+      metadata,
+      screenID,
+    },
+  }
+}
+
+/**
+ * Clears the error metadata for a given screen ID
+ * @param screenID - screen ID of the screen to clear
+ */
+export const dispatchClearMetadata = (screenID?: ScreenIDTypes): ReduxAction => {
+  return {
+    type: 'ERRORS_CLEAR_METADATA',
+    payload: {
+      screenID,
+    },
+  }
+}
+
+/**
+ * Clears the error metadata for all screen IDs
+ */
+export const dispatchClearAllMetadata = (): ReduxAction => {
+  return {
+    type: 'ERRORS_CLEAR_ALL_METADATA',
+    payload: null,
+  }
+}
+
+/**
+ * Clears the error type for all screen IDs
+ */
+export const dispatchClearErrorType = (errorType: CommonErrorTypes): ReduxAction => {
+  return {
+    type: 'ERRORS_CLEAR_ERROR_TYPE',
+    payload: {
+      errorType,
+    },
+  }
+}
+
+/**
+ * checks for downtime by getting a list from the backend API
+ * clears all metadata and current downtimes first and sets errors based on which downtime is active from API call
+ */
+export const dispatchCheckForDowntimeErrors = (): AsyncReduxAction => {
+  return async (dispatch, _getState): Promise<void> => {
+    const response = await api.get<MaintenanceWindowsGetData>('/v0/maintenance_windows')
+    if (!response) {
+      return
+    }
+    dispatch(dispatchClearAllMetadata())
+    dispatch(dispatchClearErrorType(CommonErrorTypesConstants.DOWNTIME_ERROR))
+    const maint_windows = response.data.filter((w) => DateTime.fromISO(w.startTime) <= DateTime.now() && !!DowntimeFeatureToScreenID[w.service])
+    for (const maint_window of maint_windows) {
+      const screenID = DowntimeFeatureToScreenID[maint_window.service]
+      if (!screenID) {
+        continue
+      }
+      const metadata = {
+        featureName: '',
+        endTime: '',
+      }
+      metadata.featureName = DowntimeFeatureNameConstants[maint_window.service]
+      metadata.endTime = DateTime.fromISO(maint_window.endTime).toFormat('fff')
+      dispatch(dispatchSetMetadata(metadata, screenID))
+      dispatch(dispatchSetError(CommonErrorTypesConstants.DOWNTIME_ERROR, screenID))
+    }
   }
 }
