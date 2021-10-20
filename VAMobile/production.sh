f53b62ebd0c7e3b100c1d5de4879b95f4919a01f
Patrick Saxton <patrick@urban-intellect.com>
2021-10-20 12:01:39 -0500

    Create Script for ad-hoc Production Builds (#1373)
    * add script
    
    * cleanup
    
    * send tag into build script

--- VAMobile/production.sh
+++ VAMobile/production.sh
@@ -1,33 +1,22 @@
-#!/usr/bin/env bash
-while getopts v:o: option
+while getopts v option
 do
 case "${option}"
 in
-v) version=${OPTARG};;
-o) os=${OPTARG};;
-*) ;;
+v) version=${OPTARG}
 esac
 done
 
-BASE_DIR="$PWD"
+yarn install ;
+cd ios ;
+pod install ;
+cd ../ ;
+yarn env:production ;
+yarn bundle:ios ;
+yarn bundle:android ;
+cd ios ;
 
-yarn install &&
-yarn env:production &&
+bundle update fastlane ;
+bundle exec fastlane release version:$version ;
+cd ../android ;
+bundle exec fastlane release version:$version ;
 
-if [[ -z "$os" || $os == "ios" ]]
-then
-cd "$BASE_DIR"/ios &&
-pod install &&
-cd "$BASE_DIR" &&
-yarn bundle:ios &&
-cd "$BASE_DIR"/ios &&
-fastlane release version:"$version" ;
-fi
-cd "$BASE_DIR" || exit
-
-if [[ -z "$os" || $os == "android" ]]
-then
-yarn bundle:android &&
-cd "$BASE_DIR"/android &&
-fastlane release version:"$version" ;
-fi
