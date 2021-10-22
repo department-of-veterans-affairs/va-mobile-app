3c85eea8033630c6d8a3a1aa8df73be20cfd0ebf
Teesh <teesh.s93@gmail.com>
2021-10-22 12:49:25 -0500

    Add review reset action in debug (#1847)

--- VAMobile/src/utils/inAppReviews.tsx
+++ VAMobile/src/utils/inAppReviews.tsx
@@ -7,12 +7,13 @@ export const STORAGE_REVIEW_EVENT_KEY = '@review_event'
 export const STORAGE_LAST_REVIEW_PROMPT_DATE_MILLIS = '@last_review_date'
 export const STORAGE_LAST_REVIEW_VERSION = '@last_review_version'
 export const IN_APP_REVIEW_INTERVAL_DAYS = 122
+export const IN_APP_REVIEW_ACTIONS_THRESHOLD = 7
 
 export const registerReviewEvent = async (): Promise<void> => {
   const prev = await AsyncStorage.getItem(STORAGE_REVIEW_EVENT_KEY)
   const total = prev ? parseInt(prev, 10) + 1 : 1
   await AsyncStorage.setItem(STORAGE_REVIEW_EVENT_KEY, `${total}`)
-  if (total >= 7) {
+  if (total >= IN_APP_REVIEW_ACTIONS_THRESHOLD) {
     const lastReview = await AsyncStorage.getItem(STORAGE_LAST_REVIEW_PROMPT_DATE_MILLIS)
     if (!lastReview) {
       await callReviewAPI()
@@ -31,4 +32,10 @@ const callReviewAPI = async (): Promise<void> => {
   await AsyncStorage.setItem(STORAGE_REVIEW_EVENT_KEY, '0')
   await AsyncStorage.setItem(STORAGE_LAST_REVIEW_PROMPT_DATE_MILLIS, `${DateTime.now().millisecond}`)
   await AsyncStorage.setItem(STORAGE_LAST_REVIEW_VERSION, versionName)
+}
+
+export const resetReviewActionCount = async (): Promise<void> => {
+  await AsyncStorage.removeItem(STORAGE_REVIEW_EVENT_KEY)
+  await AsyncStorage.removeItem(STORAGE_LAST_REVIEW_PROMPT_DATE_MILLIS)
+  await AsyncStorage.removeItem(STORAGE_LAST_REVIEW_VERSION)
 }
