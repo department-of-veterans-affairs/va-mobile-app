7891171ba4b14eefa8b954180028c4f18f24d636
Rafael Arroyo <rafael.arroyo@adhocteam.us>
2021-10-28 12:17:11 -0400

    Feature/1860 remove attachment confirmation (#1886)
    * Remove attachment confirmation
    
    * typo
    
    * fixing hook name
    
    * removing wrap function on removeAttachment
    
    * fixing library imports separating external with app specific
    
    * fixing more imports

--- VAMobile/src/screens/HealthScreen/SecureMessaging/ComposeMessage/ComposeMessage.tsx
+++ VAMobile/src/screens/HealthScreen/SecureMessaging/ComposeMessage/ComposeMessage.tsx
@@ -1,9 +1,7 @@
 import { InteractionManager } from 'react-native'
-import React, { FC, ReactNode, useEffect, useState } from 'react'
-
-import { ImagePickerResponse } from 'react-native-image-picker/src/types'
 import { StackScreenProps } from '@react-navigation/stack'
 import { useDispatch, useSelector } from 'react-redux'
+import React, { FC, ReactNode, useEffect, useState } from 'react'
 import _ from 'underscore'
 
 import {
@@ -35,7 +33,6 @@ import {
   SecureMessagingSystemFolderIdConstants,
   SecureMessagingTabTypesConstants,
 } from 'store/api/types'
-import { DocumentPickerResponse } from 'screens/ClaimsScreen/ClaimsStackScreens'
 import { FolderNameTypeConstants, FormHeaderTypeConstants } from 'constants/secureMessaging'
 import { HealthStackParamList } from 'screens/HealthScreen/HealthStackScreens'
 import { NAMESPACE } from 'constants/namespaces'
@@ -43,8 +40,8 @@ import { SecureMessagingState, StoreState } from 'store/reducers'
 import { getComposeMessageSubjectPickerOptions } from 'utils/secureMessaging'
 import { getMessageRecipients, getMessageSignature, resetSendMessageFailed, saveDraft, updateSecureMessagingTab } from 'store/actions'
 import { testIdProps } from 'utils/accessibility'
+import { useAttachments, useError, useMessageWithSignature, useRouteNavigation, useTheme, useTranslation, useValidateMessageWithSignature } from 'utils/hooks'
 import { useComposeCancelConfirmation } from '../CancelConfirmations/ComposeCancelConfirmation'
-import { useError, useMessageWithSignature, useRouteNavigation, useTheme, useTranslation, useValidateMessageWithSignature } from 'utils/hooks'
 
 type ComposeMessageProps = StackScreenProps<HealthStackParamList, 'ComposeMessage'>
 
@@ -58,12 +55,12 @@ const ComposeMessage: FC<ComposeMessageProps> = ({ navigation, route }) => {
     StoreState,
     SecureMessagingState
   >((state) => state.secureMessaging)
-  const { attachmentFileToAdd, attachmentFileToRemove, saveDraftConfirmFailed } = route.params
+  const { attachmentFileToAdd, saveDraftConfirmFailed } = route.params
 
   const [to, setTo] = useState('')
   const [subject, setSubject] = useState('')
   const [subjectLine, setSubjectLine] = useState('')
-  const [attachmentsList, setAttachmentsList] = useState<Array<ImagePickerResponse | DocumentPickerResponse>>([])
+  const [attachmentsList, addAttachment, removeAttachment] = useAttachments()
   const [message, setMessage] = useMessageWithSignature()
   const validateMessage = useValidateMessageWithSignature()
   const [onSendClicked, setOnSendClicked] = useState(false)
@@ -130,21 +127,10 @@ const ComposeMessage: FC<ComposeMessageProps> = ({ navigation, route }) => {
     }
     // if a file was just added, update attachmentsList and clear the route params for attachmentFileToAdd
     if (!_.isEmpty(attachmentFileToAdd) && !attachmentsList.includes(attachmentFileToAdd)) {
-      setAttachmentsList([...attachmentsList, attachmentFileToAdd])
+      addAttachment(attachmentFileToAdd)
       navigation.setParams({ attachmentFileToAdd: {} })
     }
-  }, [attachmentFileToAdd, attachmentsList, setAttachmentsList, navigation])
-
-  useEffect(() => {
-    if (attachmentFileToRemove === undefined) {
-      return
-    }
-    // if a file was just specified to be removed, update attachmentsList and clear the route params for attachmentFileToRemove
-    if (!_.isEmpty(attachmentFileToRemove) && attachmentsList.includes(attachmentFileToRemove)) {
-      setAttachmentsList(attachmentsList.filter((item) => item !== attachmentFileToRemove))
-      navigation.setParams({ attachmentFileToRemove: {} })
-    }
-  }, [attachmentFileToRemove, attachmentsList, setAttachmentsList, navigation])
+  }, [attachmentFileToAdd, attachmentsList, addAttachment, navigation])
 
   useEffect(() => {
     if (saveDraftComplete) {
@@ -169,10 +155,6 @@ const ComposeMessage: FC<ComposeMessageProps> = ({ navigation, route }) => {
 
   const isFormBlank = !(to || subject || subjectLine || attachmentsList.length || validateMessage(message))
   const isFormValid = !!(to && subject && validateMessage(message) && (subject !== CategoryTypeFields.other || subjectLine))
-
-  const removeAttachment = (attachmentFile: ImagePickerResponse | DocumentPickerResponse): void => {
-    navigation.navigate('RemoveAttachment', { origin: FormHeaderTypeConstants.compose, attachmentFileToRemove: attachmentFile })
-  }
 
   const isSetToGeneral = (text: string): boolean => {
     return text === CategoryTypeFields.other // Value of option associated with picker label 'General'
