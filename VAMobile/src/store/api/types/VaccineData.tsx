2b7bc72485ffb7aad2e08dc0939ca024a9c55ef5
John Marchi <j_marchi@digitalfoundry.com>
2021-10-28 09:27:49 -0700

    updating vaccine data, types, and views for details and list

--- VAMobile/src/store/api/types/VaccineData.tsx
+++ VAMobile/src/store/api/types/VaccineData.tsx
@@ -10,68 +10,17 @@ export type VaccineList = Array<Vaccine>
 
 export type Vaccine = {
   id: string
-  vaccineCode: string
-  recorded: string
-  primarySource: boolean
-  status: string
-  location: Location
-  manufacturer: VaccineManufacturer
-  lotNumber: string
-  expirationDate?: string
-  reaction: Array<VaccineReaction>
-  doseQuantity: DoseQuantity
-  notes: Array<VaccineNote>
-  protocolApplied: VaccineProtocolApplied
-}
-
-export type VaccineProtocolApplied = {
-  series: string
-  targetDisease: string
-  doseNumber: string
-  seriesDoses: string
-}
-
-export type Location = {
-  id: string
-  name: string
-  alias?: Array<string>
-  description?: string
-  address: LocationAddress
-}
-
-export type LocationAddress = {
   type: string
-  text?: string
-  line?: Array<string>
-  city: string
-  district?: string
-  state: string
-  postalCode: string
-  country: string
+  attributes: VaccineAttributes
 }
 
-export type VaccineManufacturer = {
-  active: boolean
-  name: string
-  alias?: Array<string>
-  address?: LocationAddress
-}
-
-export type VaccineNote = {
-  text: string
-  time: string
-}
-
-export type DoseQuantity = {
-  text: string
-}
-
-export type VaccineReaction = {
-  date?: string
-  reported?: boolean
-  detail: ReactionDetail
-}
-
-export type ReactionDetail = {
-  display: string
+export type VaccineAttributes = {
+  cvxCode: number
+  date: string
+  doseNumber: string | null
+  doseSeries: number | null
+  groupName: string
+  manufacturer: string | null
+  note: string | null
+  shortDescription: string
 }
