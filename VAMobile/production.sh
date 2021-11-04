a69955a5ce8d42bf0b698bab9180e985697fb182
Patrick Saxton <patrick@urban-intellect.com>
2021-11-04 11:33:16 -0500

    Automation - fix release build errors (#1461)
    * update ad-hoc script
    
    * update images
    
    * fix bug and add flags for deliver
    
    * ignore preview file

--- VAMobile/production.sh
+++ VAMobile/production.sh
@@ -1,22 +1,33 @@
-while getopts v option
+#!/usr/bin/env bash
+while getopts v:o: option
 do
 case "${option}"
 in
-v) version=${OPTARG}
+v) version=${OPTARG};;
+o) os=${OPTARG};;
+*) ;;
 esac
 done
 
-yarn install ;
-cd ios ;
-pod install ;
-cd ../ ;
-yarn env:production ;
-yarn bundle:ios ;
-yarn bundle:android ;
-cd ios ;
+BASE_DIR="$PWD"
 
-bundle update fastlane ;
-bundle exec fastlane release version:$version ;
-cd ../android ;
-bundle exec fastlane release version:$version ;
+yarn install &&
+yarn env:production &&
 
+if [[ -z "$os" || $os == "ios" ]]
+then
+cd "$BASE_DIR"/ios &&
+pod install &&
+cd "$BASE_DIR" &&
+yarn bundle:ios &&
+cd "$BASE_DIR"/ios &&
+fastlane release version:"$version" ;
+fi
+cd "$BASE_DIR" || exit
+
+if [[ -z "$os" || $os == "android" ]]
+then
+yarn bundle:android &&
+cd "$BASE_DIR"/android &&
+fastlane release version:"$version" ;
+fi
