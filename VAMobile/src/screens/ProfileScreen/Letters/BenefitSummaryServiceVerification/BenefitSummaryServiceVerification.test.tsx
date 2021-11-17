91a8946bdab7100955d3cb8d8138e9176e92bdf0
Narin Ratana <narin@adhocteam.us>
2021-11-17 13:23:03 -0600

    Fix for total disability on VA Benefit and Disability Letter (#1288)
    * Fix for total disability on VA Benefit and Disability Letter
    
    * Adding test and making fourth toggle dynamic

--- VAMobile/src/screens/ProfileScreen/Letters/BenefitSummaryServiceVerification/BenefitSummaryServiceVerification.test.tsx
+++ VAMobile/src/screens/ProfileScreen/Letters/BenefitSummaryServiceVerification/BenefitSummaryServiceVerification.test.tsx
@@ -1,28 +1,26 @@
 import 'react-native'
 import React from 'react'
 // Note: test renderer must be required after react-native.
-import { act, ReactTestInstance } from 'react-test-renderer'
-import { Linking, Pressable, Switch as RNSwitch } from 'react-native'
+import {act, ReactTestInstance} from 'react-test-renderer'
+import {Linking, Pressable, Switch as RNSwitch} from 'react-native'
 
-import { BasicError, ErrorComponent, LoadingComponent, Switch, TextView } from 'components'
-import { context, findByTestID, mockNavProps, mockStore, renderWithProviders } from 'testUtils'
+import {BasicError, ErrorComponent, LoadingComponent, Switch, TextView} from 'components'
+import {context, findByTestID, mockNavProps, mockStore, renderWithProviders} from 'testUtils'
 import BenefitSummaryServiceVerification from './BenefitSummaryServiceVerification'
-import { ErrorsState, initialErrorsState, InitialState } from 'store/reducers'
+import {ErrorsState, initialErrorsState, InitialState} from 'store/reducers'
 import { CharacterOfServiceConstants, LetterTypeConstants } from 'store/api/types'
 import { downloadLetter } from 'store/actions'
-
-const mockExternalLinkSpy = jest.fn()
+import { CommonErrorTypesConstants } from 'constants/errors';
 
 jest.mock('../../../../utils/hooks', () => {
-  let original = jest.requireActual('../../../../utils/hooks')
-  let theme = jest.requireActual('../../../../styles/themes/standardTheme').default
+  let original = jest.requireActual("../../../../utils/hooks")
+  let theme = jest.requireActual("../../../../styles/themes/standardTheme").default
   return {
     ...original,
-    useTheme: jest.fn(() => {
-      return { ...theme }
+    useTheme: jest.fn(()=> {
+      return {...theme}
     }),
     useRouteNavigation: () => jest.fn(),
-    useExternalLink: () => mockExternalLinkSpy,
   }
 })
 
@@ -33,11 +31,12 @@ jest.mock('../../../../store/actions', () => {
     downloadLetter: jest.fn(() => {
       return {
         type: '',
-        payload: '',
+        payload: ''
       }
-    }),
+    })
   }
 })
+
 
 context('BenefitSummaryServiceVerification', () => {
   let store: any
@@ -53,17 +52,14 @@ context('BenefitSummaryServiceVerification', () => {
       ...InitialState,
       letters: {
         loading: false,
-        letters: [],
         downloading: downloading,
         letterDownloadError: hasDownloadError ? new Error('error') : undefined,
-        mostRecentServices: [
-          {
-            branch: 'Army',
-            characterOfService: CharacterOfServiceConstants.HONORABLE,
-            enteredDate: '1990-01-01T15:00:00.000+00:00',
-            releasedDate: '1993-10-01T15:00:00.000+00:00',
-          },
-        ],
+        mostRecentServices: [{
+          branch: 'Army',
+          characterOfService: CharacterOfServiceConstants.HONORABLE,
+          enteredDate: '1990-01-01T15:00:00.000+00:00',
+          releasedDate: '1993-10-01T15:00:00.000+00:00',
+        }],
         letterBeneficiaryData: {
           militaryService: [
             {
@@ -71,7 +67,7 @@ context('BenefitSummaryServiceVerification', () => {
               characterOfService: CharacterOfServiceConstants.HONORABLE,
               enteredDate: '1990-01-01T15:00:00.000+00:00',
               releasedDate: '1993-10-01T15:00:00.000+00:00',
-            },
+            }
           ],
           benefitInformation: {
             awardEffectiveDate: awardEffectiveDate || null,
@@ -86,8 +82,8 @@ context('BenefitSummaryServiceVerification', () => {
             hasNonServiceConnectedPension: true,
             hasServiceConnectedDisabilities: false,
             hasSpecialMonthlyCompensation: true,
-          },
-        },
+          }
+        }
       },
     })
 
@@ -126,10 +122,11 @@ context('BenefitSummaryServiceVerification', () => {
     expect(combinedRating.props.children).toEqual('Your combined service-connected rating is 88%.')
 
     const totallyAndPermanent = testInstance.findAllByType(TextView)[17]
-    expect(totallyAndPermanent.props.children).toEqual("You aren't considered to be totally and permanently disabled solely due to your service-connected disabilities.")
+    expect(totallyAndPermanent.props.children).toEqual('You aren\'t considered to be totally and permanently disabled solely due to your service-connected disabilities.')
 
     const haveOneOrMoreDisabilities = testInstance.findAllByType(TextView)[18]
-    expect(haveOneOrMoreDisabilities.props.children).toEqual("You don't have one or more service-connected disabilities.")
+    expect(haveOneOrMoreDisabilities.props.children).toEqual('You don\'t have one or more service-connected disabilities.')
+
   })
 
   describe('on include military service info switch click', () => {
@@ -198,9 +195,9 @@ context('BenefitSummaryServiceVerification', () => {
   })
 
   describe('on click of send a message', () => {
-    it('should launch external link', async () => {
+    it('should call linking openUrl', async () => {
       findByTestID(testInstance, 'send-us-a-message').props.onPress()
-      expect(mockExternalLinkSpy).toHaveBeenCalledWith('https://iris.custhelp.va.gov/app/ask')
+      expect(Linking.openURL).toHaveBeenCalledWith('https://iris.custhelp.va.gov/app/ask')
     })
   })
 
@@ -219,7 +216,7 @@ context('BenefitSummaryServiceVerification', () => {
         militaryService: true,
         monthlyAward: true,
         serviceConnectedDisabilities: true,
-        serviceConnectedEvaluation: true,
+        serviceConnectedEvaluation: true
       }
       expect(downloadLetter).toBeCalledWith(LetterTypeConstants.benefitSummary, letterOptions)
     })
@@ -228,9 +225,7 @@ context('BenefitSummaryServiceVerification', () => {
   describe('when both the awardEffectiveDate and the monthly payment amount exist', () => {
     it('should display "Your current monthly award is ${{monthlyAwardAmount}}. The effective date of the last change to your current payment was {{date}}." for that switch', async () => {
       initializeTestInstance(123, date, 88)
-      expect(testInstance.findAllByType(TextView)[15].props.children).toEqual(
-        'Your current monthly payment is $123.00. The effective date of the last change to your current payment was June 06, 2013.',
-      )
+      expect(testInstance.findAllByType(TextView)[15].props.children).toEqual('Your current monthly payment is $123.00. The effective date of the last change to your current payment was June 06, 2013.')
     })
   })
 
@@ -248,6 +243,7 @@ context('BenefitSummaryServiceVerification', () => {
     })
   })
 
+
   describe('when the awardEffectiveDate does not exist and the monthly award amount does not exist', () => {
     it('should not display that switch on the screen', async () => {
       initializeTestInstance(undefined, undefined, 88)
@@ -263,12 +259,12 @@ context('BenefitSummaryServiceVerification', () => {
   })
 
   describe('when an error occurs', () => {
-    it('should render error component when there is a letter download error', async () => {
+    it('should render error component when there is a letter download error', async() => {
       initializeTestInstance(123, date, undefined, undefined, true)
       expect(testInstance.findAllByType(BasicError)).toHaveLength(1)
     })
 
-    it('should not render error component when there is no letter download error', async () => {
+    it('should not render error component when there is no letter download error', async() => {
       initializeTestInstance(123, date, undefined, undefined, false)
       expect(testInstance.findAllByType(BasicError)).toHaveLength(0)
     })
