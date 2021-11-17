e34f442394a4c4fdd56873a39a61ceeaadbd797b
Narin Ratana <narin@users.noreply.github.com>
2021-11-17 12:12:34 -0800

    Refactor for Enhanced Demo Mode (#2026)
    * Refactor mocks into separate files
    
    * Add comments
    
    * Add artificial delay for mock calls
    
    * Fix comment
    
    Co-authored-by: k_li <k_li@digitalfoundry.com>

---
+++ VAMobile/src/store/api/demo/claims.ts
@@ -0,0 +1,28 @@
+import { ClaimGetData, ClaimsAndAppealsGetData } from '../types'
+import { DemoStore } from './store'
+import { Params } from '../api'
+
+/**
+ * Type denoting the demo data store
+ */
+export type ClaimsDemoStore = {
+  '/v0/claims-and-appeals-overview': {
+    open: ClaimsAndAppealsGetData
+    closed: ClaimsAndAppealsGetData
+  }
+  '/v0/claim/600232852': ClaimGetData
+  '/v0/claim/600236068': ClaimGetData
+}
+
+/**
+ * Type to define the mock returns to keep type safety
+ */
+export type ClaimsDemoApiReturnTypes = ClaimGetData | ClaimsAndAppealsGetData
+
+export const getClaimsAndAppealsOverview = (store: DemoStore, params: Params): ClaimsAndAppealsGetData => {
+  if (params.showCompleted === 'false') {
+    return store['/v0/claims-and-appeals-overview'].open
+  } else {
+    return store['/v0/claims-and-appeals-overview'].closed
+  }
+}
