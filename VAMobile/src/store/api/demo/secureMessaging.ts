a893edb65f2cfb29849797765700500314a00049
Rafael Arroyo <rafael.arroyo@adhocteam.us>
2021-11-19 11:35:25 -0500

    Sent folder messages for demo mode

--- VAMobile/src/store/api/demo/secureMessaging.ts
+++ VAMobile/src/store/api/demo/secureMessaging.ts
@@ -1,8 +1,27 @@
 import { DemoStore } from './store'
 import { Params } from '../api'
-import { SecureMessagingFolderGetData, SecureMessagingFolderMessagesGetData, SecureMessagingFoldersGetData } from '../types'
+import {
+  SecureMessagingFolderGetData,
+  SecureMessagingFolderMessagesGetData,
+  SecureMessagingFoldersGetData,
+  SecureMessagingMessageGetData,
+  SecureMessagingThreadGetData,
+} from '../types'
 
 type MessagePageNumber = '1' | '2'
+
+type folderMessages =
+  | {
+      '/v0/messaging/health/folders/0/messages': {
+        '1': SecureMessagingFolderMessagesGetData
+        '2': SecureMessagingFolderMessagesGetData
+      }
+    }
+  | {
+      '/v0/messaging/health/folders/-1/messages': {
+        '1': SecureMessagingFolderMessagesGetData
+      }
+    }
 
 /**
  * Type denoting the demo data store for secure messaging
@@ -10,18 +29,25 @@ type MessagePageNumber = '1' | '2'
 export type SecureMessagingDemoStore = {
   '/v0/messaging/health/folders': SecureMessagingFoldersGetData
   '/v0/messaging/health/folders/0': SecureMessagingFolderGetData
-  '/v0/messaging/health/folders/0/messages': {
-    '1': SecureMessagingFolderMessagesGetData
-    '2': SecureMessagingFolderMessagesGetData
-  }
-}
+  '/v0/messaging/health/folders/-1': SecureMessagingFolderGetData
+  '/v0/messaging/health/messages/2113100': SecureMessagingMessageGetData
+  '/v0/messaging/health/messages/2098942': SecureMessagingMessageGetData
+  '/v0/messaging/health/messages/2098929': SecureMessagingMessageGetData
+  '/v0/messaging/health/messages/2098888': SecureMessagingMessageGetData
+  '/v0/messaging/health/messages/2095016': SecureMessagingMessageGetData
+  '/v0/messaging/health/messages/2113100/thread': SecureMessagingThreadGetData
+  '/v0/messaging/health/messages/2098942/thread': SecureMessagingThreadGetData
+  '/v0/messaging/health/messages/2098929/thread': SecureMessagingThreadGetData
+  '/v0/messaging/health/messages/2098888/thread': SecureMessagingThreadGetData
+  '/v0/messaging/health/messages/2095016/thread': SecureMessagingThreadGetData
+} & folderMessages
 
 /**
  * Type to define the mock returns to keep type safety
  */
-export type SecureMessagingDemoApiReturnTypes = SecureMessagingFolderGetData | SecureMessagingFolderMessagesGetData
+export type SecureMessagingDemoApiReturnTypes = SecureMessagingFolderGetData | SecureMessagingFolderMessagesGetData | SecureMessagingMessageGetData | SecureMessagingThreadGetData
 
-export const getInboxMessage = (store: DemoStore, params: Params): SecureMessagingFolderMessagesGetData => {
+export const getFolderMessages = (store: DemoStore, params: Params, endpoint: string): SecureMessagingFolderMessagesGetData => {
   const { page } = params
-  return store['/v0/messaging/health/folders/0/messages'][page as MessagePageNumber]
+  return store[endpoint as keyof folderMessages][page as MessagePageNumber] as SecureMessagingFolderMessagesGetData
 }
