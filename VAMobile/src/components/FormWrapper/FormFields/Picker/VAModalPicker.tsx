7589844fe3539cd28bce8624fb47eafd491b143a
Rafael Arroyo <rafael.arroyo@adhocteam.us>
2021-12-06 12:17:09 -0500

    Feature/2030 snack bar accessibility (#2104)
    * initial commit
    
    * work in progress
    
    * Update LoginScreen.tsx
    
    * snackbar update
    
    * Initial move code
    
    * update to move message snckbar
    
    * Update SnackBar.tsx
    
    * Accessibility for the snackbar
    
    * update to android spacing
    
    * Update App.tsx
    
    * Update secureMessaging.ts
    
    * Update SnackBar.tsx
    
    * fix lint

--- VAMobile/src/components/FormWrapper/FormFields/Picker/VAModalPicker.tsx
+++ VAMobile/src/components/FormWrapper/FormFields/Picker/VAModalPicker.tsx
@@ -182,7 +182,7 @@ const VAModalPicker: FC<VAModalPickerProps> = ({
     }
 
     return (
-      <TouchableWithoutFeedback {...props} {...testIdProps('save')} {...a11yHintProp(t('common:pickerLaunchBtn.a11yHint'))}>
+      <TouchableWithoutFeedback {...props} {...testIdProps(t(buttonText || ''))} {...a11yHintProp(t('common:pickerLaunchBtn.a11yHint'))}>
         <Box pr={theme.dimensions.headerButtonMargin} height={theme.dimensions.headerHeight} justifyContent={'center'} pl={theme.dimensions.headerButtonPadding}>
           <TextView variant="ActionBar" color={color} allowFontScaling={false} accessible={false}>
             {t(buttonText || '')}
