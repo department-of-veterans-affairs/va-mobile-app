afc8082842b44d23ea3005b9438c3ab332727d8a
Narin Ratana <narin@adhocteam.us>
2021-12-15 14:55:46 -0800

    Feature/1417 address confirmation message (#1684)
    * Initial commit work in progress
    
    * remove un-used import
    
    * Fixing failing tests
    
    * Updating per comments
    
    * Update personalInformation.tsx

--- VAMobile/src/screens/ProfileScreen/AddressValidation.test.tsx
+++ VAMobile/src/screens/ProfileScreen/AddressValidation.test.tsx
@@ -8,8 +8,9 @@ import { context, mockStore, renderWithProviders, findByTestID } from 'testUtils
 import AddressValidation from './AddressValidation'
 import { initialPersonalInformationState, InitialState } from 'store/reducers'
 import { AddressData, AddressValidationScenarioTypes, AddressValidationScenarioTypesConstants } from 'store/api/types'
-import { TextView } from 'components'
-import { finishValidateAddress, updateAddress } from 'store'
+import { AccordionCollapsible, TextView, VASelector } from 'components'
+import { updateAddress } from 'store'
+import { Pressable, TouchableWithoutFeedback } from 'react-native'
 
 const mockAddress: AddressData = {
   addressLine1: '2248 San Miguel Ave.',
@@ -48,12 +49,6 @@ jest.mock('../../store/actions', () => {
         payload: '',
       }
     }),
-    finishValidateAddress: jest.fn(() => {
-      return {
-        type: '',
-        payload: '',
-      }
-    }),
   }
 })
 
@@ -73,21 +68,7 @@ context('AddressValidation', () => {
     })
 
     act(() => {
-      component = renderWithProviders(
-        <AddressValidation
-          addressLine1={mockAddress.addressLine1}
-          city={mockAddress.city}
-          state={mockAddress.stateCode as string}
-          zipCode={mockAddress.zipCode}
-          addressId={12345}
-          country={mockAddress.countryName || ''}
-          onCancel={() => {
-            finishValidateAddress()
-            mockedNavigationGoBack()
-          }}
-        />,
-        store,
-      )
+      component = renderWithProviders(<AddressValidation addressEntered={mockAddress} addressId={12345} />, store)
     })
 
     testInstance = component.root
@@ -104,6 +85,11 @@ context('AddressValidation', () => {
   describe('when the address validation scenario type is BAD_UNIT', () => {
     it('should display the BAD_UNIT alert texts', async () => {
       prepInstanceWithStore(AddressValidationScenarioTypesConstants.BAD_UNIT_NUMBER_OVERRIDE)
+
+      act(() => {
+        testInstance.findByType(AccordionCollapsible).findByType(Pressable).props.onPress()
+      })
+
       const textViews = testInstance.findAllByType(TextView)
       const alertTitle = textViews[0]
       const alertBody = textViews[1]
@@ -119,6 +105,11 @@ context('AddressValidation', () => {
   describe('when the address validation scenario type is MISSING_UNIT_OVERRIDE', () => {
     it('should display the BAD_UNIT alert texts', async () => {
       prepInstanceWithStore(AddressValidationScenarioTypesConstants.MISSING_UNIT_OVERRIDE)
+
+      act(() => {
+        testInstance.findByType(AccordionCollapsible).findByType(Pressable).props.onPress()
+      })
+
       const textViews = testInstance.findAllByType(TextView)
       const alertTitle = textViews[0]
       const alertBody = textViews[1]
@@ -134,6 +125,11 @@ context('AddressValidation', () => {
   describe('when the address validation scenario type is SHOW_SUGGESTIONS_OVERRIDE', () => {
     it('should display the BAD_UNIT alert texts', async () => {
       prepInstanceWithStore(AddressValidationScenarioTypesConstants.SHOW_SUGGESTIONS_OVERRIDE)
+
+      act(() => {
+        testInstance.findByType(AccordionCollapsible).findByType(Pressable).props.onPress()
+      })
+
       const textViews = testInstance.findAllByType(TextView)
       const alertTitle = textViews[0]
       const alertBody = textViews[1]
@@ -141,7 +137,7 @@ context('AddressValidation', () => {
       expect(alertBody).toBeTruthy()
       expect(alertTitle.props.children).toEqual('Confirm your address')
       expect(alertBody.props.children).toEqual(
-        "We couldn't confirm your address with the U.S. Postal Service. Verify your address so we can save it to your VA profile. If the address you entered isn't correct, edit it or choose a suggested address below.",
+        "We couldn't confirm your address with the U.S. Postal Service. Verify your address so we can save it to your VA profile. If the address you entered isn't correct, edit it. If the address listed below is correct, select it.",
       )
     })
   })
@@ -149,6 +145,11 @@ context('AddressValidation', () => {
   describe('when the address validation scenario type is SHOW_SUGGESTIONS_NO_CONFIRMED_OVERRIDE', () => {
     it('should display the BAD_UNIT alert texts', async () => {
       prepInstanceWithStore(AddressValidationScenarioTypesConstants.SHOW_SUGGESTIONS_NO_CONFIRMED_OVERRIDE)
+
+      act(() => {
+        testInstance.findByType(AccordionCollapsible).findByType(Pressable).props.onPress()
+      })
+
       const textViews = testInstance.findAllByType(TextView)
       const alertTitle = textViews[0]
       const alertBody = textViews[1]
@@ -164,23 +165,18 @@ context('AddressValidation', () => {
   describe('When use this address button is pressed', () => {
     it('should call updateAddress', async () => {
       prepInstanceWithStore(AddressValidationScenarioTypesConstants.SHOW_SUGGESTIONS_OVERRIDE)
+
+      act(() => {
+        testInstance.findByType(AccordionCollapsible).findByType(Pressable).props.onPress()
+      })
+
+      testInstance.findAllByType(VASelector)[0].findByType(TouchableWithoutFeedback).props.onPress()
+
       const useThisAddressButton = findByTestID(testInstance, 'Use This Address')
       expect(useThisAddressButton).toBeTruthy()
 
       useThisAddressButton.props.onPress()
       expect(updateAddress).toBeCalled()
-    })
-  })
-
-  describe('When cancel button is pressed', () => {
-    it('should call useCancelEditAddress', async () => {
-      prepInstanceWithStore(AddressValidationScenarioTypesConstants.SHOW_SUGGESTIONS_OVERRIDE)
-      const cancelButton = findByTestID(testInstance, 'Cancel')
-      expect(cancelButton).toBeTruthy()
-
-      cancelButton.props.onPress()
-      expect(finishValidateAddress).toBeCalled()
-      expect(mockedNavigationGoBack).toBeCalled()
     })
   })
 })
