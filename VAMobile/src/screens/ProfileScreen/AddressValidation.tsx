774adac38698fba55eeed26a22ad349b6cfc1341
Narin Ratana <narin@adhocteam.us>
2021-12-15 14:50:05 -0800

    Feature/1012 canceling new address should clear current work (#1430)
    * Fixing current work not being cleared when editing an address
    
    * Fix typo
    
    * fixing test
    
    * Remove the hook
    
    Co-authored-by: Narin Ratana <narin@users.noreply.github.com>

--- VAMobile/src/screens/ProfileScreen/AddressValidation.tsx
+++ VAMobile/src/screens/ProfileScreen/AddressValidation.tsx
@@ -1,10 +1,10 @@
 import { map } from 'underscore'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigation } from '@react-navigation/native'
-import React, { FC, ReactElement, ReactNode, useEffect, useState } from 'react'
+import React, { FC, ReactElement, useEffect, useState } from 'react'
 
-import { AccordionCollapsible, Box, ButtonTypesConstants, RadioGroup, TextArea, TextView, VAButton, VAScrollView, radioOption } from 'components'
-import { AddressData, AddressValidationScenarioTypesConstants, ScreenIDTypesConstants, SuggestedAddress } from 'store/api/types'
+import { AddressValidationScenarioTypesConstants, ScreenIDTypesConstants, SuggestedAddress } from 'store/api/types'
+import { AlertBox, Box, ButtonTypesConstants, RadioGroup, TextArea, TextView, VAButton, VAScrollView } from 'components'
 import { NAMESPACE } from 'constants/namespaces'
 import { PersonalInformationState, StoreState } from 'store/reducers'
 import { ViewStyle } from 'react-native'
@@ -16,32 +16,39 @@ import { useTheme, useTranslation } from 'utils/hooks'
  *  Signifies the props that need to be passed in to {@link AddressValidation}
  */
 export type AddressValidationProps = {
-  addressEntered: AddressData
+  addressLine1: string
+  addressLine2?: string
+  addressLine3?: string
+  city: string
+  state: string
+  zipCode: string
   addressId: number
+  country: string
+  onCancel: () => void
 }
 
-const AddressValidation: FC<AddressValidationProps> = ({ addressEntered, addressId }) => {
+const AddressValidation: FC<AddressValidationProps> = ({ addressLine1, addressLine2, addressLine3, city, state, zipCode, addressId, country, onCancel }) => {
   const dispatch = useDispatch()
   const t = useTranslation(NAMESPACE.PROFILE)
   const navigation = useNavigation()
   const theme = useTheme()
 
   const { standardMarginBetween, contentMarginTop, contentMarginBottom, condensedMarginBetween } = theme.dimensions
-  const { validationKey, addressValidationScenario, confirmedSuggestedAddresses } = useSelector<StoreState, PersonalInformationState>(
+  const { addressData, validationKey, addressValidationScenario, confirmedSuggestedAddresses } = useSelector<StoreState, PersonalInformationState>(
     (storeState) => storeState.personalInformation,
   )
-  const [selectedSuggestedAddress, setSelectedSuggestedAddress] = useState<AddressData | SuggestedAddress>()
-
+  const [selectedSuggestedAddress, setSelectedSuggestedAddress] = useState(confirmedSuggestedAddresses ? confirmedSuggestedAddresses[0] : undefined)
+  const showSuggestions =
+    addressValidationScenario === AddressValidationScenarioTypesConstants.SHOW_SUGGESTIONS ||
+    addressValidationScenario === AddressValidationScenarioTypesConstants.SHOW_SUGGESTIONS_OVERRIDE
   const scrollStyles: ViewStyle = {
     flexGrow: 1,
     justifyContent: 'center',
     backgroundColor: theme.colors.background.main,
   }
   const containerStyles = {
-    flex: 0,
+    flex: 1,
     mx: theme.dimensions.gutter,
-    mb: contentMarginBottom,
-    mt: standardMarginBetween,
   }
 
   useEffect(() => {
@@ -55,10 +62,8 @@ const AddressValidation: FC<AddressValidationProps> = ({ addressEntered, address
     dispatch(finishValidateAddress())
   }
 
-  const onSetSuggestedAddress = (address: SuggestedAddress | AddressData): void => {
-    if (address) {
-      setSelectedSuggestedAddress(address)
-    }
+  const onSetSuggestedAddress = (address: SuggestedAddress): void => {
+    setSelectedSuggestedAddress(address as SuggestedAddress)
   }
 
   const getFormattedAddressLines = (line1: string, line2?: string, line3?: string): string => {
@@ -66,32 +71,32 @@ const AddressValidation: FC<AddressValidationProps> = ({ addressEntered, address
   }
 
   const onUseThisAddress = (): void => {
+    if (addressData) {
+      // overriding with an invalid address requires a validation key
+      addressData.validationKey = validationKey
+      dispatch(updateAddress(addressData, ScreenIDTypesConstants.EDIT_ADDRESS_SCREEN_ID))
+    }
+  }
+
+  const onUseSuggestedAddress = (): void => {
     if (!selectedSuggestedAddress) {
       return
     }
 
-    let address: AddressData
-
-    if ('attributes' in selectedSuggestedAddress) {
-      address = getAddressDataFromSuggestedAddress(selectedSuggestedAddress, addressId)
-    } else {
-      address = selectedSuggestedAddress
-      // overriding with an invalid address requires a validation key
-      address.validationKey = validationKey
-    }
+    const address = getAddressDataFromSuggestedAddress(selectedSuggestedAddress, addressId)
 
     dispatch(updateAddress(address, ScreenIDTypesConstants.EDIT_ADDRESS_SCREEN_ID))
   }
 
-  const getSuggestedAddressLabelArgs = (address: SuggestedAddress | AddressData): { [key: string]: string } => {
-    const suggestedAddress = 'attributes' in address ? address.attributes : address
+  const getSuggestedAddressLabelArgs = (address: SuggestedAddress): { [key: string]: string } => {
+    const suggestedAddress = address.attributes
     const addressLines = getFormattedAddressLines(suggestedAddress.addressLine1, suggestedAddress.addressLine2, suggestedAddress.addressLine3)
 
     if (suggestedAddress.province && suggestedAddress.internationalPostalCode) {
       return { addressLines: addressLines, city: suggestedAddress.city, state: suggestedAddress.province, postCode: suggestedAddress.internationalPostalCode }
     }
 
-    return { addressLines: addressLines, city: suggestedAddress.city, state: suggestedAddress.stateCode || '', postCode: suggestedAddress.zipCode }
+    return { addressLines: addressLines, city: suggestedAddress.city, state: suggestedAddress.stateCode, postCode: suggestedAddress.zipCode }
   }
 
   const getAlertTitle = (): string => {
@@ -114,6 +119,7 @@ const AddressValidation: FC<AddressValidationProps> = ({ addressEntered, address
     switch (addressValidationScenario) {
       case AddressValidationScenarioTypesConstants.SHOW_SUGGESTIONS:
       case AddressValidationScenarioTypesConstants.SHOW_SUGGESTIONS_OVERRIDE:
+        return t('editAddress.validation.confirmAddress.suggestions.body')
       case AddressValidationScenarioTypesConstants.SHOW_SUGGESTIONS_NO_CONFIRMED_OVERRIDE:
         return t('editAddress.validation.confirmAddress.noSuggestions.body')
       case AddressValidationScenarioTypesConstants.MISSING_UNIT_OVERRIDE:
@@ -121,7 +127,7 @@ const AddressValidation: FC<AddressValidationProps> = ({ addressEntered, address
       case AddressValidationScenarioTypesConstants.BAD_UNIT_NUMBER_OVERRIDE:
         return t('editAddress.validation.badUnit.body')
       default:
-        return t('editAddress.validation.confirmAddress.suggestions.body')
+        return t('editAddress.validation.confirmAddress.noSuggestions.body')
     }
   }
 
@@ -129,6 +135,7 @@ const AddressValidation: FC<AddressValidationProps> = ({ addressEntered, address
     switch (addressValidationScenario) {
       case AddressValidationScenarioTypesConstants.SHOW_SUGGESTIONS:
       case AddressValidationScenarioTypesConstants.SHOW_SUGGESTIONS_OVERRIDE:
+        return t('editAddress.validation.confirmAddress.suggestions.body.a11yLabel')
       case AddressValidationScenarioTypesConstants.SHOW_SUGGESTIONS_NO_CONFIRMED_OVERRIDE:
         return t('editAddress.validation.confirmAddress.noSuggestions.body.a11yLabel')
       default:
@@ -136,45 +143,104 @@ const AddressValidation: FC<AddressValidationProps> = ({ addressEntered, address
     }
   }
 
-  const accordionHeader = (): ReactNode => {
-    return <TextView variant="MobileBodyBold">{getAlertTitle()}</TextView>
+  const getAlert = (): ReactElement => {
+    return (
+      <Box accessibilityRole="header">
+        <AlertBox title={getAlertTitle()} border="warning" background="noCardBackground">
+          <Box>
+            <TextView color="primary" variant="MobileBody" my={standardMarginBetween} accessibilityLabel={getAlertBodyA11yLabel()}>
+              {getAlertBody()}
+            </TextView>
+          </Box>
+        </AlertBox>
+      </Box>
+    )
   }
 
-  const getAlert = (): ReactNode => {
+  const getUserEnteredAddress = (): ReactElement => {
+    const addressLines = getFormattedAddressLines(addressLine1, addressLine2, addressLine3)
+
+    const editAddressButtonProps = {
+      label: t('editAddress.validation.editAddress'),
+      testID: t('editAddress.validation.editAddress.a11yLabel'),
+      a11yHint: t('editAddress.validation.editAddress.a11yHint'),
+      onPress: onEditAddress,
+    }
+
+    const useThisAddressButtonProps = {
+      label: t('editAddress.validation.useThisAddress'),
+      testID: t('editAddress.validation.useThisAddress'),
+      a11yHint: t('editAddress.validation.useThisAddress.a11yHint'),
+      onPress: onUseThisAddress,
+    }
+
+    const formattedEnteredAddressSecondLine = state ? city + ', ' + state + ', ' + zipCode : city + ', ' + zipCode
+
     return (
-      <TextView color="primary" variant="MobileBody" my={standardMarginBetween} accessibilityLabel={getAlertBodyA11yLabel()}>
-        {getAlertBody()}
-      </TextView>
+      <TextArea>
+        <Box>
+          <TextView color="primary" variant="MobileBodyBold" accessibilityRole="header">
+            {t('editAddress.validation.youEntered')}
+          </TextView>
+        </Box>
+        <Box>
+          <TextView color="primary" variant="MobileBody" mt={standardMarginBetween}>
+            {addressLines}
+          </TextView>
+          <TextView color="primary" variant="MobileBody">
+            {formattedEnteredAddressSecondLine}
+          </TextView>
+          <TextView color="primary" variant="MobileBody" mb={standardMarginBetween}>
+            {country}
+          </TextView>
+        </Box>
+        {showSuggestions ? (
+          <Box>
+            <Box mb={condensedMarginBetween}>
+              <VAButton {...useThisAddressButtonProps} buttonType={ButtonTypesConstants.buttonPrimary} />
+            </Box>
+            <Box>
+              <VAButton {...editAddressButtonProps} buttonType={ButtonTypesConstants.buttonSecondary} />
+            </Box>
+          </Box>
+        ) : (
+          <Box>
+            <VAButton {...editAddressButtonProps} buttonType={ButtonTypesConstants.buttonPrimary} />
+          </Box>
+        )}
+      </TextArea>
     )
   }
 
   const getSuggestedAddresses = (): ReactElement => {
-    let suggestedAddressOptions: Array<radioOption<AddressData | SuggestedAddress>> = []
-
-    suggestedAddressOptions.push({
-      value: addressEntered,
-      labelKey: 'profile:editAddress.address',
-      labelArgs: getSuggestedAddressLabelArgs(addressEntered),
-      headerText: t('editAddress.validation.youEntered'),
-    })
-
-    if (confirmedSuggestedAddresses) {
-      suggestedAddressOptions = suggestedAddressOptions.concat(
-        map(confirmedSuggestedAddresses, (address, index) => {
-          return {
-            value: address,
-            labelKey: 'profile:editAddress.address',
-            labelArgs: getSuggestedAddressLabelArgs(address),
-            addHeader: index === 0 ? true : false,
-            headerText: t('editAddress.validation.suggestedAddresses'),
-          }
-        }),
-      )
+    if (!confirmedSuggestedAddresses || !selectedSuggestedAddress) {
+      return <></>
     }
+
+    const useSuggestedAddressButtonProps = {
+      testID: t('editAddress.validation.useSuggestedAddress'),
+      a11yHint: t('editAddress.validation.useSuggestedAddress.a11yHint'),
+      label: t('editAddress.validation.useSuggestedAddress'),
+    }
+
+    // only looping through confirmed suggested addresses because the whole list of suggestions may include addresses with non confirmed delivery points
+    const suggestedAddressOptions = map(confirmedSuggestedAddresses, (address) => {
+      return {
+        value: address,
+        labelKey: 'profile:editAddress.address',
+        labelArgs: getSuggestedAddressLabelArgs(address),
+      }
+    })
 
     return (
       <TextArea>
-        <RadioGroup<SuggestedAddress | AddressData> options={suggestedAddressOptions} value={selectedSuggestedAddress} onChange={onSetSuggestedAddress} />
+        <TextView variant="MobileBodyBold">{t('editAddress.validation.suggestedAddresses')}</TextView>
+        <Box mt={contentMarginTop}>
+          <RadioGroup<SuggestedAddress> options={suggestedAddressOptions} value={selectedSuggestedAddress} onChange={onSetSuggestedAddress} />
+        </Box>
+        <Box>
+          <VAButton onPress={onUseSuggestedAddress} {...useSuggestedAddressButtonProps} buttonType={ButtonTypesConstants.buttonPrimary} />
+        </Box>
       </TextArea>
     )
   }
@@ -187,20 +253,22 @@ const AddressValidation: FC<AddressValidationProps> = ({ addressEntered, address
       onPress: onUseThisAddress,
     }
 
-    const editAddressButtonProps = {
-      label: t('editAddress.validation.editAddress'),
-      testID: t('editAddress.validation.editAddress.a11yLabel'),
-      a11yHint: t('editAddress.validation.editAddress.a11yHint'),
-      onPress: onEditAddress,
+    const cancelButtonProps = {
+      testID: t('editAddress.validation.cancel'),
+      a11yHint: t('editAddress.validation.cancel.a11yHint'),
+      label: t('editAddress.validation.cancel'),
+      onPress: onCancel,
     }
 
     return (
       <Box>
-        <Box mb={condensedMarginBetween}>
-          <VAButton {...useThisAddressButtonProps} buttonType={ButtonTypesConstants.buttonPrimary} />
-        </Box>
+        {!showSuggestions && (
+          <Box mb={condensedMarginBetween}>
+            <VAButton {...useThisAddressButtonProps} buttonType={ButtonTypesConstants.buttonPrimary} />
+          </Box>
+        )}
         <Box>
-          <VAButton {...editAddressButtonProps} buttonType={ButtonTypesConstants.buttonSecondary} />
+          <VAButton {...cancelButtonProps} buttonType={ButtonTypesConstants.buttonSecondary} />
         </Box>
       </Box>
     )
@@ -208,13 +276,18 @@ const AddressValidation: FC<AddressValidationProps> = ({ addressEntered, address
 
   return (
     <VAScrollView contentContainerStyle={scrollStyles}>
-      <Box flex={1}>
-        <Box mt={contentMarginTop}>
-          <AccordionCollapsible expandedContent={getAlert()} header={accordionHeader()} alertBorder={'warning'} testID={getAlertTitle()} />
-        </Box>
-        <Box mt={contentMarginTop}>{getSuggestedAddresses()}</Box>
+      <Box mt={contentMarginTop} mx={theme.dimensions.gutter}>
+        {getAlert()}
       </Box>
-      <Box {...containerStyles}>{getFooterButtons()}</Box>
+      <Box mt={contentMarginTop}>{getUserEnteredAddress()}</Box>
+      {showSuggestions && (
+        <Box mt={contentMarginTop} mb={condensedMarginBetween}>
+          {getSuggestedAddresses()}
+        </Box>
+      )}
+      <Box {...containerStyles} mt={standardMarginBetween} mb={contentMarginBottom}>
+        {getFooterButtons()}
+      </Box>
     </VAScrollView>
   )
 }
