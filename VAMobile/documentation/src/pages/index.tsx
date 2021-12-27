6825244f2fecb01c3dbe4480e874b9f74500777e
Narin Ratana <narin@users.noreply.github.com>
2021-12-27 07:57:52 -0800

    Merge in Jen's documentation styling, some additional content updates (#2206)
    * Tweaks to CSS, home page, and building out UX pages
    
    * Reorganize sidebar.  Make updates to component details.
    
    * mobile api docs added
    
    * Add dummy circle ci config to skip on gh-pages branch
    
    Co-authored-by: Jen Ecker <jen.ecker@adhocteam.us>
    Co-authored-by: Jayson Perkins <jaysonperkins51@gmail.com>

--- VAMobile/documentation/src/pages/index.tsx
+++ VAMobile/documentation/src/pages/index.tsx
@@ -11,12 +11,19 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link className="button button--secondary button--lg" to="/docs/intro">
-            Documentation
-          </Link>
+        <div className="row">
+          <div className={clsx('col col--6', styles.heroText)}>
+            <h1 className="hero__title">{siteConfig.title}</h1>
+            <p className="hero__subtitle">{siteConfig.tagline}</p>
+            <div className={styles.buttons}>
+              <Link className="button button--lg" to="/docs/intro">
+                Get started
+              </Link>
+            </div>
+          </div>
+          <div className={clsx('col col--6', styles.heroImage)}>
+            <img src="/img/va-mobile-iphone-ss.svg" alt="Screenshot of VA Health and Benefits app" />
+          </div>
         </div>
       </div>
     </header>
