6dd94dc1e8209c8bbd7faf09acde924242d203b9
Patrick Saxton <patrick@urban-intellect.com>
2021-12-28 10:34:26 -0600

    Removing optional for list and maps (#1544)

--- VAMobile/src/store/types/secureMessaging.ts
+++ VAMobile/src/store/types/secureMessaging.ts
@@ -117,19 +117,6 @@ export type SecureMessagingFinishGetRecipients = {
 }
 
 /**
- * Redux payload for the SECURE_MESSAGING_START_GET_SIGNATURE action
- */
-export type SecureMessagingStartGetSignature = Record<string, unknown>
-
-/**
- * Redux payload for the SECURE_MESSAGING_FINISH_GET_SIGNATURE action
- */
-export type SecureMessagingFinishGetSignature = {
-  signature?: api.SecureMessagingSignatureDataAttributes
-  error?: api.APIError
-}
-
-/**
  * Redux payload for the SECURE_MESSAGING_START_SAVE_DRAFT action
  */
 export type SecureMessagingStartSaveDraft = Record<string, unknown>
@@ -188,19 +175,6 @@ export type SecureMessagingResetReplyTriageError = Record<string, unknown>
  * Redux payload for the SECURE_MESSAGING_RESET_LOADING_RECIPIENTS_COMPLETED action
  */
 export type SecureMessagingResetHasLoadedRecipients = Record<string, unknown>
-
-/**
- * Redux payload for the SECURE_MESSAGING_START_MOVE_MESSAGE action
- */
-export type SecureMessagingStartMoveMessage = { isUndo?: boolean }
-
-/**
- * Redux payload for the SECURE_MESSAGING_FINISH_MOVE_MESSAGE action
- */
-export type SecureMessagingFinishMoveMessage = {
-  isUndo?: boolean
-  error?: api.APIError
-}
 
 /**
  *  All secure messaging actions
@@ -262,8 +236,4 @@ export interface SecureMessagingActions {
   SECURE_MESSAGING_CLEAR_LOADED_MESSAGES: ActionDef<'SECURE_MESSAGING_CLEAR_LOADED_MESSAGES', SecureMessagingClearLoadedMessagesPayload>
   SECURE_MESSAGING_RESET_REPLY_TRIAGE_ERROR: ActionDef<'SECURE_MESSAGING_RESET_REPLY_TRIAGE_ERROR', SecureMessagingResetReplyTriageError>
   SECURE_MESSAGING_RESET_HAS_LOADED_RECIPIENTS: ActionDef<'SECURE_MESSAGING_RESET_HAS_LOADED_RECIPIENTS', SecureMessagingResetHasLoadedRecipients>
-  SECURE_MESSAGING_FINISH_GET_SIGNATURE: ActionDef<'SECURE_MESSAGING_FINISH_GET_SIGNATURE', SecureMessagingFinishGetSignature>
-  SECURE_MESSAGING_START_GET_SIGNATURE: ActionDef<'SECURE_MESSAGING_START_GET_SIGNATURE', SecureMessagingStartGetSignature>
-  SECURE_MESSAGING_START_MOVE_MESSAGE: ActionDef<'SECURE_MESSAGING_START_MOVE_MESSAGE', SecureMessagingStartMoveMessage>
-  SECURE_MESSAGING_FINISH_MOVE_MESSAGE: ActionDef<'SECURE_MESSAGING_FINISH_MOVE_MESSAGE', SecureMessagingFinishMoveMessage>
 }
