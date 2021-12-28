84f587047830f5ea68ec50b120223e67098fe23c
Patrick Saxton <patrick@urban-intellect.com>
2021-12-28 12:30:16 -0600

    Feature/1981 persistent snackbar  (#2094)

--- VAMobile/src/components/SnackBar.tsx
+++ VAMobile/src/components/SnackBar.tsx
@@ -1,12 +1,10 @@
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { StyleProp, StyleSheet, TouchableOpacity, View, ViewStyle } from 'react-native'
+import { StyleProp, TouchableOpacity, ViewStyle } from 'react-native'
 import { ToastProps } from 'react-native-toast-notifications/lib/typescript/toast'
-import { useFocusEffect } from '@react-navigation/native'
 import React, { FC } from 'react'
 
 import { Box } from 'components'
 import { BoxProps } from './Box'
-import { useAccessibilityFocus, useTheme } from 'utils/hooks'
+import { useTheme } from 'utils/hooks'
 import TextView from './TextView'
 import VAIcon from './VAIcon'
 import colors from '../styles/themes/VAColors'
@@ -18,42 +16,25 @@ const SnackBar: FC<ToastProps> = (toast) => {
   const { message, data } = toast
   const { onConfirmAction, isError, actionBtnText, isUndo } = data || {}
   const { dimensions } = useTheme()
-  const [focusRef, setFocus] = useAccessibilityFocus<View>()
 
-  useFocusEffect(setFocus)
-
-  const styles = StyleSheet.create({
-    safeView: {
-      flex: 1,
-      marginBottom: dimensions.snackBarMarginBottom,
-      marginLeft: dimensions.snackBarMarginLeft,
-      marginRight: dimensions.snackBarMarginRight,
-    },
-  })
-
-  const confirmBtnStlye: StyleProp<ViewStyle> = {
+  const btnStlye: StyleProp<ViewStyle> = {
     marginLeft: dimensions.snackBarBetweenSpace,
+    height: dimensions.snackBarButtonHeight,
     justifyContent: 'center',
     alignContent: 'center',
-    marginTop: dimensions.snackBarButtonTopMargin,
-    marginRight: dimensions.snackBarConfirmBtnMarginRight,
-  }
-
-  const dismissBtnStlye: StyleProp<ViewStyle> = {
-    marginLeft: dimensions.snackBarBetweenSpace,
-    justifyContent: 'center',
-    alignContent: 'center',
-    marginTop: dimensions.snackBarButtonTopMargin,
+    width: dimensions.snackBarBtuttonWidth,
   }
 
   const mainContainerProps: BoxProps = {
-    minWidth: '100%',
-    p: dimensions.snackBarPadding,
+    width: '90%',
+    px: dimensions.snackBarHorizontalPadding,
     backgroundColor: 'snackbar',
+    my: dimensions.snackBarVerticalMarging,
     borderRadius: dimensions.snackBarBorderRadius,
     flexDirection: 'row',
     flexWrap: 'wrap',
     display: 'flex',
+    py: dimensions.snackBarVerticalPadding,
     style: {
       shadowColor: colors.black,
       shadowOffset: { width: dimensions.snackBarShadowX, height: dimensions.snackBarShadowY },
@@ -62,10 +43,11 @@ const SnackBar: FC<ToastProps> = (toast) => {
   }
 
   const messageContainerProps: BoxProps = {
+    height: dimensions.snackBarButtonHeight,
     flexDirection: 'row',
-    flexWrap: 'wrap',
-    mt: dimensions.snackBarButtonTopMargin,
     alignItems: 'center',
+    flexGrow: 1,
+    mr: dimensions.snackBarBetweenSpace,
   }
 
   const btnContainerProps: BoxProps = {
@@ -82,39 +64,30 @@ const SnackBar: FC<ToastProps> = (toast) => {
   }
 
   return (
-    <SafeAreaView edges={['left', 'right']} style={styles.safeView}>
-      <Box {...mainContainerProps}>
-        <View accessible={true} accessibilityRole={'alert'} ref={focusRef}>
-          <Box {...messageContainerProps}>
-            <Box mr={dimensions.snackBarBetweenSpace} alignSelf="flex-start" mt={dimensions.snackBarIconTopMargin}>
-              <VAIcon
-                name={isError ? 'ExclamationTriangleSolid' : 'CircleCheckMark'}
-                fill={colors.white}
-                height={dimensions.snackBarIconSize}
-                width={dimensions.snackBarIconSize}
-              />
-            </Box>
-            <TextView variant={'HelperText'} color={'primaryContrast'}>
-              {message}
-            </TextView>
-          </Box>
-        </View>
-        <Box {...btnContainerProps}>
-          {!isUndo && (
-            <TouchableOpacity onPress={onActionPress} style={confirmBtnStlye} accessible={true} accessibilityRole={'button'}>
-              <TextView variant={'SnackBarBtnText'} color={'snackBarBtn'} display={'flex'}>
-                {actionBtnText || isError ? 'Retry' : 'Undo'}
-              </TextView>
-            </TouchableOpacity>
-          )}
-          <TouchableOpacity onPress={() => toast.onHide()} style={dismissBtnStlye} accessible={true} accessibilityRole={'button'}>
-            <TextView variant={'SnackBarBtnText'} color={'snackBarBtn'}>
-              {'Dismiss'}
+    <Box {...mainContainerProps}>
+      <Box {...messageContainerProps}>
+        <Box mr={dimensions.snackBarBetweenSpace}>
+          <VAIcon name={isError ? 'ExclamationTriangleSolid' : 'CircleCheckMark'} fill={colors.white} height={dimensions.snackBarIconSize} width={dimensions.snackBarIconSize} />
+        </Box>
+        <TextView variant={'HelperText'} color={'primaryContrast'}>
+          {message}
+        </TextView>
+      </Box>
+      <Box {...btnContainerProps}>
+        {!isUndo && (
+          <TouchableOpacity onPress={onActionPress} style={btnStlye}>
+            <TextView variant={'SnackBarBtnText'} color={'snackBarBtn'} display={'flex'}>
+              {actionBtnText || isError ? 'Retry' : 'Undo'}
             </TextView>
           </TouchableOpacity>
-        </Box>
+        )}
+        <TouchableOpacity onPress={() => toast.onHide()} style={btnStlye}>
+          <TextView variant={'SnackBarBtnText'} color={'snackBarBtn'}>
+            {'Dismiss'}
+          </TextView>
+        </TouchableOpacity>
       </Box>
-    </SafeAreaView>
+    </Box>
   )
 }
 
