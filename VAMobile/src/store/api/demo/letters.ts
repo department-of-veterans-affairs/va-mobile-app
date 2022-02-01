89d1377d27d582288bf770fbea74125184298fbd
Teesh <teesh.s93@gmail.com>
2022-02-01 13:55:53 -0600

    Move demo mode letter consts

--- VAMobile/src/store/api/demo/letters.ts
+++ VAMobile/src/store/api/demo/letters.ts
@@ -12,3 +12,9 @@ export type LettersDemoStore = {
  * Type to define the mock returns to keep type safety
  */
 export type LettersDemoApiReturnTypes = LettersData | LetterBeneficiaryDataPayload
+
+/**
+ * Consts to use for demo letter pdf
+ */
+export const DEMO_MODE_LETTER_ENDPOINT = 'https://department-of-veterans-affairs.github.io/va-mobile-app/demo_mode_benefit_summary.pdf'
+export const DEMO_MODE_LETTER_NAME = 'demo_mode_benefit_summary.pdf'
