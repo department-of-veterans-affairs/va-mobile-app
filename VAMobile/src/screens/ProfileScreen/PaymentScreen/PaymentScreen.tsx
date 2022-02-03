ada59a0cfd7ad871ccd83f3163491444ee36bf7e
Teesh <teesh.s93@gmail.com>
2022-02-03 12:24:59 -0600

    Add payments error state

--- VAMobile/src/screens/ProfileScreen/PaymentScreen/PaymentScreen.tsx
+++ VAMobile/src/screens/ProfileScreen/PaymentScreen/PaymentScreen.tsx
@@ -1,13 +1,20 @@
 import { StackScreenProps } from '@react-navigation/stack'
 import React, { FC } from 'react'
 
+import { ErrorComponent } from 'components'
 import { ProfileStackParamList } from '../ProfileStackScreens'
+import { ScreenIDTypesConstants } from 'store/api/types/Screens'
+import { useError } from 'utils/hooks'
 import NoPaymentsScreen from './NoPayments/NoPaymentsScreen'
 
 type PaymentScreenProps = StackScreenProps<ProfileStackParamList, 'Payments'>
 
 const PaymentScreen: FC<PaymentScreenProps> = () => {
   const payments = false
+
+  if (useError(ScreenIDTypesConstants.PAYMENTS_SCREEN_ID)) {
+    return <ErrorComponent screenID={ScreenIDTypesConstants.PAYMENTS_SCREEN_ID} />
+  }
 
   if (!payments) {
     return <NoPaymentsScreen />
