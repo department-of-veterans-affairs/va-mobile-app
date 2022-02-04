5aa7f62b96faec08d11a3faed1f1a28e6e6a0af6
Teesh <teesh.s93@gmail.com>
2022-02-04 13:14:14 -0600

    2391 Add PaymentMissing page (#2482)
    * Add PaymentMissing component
    
    * fixing conflicts and connecting the missing payment screen
    
    Co-authored-by: Rafael Arroyo <rafael.arroyo@adhocteam.us>

---
+++ VAMobile/src/screens/ProfileScreen/PaymentScreen/PaymentMissingSceen/PaymentMissingScreen.tsx
@@ -0,0 +1,45 @@
+import { StackScreenProps } from '@react-navigation/stack'
+import React, { FC, useEffect } from 'react'
+
+import { Box, ClickToCallPhoneNumber, TextArea, TextView, VAScrollView } from 'components'
+import { HiddenTitle } from 'styles/common'
+import { NAMESPACE } from 'constants/namespaces'
+import { ProfileStackParamList } from 'screens/ProfileScreen/ProfileStackScreens'
+import { testIdProps } from 'utils/accessibility'
+import { useTheme, useTranslation } from 'utils/hooks'
+
+type PaymentMissingScreenProps = StackScreenProps<ProfileStackParamList, 'PaymentMissing'>
+
+const PaymentMissing: FC<PaymentMissingScreenProps> = ({ navigation }) => {
+  const t = useTranslation(NAMESPACE.PROFILE)
+  const theme = useTheme()
+  const { contentMarginTop, noLettersPaddingY } = theme.dimensions
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerTitle: () => (
+        <HiddenTitle accessibilityLabel={t('payments.missingPayments.pageTitle')} accessibilityRole="header">
+          {t('payments.missingPayments.pageTitle')}
+        </HiddenTitle>
+      ),
+    })
+  })
+
+  return (
+    <VAScrollView {...testIdProps('payment-missing-page')}>
+      <Box mt={contentMarginTop}>
+        <TextArea>
+          <TextView variant="MobileBodyBold" color={'primaryTitle'} accessibilityRole="header">
+            {t('payments.ifIAmMissingPayemt')}
+          </TextView>
+          <TextView variant="MobileBody" py={noLettersPaddingY}>
+            {t('payments.missingPayments.body')}
+          </TextView>
+          <ClickToCallPhoneNumber phone={t('common:8008271000')} displayedText={t('common:8008271000.displayText')} />
+        </TextArea>
+      </Box>
+    </VAScrollView>
+  )
+}
+
+export default PaymentMissing
