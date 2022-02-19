40022abd8038e9f3f6aeca3b80a0ad6ce8af33f6
John Marchi <j_marchi@digitalfoundry.com>
2022-02-18 16:57:15 -0800

    removing sorting from appeals timeline

--- VAMobile/src/screens/ClaimsScreen/AppealDetailsScreen/AppealStatus/AppealTimeline/AppealTimeline.tsx
+++ VAMobile/src/screens/ClaimsScreen/AppealDetailsScreen/AppealStatus/AppealTimeline/AppealTimeline.tsx
@@ -1,10 +1,9 @@
-import React, { FC, useEffect } from 'react'
+import React, { FC } from 'react'
 
 import _ from 'underscore'
 
 import { AppealEventData } from 'store/api/types'
 import { Box } from 'components'
-import { sortByDate } from 'utils/common'
 import AppealPhase from './AppealPhase'
 
 type AppealTimelineProps = {
@@ -12,14 +11,6 @@ type AppealTimelineProps = {
 }
 
 const AppealTimeline: FC<AppealTimelineProps> = ({ events }) => {
-  useEffect(() => {
-    /** TODO: figure out the root cause of this issue. The sort is causing a crash because the events being passed in
-     * are read only.
-     */
-    const copiedEvents = [...events]
-    sortByDate(copiedEvents, 'data')
-  }, [events])
-
   return (
     <Box>
       {_.map(events, (event, index) => {
