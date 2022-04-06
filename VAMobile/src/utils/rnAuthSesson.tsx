5ab6245170598b3bf76259170ebca67965610e53
Patrick Saxton <patrick@urban-intellect.com>
2022-04-06 09:05:44 -0500

    bump to 1.4.0

--- VAMobile/src/utils/rnAuthSesson.tsx
+++ VAMobile/src/utils/rnAuthSesson.tsx
@@ -10,7 +10,7 @@ const customTabs = NativeModules.CustomTabsIntentModule
  * This function fires the native iOS log in system for OAuth and returns a promise with the callback url string to continue log in
  * @returns Promise<string> returns a promise with the callback url for log in with the OAuth exchange code query param
  */
-export const startAuthSession = async ( codeChallenge: string, stateParam: string): Promise<string> => {
+export const startAuthSession = async (codeChallenge: string, stateParam: string): Promise<string> => {
   if (isIOS()) {
     return await RnAuthSession.beginAuthSession(AUTH_ENDPOINT, AUTH_CLIENT_ID, AUTH_REDIRECT_URL, AUTH_SCOPES, codeChallenge, stateParam)
   } else {
