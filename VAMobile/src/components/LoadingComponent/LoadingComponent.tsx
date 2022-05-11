cb4d42bb53d9f143b11949afa277950310ded231
Rafael Arroyo <rafael.arroyo@adhocteam.us>
2022-05-11 15:21:55 -0400

    va spinner loading component

--- VAMobile/src/components/LoadingComponent/LoadingComponent.tsx
+++ VAMobile/src/components/LoadingComponent/LoadingComponent.tsx
@@ -1,7 +1,9 @@
-import { ActivityIndicator, ViewStyle } from 'react-native'
+import { ViewStyle } from 'react-native'
+import LottieView from 'lottie-react-native'
+import React, { FC } from 'react'
+
 import { Box, TextView, VAScrollView } from 'components'
 import { useTheme } from 'utils/hooks'
-import React, { FC } from 'react'
 
 export type LoadingComponentProps = {
   /**Text to be shown under the spinner */
@@ -17,11 +19,17 @@ const LoadingComponent: FC<LoadingComponentProps> = ({ text }) => {
     justifyContent: 'center',
   }
 
+  const spinnerStyle: ViewStyle = {
+    height: 53,
+    width: 53,
+    alignContent: 'center',
+  }
+
   return (
     <VAScrollView contentContainerStyle={scrollStyles}>
-      <Box justifyContent="center" mx={theme.dimensions.gutter} mt={theme.dimensions.contentMarginTop} mb={theme.dimensions.contentMarginBottom}>
-        <ActivityIndicator size="large" color={theme.colors.icon.spinner} />
-        <Box mt={theme.dimensions.standardMarginBetween}>
+      <Box justifyContent="center" mx={theme.dimensions.gutter} mt={theme.dimensions.contentMarginTop} mb={theme.dimensions.contentMarginBottom} alignItems={'center'}>
+        <LottieView source={require('./va-spinner.json')} autoPlay loop style={spinnerStyle} speed={0.8} />
+        <Box mt={theme.dimensions.condensedMarginBetween}>
           <TextView textAlign={'center'} variant="MobileBody">
             {text}
           </TextView>
