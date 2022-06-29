3eac83f80773416ca2c0069ba4011a6d7927904b
John Marchi <j_marchi@digitalfoundry.com>
2022-06-29 10:55:15 -0700

    Adding radio picker modal for prescription history sort and filter

---
+++ VAMobile/src/components/RadioGroupModal.tsx
@@ -0,0 +1,145 @@
+import { Modal, Pressable, PressableProps, View } from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { useTranslation } from 'react-i18next'
+import React, { FC, useState } from 'react'
+
+import { Box, BoxProps, FooterButton, RadioGroup, TextView, TextViewProps, VAScrollView, radioOption } from 'components'
+import { NAMESPACE } from 'constants/namespaces'
+import { useTheme } from 'utils/hooks'
+
+export type RadioPickerGroup = {
+  /** Optional title appearing over the radio grouping, used if there are multiple groups in one modal */
+  title?: string
+  /** Radio options to display */
+  items: Array<radioOption<string>>
+  /** Callback when the option is set */
+  onSetOption: (toSet: string) => void
+  /** Current value of the group */
+  selectedValue?: string
+}
+
+export type RadioGroupModalProps = {
+  /** Array of groups to display in the modal */
+  groups: Array<RadioPickerGroup>
+  /** Callback for when the input is confirmed */
+  onConfirm: () => void
+  /** Callback when the reset button is pressed */
+  onReset: () => void
+  /** Callback for cancelling the interaction */
+  onCancel: () => void
+  /** Text to appear on the button that launches the modal */
+  buttonText: string
+  /** Header text within the modal */
+  headerText: string
+}
+
+const RadioGroupModal: FC<RadioGroupModalProps> = ({ groups, buttonText, headerText, onConfirm, onReset, onCancel }) => {
+  const [modalVisible, setModalVisible] = useState(false)
+  const theme = useTheme()
+  const { t: tc } = useTranslation(NAMESPACE.COMMON)
+  const insets = useSafeAreaInsets()
+
+  const showModal = (): void => {
+    setModalVisible(true)
+  }
+
+  const onCancelPressed = (): void => {
+    setModalVisible(false)
+    onCancel()
+  }
+
+  const onApply = (): void => {
+    setModalVisible(false)
+    onConfirm()
+  }
+
+  const onResetPressed = (): void => {
+    onReset()
+  }
+
+  const getGroups = () =>
+    groups.map((group) => {
+      return (
+        <Box>
+          <RadioGroup options={group.items} onChange={group.onSetOption} isRadioList={true} radioListTitle={group.title} value={group.selectedValue} />
+        </Box>
+      )
+    })
+
+  const actionsBarBoxProps: BoxProps = {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: 'list',
+    minHeight: theme.dimensions.touchableMinHeight,
+    borderBottomColor: 'menuDivider',
+    borderBottomWidth: 1,
+    py: theme.dimensions.buttonPadding,
+    px: theme.dimensions.gutter,
+    ml: insets.left,
+    mr: insets.right,
+  }
+
+  const commonButtonProps: TextViewProps = {
+    variant: 'HelperText',
+    color: 'showAll',
+    allowFontScaling: false,
+    py: 3,
+  }
+
+  const cancelButtonProps: PressableProps = {
+    accessible: true,
+    accessibilityRole: 'button',
+    accessibilityHint: tc('cancel.picker.a11yHint'),
+  }
+
+  const resetButtonProps: PressableProps = {
+    accessible: true,
+    accessibilityRole: 'button',
+    accessibilityHint: tc('done.picker.a11yHint'),
+  }
+
+  const pressableProps: PressableProps = {
+    onPress: showModal,
+    accessibilityRole: 'button',
+    accessible: true,
+  }
+
+  return (
+    <View>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        supportedOrientations={['portrait', 'landscape']}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible)
+        }}>
+        <Box flex={1} flexDirection="column" accessibilityViewIsModal={true}>
+          <Box pt={insets.top} />
+          <Box backgroundColor="list" pb={insets.bottom} flex={1}>
+            <Box {...actionsBarBoxProps}>
+              <Pressable onPress={onCancelPressed} {...cancelButtonProps}>
+                <TextView {...commonButtonProps}>{tc('cancel')}</TextView>
+              </Pressable>
+              <Box flex={4}>
+                <TextView variant="MobileBodyBold" textAlign={'center'} allowFontScaling={false}>
+                  {headerText}
+                </TextView>
+              </Box>
+              <Pressable onPress={onResetPressed} {...resetButtonProps}>
+                <TextView {...commonButtonProps}>{tc('reset')}</TextView>
+              </Pressable>
+            </Box>
+            <VAScrollView bounces={false}>{getGroups()}</VAScrollView>
+            <FooterButton text={tc('apply')} backGroundColor="buttonPrimary" textColor={'navBar'} onPress={onApply} />
+          </Box>
+        </Box>
+      </Modal>
+      <Pressable {...pressableProps}>
+        <TextView>{buttonText}</TextView>
+      </Pressable>
+    </View>
+  )
+}
+
+export default RadioGroupModal
--- VAMobile/src/screens/HealthScreen/Pharmacy/PrescriptionHistory/PrescriptionHistory.tsx
+++ VAMobile/src/screens/HealthScreen/Pharmacy/PrescriptionHistory/PrescriptionHistory.tsx
@@ -1,4 +1,5 @@
-import { ReactNode, useCallback, useEffect } from 'react'
+import { ReactNode, useCallback, useEffect, useState } from 'react'
+import { find } from 'underscore'
 import { useSelector } from 'react-redux'
 import { useTranslation } from 'react-i18next'
 import React, { FC } from 'react'
@@ -7,10 +8,76 @@ import { Box, ErrorComponent, List, ListItemObj, LoadingComponent, Pagination, P
 import { NAMESPACE } from 'constants/namespaces'
 import { PrescriptionListItem } from '../PrescriptionCommon'
 import { PrescriptionState, getPrescriptions } from 'store/slices/prescriptionSlice'
+import { RefillStatusConstants } from 'store/api/types'
 import { RootState } from 'store'
 import { ScreenIDTypesConstants } from 'store/api/types/Screens'
 import { StyleProp, ViewStyle } from 'react-native'
+import { getTranslation } from 'utils/formattingUtils'
 import { useAppDispatch, useError, useRouteNavigation, useTheme } from 'utils/hooks'
+import RadioGroupModal, { RadioGroupModalProps } from 'components/RadioGroupModal'
+
+const sortByOptions = [
+  { display: 'prescriptions.sort.facility', value: 'facilityName' },
+  { display: 'prescriptions.sort.fillDate', value: 'refillDate' },
+  { display: 'prescriptions.sort.medication', value: 'prescriptionName' },
+  { display: 'prescriptions.sort.refills', value: 'refillRemaining' },
+]
+
+const sortOrderOptions = [
+  { display: 'prescriptions.sort.atoz', value: 'asc' },
+  { display: 'prescriptions.sort.ztoa', value: 'desc' },
+]
+
+const filterOptions = [
+  {
+    display: 'prescription.filter.all',
+    value: '',
+  },
+  {
+    display: 'prescription.history.tag.active',
+    value: RefillStatusConstants.ACTIVE,
+  },
+  {
+    display: 'prescription.history.tag.active.hold',
+    value: RefillStatusConstants.HOLD,
+  },
+  {
+    display: 'prescription.history.tag.active.parked',
+    value: RefillStatusConstants.ACTIVE_PARKED,
+  },
+  {
+    display: 'prescription.history.tag.active.inProgress',
+    value: RefillStatusConstants.REFILL_IN_PROCESS,
+  },
+  {
+    display: 'prescription.history.tag.active.submitted',
+    value: RefillStatusConstants.SUBMITTED,
+  },
+  {
+    display: 'prescription.history.tag.active.suspended',
+    value: RefillStatusConstants.SUSPENDED,
+  },
+  {
+    display: 'prescription.history.tag.discontinued',
+    value: RefillStatusConstants.DISCONTINUED,
+  },
+  {
+    display: 'prescription.history.tag.expired',
+    value: RefillStatusConstants.EXPIRED,
+  },
+  {
+    display: 'prescription.history.tag.nonVerified',
+    value: RefillStatusConstants.NON_VERIFIED,
+  },
+  {
+    display: 'prescription.history.tag.transferred',
+    value: RefillStatusConstants.TRANSFERRED,
+  },
+  {
+    display: 'prescription.history.tag.unknown',
+    value: RefillStatusConstants.UNKNOWN,
+  },
+]
 
 const PrescriptionHistory: FC = ({}) => {
   const dispatch = useAppDispatch()
@@ -19,6 +86,14 @@ const PrescriptionHistory: FC = ({}) => {
   const theme = useTheme()
   const { t } = useTranslation(NAMESPACE.HEALTH)
   const navigateTo = useRouteNavigation()
+
+  const [selectedFilter, setSelectedFilter] = useState('')
+  const [selectedSortBy, setSelectedSortBy] = useState('')
+  const [selectedSortOn, setSelectedSortOn] = useState('')
+
+  const [filterToUse, setFilterToUse] = useState('')
+  const [sortByToUse, setSortByToUse] = useState('')
+  const [sortOnToUse, setSortOnToUse] = useState('')
 
   const requestPage = useCallback(
     (requestedPage: number) => {
@@ -71,9 +146,97 @@ const PrescriptionHistory: FC = ({}) => {
     return <Pagination {...paginationProps} />
   }
 
+  const getDisplayForValue = (options: { display: string; value: string }[], val: string) => {
+    const display = find(options, (f) => {
+      return f.value === val
+    })
+
+    return getTranslation(display?.display || '', t)
+  }
+
+  const sortByRadioOptions = sortByOptions.map((option) => {
+    return {
+      value: option.value,
+      labelKey: getTranslation(option.display, t),
+    }
+  })
+
+  const sortOrderRadioOptions = sortOrderOptions.map((option) => {
+    return {
+      value: option.value,
+      labelKey: getTranslation(option.display, t),
+    }
+  })
+
+  const sortProps: RadioGroupModalProps = {
+    groups: [
+      {
+        items: sortByRadioOptions,
+        onSetOption: (newSortBy: string) => {
+          setSelectedSortBy(newSortBy)
+        },
+        selectedValue: selectedSortBy,
+        title: t('prescriptions.sort.by'),
+      },
+      {
+        items: sortOrderRadioOptions,
+        onSetOption: (newSortOn: string) => {
+          setSelectedSortOn(newSortOn)
+        },
+        selectedValue: selectedSortOn,
+        title: t('prescriptions.sort.order'),
+      },
+    ],
+    buttonText: `${t('prescriptions.sort.by')}: ${getDisplayForValue(sortByOptions, sortByToUse)}`,
+    headerText: t('prescription.filter.sort'),
+    onConfirm: () => {
+      setSortOnToUse(selectedSortOn)
+      setSortByToUse(selectedSortBy)
+    },
+    onReset: () => {},
+    onCancel: () => {
+      setSelectedSortBy(sortByToUse)
+      setSelectedSortOn(sortOnToUse)
+    },
+  }
+
+  const filterRadioOptions = filterOptions.map((option) => {
+    return {
+      value: option.value,
+      labelKey: getTranslation(option.display, t),
+    }
+  })
+
+  const filterButtonText = `${t('prescription.filter.by')}: ${getDisplayForValue(filterOptions, filterToUse)}`
+
+  const filterProps: RadioGroupModalProps = {
+    groups: [
+      {
+        items: filterRadioOptions,
+        onSetOption: (newFilter: string) => {
+          setSelectedFilter(newFilter)
+        },
+        selectedValue: selectedFilter,
+      },
+    ],
+    buttonText: filterButtonText,
+    headerText: t('prescription.filter.status'),
+    onConfirm: () => {
+      setFilterToUse(selectedFilter)
+    },
+    onReset: () => {},
+    onCancel: () => {
+      setSelectedFilter(filterToUse)
+    },
+  }
+
   return (
-    <Box mt={theme.dimensions.contentMarginTop} display={'flex'} flexDirection={'column'} flex={1}>
+    <Box pt={theme.dimensions.contentMarginTop} display={'flex'} flexDirection={'column'} flex={1} backgroundColor={'main'}>
       <Box mx={theme.dimensions.gutter}>
+        <Box>
+          <RadioGroupModal {...filterProps} />
+          <RadioGroupModal {...sortProps} />
+        </Box>
         <TextView variant={'HelperText'}>{t('prescriptions.header.helper')}</TextView>
         <TextView mt={theme.dimensions.standardMarginBetween} mb={theme.dimensions.condensedMarginBetween} variant={'MobileBodyBold'}>
           {t('prescription.history.list.title', { count: prescriptionPagination.totalEntries })}
