d2d2a92067a61525f904a9e43785cdae87082a05
Jon Bindbeutel <compjon@gmail.com>
2022-07-15 12:38:33 -0400

    First Attempt

--- VAMobile/src/components/FormWrapper/FormFields/formFieldUtils.tsx
+++ VAMobile/src/components/FormWrapper/FormFields/formFieldUtils.tsx
@@ -8,39 +8,23 @@ import { VATheme } from '../../../styles/theme'
 /**
  * Creates the label for the picker and text input components
  */
-const generateInputLabel = (
-  error: string | undefined,
-  disabled: boolean | undefined,
-  isRequiredField: boolean | undefined,
-  labelKey: string,
-  t: TFunction,
-  isHelperText: boolean,
-): ReactElement => {
-  // Only to 'MobileBodyBold' if there is an error; otherwise 'MobileBody
-  const variant = error ? 'MobileBodyBold' : 'MobileBody'
-  const color: ColorVariant | undefined = disabled ? 'placeholder' : undefined
+// const generateInputLabel = (
+//   error: string | undefined,
+//   isRequiredField: boolean | undefined,
+//   labelKey: string,
+//   t: TFunction,
+//   isHelperText: boolean,
+// ): ReactElement => {
+//   const variant = error ? 'MobileBodyBold' : 'MobileBody'
 
-  const labelProps: TextViewProps = {
-    color,
-    variant,
-  }
-
-  const label = <TextView {...labelProps}>{t(labelKey)}</TextView>
-
-  if (isRequiredField) {
-    return (
-      <Box display="flex" flexDirection="row" flexWrap="wrap" mb={isHelperText ? 0 : 8}>
-        {label}
-        <TextView>&nbsp;</TextView>
-        <TextView color={color} variant={variant}>
-          {t('common:required')}
-        </TextView>
-      </Box>
-    )
-  }
-
-  return label
-}
+//   return (
+//     <Box display="flex" flexDirection="row" flexWrap="wrap" mb={isHelperText ? 0 : 8}>
+//       <TextView color={color} variant={variant}>
+//         {t(labelKey)} {isRequiredField ? t('common:required') : ''}
+//       </TextView>
+//     </Box>
+//   )
+// }
 
 /**
  * Renders the label section consisting of the label, potential required text, and potential helper text
@@ -48,16 +32,20 @@ const generateInputLabel = (
  */
 export const renderInputLabelSection = (
   error: string | undefined,
-  disabled: boolean | undefined,
   isRequiredField: boolean | undefined,
   labelKey: string,
   t: TFunction,
   helperTextKey: string | undefined,
 ): ReactElement => {
   const isHelperText = !!helperTextKey
+  const variant = error ? 'MobileBodyBold' : 'MobileBody'
   return (
     <Box>
-      {generateInputLabel(error, disabled, isRequiredField, labelKey, t, isHelperText)}
+      <Box display="flex" flexDirection="row" flexWrap="wrap" mb={isHelperText ? 0 : 8}>
+        <TextView variant={variant}>
+          {t(labelKey)} {isRequiredField ? t('common:required') : ''}
+        </TextView>
+      </Box>
       {isHelperText && (
         <TextView mb={8} variant="HelperText">
           {t(helperTextKey)}
@@ -102,7 +90,7 @@ export const getInputWrapperProps = (theme: VATheme, error: string | undefined, 
 /**
  * Returns an error message for the given error for the picker and text input components
  */
-export const renderInputError = (theme: VATheme, error: string): ReactElement => {
+export const renderInputError = (error: string): ReactElement => {
   return (
     <TextView variant="MobileBodyBold" color="error" mb={3}>
       {error}
