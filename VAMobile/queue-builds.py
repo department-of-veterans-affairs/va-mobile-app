a9a8be9a1da69179b5604de148f9d953d59b79be
Patrick Saxton <patrick@urban-intellect.com>
2022-07-21 09:00:21 -0500

    remove as user

--- VAMobile/queue-builds.py
+++ VAMobile/queue-builds.py
@@ -75,7 +75,6 @@ while True:
       headers={"Authorization": f"Bearer {os.getenv('SLACK_API_TOKEN')}", "Content-Type":"application/json"},
       json={"channel":"va-mobile-app-automation-test-channel",
         "text": f"A CircleCi build job exceeded queue time. Please see {os.getenv('CIRCLE_BUILD_URL')}",
-        "as_user": False,
         "icon_emoji": ":sad-robot:"
       }
     )
