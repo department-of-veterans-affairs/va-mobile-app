f355f97a1bb2e5d6ffe7e659caad03beadd1d32c
TKDickson <94404065+TKDickson@users.noreply.github.com>
2022-08-02 11:02:58 -0500

    Update ProfileScreen.e2e.ts
    * made it to profile (woo!), updated some matchers to pass

--- VAMobile/e2e/tests/ProfileScreen.e2e.ts
+++ VAMobile/e2e/tests/ProfileScreen.e2e.ts
@@ -1,22 +1,24 @@
 import { expect, device, by, element, waitFor } from 'detox'
 import { loginToDemoMode, openProfile } from './utils'
-import { HomeE2eIdConstants } from './HomeScreen.e2e'
 
 export const ProfileE2eIdConstants = {
   PROFILE_PAGE_ID: 'Profile-page',
   DISABILITY_RATING_ROW_TEXT: 'Disability rating',
   PERSONAL_CONTACT_INFO_ROW_TEXT: 'Personal and contact information',
-  MILITARY_INFO_ROW_TEXT: 'Military Information',
-  DIRECT_DEPOSIT_ROW_TEXT: 'Direct Deposit',
+  MILITARY_INFO_ROW_TEXT: 'Military information',
+  DIRECT_DEPOSIT_ROW_TEXT: 'Direct deposit information',
   LETTERS_ROW_LABEL: 'V-A letters and documents',
-  SETTINGS_ROW_TEXT: 'Settings'
-
-
-  // sign out button
-  // Profile banner
+  SETTINGS_ROW_TEXT: 'Settings',
+  PAYMENTS_ROW_TEXT: 'Payments',
+  SIGN_OUT_BTN_ID: 'Sign out',
+  BANNER_BRANCH_ICON_ID: 'Coast-Guard-seal', // all banner elements specific to demo mode user
+  BANNER_NAME_TEXT: 'Kimberly Washington',
+  BANNER_BRANCH_ID: 'Coast-Guard',
+  BANNER_DISABILITY_ID: 'Your disability rating: ',
+  BANNER_COMBINED_PCT_ID: '100% service connected'
 }
 
-/** Sadly, all of this is busted because of tab navigation 
+
 
 beforeAll(async () => {
   await loginToDemoMode()
@@ -31,20 +33,25 @@ describe('Profile Screen', () => {
 
     await expect(element(by.text(ProfileE2eIdConstants.DISABILITY_RATING_ROW_TEXT))).toExist()
     await expect(element(by.text(ProfileE2eIdConstants.PERSONAL_CONTACT_INFO_ROW_TEXT))).toExist()
-    await expect(element(by.text(ProfileE2eIdConstants.MILITARY_INFO_ROW_TEXT))).toExist()
+    await expect(element(by.text(ProfileE2eIdConstants.MILITARY_INFO_ROW_TEXT))).toExist() 
     await expect(element(by.text(ProfileE2eIdConstants.DIRECT_DEPOSIT_ROW_TEXT))).toExist()
-    await expect(element(by.text(ProfileE2eIdConstants.LETTERS_ROW_LABEL))).toExist()
-    await expect(element(by.text(HomeE2eIdConstants.PAYMENTS_BTN_ID))).toExist() // homescreen button and profile row text use the same string
+    await expect(element(by.label(ProfileE2eIdConstants.LETTERS_ROW_LABEL))).toExist()
+    await expect(element(by.text(ProfileE2eIdConstants.PAYMENTS_ROW_TEXT))).toExist() 
     await expect(element(by.text(ProfileE2eIdConstants.SETTINGS_ROW_TEXT))).toExist()
   })
 
-  // Haven't mapped out the profile banner elements yet
-  it('should show profile banner elements', async () => {
-
-    await expect(element(by.id(ProfileE2eIdConstants.HOME_HEADER_ID))).toExist()
-
+  it('should show sign out button', async () => {
+    await expect(element(by.id(ProfileE2eIdConstants.SIGN_OUT_BTN_ID))).toExist()
   })
+
+  it('should show profile banner elements', async () => {
+    // await expect(element(by.id(ProfileE2eIdConstants.BANNER_BRANCH_ICON_ID))).toExist()  having trouble matching to the icon
+    await expect(element(by.text(ProfileE2eIdConstants.BANNER_NAME_TEXT))).toExist()
+    await expect(element(by.id(ProfileE2eIdConstants.BANNER_BRANCH_ID))).toExist()
+    await expect(element(by.id(ProfileE2eIdConstants.BANNER_DISABILITY_ID))).toExist()
+    await expect(element(by.id(ProfileE2eIdConstants.BANNER_COMBINED_PCT_ID))).toExist()
+  })
+  
 })
 
 
-*/
