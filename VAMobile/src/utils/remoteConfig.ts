9eb51888a6b1755f976189b7810c2fbb130a5f50
Narin Ratana <narin@adhocteam.us>
2022-08-08 10:08:54 -0700

    Override remote config for dev and staging

--- VAMobile/src/utils/remoteConfig.ts
+++ VAMobile/src/utils/remoteConfig.ts
@@ -12,33 +12,30 @@ export type RemoteConfigValues = {
   testFeature: boolean
 }
 
-export const FeatureFlipperConstants: {
-  testFeature: string
-} = {
-  testFeature: 'testFeature',
-}
-
-const defaults: RemoteConfigValues = {
-  testFeature: false,
-}
-
 const devDefaults: RemoteConfigValues = {
   testFeature: true,
 }
 
-export const activateRemoteConfig = async (): Promise<void> => {
-  console.debug('Remote Config: Setting defaults')
-  await remoteConfig().setDefaults(isProduction ? defaults : devDefaults)
+const productionDefaults: RemoteConfigValues = {
+  testFeature: false,
+}
 
+export const activateRemoteConfig = async (): Promise<void> => {
   /**
    * If in production, fetch and activate remote settings.  Otherwise,
    * we'll use the devDefaults for dev and staging defined above.
    */
   if (isProduction) {
+    console.debug('Remote Config: Settigns defaults')
+    // Sets defaults for remote config for use prior to fetching and activating
+    await remoteConfig().setDefaults(productionDefaults)
+
     console.debug('Remote Config: Fetching and activating')
-    // Fetch config and activate. Default cache is 12 hours
+    // Fetch config and activate. Default cache is 12 hours.
     await remoteConfig().fetchAndActivate()
   }
 }
 
-export const featureEnabled = (feature: FeatureFlipperType): boolean => remoteConfig().getValue(feature)?.asBoolean()
+export const featureEnabled = (feature: FeatureFlipperType): boolean => {
+  return isProduction ? remoteConfig().getValue(feature)?.asBoolean() : devDefaults[feature]
+}
