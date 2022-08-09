d45d9cb232050cf4539608a70b1b715769171bc7
Narin Ratana <narin@adhocteam.us>
2022-08-08 22:33:55 -0700

    Add try catch

--- VAMobile/src/utils/remoteConfig.ts
+++ VAMobile/src/utils/remoteConfig.ts
@@ -1,4 +1,5 @@
 import { EnvironmentTypesConstants } from 'constants/common'
+import { logNonFatalErrorToFirebase } from './analytics'
 import getEnv from 'utils/env'
 import remoteConfig from '@react-native-firebase/remote-config'
 
@@ -6,7 +7,7 @@ const { ENVIRONMENT } = getEnv()
 
 const isProduction = ENVIRONMENT === EnvironmentTypesConstants.Production
 
-type FeatureFlipperType = 'testFeature'
+type FeatureToggleType = 'testFeature'
 
 export type RemoteConfigValues = {
   testFeature: boolean
@@ -25,17 +26,24 @@ export const activateRemoteConfig = async (): Promise<void> => {
    * If in production, fetch and activate remote settings.  Otherwise,
    * we'll use the devDefaults for dev and staging defined above.
    */
-  if (isProduction) {
-    console.debug('Remote Config: Settigns defaults')
-    // Sets defaults for remote config for use prior to fetching and activating
-    await remoteConfig().setDefaults(productionDefaults)
+  try {
+    if (isProduction) {
+      console.debug('Remote Config: Settigns defaults')
+      // Sets defaults for remote config for use prior to fetching and activating
+      await remoteConfig().setDefaults(productionDefaults)
 
-    console.debug('Remote Config: Fetching and activating')
-    // Fetch config and activate. Default cache is 12 hours.
-    await remoteConfig().fetchAndActivate()
+      console.debug('Remote Config: Fetching and activating')
+      // Fetch config and activate. Default cache is 12 hours.
+      await remoteConfig().fetchAndActivate()
+    }
+  } catch (err) {
+    logNonFatalErrorToFirebase(err, 'activateRemoteConfig: Firebase Remote Config Error')
+    console.debug('activateRemoteConfig: Failed to activate remote config')
+    console.error(err)
+    return undefined
   }
 }
 
-export const featureEnabled = (feature: FeatureFlipperType): boolean => {
+export const featureEnabled = (feature: FeatureToggleType): boolean => {
   return isProduction ? remoteConfig().getValue(feature)?.asBoolean() : devDefaults[feature]
 }
