e3d5e2604a6261b0a622cde7b0d16b2470a7391f
John Marchi <j_marchi@digitalfoundry.com>
2022-09-14 14:42:18 -0700

    updating appointment tag colors (#3794)
    Co-authored-by: Patrick Saxton <patrick@urban-intellect.com>

--- VAMobile/src/utils/appointments.tsx
+++ VAMobile/src/utils/appointments.tsx
@@ -205,11 +205,11 @@ export const getTextLinesForAppointmentListItem = (appointment: AppointmentData,
   const isPendingAppointment = attributes.isPending && (attributes.status === AppointmentStatusConstants.SUBMITTED || attributes.status === AppointmentStatusConstants.CANCELLED)
 
   if (attributes.status === AppointmentStatusConstants.CANCELLED) {
-    textLines.push({ text: t('appointments.canceled'), textTag: { backgroundColor: 'inactiveTag', variant: 'LabelTagBold' }, mb: condensedMarginBetween })
+    textLines.push({ text: t('appointments.canceled'), textTag: { backgroundColor: 'inactiveTag', variant: 'LabelTagBold', color: 'labelTag' }, mb: condensedMarginBetween })
   } else if (attributes.status === AppointmentStatusConstants.BOOKED) {
-    textLines.push({ text: t('appointments.confirmed'), textTag: { backgroundColor: 'activeTag', variant: 'LabelTagBold' }, mb: condensedMarginBetween })
+    textLines.push({ text: t('appointments.confirmed'), textTag: { backgroundColor: 'activeTag', variant: 'LabelTagBold', color: 'labelTag' }, mb: condensedMarginBetween })
   } else if (isPendingAppointment) {
-    textLines.push({ text: t('appointments.pending'), textTag: { backgroundColor: 'warningTag', color: 'warningTag', variant: 'LabelTagBold' }, mb: condensedMarginBetween })
+    textLines.push({ text: t('appointments.pending'), textTag: { backgroundColor: 'warningTag', color: 'labelTag', variant: 'LabelTagBold' }, mb: condensedMarginBetween })
   }
 
   // pending appointments
