073da56b881a4921c514bee6044571409ba7bd44
k_li <k_li@digitalfoundry.com>
2022-10-20 14:02:40 -0700

    Multiple tracking numbers per prescription (#4036)

--- VAMobile/src/screens/HealthScreen/Pharmacy/RefillTrackingDetails/RefillTrackingDetails.test.tsx
+++ VAMobile/src/screens/HealthScreen/Pharmacy/RefillTrackingDetails/RefillTrackingDetails.test.tsx
@@ -13,6 +13,7 @@ import {
   defaultPrescriptionsList as mockData,
   emptyStatePrescriptionList as emptyMockData,
   emptyStateTrackingInfoList as emptyTrackingMockData,
+  multipleTrackingInfoList as multipleTrackingInfoData,
 } from 'utils/tests/prescription'
 import { DateTime } from 'luxon'
 import { ScreenIDTypesConstants } from 'store/api/types'
@@ -50,7 +51,7 @@ context('RefillTrackingDetails', () => {
     it('should show None Noted for applicable properties', async () => {
       when(api.get as jest.Mock)
       .calledWith(`/v0/health/rx/prescriptions/20004342/tracking`)
-      .mockResolvedValue({ data: emptyTrackingMockData[0] })
+      .mockResolvedValue({ data: emptyTrackingMockData })
 
       await waitFor(() => {
         initializeTestInstance(undefined, emptyMockData[0] as PrescriptionData)
@@ -65,7 +66,7 @@ context('RefillTrackingDetails', () => {
       // Disclaimer
       expect(texts[2].props.children).toEqual('We share tracking information here for up to 15 days, even if you\'ve received your prescription.')
 
-      // Tracking information Card
+      // Tracking Information
       expect(texts[3].props.children).toEqual('Tracking number')
       expect(texts[4].props.children).toEqual('None noted')
       expect(texts[5].props.children).toEqual('Delivery service: None noted')
@@ -74,6 +75,86 @@ context('RefillTrackingDetails', () => {
       // Other prescriptions
       expect(texts[7].props.children).toEqual('Other prescriptions in this package:')
       expect(texts[8].props.children).toEqual('There are no other prescriptions in this package.')
+    })
+  })
+
+  describe('when there are one tracking for a prescription', () => {
+    it('should show tracking information', async  () => {
+      when(api.get as jest.Mock)
+      .calledWith(`/v0/health/rx/prescriptions/20004342/tracking`)
+      .mockResolvedValue({ data: [multipleTrackingInfoData[0]] })
+
+      await waitFor(() => {
+        initializeTestInstance(undefined, mockData[0] as PrescriptionData)
+      })
+
+      const texts = testInstance.findAllByType(TextView)
+
+      // Header
+      expect(texts[0].props.children).toEqual('ALLOPURINOL 100MG TAB')
+      expect(texts[1].props.children).toEqual('Rx #: 3636691')
+
+      // Disclaimer
+      expect(texts[2].props.children).toEqual('We share tracking information here for up to 15 days, even if you\'ve received your prescription.')
+
+      // Tracking Information
+      expect(texts[3].props.children).toEqual('Tracking number')
+      expect(texts[4].props.children).toEqual('7534533636856')
+      expect(texts[5].props.children).toEqual('Delivery service: DHL')
+      expect(texts[6].props.children).toEqual('Date shipped: 06/14/2022')
+      // Other Prescriptions
+      expect(texts[7].props.children).toEqual('Other prescriptions in this package:')
+      expect(texts[8].props.children).toEqual('LAMIVUDINE 10MG TAB')
+      expect(texts[9].props.children).toEqual('Rx #: 2336800')
+      expect(texts[10].props.children).toEqual('ZIDOVUDINE 1MG CAP')
+      expect(texts[11].props.children).toEqual('Rx #: None noted')
+    })
+  })
+
+  describe('when there are multiple tracking for a prescription', () => {
+    it('should show information for each tracking with "x of total" header ', async  () => {
+      when(api.get as jest.Mock)
+      .calledWith(`/v0/health/rx/prescriptions/20004342/tracking`)
+      .mockResolvedValue({ data: multipleTrackingInfoData })
+
+      await waitFor(() => {
+        initializeTestInstance(undefined, mockData[0] as PrescriptionData)
+      })
+
+      const texts = testInstance.findAllByType(TextView)
+
+      // Header
+      expect(texts[0].props.children).toEqual('ALLOPURINOL 100MG TAB')
+      expect(texts[1].props.children).toEqual('Rx #: 3636691')
+
+      // Disclaimer
+      expect(texts[2].props.children).toEqual('We share tracking information here for up to 15 days, even if you\'ve received your prescription.')
+
+      // Tracking Information #1
+      expect(texts[3].props.children).toEqual('Package 1 of 2')
+      expect(texts[4].props.children).toEqual('Tracking number')
+      expect(texts[5].props.children).toEqual('7534533636856')
+      expect(texts[6].props.children).toEqual('Delivery service: DHL')
+      expect(texts[7].props.children).toEqual('Date shipped: 06/14/2022')
+      // Other Prescriptions
+      expect(texts[8].props.children).toEqual('Other prescriptions in this package:')
+      expect(texts[9].props.children).toEqual('LAMIVUDINE 10MG TAB')
+      expect(texts[10].props.children).toEqual('Rx #: 2336800')
+      expect(texts[11].props.children).toEqual('ZIDOVUDINE 1MG CAP')
+      expect(texts[12].props.children).toEqual('Rx #: None noted')
+
+      // Tracking Information #2
+      expect(texts[13].props.children).toEqual('Package 2 of 2')
+      expect(texts[14].props.children).toEqual('Tracking number')
+      expect(texts[15].props.children).toEqual('5634533636812')
+      expect(texts[16].props.children).toEqual('Delivery service: USPS')
+      expect(texts[17].props.children).toEqual('Date shipped: 06/28/2022')
+      // Other Prescriptions
+      expect(texts[18].props.children).toEqual('Other prescriptions in this package:')
+      expect(texts[19].props.children).toEqual('AMLODIPINE BESYLATE 10MG TAB')
+      expect(texts[20].props.children).toEqual('Rx #: 3636711A')
+      expect(texts[21].props.children).toEqual('ZIDOVUDINE 1MG CAP')
+      expect(texts[22].props.children).toEqual('Rx #: 4636722C')
     })
   })
 
