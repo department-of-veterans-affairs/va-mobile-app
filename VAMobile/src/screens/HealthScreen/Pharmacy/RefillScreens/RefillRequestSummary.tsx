e020bfae5cc5ec32c4c93d6ef7f56738227214a3
Dylan Nienberg <dylannienberg@DylanNinbergMBP.attlocal.net>
2022-11-08 13:42:53 -0600

    Moving the text to separate text views.

--- VAMobile/src/screens/HealthScreen/Pharmacy/RefillScreens/RefillRequestSummary.tsx
+++ VAMobile/src/screens/HealthScreen/Pharmacy/RefillScreens/RefillRequestSummary.tsx
@@ -172,12 +172,14 @@ const RefillRequestSummary: FC<RefillRequestSummaryProps> = ({ navigation }) => 
     return (
       <Box {...borderProps}>
         <TextView variant="HelperTextBold">{t('prescriptions.refillRequestSummary.whatsNext')}</TextView>
-        <TextView variant="MobileBody" accessibilityLabel={t('prescriptions.refillRequestSummary.yourRefills.successPt1.a11y')}>
-          {t('prescriptions.refillRequestSummary.yourRefills.successPt1')}
-        </TextView>
-        <TextView variant="MobileBody" accessibilityLabel={t('prescriptions.refillRequestSummary.yourRefills.successPt2.a11y')} my={theme.dimensions.standardMarginBetween}>
-          {t('prescriptions.refillRequestSummary.yourRefills.successPt2')}
-        </TextView>
+        <Box mb={theme.dimensions.standardMarginBetween}>
+          <TextView variant="MobileBody" accessibilityLabel={t('prescriptions.refillRequestSummary.yourRefills.successPt1.a11y')}>
+            {t('prescriptions.refillRequestSummary.yourRefills.successPt1')}
+          </TextView>
+          <TextView variant="MobileBody" accessibilityLabel={t('prescriptions.refillRequestSummary.yourRefills.successPt2.a11y')} mt={theme.dimensions.standardMarginBetween}>
+            {t('prescriptions.refillRequestSummary.yourRefills.successPt2')}
+          </TextView>
+        </Box>
         <VAButton
           onPress={navigateTo('PrescriptionHistory', { startingTab: PrescriptionHistoryTabConstants.PENDING })}
           label={t('prescriptions.refillRequestSummary.pendingRefills')}
