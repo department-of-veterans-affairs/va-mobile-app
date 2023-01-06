5e5375d43a985cb4243248edcf770c12d55318cb
Theo Bentum <theo@adhocteam.us>
2023-01-06 12:32:41 -0800

    Fix ComposeMessage tests

--- VAMobile/src/screens/HealthScreen/SecureMessaging/ComposeMessage/ComposeMessage.test.tsx
+++ VAMobile/src/screens/HealthScreen/SecureMessaging/ComposeMessage/ComposeMessage.test.tsx
@@ -233,7 +233,7 @@ context('ComposeMessage', () => {
   describe('on click of the crisis line banner', () => {
     it('should call useRouteNavigation', async () => {
       await waitFor(() => {
-        testInstance.findByType(TouchableWithoutFeedback).props.onPress()
+        testInstance.findAllByType(TouchableWithoutFeedback)[1].props.onPress()
         expect(navigateToVeteransCrisisLineSpy).toHaveBeenCalled()
       })
     })
@@ -275,7 +275,7 @@ context('ComposeMessage', () => {
 
       const textViews = testInstance.findAllByType(TextView)
 
-      expect(textViews[9].props.children).toEqual(['Subject Line', ' ', '(Required)'])
+      expect(textViews[11].props.children).toEqual(['Subject Line', ' ', '(Required)'])
     })
   })
 
