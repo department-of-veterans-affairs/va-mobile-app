967eca522be7fe8565aa2ac9e0aa8505a526bfc2
Theo Bentum <theo@adhocteam.us>
2023-01-10 11:51:34 -0800

    Fix unit tests

--- VAMobile/src/screens/HealthScreen/SecureMessaging/ViewMessage/ViewMessageScreen.test.tsx
+++ VAMobile/src/screens/HealthScreen/SecureMessaging/ViewMessage/ViewMessageScreen.test.tsx
@@ -227,8 +227,8 @@ context('ViewMessageScreen', () => {
   describe('when first message and last message is clicked', () => {
     it('should close first accordion and expand last accordion', async () => {
       await waitFor(() => {
-        testInstance.findAllByType(Pressable)[1].props.onPress()
-        testInstance.findAllByType(Pressable)[3].props.onPress()
+        testInstance.findAllByType(Pressable)[2].props.onPress()
+        testInstance.findAllByType(Pressable)[4].props.onPress()
         expect(testInstance.findAllByType(TextView)[7].props.children).toBe('mock sender 2')
         // Used to display last message's contents, but now the textview after the date is the bottom Reply button's text
         expect(testInstance.findAllByType(TextView)[9].props.children).toBe('mock sender 1')
