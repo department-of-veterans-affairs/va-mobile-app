e91c30640b063f6fe1539b98c51e7ecaf868d586
Dylan Nienberg <dylannienberg@gmail.com>
2023-02-22 09:39:55 -0600

    copy updated

--- VAMobile/src/screens/HealthScreen/Pharmacy/RefillScreens/RefillRequestSummary.tsx
+++ VAMobile/src/screens/HealthScreen/Pharmacy/RefillScreens/RefillRequestSummary.tsx
@@ -190,7 +190,7 @@ const RefillRequestSummary: FC<RefillRequestSummaryProps> = ({ navigation }) => 
   }
 
   if (showLoadingScreenRequestRefillsRetry) {
-    return <LoadingComponent text={t('prescriptions.refill.submit')} />
+    return <LoadingComponent text={tc('prescriptions.refill.send', { count: 1 })} />
   }
 
   return (
