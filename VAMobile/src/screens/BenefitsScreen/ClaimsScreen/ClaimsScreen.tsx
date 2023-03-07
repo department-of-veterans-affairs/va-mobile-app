a452b5542be644c4487c37a972a5d9d75e1c19f0
Jon Bindbeutel <compjon@gmail.com>
2023-03-07 09:06:17 -0500

    removed useRef.

--- VAMobile/src/screens/BenefitsScreen/ClaimsScreen/ClaimsScreen.tsx
+++ VAMobile/src/screens/BenefitsScreen/ClaimsScreen/ClaimsScreen.tsx
@@ -1,6 +1,6 @@
 import { StackScreenProps } from '@react-navigation/stack'
 import { useTranslation } from 'react-i18next'
-import React, { FC, ReactElement, useEffect, useRef, useState } from 'react'
+import React, { FC, ReactElement, useEffect, useState } from 'react'
 
 import { AlertBox, Box, ErrorComponent, FeatureLandingTemplate, LoadingComponent, SegmentedControl } from 'components'
 import { AuthorizedServicesState, ClaimsAndAppealsState, PersonalInformationState, getProfileInfo, prefetchClaimsAndAppeals } from 'store/slices'
