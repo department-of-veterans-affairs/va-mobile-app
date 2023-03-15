40867b52a723c96843f8347f40937fcbcbb14a55
Dylan Nienberg <dylannienberg@gmail.com>
2023-03-15 09:30:24 -0500

    fixed Prepare for video TextArea usage

--- VAMobile/src/screens/HealthScreen/Appointments/UpcomingAppointments/PrepareForVideoVisit/PrepareForVideoVisit.tsx
+++ VAMobile/src/screens/HealthScreen/Appointments/UpcomingAppointments/PrepareForVideoVisit/PrepareForVideoVisit.tsx
@@ -27,44 +27,42 @@ const PrepareForVideoVisit: FC<PrepareForVideoVisitProps> = ({ navigation }) => 
 
   return (
     <LargePanel title={tc('appointmentsHelp')} rightButtonText={tc('close')}>
-      <Box mb={theme.dimensions.contentMarginBottom}>
-        <TextArea>
-          <TextView variant="MobileBodyBold" accessibilityRole="header">
-            {t('prepareForVideoVisit.beforeYourAppointment')}
-          </TextView>
-          <TextView variant="MobileBody" mt={theme.dimensions.standardMarginBetween}>
-            {t('prepareForVideoVisit.downloadBasedOnDevice')}
-          </TextView>
-          <TextView variant="MobileBody" mt={theme.dimensions.standardMarginBetween}>
-            {t('prepareForVideoVisit.cameraAndMicrophone')}
-          </TextView>
-          <TextView variant="MobileBody" mt={theme.dimensions.standardMarginBetween}>
-            {t('prepareForVideoVisit.joinBy')}
-          </TextView>
+      <Box mb={theme.dimensions.contentMarginBottom} mx={theme.dimensions.gutter} mt={theme.dimensions.cardPadding}>
+        <TextView variant="MobileBodyBold" accessibilityRole="header">
+          {t('prepareForVideoVisit.beforeYourAppointment')}
+        </TextView>
+        <TextView variant="MobileBody" mt={theme.dimensions.standardMarginBetween}>
+          {t('prepareForVideoVisit.downloadBasedOnDevice')}
+        </TextView>
+        <TextView variant="MobileBody" mt={theme.dimensions.standardMarginBetween}>
+          {t('prepareForVideoVisit.cameraAndMicrophone')}
+        </TextView>
+        <TextView variant="MobileBody" mt={theme.dimensions.standardMarginBetween}>
+          {t('prepareForVideoVisit.joinBy')}
+        </TextView>
 
-          <TextView variant="MobileBodyBold" accessibilityRole="header" mt={theme.dimensions.standardMarginBetween}>
-            {t('prepareForVideoVisit.toHaveBestExperience')}
-          </TextView>
-          <TextView variant="MobileBody" mt={theme.dimensions.standardMarginBetween}>
-            {t('prepareForVideoVisit.connectFromQuietPlace')}
-          </TextView>
-          <TextView variant="MobileBody" mt={theme.dimensions.standardMarginBetween}>
-            {t('prepareForVideoVisit.checkConnection')}
-          </TextView>
-          <TextView variant="MobileBody" mt={theme.dimensions.standardMarginBetween}>
-            {t('prepareForVideoVisit.connectWithWifi')}
-          </TextView>
+        <TextView variant="MobileBodyBold" accessibilityRole="header" mt={theme.dimensions.standardMarginBetween}>
+          {t('prepareForVideoVisit.toHaveBestExperience')}
+        </TextView>
+        <TextView variant="MobileBody" mt={theme.dimensions.standardMarginBetween}>
+          {t('prepareForVideoVisit.connectFromQuietPlace')}
+        </TextView>
+        <TextView variant="MobileBody" mt={theme.dimensions.standardMarginBetween}>
+          {t('prepareForVideoVisit.checkConnection')}
+        </TextView>
+        <TextView variant="MobileBody" mt={theme.dimensions.standardMarginBetween}>
+          {t('prepareForVideoVisit.connectWithWifi')}
+        </TextView>
 
-          <TextView variant="MobileBodyBold" accessibilityRole="header" mt={theme.dimensions.standardMarginBetween}>
-            {t('prepareForVideoVisit.medicationReview')}
-          </TextView>
-          <TextView variant="MobileBody" mt={theme.dimensions.standardMarginBetween}>
-            {t('prepareForVideoVisit.reviewEverything')}
-          </TextView>
-          <TextView variant="MobileBody" mt={theme.dimensions.standardMarginBetween}>
-            {t('prepareForVideoVisit.beReadyToTalk')}
-          </TextView>
-        </TextArea>
+        <TextView variant="MobileBodyBold" accessibilityRole="header" mt={theme.dimensions.standardMarginBetween}>
+          {t('prepareForVideoVisit.medicationReview')}
+        </TextView>
+        <TextView variant="MobileBody" mt={theme.dimensions.standardMarginBetween}>
+          {t('prepareForVideoVisit.reviewEverything')}
+        </TextView>
+        <TextView variant="MobileBody" mt={theme.dimensions.standardMarginBetween}>
+          {t('prepareForVideoVisit.beReadyToTalk')}
+        </TextView>
       </Box>
     </LargePanel>
   )
