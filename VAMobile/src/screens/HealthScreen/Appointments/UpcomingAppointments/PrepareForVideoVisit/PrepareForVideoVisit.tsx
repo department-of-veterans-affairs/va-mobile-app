0c9049c56f8927cf809a0406929b11b92b2b84f4
Dylan Nienberg <87150991+Sparowhawk@users.noreply.github.com>
2023-03-22 15:30:36 -0500

    bug/5092-Dylan-PrepareForVideoTextAreaFix (#5114)
    * fixed Prepare for video TextArea usage
    
    * fixed Prepare for video TextArea usage
    
    * adjusted header banner wrapping
    
    * adjusted header banner wrapping
    
    * not part of this fix
    
    * adjusted for Android
    
    * removing the bottom change as a LargePanel Issue
    
    ---------
    
    Co-authored-by: Jon Bindbeutel <compjon@gmail.com>

--- VAMobile/src/screens/HealthScreen/Appointments/UpcomingAppointments/PrepareForVideoVisit/PrepareForVideoVisit.tsx
+++ VAMobile/src/screens/HealthScreen/Appointments/UpcomingAppointments/PrepareForVideoVisit/PrepareForVideoVisit.tsx
@@ -2,7 +2,7 @@ import { StackScreenProps } from '@react-navigation/stack/lib/typescript/src/typ
 import { useTranslation } from 'react-i18next'
 import React, { FC, useEffect } from 'react'
 
-import { Box, LargePanel, TextArea, TextView } from 'components'
+import { Box, LargePanel, TextView } from 'components'
 import { HealthStackParamList } from '../../../HealthStackScreens'
 import { HiddenTitle } from 'styles/common'
 import { NAMESPACE } from 'constants/namespaces'
@@ -27,44 +27,42 @@ const PrepareForVideoVisit: FC<PrepareForVideoVisitProps> = ({ navigation }) => 
 
   return (
     <LargePanel title={tc('appointmentsHelp')} rightButtonText={tc('close')}>
-      <Box mb={theme.dimensions.contentMarginBottom}>
-        <TextArea>
-          <TextView variant="MobileBodyBold" accessibilityRole="header">
-            {t('prepareForVideoVisit.beforeYourAppointment')}
-          </TextView>
-          <TextView variant="MobileBody" mt={theme.dimensions.standardMarginBetween}>
-            {t('prepareForVideoVisit.downloadBasedOnDevice')}
-          </TextView>
-          <TextView variant="MobileBody" mt={theme.dimensions.standardMarginBetween}>
-            {t('prepareForVideoVisit.cameraAndMicrophone')}
-          </TextView>
-          <TextView variant="MobileBody" mt={theme.dimensions.standardMarginBetween}>
-            {t('prepareForVideoVisit.joinBy')}
-          </TextView>
+      <Box mb={theme.dimensions.contentMarginBottom} mx={theme.dimensions.gutter} mt={theme.dimensions.cardPadding}>
+        <TextView variant="MobileBodyBold" accessibilityRole="header">
+          {t('prepareForVideoVisit.beforeYourAppointment')}
+        </TextView>
+        <TextView variant="MobileBody" mt={theme.dimensions.standardMarginBetween}>
+          {t('prepareForVideoVisit.downloadBasedOnDevice')}
+        </TextView>
+        <TextView variant="MobileBody" mt={theme.dimensions.standardMarginBetween}>
+          {t('prepareForVideoVisit.cameraAndMicrophone')}
+        </TextView>
+        <TextView variant="MobileBody" mt={theme.dimensions.standardMarginBetween}>
+          {t('prepareForVideoVisit.joinBy')}
+        </TextView>
 
-          <TextView variant="MobileBodyBold" accessibilityRole="header" mt={theme.dimensions.standardMarginBetween}>
-            {t('prepareForVideoVisit.toHaveBestExperience')}
-          </TextView>
-          <TextView variant="MobileBody" mt={theme.dimensions.standardMarginBetween}>
-            {t('prepareForVideoVisit.connectFromQuietPlace')}
-          </TextView>
-          <TextView variant="MobileBody" mt={theme.dimensions.standardMarginBetween}>
-            {t('prepareForVideoVisit.checkConnection')}
-          </TextView>
-          <TextView variant="MobileBody" mt={theme.dimensions.standardMarginBetween}>
-            {t('prepareForVideoVisit.connectWithWifi')}
-          </TextView>
+        <TextView variant="MobileBodyBold" accessibilityRole="header" mt={theme.dimensions.standardMarginBetween}>
+          {t('prepareForVideoVisit.toHaveBestExperience')}
+        </TextView>
+        <TextView variant="MobileBody" mt={theme.dimensions.standardMarginBetween}>
+          {t('prepareForVideoVisit.connectFromQuietPlace')}
+        </TextView>
+        <TextView variant="MobileBody" mt={theme.dimensions.standardMarginBetween}>
+          {t('prepareForVideoVisit.checkConnection')}
+        </TextView>
+        <TextView variant="MobileBody" mt={theme.dimensions.standardMarginBetween}>
+          {t('prepareForVideoVisit.connectWithWifi')}
+        </TextView>
 
-          <TextView variant="MobileBodyBold" accessibilityRole="header" mt={theme.dimensions.standardMarginBetween}>
-            {t('prepareForVideoVisit.medicationReview')}
-          </TextView>
-          <TextView variant="MobileBody" mt={theme.dimensions.standardMarginBetween}>
-            {t('prepareForVideoVisit.reviewEverything')}
-          </TextView>
-          <TextView variant="MobileBody" mt={theme.dimensions.standardMarginBetween}>
-            {t('prepareForVideoVisit.beReadyToTalk')}
-          </TextView>
-        </TextArea>
+        <TextView variant="MobileBodyBold" accessibilityRole="header" mt={theme.dimensions.standardMarginBetween}>
+          {t('prepareForVideoVisit.medicationReview')}
+        </TextView>
+        <TextView variant="MobileBody" mt={theme.dimensions.standardMarginBetween}>
+          {t('prepareForVideoVisit.reviewEverything')}
+        </TextView>
+        <TextView variant="MobileBody" mt={theme.dimensions.standardMarginBetween}>
+          {t('prepareForVideoVisit.beReadyToTalk')}
+        </TextView>
       </Box>
     </LargePanel>
   )
