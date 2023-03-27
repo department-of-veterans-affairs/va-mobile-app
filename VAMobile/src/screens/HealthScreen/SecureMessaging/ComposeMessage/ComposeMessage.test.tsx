da80bfcd29921914c78b8c2aa172b054c45d0197
Tim Wright <timwright12@gmail.com>
2023-03-27 10:37:07 -0400

    whitespace commit

--- VAMobile/src/screens/HealthScreen/SecureMessaging/ComposeMessage/ComposeMessage.test.tsx
+++ VAMobile/src/screens/HealthScreen/SecureMessaging/ComposeMessage/ComposeMessage.test.tsx
@@ -310,7 +310,7 @@ context('ComposeMessage', () => {
         })
       })
 
-      it('should display a fieldå error for that field', async () => {
+      it('should display a fieldå error for that field', async () => { 
         await waitFor(() => {
           expect(findByTypeWithText(testInstance, TextView, 'Select a recipient')).toBeTruthy()
           expect(findByTypeWithText(testInstance, TextView, 'Subject is required')).toBeTruthy()
