56d0bdbe51de0382f977e7a9d64fce71b594b8ea
Tim Wright <timwright12@gmail.com>
2023-04-03 09:35:26 -0400

    Updated default Rx config to true

--- VAMobile/src/utils/remoteConfig.ts
+++ VAMobile/src/utils/remoteConfig.ts
@@ -36,7 +36,7 @@ export let devConfig: FeatureToggleValues = {
 
 export const productionDefaults: FeatureToggleValues = {
   appointmentRequests: false,
-  prescriptions: false,
+  prescriptions: true,
   SIS: true,
   testFeature: false,
   inAppUpdates: false,
