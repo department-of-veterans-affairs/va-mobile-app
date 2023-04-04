7faf5966840c31ac77549cdca21185b89652e49d
rbontrager <rachael.bontrager@adhocteam.us>
2023-04-04 09:43:02 -0500

    Detox Military Info Changes

--- VAMobile/e2e/tests/HomeScreen.e2e.ts
+++ VAMobile/e2e/tests/HomeScreen.e2e.ts
@@ -1,41 +1,70 @@
 import { expect, device, by, element, waitFor } from 'detox'
-import { CommonE2eIdConstants, loginToDemoMode } from './utils'
+import { CommonE2eIdConstants, loginToDemoMode, backButton } from './utils'
 
 export const HomeE2eIdConstants = {
-  HOME_PAGE_ID: 'Home-page',
-  CLAIMS_APPEAL_BTN_ID: 'Claims and appeals',
-  LETTERS_BTN_ID: 'Letters',
-  HEALTH_CARE_BTN_ID: 'Health care',
-  HOME_HEADER_ID: 'Home-page-header',
-  GREETING_ID: 'greeting-text',
-  PAYMENTS_BTN_ID: 'Payments',
-  LOCATION_FINDER_ROW_ID: 'Find a V-A Location',
-  CONTACT_VA_ROW_ID: 'Contact V-A',
-  COVID_ROW_TEXT: 'V\ufeffA COVID-19 updates'
+	GREETING_ID: 'greeting-text',
+	PAYMENTS_BTN_ID: 'Payments',
+	LOCATION_FINDER_ROW_LABEL: 'Find a V-A Location, go to V-A Facility Locator',
+	CONTACT_VA_ROW_LABEL: 'Contact V-A, go to V-A Contact Information page',
+	COVID_ROW_LABEL: 'V﻿A COVID-19 updates, Get the latest COVID-19 updates',
+	HOME_PAGE_USER_NAME: 'Kimberly Washington',
+	HOME_PAGE_MILITARY_BRANCH: 'United States Coast Guard',
+	VETERANS_LINE_ID: 'talk-to-the-veterans-crisis-line-now, Go to Veterans Crisis Line',
+	CONTACT_VA_TITLE: 'Call My V-A 4 1 1',
+	CONTACT_VA_BODY: 'My V-A 4 1 1 is our main V-A information line. We can help connect you to any of our V-A contact centers.',
+	CONTACT_VA_PHONE_NUMBER: '8 0 0 6 9 8 2 4 1 1, Dials this number via your device’s call function',
+	CONTACT_VA_TTY: 'TTY: 7 1 1, Dials this number via your device’s call function',
+	WEBVIEW_ID: 'Webview-web'
 }
 
 beforeAll(async () => {
-  await loginToDemoMode()
+	await loginToDemoMode()
 })
 
 describe('Home Screen', () => {
-  it('should show primary home page content', async () => {
-    await waitFor(element(by.id(HomeE2eIdConstants.HOME_PAGE_ID)))
-      .toExist()
-      .withTimeout(2000)
+	it('should show primary home page content', async () => {
+		await expect(element(by.label(HomeE2eIdConstants.VETERANS_LINE_ID))).toExist()
+		await expect(element(by.text(HomeE2eIdConstants.HOME_PAGE_USER_NAME))).toExist()
+		await expect(element(by.text(HomeE2eIdConstants.HOME_PAGE_MILITARY_BRANCH))).toExist()
+		await expect(element(by.label(HomeE2eIdConstants.LOCATION_FINDER_ROW_LABEL))).toExist()
+		await expect(element(by.label(HomeE2eIdConstants.CONTACT_VA_ROW_LABEL))).toExist()
+		await expect(element(by.label(HomeE2eIdConstants.COVID_ROW_LABEL))).toExist()
+	})
 
-    await expect(element(by.id(HomeE2eIdConstants.HOME_HEADER_ID))).toExist()
-    // await expect(element(by.text(HomeE2eIdConstants.GREETING_ID))).toBeVisible() TKD testID doesn't exist anymore
-    await expect(element(by.id(HomeE2eIdConstants.CLAIMS_APPEAL_BTN_ID))).toExist()
-    await expect(element(by.id(HomeE2eIdConstants.HEALTH_CARE_BTN_ID))).toExist()
-    await expect(element(by.id(HomeE2eIdConstants.LETTERS_BTN_ID))).toExist()
-    await expect(element(by.id(CommonE2eIdConstants.VETERAN_CRISIS_LINE_BTN_ID))).toExist()
-    await expect(element(by.id(HomeE2eIdConstants.PAYMENTS_BTN_ID))).toExist()
-  })
+	it('should open the Contact VA screen when clicked and show the correct info', async () => {
+		await element(by.label(HomeE2eIdConstants.CONTACT_VA_ROW_LABEL)).tap()
+		await expect(element(by.label(HomeE2eIdConstants.VETERANS_LINE_ID))).toExist()
+		await expect(element(by.label(HomeE2eIdConstants.CONTACT_VA_TITLE))).toExist()
+		await expect(element(by.label(HomeE2eIdConstants.CONTACT_VA_BODY))).toExist()
+		await expect(element(by.label(HomeE2eIdConstants.CONTACT_VA_PHONE_NUMBER))).toExist()
+		await expect(element(by.label(HomeE2eIdConstants.CONTACT_VA_TTY))).toExist()
+		await backButton()
+	})
 
-  it('should show secondary home page content', async () => {
-    await expect(element(by.id(HomeE2eIdConstants.LOCATION_FINDER_ROW_ID))).toExist()
-    await expect(element(by.id(HomeE2eIdConstants.CONTACT_VA_ROW_ID))).toExist()
-    await expect(element(by.text(HomeE2eIdConstants.COVID_ROW_TEXT))).toExist()
-  })
+	it('should open Find a VA location when clicked and show the correct info', async () => {
+		await element(by.label(HomeE2eIdConstants.LOCATION_FINDER_ROW_LABEL)).tap()
+		await expect(element(by.id(HomeE2eIdConstants.WEBVIEW_ID))).toExist()
+		/*if (device.getPlatform() === 'android') {
+			const innerElement = web.element(by.web.id(HomeE2eIdConstants.WEBVIEW_ID))
+			await expect(innerElement).toHaveText('Find VA locations')
+			await expect(element(by.id(HomeE2eIdConstants.WEBVIEW_ID))).toExist()
+		} else {
+			await expect(element(by.id(HomeE2eIdConstants.WEBVIEW_ID))).toExist()
+		}*/
+		await backButton()
+	})
+
+	it('should open Va Covid-19 updtaes when tapped and show the correct info', async () => {
+		await element(by.label(HomeE2eIdConstants.COVID_ROW_LABEL)).tap()
+		await expect(element(by.id(HomeE2eIdConstants.WEBVIEW_ID))).toExist()
+		/*
+		if (device.getPlatform() === 'android') {
+			const innerElement = web.element(by.web.id(HomeE2eIdConstants.WEBVIEW_ID))
+			await expect(innerElement).toHaveText('Coronavirus FAQs: What Veterans need to know')
+			await expect(element(by.id(HomeE2eIdConstants.WEBVIEW_ID))).toExist()
+		} else {
+			await expect(element(by.id(HomeE2eIdConstants.WEBVIEW_ID))).toExist()
+		}*/
+		await backButton()
+	})
 })
--- VAMobile/e2e/tests/VeteransCrisisLine.e2e.ts
+++ VAMobile/e2e/tests/VeteransCrisisLine.e2e.ts
@@ -1,24 +1,81 @@
 import { expect, device, by, element, waitFor } from 'detox'
-import { CommonE2eIdConstants, loginToDemoMode, openVeteransCrisisLine } from './utils'
+import { CommonE2eIdConstants, loginToDemoMode, openVeteransCrisisLine, checkImages} from './utils'
+import { setTimeout } from "timers/promises"
 
 export const VCLE2eIdConstants = {
-    VETERANS_CRISIS_LINE_PAGE_ID: 'Veterans-Crisis-Line-page',
-    VCL_URL_LABEL: 'Veterans Crisis Line .net',
-    VCL_HERE_FOR_YOU_LABEL: 'We’re here anytime, day or night – 24 7'
-  }
+VETERANS_CRISIS_LINE_PAGE_TEXT: 'Veterans Crisis Line',
+VCL_URL_LABEL: 'Veterans Crisis Line .net, Takes you to a resource page. This page will open in your device\'s browser',
+VCL_HERE_FOR_YOU_LABEL: 'We’re here anytime, day or night – 24 7',
+VCL_PHONE_NUMBER_LABEL: 'Call 800-273-8255 and select 1, Dials this number via your device’s call function',
+VCL_TEXT_NUMBER_LABEL: 'text 8 3 8 2 5 5, Opens a new text message with this number via your device’s text messaging function',
+VCL_CHAT_LABEL: 'Start a confidential chat, Takes you to a crisis chat web page. This page will open in your device\'s browser',
+VCL_PHONE_NUMBER_TTY: '800-799-4889, Dials this number via your device’s call function'
+}
 
 beforeAll(async () => {
-  await loginToDemoMode()
-  await openVeteransCrisisLine()
+	await loginToDemoMode()
+	await openVeteransCrisisLine()
 })
 
 describe('Veterans Crisis Line', () => {
-  it('should show some VCL content', async () => {
-    await waitFor(element(by.id(VCLE2eIdConstants.VETERANS_CRISIS_LINE_PAGE_ID)))
-      .toExist()
-      .withTimeout(2000)
+	it('should show some VCL content', async () => {
+		await waitFor(element(by.text(VCLE2eIdConstants.VETERANS_CRISIS_LINE_PAGE_TEXT)))
+		  .toExist()
+		  .withTimeout(2000)
 
-    await expect(element(by.label(VCLE2eIdConstants.VCL_HERE_FOR_YOU_LABEL))).toExist()
-    await expect(element(by.label(VCLE2eIdConstants.VCL_URL_LABEL))).toExist()
-  })
+		await expect(element(by.label(VCLE2eIdConstants.VCL_HERE_FOR_YOU_LABEL))).toExist()
+	})
+	
+	it('should verify that Veterans crisis line phone number is there and clickable', async () => {
+		await expect(element(by.label(VCLE2eIdConstants.VCL_PHONE_NUMBER_LABEL))).toExist()
+		await element(by.label(VCLE2eIdConstants.VCL_PHONE_NUMBER_LABEL)).tap()
+		if (device.getPlatform() === 'android') {
+			await setTimeout(5000)
+			var tempPath = await device.takeScreenshot('AndroidCallingScreen')
+			await device.launchApp({newInstance: false})
+		} else {
+			await element(by.text(CommonE2eIdConstants.CANCEL_UNIVERSAL_TEXT)).tap()
+		}
+	})
+	
+	it('should verify that Veterans crisis line text number is there and clickable', async () => {
+		await expect(element(by.label(VCLE2eIdConstants.VCL_TEXT_NUMBER_LABEL))).toExist()
+		await element(by.label(VCLE2eIdConstants.VCL_TEXT_NUMBER_LABEL)).tap()
+		await setTimeout(5000)
+		var tempPath = await device.takeScreenshot('PhoneMessagingScreen')
+		await device.launchApp({newInstance: false})
+	})
+	
+	it('should verify that Veterans crisis chat option is there and clickable', async () => {
+		await expect(element(by.label(VCLE2eIdConstants.VCL_CHAT_LABEL))).toExist()
+		await element(by.label(VCLE2eIdConstants.VCL_CHAT_LABEL)).tap()
+		await element(by.text(CommonE2eIdConstants.CANCEL_UNIVERSAL_TEXT)).tap()
+		await element(by.label(VCLE2eIdConstants.VCL_CHAT_LABEL)).tap()
+		await element(by.text(CommonE2eIdConstants.OK_UNIVERSAL_TEXT)).tap()
+		await setTimeout(5000)
+		var tempPath = await device.takeScreenshot('ChatScreen')
+		await device.launchApp({newInstance: false})
+	})
+	
+	it('should verify that Veterans crisis line TTY phone number is there and clickable', async () => {
+		await expect(element(by.label(VCLE2eIdConstants.VCL_PHONE_NUMBER_TTY))).toExist()
+		await element(by.label(VCLE2eIdConstants.VCL_PHONE_NUMBER_TTY)).tap()
+		if (device.getPlatform() === 'android') {
+			var tempPath = await device.takeScreenshot('AndroidTTYCallingScreen')
+			await device.launchApp({newInstance: false})
+		} else {
+			await element(by.text(CommonE2eIdConstants.CANCEL_UNIVERSAL_TEXT)).tap()
+		}
+	})
+	
+	it('should verify that Veterans crisis line link is there and clickable', async () => {
+		await expect(element(by.label(VCLE2eIdConstants.VCL_URL_LABEL))).toExist()
+		await element(by.label(VCLE2eIdConstants.VCL_URL_LABEL)).tap()
+		await element(by.text(CommonE2eIdConstants.CANCEL_UNIVERSAL_TEXT)).tap()
+		await element(by.label(VCLE2eIdConstants.VCL_URL_LABEL)).tap()
+		await element(by.text(CommonE2eIdConstants.OK_UNIVERSAL_TEXT)).tap()
+		await setTimeout(5000)
+		var tempPath = await device.takeScreenshot('VeteransCrisisLineLink')
+		await device.launchApp({newInstance: false})
+	})
 })
