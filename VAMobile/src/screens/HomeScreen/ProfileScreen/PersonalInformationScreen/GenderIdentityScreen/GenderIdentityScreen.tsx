2cfe4c1fdfe96277e82fad5da0d2e21e89c24e83
Narin Ratana <narin@adhocteam.us>
2023-04-21 11:33:55 -0700

    Add PPGI analytics events

--- VAMobile/src/screens/HomeScreen/ProfileScreen/PersonalInformationScreen/GenderIdentityScreen/GenderIdentityScreen.tsx
+++ VAMobile/src/screens/HomeScreen/ProfileScreen/PersonalInformationScreen/GenderIdentityScreen/GenderIdentityScreen.tsx
@@ -5,12 +5,14 @@ import { useTranslation } from 'react-i18next'
 import React, { FC, useEffect, useState } from 'react'
 
 import { Box, ErrorComponent, FullScreenSubtask, LoadingComponent, RadioGroup, RadioGroupProps, TextView, radioOption } from 'components'
+import { Events } from 'constants/analytics'
 import { HomeStackParamList } from 'screens/HomeScreen/HomeStackScreens'
 import { NAMESPACE } from 'constants/namespaces'
 import { PersonalInformationState, dispatchFinishEditGenderIdentity, getGenderIdentityOptions, updateGenderIdentity } from 'store/slices'
 import { RootState } from 'store'
 import { ScreenIDTypesConstants } from 'store/api/types/Screens'
 import { SnackbarMessages } from 'components/SnackBar'
+import { logAnalyticsEvent } from 'utils/analytics'
 import { useAppDispatch, useError, useRouteNavigation, useTheme } from 'utils/hooks'
 
 type GenderIdentityScreenProps = StackScreenProps<HomeStackParamList, 'GenderIdentity'>
@@ -69,6 +71,11 @@ const GenderIdentityScreen: FC<GenderIdentityScreenProps> = ({ navigation }) => 
     })
   }
 
+  const goToWhatToKnow = (): void => {
+    logAnalyticsEvent(Events.vama_gender_id_help)
+    navigateTo('WhatToKnow')()
+  }
+
   const radioGroupProps: RadioGroupProps<string> = {
     error,
     isRadioList: false,
@@ -114,7 +121,7 @@ const GenderIdentityScreen: FC<GenderIdentityScreenProps> = ({ navigation }) => 
           <TextView variant="MobileBodyBold">{t('personalInformation.genderIdentity.preferNotToAnswer')}</TextView>
         </TextView>
         <RadioGroup {...radioGroupProps} />
-        <Pressable onPress={navigateTo('WhatToKnow')} accessibilityRole="link" accessible={true}>
+        <Pressable onPress={goToWhatToKnow} accessibilityRole="link" accessible={true}>
           <TextView variant="MobileBodyLink">{t('personalInformation.genderIdentity.whatToKnow')}</TextView>
         </Pressable>
       </Box>
