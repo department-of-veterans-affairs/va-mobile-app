eb688721f429128f9c69a6349b72e178f993ff2b
Jon Bindbeutel <compjon@gmail.com>
2023-04-25 14:37:25 -0400

    Binny cherry pick 4966 to staging (#5476)
    * Add snackBar message on error
    
    * Set withNavBar to true to add vertical offset to error snackbars
    
    * Add retry functionality to snackbar
    
    ---------
    
    Co-authored-by: Narin Ratana <narin@adhocteam.us>

--- VAMobile/src/store/slices/personalInformationSlice.ts
+++ VAMobile/src/store/slices/personalInformationSlice.ts
@@ -514,11 +514,11 @@ export const updatePreferredName =
       dispatch(dispatchFinishSaveUpdatePreferredName())
       showSnackBar(messages.successMsg, dispatch, undefined, true, false)
     } catch (err) {
-      console.debug('error updating name')
       if (isErrorObject(err)) {
         logNonFatalErrorToFirebase(err, `updatePreferredName: ${personalInformationNonFatalErrorString}`)
         dispatch(dispatchFinishSaveUpdatePreferredName(err))
         dispatch(dispatchSetError({ errorType: getCommonErrorFromAPIError(err), screenID }))
+        showSnackBar(messages.errorMsg, dispatch, retryFunction, false, true, true)
       }
     }
   }
@@ -559,7 +559,7 @@ export const updateGenderIdentity =
         logNonFatalErrorToFirebase(error, `updateGenderIdentity: ${personalInformationNonFatalErrorString}`)
         dispatch(dispatchFinishUpdateGenderIdentity(error))
         dispatch(dispatchSetError({ errorType: getCommonErrorFromAPIError(error), screenID }))
-        showSnackBar(messages.errorMsg, dispatch, retryFunction, false, true)
+        showSnackBar(messages.errorMsg, dispatch, retryFunction, false, true, true)
       }
     }
   }
