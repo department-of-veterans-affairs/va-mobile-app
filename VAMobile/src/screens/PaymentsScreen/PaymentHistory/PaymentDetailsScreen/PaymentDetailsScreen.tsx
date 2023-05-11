78bcbc21601bda41316b4303a11a4cb631fd6172
Dylan Nienberg <dylannienberg@gmail.com>
2023-05-11 12:45:53 -0500

    fixed insets

--- VAMobile/src/screens/PaymentsScreen/PaymentHistory/PaymentDetailsScreen/PaymentDetailsScreen.tsx
+++ VAMobile/src/screens/PaymentsScreen/PaymentHistory/PaymentDetailsScreen/PaymentDetailsScreen.tsx
@@ -57,7 +57,7 @@ const PaymentDetailsScreen: FC<PaymentDetailsScreenProps> = ({ navigation, route
 
   return (
     <FeatureLandingTemplate backLabel={t('history.title')} backLabelOnPress={navigation.goBack} title={t('paymentDetails.title')}>
-      <Box mt={contentMarginTop} mb={theme?.dimensions?.contentMarginBottom}>
+      <Box mt={theme?.dimensions?.contentMarginTop} mb={theme?.dimensions?.contentMarginBottom}>
         <TextArea>
           <TextView variant="MobileBody" mb={theme?.dimensions?.standardMarginBetween}>
             {formatDateUtc(date, 'MMMM d, yyyy')}
