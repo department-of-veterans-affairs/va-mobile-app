08b009b684e0882684ef2dd6077c262d65f6de12
Dylan Nienberg <dylannienberg@gmail.com>
2023-05-11 19:16:32 -0500

    fixed spacing

--- VAMobile/src/screens/PaymentsScreen/DirectDepositScreen/DirectDepositScreen.tsx
+++ VAMobile/src/screens/PaymentsScreen/DirectDepositScreen/DirectDepositScreen.tsx
@@ -28,7 +28,7 @@ const DirectDepositScreen: FC<DirectDepositScreenProps> = ({ navigation }) => {
   const theme = useTheme()
   const ddNotInDowntime = !useDowntime(DowntimeFeatureTypeConstants.directDepositBenefits)
 
-  const { gutter, contentMarginBottom } = theme.dimensions
+  const { gutter, contentMarginTop, contentMarginBottom } = theme.dimensions
 
   useFocusEffect(
     useCallback(() => {
@@ -88,7 +88,7 @@ const DirectDepositScreen: FC<DirectDepositScreenProps> = ({ navigation }) => {
 
   return (
     <FeatureLandingTemplate backLabel={t('payments.title')} backLabelOnPress={navigation.goBack} title={t('directDeposit.title')}>
-      <Box mx={gutter}>
+      <Box mx={gutter} mt={contentMarginTop}>
         <TextView variant="MobileBody" mb={theme.dimensions.standardMarginBetween} {...testIdProps(t('directDeposit.viewAndEditTextA11yLabel'))}>
           {t('directDeposit.viewAndEditText')}
         </TextView>
