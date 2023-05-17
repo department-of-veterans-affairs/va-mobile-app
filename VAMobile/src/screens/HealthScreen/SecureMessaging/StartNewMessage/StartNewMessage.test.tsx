cbd25719cf94dd01cf371a3ac705394cd1f829e1
Dylan Nienberg <87150991+Sparowhawk@users.noreply.github.com>
2023-05-17 12:37:12 -0500

    feature/4981-Dylan-ChangeComposteToStartNewMessage (#5608)
    * finished I think
    
    * finished I think
    
    ---------
    
    Co-authored-by: Jon Bindbeutel <compjon@gmail.com>

--- VAMobile/src/screens/HealthScreen/SecureMessaging/StartNewMessage/StartNewMessage.test.tsx
+++ VAMobile/src/screens/HealthScreen/SecureMessaging/StartNewMessage/StartNewMessage.test.tsx
@@ -7,7 +7,7 @@ import { StackNavigationOptions } from '@react-navigation/stack/lib/typescript/s
 
 import * as api from 'store/api'
 import { context, findByTypeWithText, mockNavProps, render, RenderAPI, waitFor } from 'testUtils'
-import ComposeMessage from './ComposeMessage'
+import StartNewMessage from './StartNewMessage'
 import { Pressable, TouchableWithoutFeedback } from 'react-native'
 import { AlertBox, ErrorComponent, FormWrapper, LoadingComponent, TextView, VAModalPicker, VATextInput } from 'components'
 import { initializeErrorsByScreenID, InitialState, saveDraft, updateSecureMessagingTab } from 'store/slices'
@@ -77,7 +77,7 @@ jest.mock('../CancelConfirmations/ComposeCancelConfirmation', () => {
   }
 })
 
-context('ComposeMessage', () => {
+context('StartNewMessage', () => {
   let component: RenderAPI
   let testInstance: ReactTestInstance
   let props: any
@@ -128,7 +128,7 @@ context('ComposeMessage', () => {
       { params: params },
     )
 
-    component = render(<ComposeMessage {...props} />, {
+    component = render(<StartNewMessage {...props} />, {
       preloadedState: {
         ...InitialState,
         secureMessaging: {
