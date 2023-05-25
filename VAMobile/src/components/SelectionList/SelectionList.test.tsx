a1ed4e27da29e764a1f6a21135da16f40e102a2e
Chris Alexander <chris.alexander@adhocteam.us>
2023-05-25 15:55:42 -0600

    Modify a couple test suites to use best practices

--- VAMobile/src/components/SelectionList/SelectionList.test.tsx
+++ VAMobile/src/components/SelectionList/SelectionList.test.tsx
@@ -1,71 +1,31 @@
 import 'react-native'
 import React from 'react'
-// Note: test renderer must be required after react-native.
-import 'jest-styled-components'
-import {ReactTestInstance} from 'react-test-renderer'
-import Mock = jest.Mock
-import { Pressable } from 'react-native'
+import { fireEvent, screen } from '@testing-library/react-native'
 
-import { context, render, RenderAPI, waitFor } from 'testUtils'
+import { render } from 'testUtils'
 import SelectionList from './SelectionList'
-import {Box, TextView, VAButton} from "../index";
-import LoginScreen from "../../screens/auth/LoginScreen";
-import SelectionListItem from "./SelectionListItem";
+import { TextView } from '../index'
 
-
-jest.mock('utils/hooks', () => {
-  let original = jest.requireActual('utils/hooks')
-  let theme = jest.requireActual('styles/themes/standardTheme').default
-
-  return {
-    ...original,
-    useTheme: jest.fn(() => {
-      return { ...theme }
-    }),
-  }
-})
-
-context('SelectionList', () => {
-  let component: RenderAPI
-  let testInstance: ReactTestInstance
-
-  const initializeTestInstance = (useImage = false) => {
-    const listContent = [{ content: <TextView>item 1</TextView> }, { content:<TextView>item 2</TextView> }]
-
-    component = render(<SelectionList items={listContent} />)
-    testInstance = component.container
-  }
-
+describe('SelectionList', () => {
   beforeEach(() => {
-    initializeTestInstance()
+    const items = [{ content: <TextView>One</TextView> }, { content: <TextView>Two</TextView> }]
+    render(<SelectionList items={items} />)
   })
 
-  it('initializes correctly', async () => {
-    expect(component).toBeTruthy()
+  it('renders items', () => {
+    expect(screen.getByText('One')).toBeTruthy()
+    expect(screen.getByText('Two')).toBeTruthy()
   })
 
-  it('should have the right number of list items', async () => {
-    expect(testInstance.findAllByType(SelectionListItem).length).toBe(2)
-  })
+  it('toggles items when "Select all" is pressed', () => {
+    fireEvent.press(screen.getByText('Select all'))
+    expect(screen.getByRole('checkbox', { name: 'One', checked: true })).toBeTruthy()
+    expect(screen.getByRole('checkbox', { name: 'Two', checked: true })).toBeTruthy()
+    expect(screen.getByText('2/2 selected')).toBeTruthy()
 
-  it('should toggle items when pressing select all', async () => {
-    const items = testInstance.findAllByType(SelectionListItem)
-
-    expect(items[0].props.isSelected).toBe(false)
-    expect(items[1].props.isSelected).toBe(false)
-
-    await waitFor(() => {
-      testInstance.findAllByType(Pressable)[0].props.onPress()
-    })
-
-    expect(items[0].props.isSelected).toBe(true)
-    expect(items[1].props.isSelected).toBe(true)
-
-    await waitFor(() => {
-      testInstance.findAllByType(Pressable)[0].props.onPress()
-    })
-
-    expect(items[0].props.isSelected).toBe(false)
-    expect(items[1].props.isSelected).toBe(false)
+    fireEvent.press(screen.getByText('Select all'))
+    expect(screen.getByRole('checkbox', { name: 'One', checked: false })).toBeTruthy()
+    expect(screen.getByRole('checkbox', { name: 'Two', checked: false })).toBeTruthy()
+    expect(screen.getByText('0/2 selected')).toBeTruthy()
   })
 })
