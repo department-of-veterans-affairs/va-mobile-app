03f02a073ce87f58a58c46b97e6089d92053b522
Narin Ratana <narin@adhocteam.us>
2023-05-26 09:59:32 -0700

    Disable global clearmocks afterEach. Mock safeareacontext

--- VAMobile/jest/testSetup.ts
+++ VAMobile/jest/testSetup.ts
@@ -26,6 +26,9 @@ NativeModules.RNInAppUpdate = {
 
 jest.mock('react-native/Libraries/EventEmitter/NativeEventEmitter')
 
+import mockSafeAreaContext from 'react-native-safe-area-context/jest/mock'
+jest.mock('react-native-safe-area-context', () => mockSafeAreaContext)
+
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper')
 
 jest.mock('react-native/Libraries/Linking/Linking', () => {
@@ -46,20 +49,8 @@ jest.mock('../src/store/api', () => ({
   setRefreshToken: jest.fn(),
 }))
 
-jest.mock('react-native-safe-area-context', () => {
-  const inset = { top: 0, right: 0, bottom: 0, left: 0 }
-  return {
-    ...jest.requireActual('react-native-safe-area-context'),
-    SafeAreaProvider: jest.fn(({ children }) => children),
-    SafeAreaConsumer: jest.fn(({ children }) => children(inset)),
-    useSafeAreaInsets: jest.fn(() => inset),
-    useSafeAreaFrame: jest.fn(() => ({ x: 0, y: 0, width: 390, height: 844 })),
-  }
-})
-
 // jest.mock('../src/utils/hooks', () => {
 //   let original = jest.requireActual('../src/utils/hooks')
-//   console.debug(original)
 //   return {
 //     ...original,
 //     useRouteNavigation: () => {
@@ -287,5 +278,12 @@ jest.mock('utils/homeScreenAlerts', () => {
     getVersionSkipped: jest.fn(),
     getStoreVersion: jest.fn(),
     getEncourageUpdateLocalVersion: jest.fn(),
+  }
+})
+
+jest.mock('react-native-webview', () => {
+  const { View } = require('react-native')
+  return {
+    WebView: View,
   }
 })
