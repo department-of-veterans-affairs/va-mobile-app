6e8270d30733427dd9cbc9028a740faba4af7f7b
Dylan Nienberg <dylannienberg@gmail.com>
2023-05-26 13:27:03 -0500

    fixed another suite

--- VAMobile/src/App.test.tsx
+++ VAMobile/src/App.test.tsx
@@ -28,6 +28,10 @@ jest.mock('react-native-keyboard-manager', () => ({
 }))
 
 context('App', () => {
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+  
   it('initializes correctly', async () => {
     let component: any
     jest.mock('./store', () => ({
