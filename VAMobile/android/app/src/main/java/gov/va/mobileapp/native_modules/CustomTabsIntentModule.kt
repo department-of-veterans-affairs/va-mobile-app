ab9adf3e47604b13c495a8508ec45c5a636662a4
Narin Ratana <narin@adhocteam.us>
2023-06-21 19:26:46 -0700

    Set default and dark mode color schemes. Format using Kotlin formatter

--- VAMobile/android/app/src/main/java/gov/va/mobileapp/native_modules/CustomTabsIntentModule.kt
+++ VAMobile/android/app/src/main/java/gov/va/mobileapp/native_modules/CustomTabsIntentModule.kt
@@ -1,50 +1,97 @@
 package gov.va.mobileapp.native_modules
+
 import android.content.Intent.*
 import android.net.Uri
-import android.util.Log
 import androidx.browser.customtabs.CustomTabColorSchemeParams
 import androidx.browser.customtabs.CustomTabsIntent
+import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import gov.va.mobileapp.R
 
+class CustomTabsIntentModule(private val context: ReactApplicationContext) :
+        ReactContextBaseJavaModule(context) {
 
-class CustomTabsIntentModule(private val context: ReactApplicationContext) : ReactContextBaseJavaModule(context) {
     @ReactMethod
-    fun openGoogle(authEndPoint: String, clientId: String, redirectUrl: String, authScopes: String, codeChallenge: String, state: String, SISEnabled: Boolean) {
-        val googleURI = Uri.parse(authEndPoint).buildUpon().also {
-            with(it) {
-                appendQueryParameter("code_challenge_method", "S256")
-                appendQueryParameter("code_challenge", codeChallenge)
+    fun beginAuthSession(
+            authEndPoint: String,
+            clientId: String,
+            redirectUrl: String,
+            authScopes: String,
+            codeChallenge: String,
+            state: String,
+            SISEnabled: Boolean,
+            promise: Promise
+    ) {
+        try {
+            val authURI =
+                    Uri.parse(authEndPoint)
+                            .buildUpon()
+                            .also {
+                                with(it) {
+                                    appendQueryParameter("code_challenge_method", "S256")
+                                    appendQueryParameter("code_challenge", codeChallenge)
 
-                if (SISEnabled) {
-                  appendQueryParameter("application", "vamobile")
-                  appendQueryParameter("oauth", "true")
-                } else {
-                  appendQueryParameter("client_id", clientId)
-                  appendQueryParameter("redirect_uri", redirectUrl)
-                  appendQueryParameter("scope", authScopes)
-                  appendQueryParameter("response_type", "code")
-                  appendQueryParameter("response_mode", "query")
-                  appendQueryParameter("state", state)
-                }
-            }
-        }.build()
-        Log.d(name, googleURI.toString())
-        val colorScheme = CustomTabColorSchemeParams.Builder().also {
-            it.setNavigationBarColor(context.resources.getColor(R.color.primaryDarker, context.theme))
-        }.build()
-        val customTabsIntent = CustomTabsIntent.Builder().apply {
-            setDefaultColorSchemeParams(colorScheme)
+                                    if (SISEnabled) {
+                                        appendQueryParameter("application", "vamobile")
+                                        appendQueryParameter("oauth", "true")
+                                    } else {
+                                        appendQueryParameter("client_id", clientId)
+                                        appendQueryParameter("redirect_uri", redirectUrl)
+                                        appendQueryParameter("scope", authScopes)
+                                        appendQueryParameter("response_type", "code")
+                                        appendQueryParameter("response_mode", "query")
+                                        appendQueryParameter("state", state)
+                                    }
+                                }
+                            }
+                            .build()
 
-        }.build().also {
-            it.intent.addFlags(FLAG_ACTIVITY_NEW_TASK)
-            it.intent.addFlags(FLAG_ACTIVITY_NO_HISTORY)
+            val defaultScheme =
+                    CustomTabColorSchemeParams.Builder()
+                            .also {
+                                it.setToolbarColor(
+                                        context.resources.getColor(
+                                                R.color.grayLightest,
+                                                context.theme
+                                        )
+                                )
+                            }
+                            .build()
+
+            val darkScheme =
+                    CustomTabColorSchemeParams.Builder()
+                            .also {
+                                it.setToolbarColor(
+                                        context.resources.getColor(
+                                                R.color.darkmodeGrayDark,
+                                                context.theme
+                                        )
+                                )
+                            }
+                            .build()
+
+            val customTabsIntent =
+                    CustomTabsIntent.Builder()
+                            .apply {
+                                setDefaultColorSchemeParams(defaultScheme)
+                                setColorSchemeParams(CustomTabsIntent.COLOR_SCHEME_DARK, darkScheme)
+                                setShareState(CustomTabsIntent.SHARE_STATE_OFF)
+                                setShowTitle(true)
+                            }
+                            .build()
+                            .also {
+                                it.intent.addFlags(FLAG_ACTIVITY_NEW_TASK)
+                                it.intent.addFlags(FLAG_ACTIVITY_NO_HISTORY)
+                            }
+
+            context.currentActivity?.apply { customTabsIntent.launchUrl(this, authURI) }
+            promise.resolve(authURI.toString())
+        } catch (e: Throwable) {
+            promise.reject("Custom Tabs Error", e)
         }
-        Log.d(name, "launching intent")
-        context.currentActivity?.apply { customTabsIntent.launchUrl(this, googleURI)}
     }
 
-    override fun getName() =  "CustomTabsIntentModule"
+    override fun getName() = "CustomTabsIntentModule"
 }
