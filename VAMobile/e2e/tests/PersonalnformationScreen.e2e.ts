7b38b1d3e117d2821a37ec1ea8ed45cbae57c311
Chris Alexander <chris.alexander@adhocteam.us>
2023-06-26 22:06:23 -0600

    Abstract out scrollToThenTap function

--- VAMobile/e2e/tests/PersonalnformationScreen.e2e.ts
+++ VAMobile/e2e/tests/PersonalnformationScreen.e2e.ts
@@ -3,19 +3,29 @@ import { setTimeout } from 'timers/promises'
 
 import { loginToDemoMode, openPersonalInformation, openProfile } from './utils'
 
-export const PersonalInformationE2eIdConstants = {
+export const PersonalInfoConstants = {
   PERSONAL_INFORMATION_TEXT: 'Personal information',
   HOW_TO_UPDATE_LINK_TEXT: 'How to update or fix an error in your legal name',
-  NEAREST_CENTER_LINK_TEXT: 'Find nearest V-A medical center',
+  LEARN_HOW_LINK_TEXT: 'Learn how to change your legal name on file with the VA',
+  NEAREST_CENTER_LINK_TEXT: 'Find nearest VA medical center',
   PHONE_LINK_TEXT: '800-827-1000',
   TTY_LINK_TEXT: 'TTY: 711',
+}
+
+const scrollToThenTap = async (text: string) => {
+  await waitFor(element(by.text(text)))
+    .toBeVisible()
+    .whileElement(by.id('PersonalInformationTestID'))
+    .scroll(500, 'down')
+
+  await element(by.text(text)).tap()
 }
 
 beforeAll(async () => {
   await loginToDemoMode()
   await openProfile()
   await openPersonalInformation()
-  await waitFor(element(by.text('Personal information')))
+  await waitFor(element(by.text(PersonalInfoConstants.PERSONAL_INFORMATION_TEXT)))
     .toExist()
     .withTimeout(10000)
 })
@@ -33,41 +43,26 @@ describe('Personal Information Screen', () => {
   })
 
   it('should tap links in "How to update" large panel', async () => {
-    await element(by.text(PersonalInformationE2eIdConstants.HOW_TO_UPDATE_LINK_TEXT)).tap()
+    await element(by.text(PersonalInfoConstants.HOW_TO_UPDATE_LINK_TEXT)).tap()
     await expect(element(by.text('Profile help'))).toExist()
 
-    await element(by.text('Learn how to change your legal name on file with the VA')).tap()
+    await element(by.text(PersonalInfoConstants.LEARN_HOW_LINK_TEXT)).tap()
     await setTimeout(5000)
     await device.takeScreenshot('PersonalInformationChangeNameLink')
     await element(by.text('Done')).tap()
 
-    await waitFor(element(by.label(PersonalInformationE2eIdConstants.NEAREST_CENTER_LINK_TEXT)))
-      .toBeVisible()
-      .whileElement(by.id('PersonalInformationTestID'))
-      .scroll(500, 'down')
-
-    await element(by.label(PersonalInformationE2eIdConstants.NEAREST_CENTER_LINK_TEXT)).tap()
+    await scrollToThenTap(PersonalInfoConstants.NEAREST_CENTER_LINK_TEXT)
     await setTimeout(5000)
     await device.takeScreenshot('PersonalInformationFindVACenterLink')
     await element(by.text('Done')).tap()
 
     if (device.getPlatform() === 'android') {
-      await waitFor(element(by.text(PersonalInformationE2eIdConstants.PHONE_LINK_TEXT)))
-        .toBeVisible()
-        .whileElement(by.id('PersonalInformationTestID'))
-        .scroll(500, 'down')
-
-      await element(by.text(PersonalInformationE2eIdConstants.PHONE_LINK_TEXT)).tap()
+      await scrollToThenTap(PersonalInfoConstants.PHONE_LINK_TEXT)
       await setTimeout(5000)
       await device.takeScreenshot('PersonalInformationPhoneNumber')
       await device.launchApp({ newInstance: false })
 
-      await waitFor(element(by.text(PersonalInformationE2eIdConstants.TTY_LINK_TEXT)))
-        .toBeVisible()
-        .whileElement(by.id('PersonalInformationTestID'))
-        .scroll(500, 'down')
-
-      await element(by.text(PersonalInformationE2eIdConstants.TTY_LINK_TEXT)).tap()
+      await scrollToThenTap(PersonalInfoConstants.TTY_LINK_TEXT)
       await setTimeout(5000)
       await device.takeScreenshot('PersonalInformationTTY')
     }
