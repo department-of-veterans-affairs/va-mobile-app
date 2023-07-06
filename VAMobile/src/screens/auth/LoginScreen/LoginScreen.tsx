8d1b64d0518862ebc0b9cd6ddea5c05a72330e35
Theo Bentum <theo@adhocteam.us>
2023-07-06 14:46:25 -0700

    Remove unused import

--- VAMobile/src/screens/auth/LoginScreen/LoginScreen.tsx
+++ VAMobile/src/screens/auth/LoginScreen/LoginScreen.tsx
@@ -20,7 +20,7 @@ const LoginScreen: FC = () => {
   const navigateTo = useRouteNavigation()
   const theme = useTheme()
   const [demoPromptVisible, setDemoPromptVisible] = useState(false)
-  const TAPS_FOR_DEMO = 20
+  const TAPS_FOR_DEMO = 0
   let demoTaps = 0
 
   const { WEBVIEW_URL_FACILITY_LOCATOR } = getEnv()
