688fad91e1815b25e07cea014301cd0bfbc9978a
Dylan Nienberg <dylannienberg@gmail.com>
2023-07-11 16:03:07 -0500

    finished

--- VAMobile/src/utils/claims.tsx
+++ VAMobile/src/utils/claims.tsx
@@ -7,6 +7,7 @@ import React, { ReactElement } from 'react'
 import { Box, BoxProps, TextView, VAIcon } from 'components'
 import { ClaimAttributesData, ClaimEventData, ClaimPhaseData, FILE_REQUEST_STATUS, FILE_REQUEST_TYPE } from 'store/api/types'
 import { MAX_NUM_PHOTOS } from 'constants/claims'
+import { isIOS } from './platform'
 
 /** function that determines if a request file has been uploaded or received for a claim's event*/
 export const hasUploadedOrReceived = (event: ClaimEventData): boolean => {
