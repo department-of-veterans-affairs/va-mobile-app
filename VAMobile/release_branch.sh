4f29195968a96a371a5726b7b8402638b3fd1913
Narin Ratana <narin@adhocteam.us>
2023-07-13 14:26:06 -0700

    Revert release_branch workflow and scripts.  Remove v2.0.0 version bump which is no longer needed

--- VAMobile/release_branch.sh
+++ VAMobile/release_branch.sh
@@ -48,8 +48,8 @@ while getopts h option
 done
 
 # First release branch was 08-04-2021. check and see that we are at TWO WEEK interval (14 days)
-# if [[ $[$((($(date +%s)-$(date +%s --date "2021-08-04"))/(3600*24)))%14] == 0 ]]
-# then
+if [[ $[$((($(date +%s)-$(date +%s --date "2021-08-04"))/(3600*24)))%14] == 0 ]]
+then
 
   echo "Checking out and pulling latest from main branch"
   git checkout main &&
@@ -62,11 +62,6 @@ done
   echo "Incrementing latest tag $latest by minor version"
   next=$(increment_version "$latest" 1) &&
   
-  if [[ $(date +%F) == "2023-03-29" ]]
-  then
-    next="v2.0.0"
-  fi
-
   echo "Next version: $next"
   echo
   echo "Checking out and pulling latest from develop branch"
@@ -81,12 +76,12 @@ done
 
   echo "Tag branch for Release Candidate build"
   TAG="RC-$next-$(date +%m%d%y-%H%M)"
-  # git tag -a "$TAG" -m "Release Candidate for $next. tagged on $(date +%m/%d/%y) at $(date +%H:%M)"
+  git tag -a "$TAG" -m "Release Candidate for $next. tagged on $(date +%m/%d/%y) at $(date +%H:%M)"
   git push origin "$TAG"
 
   echo "Successfully tagged for Release Candidate builds: $TAG"
   echo "Exit"
-# else
-#   echo "Not scheduled for new release branch this week. Exiting."
-#   exit 0
-# fi
+else
+  echo "Not scheduled for new release branch this week. Exiting."
+  exit 0
+fi
