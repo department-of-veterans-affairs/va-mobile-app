39d9da283205bfbad04f944d4d2cb30d53a91a16
Dylan Nienberg <87150991+Sparowhawk@users.noreply.github.com>
2023-07-17 13:03:11 -0500

    Feature/6056 dylan message card view messages2 (#6251)
    * finished
    
    * linting
    
    * linting
    
    * collapsibleMessage tests updated
    
    * collapsibleMessage tests updated
    
    * ViewMessageScreen tests
    
    * fixed ReplyMessage.test.tsx
    
    * fixed unit tests
    
    * unit test
    
    * unit tests
    
    * unit tests
    
    * if screen reader is enabled hide the preview lines
    
    * if screen reader is enabled hide the preview lines
    
    * trim leading whitespace for text preview
    
    * fixed attachments
    
    * fixed attachments
    
    * fixed attachments
    
    * fixed conditional
    
    * fixed to work with v1 threading api and v0 of the other message apis
    
    * pulled out logic to a general boolean
    
    * finished v2
    
    * finished v2
    
    * finished v2
    
    * merge
    
    * finished
    
    * merged
    
    * fixing message threading demo mode data

--- VAMobile/src/store/api/demo/secureMessaging.ts
+++ VAMobile/src/store/api/demo/secureMessaging.ts
@@ -34,29 +34,29 @@ type folderMessages =
  */
 type inboxFolderMessages = {
   '/v0/messaging/health/messages/2092809': SecureMessagingMessageGetData
-  '/v1/messaging/health/messages/2092809/thread': SecureMessagingThreadGetData
+  '/v1/messaging/health/messages/2092809/thread?excludeProvidedMessage=true': SecureMessagingThreadGetData
   '/v0/messaging/health/messages/2092803': SecureMessagingMessageGetData
-  '/v1/messaging/health/messages/2092803/thread': SecureMessagingThreadGetData
+  '/v1/messaging/health/messages/2092803/thread?excludeProvidedMessage=true': SecureMessagingThreadGetData
   '/v0/messaging/health/messages/2092789': SecureMessagingMessageGetData
-  '/v1/messaging/health/messages/2092789/thread': SecureMessagingThreadGetData
+  '/v1/messaging/health/messages/2092789/thread?excludeProvidedMessage=true': SecureMessagingThreadGetData
   '/v0/messaging/health/messages/2092775': SecureMessagingMessageGetData
-  '/v1/messaging/health/messages/2092775/thread': SecureMessagingThreadGetData
+  '/v1/messaging/health/messages/2092775/thread?excludeProvidedMessage=true': SecureMessagingThreadGetData
   '/v0/messaging/health/messages/2088259': SecureMessagingMessageGetData
-  '/v1/messaging/health/messages/2088259/thread': SecureMessagingThreadGetData
+  '/v1/messaging/health/messages/2088259/thread?excludeProvidedMessage=true': SecureMessagingThreadGetData
   '/v0/messaging/health/messages/2088250': SecureMessagingMessageGetData
-  '/v1/messaging/health/messages/2088250/thread': SecureMessagingThreadGetData
+  '/v1/messaging/health/messages/2088250/thread?excludeProvidedMessage=true': SecureMessagingThreadGetData
   '/v0/messaging/health/messages/2060125': SecureMessagingMessageGetData
-  '/v1/messaging/health/messages/2060125/thread': SecureMessagingThreadGetData
+  '/v1/messaging/health/messages/2060125/thread?excludeProvidedMessage=true': SecureMessagingThreadGetData
   '/v0/messaging/health/messages/2060114': SecureMessagingMessageGetData
-  '/v1/messaging/health/messages/2060114/thread': SecureMessagingThreadGetData
+  '/v1/messaging/health/messages/2060114/thread?excludeProvidedMessage=true': SecureMessagingThreadGetData
   '/v0/messaging/health/messages/2060047': SecureMessagingMessageGetData
-  '/v1/messaging/health/messages/2060047/thread': SecureMessagingThreadGetData
+  '/v1/messaging/health/messages/2060047/thread?excludeProvidedMessage=true': SecureMessagingThreadGetData
   '/v0/messaging/health/messages/2060025': SecureMessagingMessageGetData
-  '/v1/messaging/health/messages/2060025/thread': SecureMessagingThreadGetData
+  '/v1/messaging/health/messages/2060025/thread?excludeProvidedMessage=true': SecureMessagingThreadGetData
   '/v0/messaging/health/messages/2060013': SecureMessagingMessageGetData
-  '/v1/messaging/health/messages/2060013/thread': SecureMessagingThreadGetData
+  '/v1/messaging/health/messages/2060013/thread?excludeProvidedMessage=true': SecureMessagingThreadGetData
   '/v0/messaging/health/messages/2060006': SecureMessagingMessageGetData
-  '/v1/messaging/health/messages/2060006/thread': SecureMessagingThreadGetData
+  '/v1/messaging/health/messages/2060006/thread?excludeProvidedMessage=true': SecureMessagingThreadGetData
 }
 
 /**
@@ -68,11 +68,11 @@ type sentFolderMessages = {
   '/v0/messaging/health/messages/2098929': SecureMessagingMessageGetData
   '/v0/messaging/health/messages/2098888': SecureMessagingMessageGetData
   '/v0/messaging/health/messages/2095016': SecureMessagingMessageGetData
-  '/v1/messaging/health/messages/2113100/thread': SecureMessagingThreadGetData
-  '/v1/messaging/health/messages/2098942/thread': SecureMessagingThreadGetData
-  '/v1/messaging/health/messages/2098929/thread': SecureMessagingThreadGetData
-  '/v1/messaging/health/messages/2098888/thread': SecureMessagingThreadGetData
-  '/v1/messaging/health/messages/2095016/thread': SecureMessagingThreadGetData
+  '/v1/messaging/health/messages/2113100/thread?excludeProvidedMessage=true': SecureMessagingThreadGetData
+  '/v1/messaging/health/messages/2098942/thread?excludeProvidedMessage=true': SecureMessagingThreadGetData
+  '/v1/messaging/health/messages/2098929/thread?excludeProvidedMessage=true': SecureMessagingThreadGetData
+  '/v1/messaging/health/messages/2098888/thread?excludeProvidedMessage=true': SecureMessagingThreadGetData
+  '/v1/messaging/health/messages/2095016/thread?excludeProvidedMessage=true': SecureMessagingThreadGetData
 }
 
 /**
@@ -82,9 +82,9 @@ type draftFolderMessages = {
   '/v0/messaging/health/messages/2113141': SecureMessagingMessageGetData
   '/v0/messaging/health/messages/2113020': SecureMessagingMessageGetData
   '/v0/messaging/health/messages/2092803': SecureMessagingMessageGetData
-  '/v1/messaging/health/messages/2113141/thread': SecureMessagingThreadGetData
-  '/v1/messaging/health/messages/2113020/thread': SecureMessagingThreadGetData
-  '/v1/messaging/health/messages/2092803/thread': SecureMessagingThreadGetData
+  '/v1/messaging/health/messages/2113141/thread?excludeProvidedMessage=true': SecureMessagingThreadGetData
+  '/v1/messaging/health/messages/2113020/thread?excludeProvidedMessage=true': SecureMessagingThreadGetData
+  '/v1/messaging/health/messages/2092803/thread?excludeProvidedMessage=true': SecureMessagingThreadGetData
   '"/v0/messaging/health/recipients"': SecureMessagingRecipientData
 }
 
