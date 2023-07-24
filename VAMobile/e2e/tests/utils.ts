c4e53af38a7e92edbdb74dcdca3c438bd3b39466
rbontrager <rachael.bontrager@adhocteam.us>
2023-07-24 11:45:57 -0500

    Detox test for Vaccine History and Details
    Initial detox test for vaccine history and details

--- VAMobile/e2e/tests/utils.ts
+++ VAMobile/e2e/tests/utils.ts
@@ -21,6 +21,7 @@ export const CommonE2eIdConstants = {
   LETTERS_ROW_TEXT: 'VA letters and documents',
   SETTINGS_ROW_TEXT: 'Settings',
   MILITARY_INFORMATION_ROW_TEXT: 'Military information',
+  VACCINE_RECORDS_BUTTON_TEXT: 'V\ufeffA vaccine records',
   SIGN_OUT_BTN_ID: 'Sign out',
   SIGN_OUT_CONFIRM_TEXT: 'Sign out?',
   BACK_BTN_LABEL: 'Back',
@@ -204,6 +205,10 @@ export async function openBenefits() {
 
 export async function openLetters() {
   await element(by.text(CommonE2eIdConstants.LETTERS_ROW_TEXT)).tap() 
+}
+
+export async function openVaccineRecords() {
+  await element(by.text(CommonE2eIdConstants.VACCINE_RECORDS_BUTTON_TEXT)).tap()
 }
 
 /**
