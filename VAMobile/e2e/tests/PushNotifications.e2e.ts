67d4b82b10e309960b8d24ec6a796ae5515e8bca
Theo Bentum <theo@adhocteam.us>
2023-08-08 09:51:28 -0700

    Add test for navigating back

--- VAMobile/e2e/tests/PushNotifications.e2e.ts
+++ VAMobile/e2e/tests/PushNotifications.e2e.ts
@@ -1,7 +1,12 @@
-import { by, device, element, waitFor } from 'detox'
-import { loginToDemoMode } from './utils';
+import { by, device, element, expect, waitFor } from 'detox'
+import { backButton, loginToDemoMode } from './utils';
 
-const SCREEN_TITLE = 'Review message'
+const PushNotificationsConstants = {
+  HEALTH_SCREEN_TITLE: 'Health',
+  MESSAGE_COMPOSE_BUTTON_TEXT: 'Start new message',
+  REVIEW_MESSAGE_SCREEN_TITLE: 'Review message'
+}
+
 const mockNotification = {
   "trigger": {
     "type": "push"
@@ -17,20 +22,28 @@ describe('Push Notifications', () => {
   it('should navigate to appropriate screen when launching the app from a dead state', async () => {
     await device.launchApp({delete: true, permissions: {notifications: 'YES'}, newInstance: true, userNotification: mockNotification});
     await loginToDemoMode()
-    await waitFor(element(by.text(SCREEN_TITLE))).toExist().withTimeout(8000)
+    await waitFor(element(by.text(PushNotificationsConstants.REVIEW_MESSAGE_SCREEN_TITLE))).toExist().withTimeout(8000)
   });
+
+  it('should navigate back to home screen after launch', async() => {
+    await backButton()
+    await waitFor(element(by.text(PushNotificationsConstants.MESSAGE_COMPOSE_BUTTON_TEXT))).toExist()
+    await backButton()
+    await expect(element(by.text('Home'))).toExist()
+  });
+
   it('should navigate to appropriate screen when the app is in the background', async () => {
     await device.launchApp({newInstance: true});
     await loginToDemoMode()
     await device.sendToHome();
     await device.launchApp({newInstance: false, userNotification: mockNotification});
-    await waitFor(element(by.text(SCREEN_TITLE))).toExist().withTimeout(8000)
+    await waitFor(element(by.text(PushNotificationsConstants.REVIEW_MESSAGE_SCREEN_TITLE))).toExist().withTimeout(8000)
   });
 
-  it('should navigate to the appropriate screen when the app is in the foreground', async () => {
+  it('should navigate to appropriate screen when the app is in the foreground', async () => {
     await device.launchApp({newInstance: true});
     await loginToDemoMode()
     await device.sendUserNotification(mockNotification);
-    await waitFor(element(by.text(SCREEN_TITLE))).toExist().withTimeout(8000)
+    await waitFor(element(by.text(PushNotificationsConstants.REVIEW_MESSAGE_SCREEN_TITLE))).toExist().withTimeout(8000)
   });
 });
