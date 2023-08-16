a9da103587771ee7fda63dae3f347d5372e5c2d8
Theo Bentum <theo@adhocteam.us>
2023-08-16 12:55:31 -0700

    Revert "Clean up sync screen test"
    This reverts commit 8c072a9f1adf17af092035d9c3c453ee0d72610e.

--- VAMobile/src/screens/SyncScreen/SyncScreen.test.tsx
+++ VAMobile/src/screens/SyncScreen/SyncScreen.test.tsx
@@ -4,10 +4,10 @@ import React from 'react'
 import { context, render, RenderAPI } from 'testUtils'
 import { ReactTestInstance } from 'react-test-renderer'
 
-import { initialAuthorizedServicesState, initialAuthState, initialMilitaryServiceState, initialPersonalInformationState } from 'store/slices'
+import { initialAuthorizedServicesState, initialAuthState, initialDisabilityRatingState, initialMilitaryServiceState, initialPersonalInformationState } from 'store/slices'
 import { SyncScreen } from './index'
 import TextView from '../../components/TextView'
-import { completeSync, getProfileInfo, getServiceHistory } from 'store/slices'
+import { completeSync, getDisabilityRating, getProfileInfo, getServiceHistory } from 'store/slices'
 
 jest.mock('store/slices', () => {
   let actual = jest.requireActual('store/slices')
@@ -31,6 +31,12 @@ jest.mock('store/slices', () => {
         payload: '',
       }
     }),
+    getDisabilityRating: jest.fn(() => {
+      return {
+        type: '',
+        payload: '',
+      }
+    }),
   }
 })
 
@@ -39,9 +45,10 @@ context('SyncScreen', () => {
   let component: RenderAPI
   let testInstance: ReactTestInstance
 
-  const initializeTestInstance = (militaryLoading = true, profileLoading = true, loggedIn = false, loggingOut = false, syncing = true): void => {
+  const initializeTestInstance = (militaryLoading = true, profileLoading = true, disabilityRatingLoading = true, loggedIn = false, loggingOut = false, syncing = true): void => {
     store = {
       auth: { ...initialAuthState, loggedIn, loggingOut, syncing },
+      disabilityRating: { ...initialDisabilityRatingState, preloadComplete: !disabilityRatingLoading },
       militaryService: { ...initialMilitaryServiceState, preloadComplete: !militaryLoading },
       personalInformation: { ...initialPersonalInformationState, preloadComplete: !profileLoading },
       authorizedServices: {
@@ -92,18 +99,27 @@ context('SyncScreen', () => {
       initializeTestInstance(true, true, true, true, false)
       expect(getProfileInfo).toHaveBeenCalled()
       expect(getServiceHistory).not.toHaveBeenCalled()
+      expect(getDisabilityRating).not.toHaveBeenCalled()
     })
 
     it('should load military history after profile has loaded', async () => {
       initializeTestInstance(true, false, true, true, false)
       expect(getProfileInfo).not.toHaveBeenCalled()
       expect(getServiceHistory).toHaveBeenCalled()
+      expect(getDisabilityRating).not.toHaveBeenCalled()
+    })
+
+    it('should load disability ratings after military history has loaded', async () => {
+      initializeTestInstance(false, false, true, true, false)
+      expect(getProfileInfo).not.toHaveBeenCalled()
+      expect(getServiceHistory).not.toHaveBeenCalled()
+      expect(getDisabilityRating).toHaveBeenCalled()
     })
   })
 
   describe('sync completion', () => {
     it('should complete the sync when all loading is finished', async () => {
-      initializeTestInstance(false, false, true, false)
+      initializeTestInstance(false, false, false, true, false)
       expect(completeSync).toHaveBeenCalled()
     })
   })
