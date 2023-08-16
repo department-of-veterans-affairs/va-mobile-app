4f9b2a02ddf654e4cafe864fe9be4efd171f4826
Dylan Nienberg <dylannienberg@gmail.com>
2023-08-16 08:27:29 -0500

    removed nevers

--- VAMobile/src/screens/HealthScreen/SecureMessaging/StartNewMessageButton/StartNewMessageButton.tsx
+++ VAMobile/src/screens/HealthScreen/SecureMessaging/StartNewMessageButton/StartNewMessageButton.tsx
@@ -3,18 +3,20 @@ import React, { FC } from 'react'
 
 import { Box, ButtonTypesConstants, VAButton, VAButtonProps } from 'components'
 import { Events } from 'constants/analytics'
+import { HealthStackParamList } from 'screens/HealthScreen/HealthStackScreens'
 import { NAMESPACE } from 'constants/namespaces'
+import { StackNavigationProp } from '@react-navigation/stack'
 import { logAnalyticsEvent } from 'utils/analytics'
 import { useNavigation } from '@react-navigation/native'
 import { useTheme } from 'utils/hooks'
 
 const StartNewMessageButton: FC = () => {
   const { t } = useTranslation(NAMESPACE.HEALTH)
-  const navigation = useNavigation()
+  const navigation = useNavigation<StackNavigationProp<HealthStackParamList, keyof HealthStackParamList>>()
   const theme = useTheme()
   const onPress = () => {
     logAnalyticsEvent(Events.vama_sm_start())
-    navigation.navigate('StartNewMessage' as never, { attachmentFileToAdd: {}, attachmentFileToRemove: {} } as never)
+    navigation.navigate('StartNewMessage', { attachmentFileToAdd: {}, attachmentFileToRemove: {} })
   }
 
   const startNewMessageButtonProps: VAButtonProps = {
