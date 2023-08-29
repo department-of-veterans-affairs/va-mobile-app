7bea11f075374d5081400e85e1ec203736949513
Tim Wright <timwright12@gmail.com>
2023-08-29 14:14:46 -0400

    Adding some tests back in

--- VAMobile/e2e/tests/DisabilityRatings.e2e.ts
+++ VAMobile/e2e/tests/DisabilityRatings.e2e.ts
@@ -1,4 +1,4 @@
-import { expect, device, by, element } from 'detox'
+import { expect, device, by, element} from 'detox'
 import {loginToDemoMode, openBenefits, openDisabilityRating } from './utils'
 import { setTimeout } from 'timers/promises'
 
@@ -19,7 +19,6 @@ beforeAll(async () => {
 
 describe('Veterans Crisis Line', () => {
   it('should match the disability ratings page design', async () => {
-    await device.takeScreenshot('StartDisabilityRatingsPageDesign')
     await expect(element(by.text(DisabilityRatingsIdConstants.COMBINED_DISABILITY_RATING_TEXT))).toExist()
     await expect(element(by.text(DisabilityRatingsIdConstants.COMBINED_DISABILITY_RATING_TEXT))).toExist()
     await expect(element(by.text(DisabilityRatingsIdConstants.INDIVIDUAL_RATING_TEXT))).toExist()
