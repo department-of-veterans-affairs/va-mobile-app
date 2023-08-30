b153d192ecc8ae5070dedc94f1f187b6332cbfc6
Tim Wright <timwright12@gmail.com>
2023-08-30 16:52:12 -0400

    Run HomeScreen retry  tests

--- VAMobile/e2e/tests/HomeScreen.e2e.ts
+++ VAMobile/e2e/tests/HomeScreen.e2e.ts
@@ -4,9 +4,9 @@ import { CommonE2eIdConstants, loginToDemoMode, backButton } from './utils'
 export const HomeE2eIdConstants = {
 	GREETING_ID: 'greeting-text',
 	PAYMENTS_BTN_ID: 'Payments',
-	LOCATION_FINDER_ROW_ID: 'Find a V\ufeffA location',
-	CONTACT_VA_ROW_ID: 'Contact V\ufeffA',
-	COVID_ROW_ID: 'Get the latest COVID\ufeff19 updates',
+	LOCATION_FINDER_ROW_ID: 'Find a  V-A  location',
+	CONTACT_VA_ROW_ID: 'Contact  V-A ',
+	COVID_ROW_ID: 'Get the latest  COVID-19  updates',
 	HOME_PAGE_USER_NAME: 'Kimberly Washington',
 	HOME_PAGE_MILITARY_BRANCH: 'United States Coast Guard',
 	CONTACT_VA_TITLE: 'Call My V-A 4 1 1',
