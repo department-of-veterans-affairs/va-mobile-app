bbcc8c32efcd69083569d132fa88fa682383809a
Theo Bentum <theo@adhocteam.us>
2023-09-14 00:45:43 -0700

    Add mutation for validating address

---
+++ VAMobile/src/api/contactInformation/validateAddress.tsx
@@ -0,0 +1,35 @@
+import { Params as APIParams, post } from 'store/api'
+import { AddressData, AddressValidationData, ValidateAddressData } from 'api/types'
+import { getConfirmedSuggestions, getSuggestedAddresses, getValidationKey } from 'utils/personalInformation'
+import { isErrorObject } from 'utils/common'
+import { logNonFatalErrorToFirebase } from 'utils/analytics'
+import { useMutation } from '@tanstack/react-query'
+
+/**
+ * Validate's an address and returns the validation key of the suggested address with the highest confidence score
+ */
+export const validateAddress = async (addressData: AddressData): Promise<ValidateAddressData> => {
+  try {
+    const response = await post<AddressValidationData>('/v0/user/addresses/validate', addressData as unknown as APIParams)
+    const suggestedAddresses = getSuggestedAddresses(response)
+    const confirmedSuggestedAddresses = getConfirmedSuggestions(suggestedAddresses)
+    const validationKey = getValidationKey(suggestedAddresses)
+    return { confirmedSuggestedAddresses, suggestedAddresses, validationKey }
+  } catch (error) {
+    throw error
+  }
+}
+
+/**
+ * Returns a mutation for validating an address
+ */
+export const useValidateAddress = () => {
+  return useMutation({
+    mutationFn: validateAddress,
+    onError: async (error) => {
+      if (isErrorObject(error)) {
+        logNonFatalErrorToFirebase(error, 'validateAddress: Service error')
+      }
+    },
+  })
+}
