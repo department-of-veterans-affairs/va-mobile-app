568ea1ce097db91bb65856a997dfa32a7b9dc856
Dylan Nienberg <87150991+Sparowhawk@users.noreply.github.com>
2023-10-10 10:23:08 -0500

    Update VAMobile/src/api/types/PersonalInformationData.ts
    Co-authored-by: Theo Bentum <theo@adhocteam.us>

--- VAMobile/src/api/types/PersonalInformationData.ts
+++ VAMobile/src/api/types/PersonalInformationData.ts
@@ -8,7 +8,7 @@ export type PersonalInformationPayload = {
       lastName: string
       signinEmail: string
       signinService: string
-      birthDate: string
+      birthDate: string | null
     }
   }
 }
