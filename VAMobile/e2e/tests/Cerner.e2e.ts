cecabf6dc5af8cbfa182b4743f02e4686abc54a3
rbontrager <rachael.bontrager@adhocteam.us>
2023-10-13 13:58:42 -0500

    Remove mock data changes

--- VAMobile/e2e/tests/Cerner.e2e.ts
+++ VAMobile/e2e/tests/Cerner.e2e.ts
@@ -1,23 +1,20 @@
 import { expect, device, by, element} from 'detox'
-import {loginToDemoMode, openHealth, changeMockData, openAppointments, openMessages } from './utils'
+import {loginToDemoMode, openHealth, openAppointments, openMessages } from './utils'
 import { setTimeout } from 'timers/promises'
 
 export const CernerIdConstants = {
   GO_TO_VA_HEALTH_LINK_ID: 'goToMyVAHealthTestID',
   HEALTH_CATEGORY_ID: 'healthCategoryTestID',
-  CERNER_NOTE_HEADING_TEXT: 'Some of your VA health care team may be using the My VA Health portal',
+  CERNER_NOTE_HEADING_TEXT: 'Your VA health care team may be using the My VA Health portal',
   CERNER_NOTE_RECORDS_SHOW_TEXT: 'Our records show you’re registered at:',
   CERNER_NOTE_FACILITY_TEXT: 'Cary VA Medical Center (Now using My VA Health)',
+  CERNER_NOTE_FACILITY_2_TEXT: 'Cheyenne VA Medical Center (Now using My VA Health)',
   CERNER_NOTE_MESSAGES_TEXT: 'This facility currently uses our My VA Health portal. You\'ll need to go there to send your message.',
   CERNER_NOTE_MESSAGES_HEADER_TEXT: 'Make sure you\'re in the right health portal',
 }
 
 beforeAll(async () => {
   if(device.getPlatform() === 'android') {
-    await changeMockData('getFacilitiesInfo.json', ['/v0/facilities-info', 'data', 'attributes', {'facilities': 0}, 'cerner'], true)
-    await device.launchApp({newInstance: true})
-    await changeMockData('getFacilitiesInfo.json', ['/v0/facilities-info', 'data', 'attributes', {'facilities': 1}, 'cerner'], false)
-    await device.launchApp({newInstance: true})
     await loginToDemoMode()
     await openHealth()
   }
@@ -29,11 +26,12 @@ describe(':android: Cerner Notice', () => {
     await element(by.text(CernerIdConstants.CERNER_NOTE_HEADING_TEXT)).tap()
     await element(by.id(CernerIdConstants.HEALTH_CATEGORY_ID)).swipe('up')
     await expect(element(by.text(CernerIdConstants.CERNER_NOTE_FACILITY_TEXT))).toExist()
-    await expect(element(by.text(CernerIdConstants.CERNER_NOTE_FACILITY_TEXT))).toExist()
+    await expect(element(by.text(CernerIdConstants.CERNER_NOTE_FACILITY_2_TEXT))).toExist()
     await expect(element(by.id(CernerIdConstants.GO_TO_VA_HEALTH_LINK_ID))).toExist()
   })
 
   it('should tap the go to My VA Health link and verify the correct webpage is opened', async () => {
+    await element(by.id(CernerIdConstants.HEALTH_CATEGORY_ID)).scrollTo('bottom')
     await element(by.id(CernerIdConstants.GO_TO_VA_HEALTH_LINK_ID)).tap()
     await element(by.text('Ok')).tap()
     await setTimeout(5000)
@@ -44,7 +42,7 @@ describe(':android: Cerner Notice', () => {
   it('should tap on the cerner notification and verify the alert closes', async () => {
     await element(by.text(CernerIdConstants.CERNER_NOTE_HEADING_TEXT)).tap()
     await expect(element(by.text(CernerIdConstants.CERNER_NOTE_FACILITY_TEXT))).not.toExist()
-    await expect(element(by.text(CernerIdConstants.CERNER_NOTE_FACILITY_TEXT))).not.toExist()
+    await expect(element(by.text(CernerIdConstants.CERNER_NOTE_FACILITY_2_TEXT))).not.toExist()
   })
 
   it('should tap on appointments and verify the cerner notification is present and collapsed', async () => {
@@ -61,22 +59,6 @@ describe(':android: Cerner Notice', () => {
     await expect(element(by.text(CernerIdConstants.CERNER_NOTE_MESSAGES_TEXT))).not.toExist()
   })
 
-  it('should tap on the cerner notification and verify the correct information is displayed for one facility', async () => {
-    await element(by.text(CernerIdConstants.CERNER_NOTE_MESSAGES_HEADER_TEXT)).tap()
-    await expect(element(by.text('Sending a message to a care team at Cary VA Medical Center?'))).toExist()
-    await expect(element(by.text(CernerIdConstants.CERNER_NOTE_MESSAGES_TEXT))).toExist()
-    await expect(element(by.id(CernerIdConstants.GO_TO_VA_HEALTH_LINK_ID))).toExist()
-  })
-
-  it('should add another facility to the mock data file', async () => {
-    //await changeMockData('profile.json', ['/v1/user', 'data', 'attributes', 'health', {'facilities': 1}, 'facilityName'], 'San Francisco VA Medical Center')
-    await changeMockData('getFacilitiesInfo.json', ['/v0/facilities-info', 'data', 'attributes', {'facilities': 1}, 'cerner'], true)
-    await device.launchApp({newInstance: true})
-		await loginToDemoMode()
-		await openHealth()
-    await openMessages()
-  })
-
   it('should tap on the cerner notification and verify the correct information is displayed for multiple facilities', async () => {
     await element(by.text(CernerIdConstants.CERNER_NOTE_MESSAGES_HEADER_TEXT)).tap()
     await expect(element(by.text('Sending a message to a care team at one of these health facilities?'))).toExist()
@@ -84,24 +66,5 @@ describe(':android: Cerner Notice', () => {
     await expect(element(by.text('Cary VA Medical Center'))).toExist()
     await expect(element(by.text('These facilities currently use our My VA Health portal. You\'ll need to go there to send your message.'))).toExist()
     await expect(element(by.id(CernerIdConstants.GO_TO_VA_HEALTH_LINK_ID))).toExist()
-  })
-
-  it('should remove secure messaging from authorizedServices in the mock data file', async () => {
-    await changeMockData('getAuthorizedServices.json', ['/v0/user/authorized-services', 'data', 'attributes', 'authorizedServices', 'secureMessaging'], false)
-    await device.launchApp({newInstance: true})
-    await loginToDemoMode()
-    await openHealth()
-    await openMessages()
-  })
-
-  it('should verify that no cerner notification is displayed when the user does not have access to secure messaging', async () => {
-    await expect(element(by.text('You\'re not currently enrolled to use Secure Messaging'))).toExist()
-  })
-
-  it('should reset mock data', async () => {
-    await changeMockData('getAuthorizedServices.json', ['/v0/user/authorized-services', 'data', 'attributes', 'authorizedServices', 'secureMessaging'], true)
-    await device.launchApp({newInstance: true})
-    await changeMockData('getFacilitiesInfo.json', ['/v0/facilities-info', 'data', 'attributes', {'facilities': 1}, 'cerner'], false)
-    await device.launchApp({newInstance: true})
   })
 })
