aaeffd13cb0e7efc173313bf6069d8f0d8d2e139
Theo Bentum <theo@adhocteam.us>
2023-10-17 00:24:38 -0700

    Replace instances of querystringify with URLSearchParams

--- VAMobile/src/utils/location.tsx
+++ VAMobile/src/utils/location.tsx
@@ -1,6 +1,5 @@
 import { AppointmentLocation } from '../store/api'
 import { isIOS } from './platform'
-import qs from 'querystringify'
 
 const APPLE_MAPS_BASE_URL = 'https://maps.apple.com/'
 const GOOGLE_MAPS_BASE_URL = 'https://www.google.com/maps/dir/'
@@ -9,17 +8,17 @@ export const getDirectionsUrl = (location: AppointmentLocation): string => {
   const { name, lat, long } = location
   const destinationString = `${Object.values(location.address || {}).join(' ')}+${name}+${lat},${long}`
   if (isIOS()) {
-    const q = qs.stringify({
+    const queryString = new URLSearchParams({
       // apply type parameter = m (map)
       t: 'm',
       daddr: destinationString,
-    })
-    return `${APPLE_MAPS_BASE_URL}?${q}`
+    }).toString()
+    return `${APPLE_MAPS_BASE_URL}?${queryString}`
   } else {
-    const q = qs.stringify({
-      api: 1,
+    const queryString = new URLSearchParams({
+      api: '1',
       destination: destinationString,
-    })
-    return `${GOOGLE_MAPS_BASE_URL}?${q}`
+    }).toString()
+    return `${GOOGLE_MAPS_BASE_URL}?${queryString}`
   }
 }
