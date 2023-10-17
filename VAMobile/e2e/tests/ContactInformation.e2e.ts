7614a68bb4cbf8718a018a0cc9168906bbc6d52d
rbontrager <rachael.bontrager@adhocteam.us>
2023-10-17 10:23:17 -0500

    feature/7018-rachael-detox-contact-info-updates
    Update contact info

--- VAMobile/e2e/tests/ContactInformation.e2e.ts
+++ VAMobile/e2e/tests/ContactInformation.e2e.ts
@@ -18,7 +18,193 @@ export const ContactInfoE2eIdConstants = {
   STATE_ID: 'stateTestID',
   ZIP_CODE_ID: 'zipCodeTestID',
   PHONE_NUMBER_EXTENSION_ID: 'phoneNumberExtensionTestID',
-  PHONE_NUMBER_ID: 'phoneNumberTestID'
+  PHONE_NUMBER_ID: 'phoneNumberTestID',
+  CANCEL_DELETE_TEXT: device.getPlatform() === 'ios' ? 'Delete Changes' : 'Delete Changes ',
+  CANCEL_KEEP_EDITING_TEXT: device.getPlatform() === 'ios' ? 'Keep Editing' : 'Keep Editing ',
+  REMOVE_KEEP_TEXT: 'Keep',
+  REMOVE_REMOVE_TEXT: 'Remove',
+  SAVE_TEXT: 'Save',
+  DISMISS_TEXT: 'Dismiss'
+}
+
+export async function updateAddress() {
+  await element(by.id(ContactInfoE2eIdConstants.STREET_ADDRESS_LINE_2_ID)).tapReturnKey()
+  await element(by.id(ContactInfoE2eIdConstants.COUNTRY_PICKER_ID)).tap()
+  await element(by.text('United States')).tap()
+  await element(by.text('Done')).tap()
+  await element(by.id(ContactInfoE2eIdConstants.STREET_ADDRESS_LINE_1_ID)).typeText('3101 N Fort Valley Rd')
+  await element(by.id(ContactInfoE2eIdConstants.STREET_ADDRESS_LINE_1_ID)).tapReturnKey()
+  await element(by.id('EditAddressTestID')).scrollTo('bottom')
+  await element(by.id(ContactInfoE2eIdConstants.CITY_TEST_ID)).replaceText('Flagstaff')
+  await element(by.id('stateTestID')).tap()
+  await element(by.text('Arizona')).atIndex(0).tap()
+  await element(by.text('Done')).tap()
+  await element(by.id(ContactInfoE2eIdConstants.ZIP_CODE_ID)).replaceText('86001')
+}
+
+export async function validateAddresses(addressID, addressType) {
+  it('should open and update the ' + addressType + ' address', async () => {
+    await element(by.id(addressID)).tap()
+    await element(by.id(ContactInfoE2eIdConstants.STREET_ADDRESS_LINE_2_ID)).replaceText('2')
+    if(addressType === 'Home') {
+      await updateAddress()
+    }
+  })
+
+  it('should tap on the cancel button and verify a delete changes pop up appears', async () => {
+    await element(by.text('Cancel')).tap()
+    await expect(element(by.text('Delete changes to your ' + addressType.toLowerCase() + ' address?'))).toExist()
+    await expect(element(by.text(ContactInfoE2eIdConstants.CANCEL_DELETE_TEXT))).toExist()
+    await expect(element(by.text(ContactInfoE2eIdConstants.CANCEL_KEEP_EDITING_TEXT))).toExist()
+  })
+
+  it('should tap keep editing and return to the ' + addressType + ' address screen', async () => {
+    await element(by.text(ContactInfoE2eIdConstants.CANCEL_KEEP_EDITING_TEXT)).tap()
+    await expect(element(by.text('United States'))).toExist()
+    await expect(element(by.text('3101 N Fort Valley Rd')).atIndex(0)).toExist()
+    await expect(element(by.text('2'))).toExist()
+    await expect(element(by.text('Flagstaff'))).toExist()
+    await expect(element(by.text('Arizona'))).toExist()
+    await expect(element(by.text('86001'))).toExist()
+  })
+
+  it('should tap on the cancel button, tap delete, and return to the contact info screen', async () => {
+    await element(by.text('Cancel')).tap()
+    await element(by.text(ContactInfoE2eIdConstants.CANCEL_DELETE_TEXT)).tap()
+    await expect(element(by.id(addressID))).toExist()
+  })
+
+  it('should open and update the ' + addressType + ' address', async () => {
+    await element(by.id(addressID)).tap()
+    await element(by.id(ContactInfoE2eIdConstants.STREET_ADDRESS_LINE_2_ID)).replaceText('2')
+    if(addressType === 'Home') {
+      await updateAddress()
+    }
+  })
+
+  it('should tap on the save button and verify your address screen is displayed', async () => {
+    await element(by.text(ContactInfoE2eIdConstants.SAVE_TEXT)).tap()
+    await expect(element(by.id('verifyYourAddressTestID'))).toExist()
+    await element(by.id('verifyYourAddressTestID')).tap()
+    await expect(element(by.text('We can\'t confirm the address you entered with the U.S. Postal Service.'))).toExist()
+    await element(by.text('Verify your address')).tap()
+  })
+
+  it('should tap on edit address and return to the ' + addressType + ' address screen', async () => {
+    await element(by.id('Edit this address.')).tap()
+    await expect(element(by.text('United States'))).toExist()
+    await expect(element(by.text('3101 N Fort Valley Rd')).atIndex(0)).toExist()
+    await expect(element(by.text('2'))).toExist()
+    await expect(element(by.text('Flagstaff'))).toExist()
+    await expect(element(by.text('Arizona'))).toExist()
+    await expect(element(by.text('86001'))).toExist()
+  })
+
+  it('should tap on the save button and verify your address screen is displayed', async () => {
+    await element(by.text(ContactInfoE2eIdConstants.SAVE_TEXT)).tap()
+    await expect(element(by.id('verifyYourAddressTestID'))).toExist()
+  })
+
+  it('should tap on use this address and return to the contact information screen', async () => {
+    await element(by.id('suggestedAddressTestID')).tap()
+    await element(by.id('Use this address')).tap()
+    await expect(element(by.text(addressType + ' address saved'))).toExist()
+    await element(by.text(ContactInfoE2eIdConstants.DISMISS_TEXT)).tap()
+  })
+
+  it('should verify that the ' + addressType + ' address has been updated', async () => {
+    await expect(element(by.id(addressType + ' address 3101 N Fort Valley Rd, 2 Flagstaff, AZ, 86001'))).toExist()
+  })
+}
+
+export async function validatePhoneNumbers(phoneID, phoneType) {
+  it ('should open the ' + phoneType + ' phone number', async () => {
+    if(phoneType === 'Mobile') {
+      await element(by.id('ContactInfoTestID')).scrollTo('bottom')
+    }
+    await element(by.id(phoneID)).tap()
+  })
+
+  it('should update the ' + phoneType + ' phone with an extension', async () => {
+    await element(by.id(ContactInfoE2eIdConstants.PHONE_NUMBER_ID)).clearText()
+    await element(by.id(ContactInfoE2eIdConstants.PHONE_NUMBER_ID)).typeText('276-608-6180')
+    await element(by.id(ContactInfoE2eIdConstants.PHONE_NUMBER_ID)).tapReturnKey()
+    await element(by.id(ContactInfoE2eIdConstants.PHONE_NUMBER_EXTENSION_ID)).replaceText('1234')
+  })
+
+  it('should tap on the cancel button and verify a delete changes pop up appears', async () => {
+    await element(by.text('Cancel')).tap()
+    await expect(element(by.text('Delete changes to your ' + phoneType.toLowerCase() + ' phone number?'))).toExist()
+    await expect(element(by.text(ContactInfoE2eIdConstants.CANCEL_DELETE_TEXT))).toExist()
+    await expect(element(by.text(ContactInfoE2eIdConstants.CANCEL_KEEP_EDITING_TEXT))).toExist()
+  })
+
+  it('should tap keep editing and return to the ' + phoneType + ' phone screen', async() => {
+    await element(by.text(ContactInfoE2eIdConstants.CANCEL_KEEP_EDITING_TEXT)).tap()
+    await expect(element(by.text('276-608-6180')).atIndex(0)).toExist()
+    await expect(element(by.text('1234'))).toExist()    
+  })
+
+  it('should tap cancel, tap delete changes, and verify the contact info page is displayed', async () => {
+    await element(by.text('Cancel')).tap()
+    await element(by.text(ContactInfoE2eIdConstants.CANCEL_DELETE_TEXT)).tap()
+    await expect(element(by.id(phoneID))).toExist()
+  })
+
+  it('should update the ' + phoneType + ' with an extension', async () => {
+    await element(by.id(phoneID)).tap()
+    await element(by.id(ContactInfoE2eIdConstants.PHONE_NUMBER_ID)).clearText()
+    await element(by.id(ContactInfoE2eIdConstants.PHONE_NUMBER_ID)).typeText('276-608-6180')
+    await element(by.id(ContactInfoE2eIdConstants.PHONE_NUMBER_ID)).tapReturnKey()
+    await element(by.id(ContactInfoE2eIdConstants.PHONE_NUMBER_EXTENSION_ID)).replaceText('1234')
+    await element(by.text(ContactInfoE2eIdConstants.SAVE_TEXT)).tap()
+    await expect(element(by.text(phoneType + ' phone saved'))).toExist()
+    await element(by.text(ContactInfoE2eIdConstants.DISMISS_TEXT)).tap()
+  })
+  
+  it('should update the ' + phoneType + ' phone number and remove the extension', async () => {
+    await element(by.id(phoneID)).tap()
+    await element(by.id(ContactInfoE2eIdConstants.PHONE_NUMBER_EXTENSION_ID)).clearText()
+    await element(by.text(ContactInfoE2eIdConstants.SAVE_TEXT)).tap()
+    await expect(element(by.text(phoneType + ' phone saved'))).toExist()
+    await element(by.text(ContactInfoE2eIdConstants.DISMISS_TEXT)).tap()
+  })
+}
+
+export async function removeContactInfoFeature(contactInfoTypeText, type) {
+  it ('should tap remove ' + type + ' and verify the remove pop up appears', async () => {
+    if(type === 'mobile phone' || type === 'email address') {
+      await element(by.id('ContactInfoTestID')).scrollTo('bottom')
+    }
+    await element(by.id(contactInfoTypeText)).tap()
+    await element(by.text('Remove ' + type)).tap()
+    await expect(element(by.text('Remove your ' + type + '?'))).toExist()
+    await expect(element(by.text('We\'ll remove your ' + type + ' from many VA records. You can always add it to your profile again.'))).toExist()
+  })
+
+  it('should tap keep and verify the ' + type + ' information appears', async () => {
+    await element(by.text(ContactInfoE2eIdConstants.REMOVE_KEEP_TEXT)).tap()
+    if(type === 'mobile phone' || type === 'work phone' || type == 'home phone'){
+      await expect(element(by.text('276-608-6180')).atIndex(1)).toExist()
+    } else if (type === 'home address') {
+      await expect(element(by.text('3101 N Fort Valley Rd'))).toExist()
+    } else if (type === 'email address') {
+      await expect(element(by.text('attended1@gmail.com')).atIndex(1)).toExist()
+    }
+  })
+
+  it('should remove the ' + type + ' and verify it has been removed in contact info', async () => {
+    await element(by.text('Remove ' + type)).tap()
+    await element(by.text(ContactInfoE2eIdConstants.REMOVE_REMOVE_TEXT)).tap()
+    await element(by.text(ContactInfoE2eIdConstants.DISMISS_TEXT)).tap()
+    if(type === 'home phone' || type == 'work phone'){
+      await expect(element(by.text('Add your ' + type + ' number'))).toExist()
+    } else if (type === 'mobile phone') {
+      await expect(element(by.text('Add your cell phone number'))).toExist()
+    } else {
+      await expect(element(by.text('Add your ' + type))).toExist()
+    }
+  })
 }
 
 beforeAll(async () => {
@@ -45,141 +231,11 @@ describe('Contact Info Screen', () => {
     await element(by.text('Close')).atIndex(0).tap()
   })
 
-  it('should open and update the mailing address', async () => {
-    await element(by.id(ContactInfoE2eIdConstants.MAILING_ADDRESS_ID)).tap()
-    await element(by.id(ContactInfoE2eIdConstants.STREET_ADDRESS_LINE_2_ID)).replaceText('2')
-  })
-
-  it('should tap on the save button and verify your address screen is displayed', async () => {
-    await element(by.text('Save')).tap()
-    await expect(element(by.id('verifyYourAddressTestID'))).toExist()
-    await element(by.id('verifyYourAddressTestID')).tap()
-    await expect(element(by.text('We can\'t confirm the address you entered with the U.S. Postal Service.'))).toExist()
-    await element(by.text('Verify your address')).tap()
-  })
-
-  it('should tap on edit address and return to the mailing address screen', async () => {
-    await element(by.id('Edit this address.')).tap()
-    await expect(element(by.text('United States'))).toExist()
-    await expect(element(by.text('3101 N Fort Valley Rd')).atIndex(0)).toExist()
-    await expect(element(by.text('2'))).toExist()
-    await expect(element(by.text('Flagstaff'))).toExist()
-    await expect(element(by.text('Arizona'))).toExist()
-    await expect(element(by.text('86001'))).toExist()
-  })
-
-  it('should tap on the save button and verify your address screen is displayed', async () => {
-    await element(by.text('Save')).tap()
-    await expect(element(by.id('verifyYourAddressTestID'))).toExist()
-  })
-
-  it('should tap on use this address and return to the contact information screen', async () => {
-    await element(by.id('suggestedAddressTestID')).tap()
-    await element(by.id('Use this address')).tap()
-    await expect(element(by.text('Mailing address saved'))).toExist()
-    await element(by.text('Dismiss')).tap()
-  })
-
-  it('should open the home address', async () => {
-    await element(by.text('Home address')).tap()
-  })
-
-  it('should select the I live on a U.S. military base... checkbox and verify that the correct fields are displayed', async () => {
-    await element(by.id('USMilitaryBaseCheckboxTestID')).tap()
-    await expect(element(by.id(ContactInfoE2eIdConstants.STREET_ADDRESS_LINE_1_ID))).toExist()
-    await expect(element(by.id(ContactInfoE2eIdConstants.STREET_ADDRESS_LINE_2_ID))).toExist()
-    await expect(element(by.id(ContactInfoE2eIdConstants.STREET_ADDRESS_LINE_3_ID))).toExist()
-    await expect(element(by.id(ContactInfoE2eIdConstants.MILITARY_POST_OFFICE_ID))).toExist()
-    await expect(element(by.id(ContactInfoE2eIdConstants.STATE_ID))).toExist()
-    await expect(element(by.text('Country (Required)'))).not.toBeVisible()
-  })
-
-  it('should unselect I live on a U.S military base... checkbox and update the home address', async () => {
-    await element(by.id('USMilitaryBaseCheckboxTestID')).tap()
-    await element(by.id(ContactInfoE2eIdConstants.COUNTRY_PICKER_ID)).tap()
-    await element(by.text('United States')).tap()
-    await element(by.text('Done')).tap()
-    await element(by.id(ContactInfoE2eIdConstants.STREET_ADDRESS_LINE_1_ID)).clearText()
-    await element(by.id(ContactInfoE2eIdConstants.STREET_ADDRESS_LINE_1_ID)).replaceText('3101 N Fort Valley Rd')
-    await element(by.id(ContactInfoE2eIdConstants.STREET_ADDRESS_LINE_2_ID)).replaceText('2')
-    await element(by.id('EditAddressTestID')).scrollTo('bottom')
-    await element(by.id(ContactInfoE2eIdConstants.CITY_TEST_ID)).replaceText('Flagstaff')
-    await element(by.id('stateTestID')).tap()
-    await element(by.text('Arizona')).tap()
-    await element(by.text('Done')).tap()
-    await element(by.id(ContactInfoE2eIdConstants.ZIP_CODE_ID)).replaceText('86001')
-    await element(by.text('Save')).tap()
-    await element(by.id('suggestedAddressTestID')).tap()
-    await element(by.id('Use this address')).tap()
-    await expect(element(by.text('Home address saved'))).toExist()
-    await element(by.text('Dismiss')).tap()
-  })
-
-  it ('should open the home phone number', async () => {
-    await element(by.id(ContactInfoE2eIdConstants.HOME_PHONE_ID)).tap()
-  })
-
-  it('should update the home phone with an extension', async () => {
-    await element(by.id(ContactInfoE2eIdConstants.PHONE_NUMBER_ID)).replaceText('276-608-6180')
-    await element(by.id(ContactInfoE2eIdConstants.PHONE_NUMBER_EXTENSION_ID)).replaceText('1234')
-    await element(by.text('Save')).tap()
-    await expect(element(by.text('Home phone saved'))).toExist()
-    await element(by.text('Dismiss')).tap()
-  })
-  
-  it('should update the home phone number and remove the extension', async () => {
-    await element(by.id(ContactInfoE2eIdConstants.HOME_PHONE_ID)).tap()
-    await element(by.id(ContactInfoE2eIdConstants.PHONE_NUMBER_EXTENSION_ID)).clearText()
-    await element(by.text('Save')).tap()
-    await expect(element(by.text('Home phone saved'))).toExist()
-    await element(by.text('Dismiss')).tap()
-  })
-
-  it ('should open the work phone number', async () => {
-    await element(by.id(ContactInfoE2eIdConstants.WORK_PHONE_ID)).tap()
-  })
-
-  it('should update the work phone with an extension', async () => {
-    await element(by.id(ContactInfoE2eIdConstants.PHONE_NUMBER_ID)).replaceText('276-608-6180')
-    await element(by.id(ContactInfoE2eIdConstants.PHONE_NUMBER_EXTENSION_ID)).replaceText('1234')
-    await element(by.text('Save')).tap()
-    await expect(element(by.text('Work phone saved'))).toExist()
-    await element(by.text('Dismiss')).tap()
-  })
-
-  it('should update the work phone number and remove the extension', async () => {
-    await element(by.id(ContactInfoE2eIdConstants.WORK_PHONE_ID)).tap()
-    await element(by.id(ContactInfoE2eIdConstants.PHONE_NUMBER_EXTENSION_ID)).clearText()
-    await element(by.text('Save')).tap()
-    await expect(element(by.text('Work phone saved'))).toExist()
-    await element(by.text('Dismiss')).tap()
-  })
-
-  it ('should open the mobile phone number', async () => {
-    await device.launchApp({newInstance: true})
-    await loginToDemoMode()
-    await openProfile()
-    await openContactInfo()
-    await element(by.id('ContactInfoTestID')).scrollTo('bottom')
-    await element(by.id(ContactInfoE2eIdConstants.MOBILE_PHONE_ID)).tap()
-  })
-
-  it('should update the mobile phone with an extension', async () => {
-    await element(by.id(ContactInfoE2eIdConstants.PHONE_NUMBER_ID)).replaceText('276-608-6180')
-    await element(by.id(ContactInfoE2eIdConstants.PHONE_NUMBER_EXTENSION_ID)).replaceText('1234')
-    await element(by.text('Save')).tap()
-    await expect(element(by.text('Mobile phone saved'))).toExist()
-    await element(by.text('Dismiss')).tap()
-  })
-
-  it('should update the mobile phone number and remove the extension', async () => {
-    await element(by.id('ContactInfoTestID')).scrollTo('bottom')
-    await element(by.id(ContactInfoE2eIdConstants.MOBILE_PHONE_ID)).tap()
-    await element(by.id(ContactInfoE2eIdConstants.PHONE_NUMBER_EXTENSION_ID)).clearText()
-    await element(by.text('Save')).tap()
-    await expect(element(by.text('Mobile phone saved'))).toExist()
-    await element(by.text('Dismiss')).tap()
-  })
+  validateAddresses(ContactInfoE2eIdConstants.MAILING_ADDRESS_ID, 'Mailing')
+  validateAddresses('Home address Add your home address', 'Home')
+  validatePhoneNumbers(ContactInfoE2eIdConstants.HOME_PHONE_ID, 'Home')
+  validatePhoneNumbers(ContactInfoE2eIdConstants.WORK_PHONE_ID, 'Work')
+  validatePhoneNumbers(ContactInfoE2eIdConstants.MOBILE_PHONE_ID, 'Mobile')
 
   it ('should open the email address', async () => {
     await element(by.id('ContactInfoTestID')).scrollTo('bottom')
@@ -188,17 +244,24 @@ describe('Contact Info Screen', () => {
 
   it('should update the email address with a +', async () => {
     await element(by.id('emailAddressEditTestID')).replaceText('attended1+@gmail.com')
-    await element(by.text('Save')).tap()
+    await element(by.text(ContactInfoE2eIdConstants.SAVE_TEXT)).tap()
     await expect(element(by.text('Email address saved'))).toExist()
-    await element(by.text('Dismiss')).tap()
+    await element(by.text(ContactInfoE2eIdConstants.DISMISS_TEXT)).tap()
   })
 
   it('should update the email address and remove the +', async () => {
     await element(by.id('ContactInfoTestID')).scrollTo('bottom')
     await element(by.id(ContactInfoE2eIdConstants.EMAIL_ADDRESS_ID)).tap()
     await element(by.id('emailAddressEditTestID')).replaceText('attended1@gmail.com')
-    await element(by.text('Save')).tap()
+    await element(by.text(ContactInfoE2eIdConstants.SAVE_TEXT)).tap()
     await expect(element(by.text('Email address saved'))).toExist()
-    await element(by.text('Dismiss')).tap()
+    await element(by.text(ContactInfoE2eIdConstants.DISMISS_TEXT)).tap()
+    await element(by.id('ContactInfoTestID')).scrollTo('top')
   })
+
+  removeContactInfoFeature('Home address 3101 N Fort Valley Rd, 2 Flagstaff, AZ, 86001', 'home address')
+  removeContactInfoFeature(ContactInfoE2eIdConstants.WORK_PHONE_ID, 'work phone')
+  removeContactInfoFeature(ContactInfoE2eIdConstants.HOME_PHONE_ID, 'home phone')
+  removeContactInfoFeature(ContactInfoE2eIdConstants.MOBILE_PHONE_ID, 'mobile phone')
+  removeContactInfoFeature(ContactInfoE2eIdConstants.EMAIL_ADDRESS_ID, 'email address')
 })
