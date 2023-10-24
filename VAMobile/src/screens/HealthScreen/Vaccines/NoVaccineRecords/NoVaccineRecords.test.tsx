9b5ced1cddea20cf221d19922153a405033e390a
Dylan Nienberg <dylannienberg@gmail.com>
2023-10-24 13:12:43 -0500

    letters

--- VAMobile/src/screens/HealthScreen/Vaccines/NoVaccineRecords/NoVaccineRecords.test.tsx
+++ VAMobile/src/screens/HealthScreen/Vaccines/NoVaccineRecords/NoVaccineRecords.test.tsx
@@ -1,36 +1,34 @@
 import 'react-native'
 import React from 'react'
 
-import { context, render, RenderAPI } from 'testUtils'
-import { act } from 'react-test-renderer'
-
+import { context, render } from 'testUtils'
+import { screen, fireEvent } from '@testing-library/react-native'
 import NoVaccineRecords from './NoVaccineRecords'
-import { Linking, TouchableWithoutFeedback } from 'react-native'
+import { Linking } from 'react-native'
 
 context('NoVaccineRecords', () => {
-  let component: RenderAPI
-  let testInstance: any
-
   beforeEach(() => {
-    component = render(<NoVaccineRecords />)
-
-    testInstance = component.UNSAFE_root
+    render(<NoVaccineRecords />)
   })
 
   it('initializes correctly', async () => {
-    expect(component).toBeTruthy()
+    expect(screen.getByText("We couldn't find information about your VA vaccines")).toBeTruthy()
+    expect(screen.getByText("We're sorry. We update your vaccine records every 24 hours, but new records can take up to 36 hours to appear.")).toBeTruthy()
+    expect(screen.getByText("If you think your vaccine records should be here, call our MyVA411 main information line. We're here 24/7.")).toBeTruthy()
+    expect(screen.getByText('800-698-2411')).toBeTruthy()
+    expect(screen.getByText('TTY: 711')).toBeTruthy()
   })
 
   describe('when the My HealtheVet phone number link is clicked', () => {
     it('should call Linking open url with the parameter tel:8006982411', async () => {
-      testInstance.findAllByType(TouchableWithoutFeedback)[0].props.onPress()
+      fireEvent.press(screen.getByText('800-698-2411'))
       expect(Linking.openURL).toBeCalledWith('tel:8006982411')
     })
   })
 
   describe('when the call TTY phone link is clicked', () => {
     it('should call Linking open url with the parameter tel:711', async () => {
-      testInstance.findAllByType(TouchableWithoutFeedback)[1].props.onPress()
+      fireEvent.press(screen.getByText('TTY: 711'))
       expect(Linking.openURL).toBeCalledWith('tel:711')
     })
   })
