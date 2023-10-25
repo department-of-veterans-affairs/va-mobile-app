c79ee1b11a17632ac07672fe4ef6ad137e07dd11
Dylan Nienberg <dylannienberg@gmail.com>
2023-10-25 12:35:45 -0500

    finished

--- VAMobile/src/screens/BiometricsPreferenceScreen/BiometricsPreferenceScreen.test.tsx
+++ VAMobile/src/screens/BiometricsPreferenceScreen/BiometricsPreferenceScreen.test.tsx
@@ -1,13 +1,9 @@
 import 'react-native'
 import React from 'react'
 
-// Note: test renderer must be required after react-native.
-import { context, render, waitFor, RenderAPI } from 'testUtils'
-import { ReactTestInstance } from 'react-test-renderer'
-
+import { context, render } from 'testUtils'
+import { screen, fireEvent } from '@testing-library/react-native'
 import BiometricsPreferenceScreen from './BiometricsPreferenceScreen'
-
-import { TextView, VAButton } from 'components'
 import { BIOMETRY_TYPE } from 'react-native-keychain'
 import { InitialState, setBiometricsPreference, setDisplayBiometricsPreferenceScreen } from 'store/slices'
 
@@ -31,11 +27,8 @@ jest.mock('store/slices', () => {
 })
 
 context('BiometricsPreferenceScreen', () => {
-  let component: RenderAPI
-  let testInstance: ReactTestInstance
-
   const initializeTestInstance = async (biometric = BIOMETRY_TYPE.TOUCH_ID) => {
-    component = render(<BiometricsPreferenceScreen />, {
+    render(<BiometricsPreferenceScreen />, {
       preloadedState: {
         ...InitialState,
         auth: {
@@ -44,8 +37,6 @@ context('BiometricsPreferenceScreen', () => {
         },
       },
     })
-
-    testInstance = component.UNSAFE_root
   }
 
   beforeEach(() => {
@@ -53,12 +44,25 @@ context('BiometricsPreferenceScreen', () => {
   })
 
   it('initializes correctly', async () => {
-    expect(component).toBeTruthy()
+    expect(screen.getByText('Do you want to allow us to use Touch ID for sign in?')).toBeTruthy()
+    expect(screen.getByText('Touch ID lets you use your fingerprint to sign in to this app.\nYou can always change this later in your app settings.')).toBeTruthy()
+    initializeTestInstance(BIOMETRY_TYPE.FACE_ID)
+    expect(screen.getByText('Do you want to allow us to use Face ID for sign in?')).toBeTruthy()
+    expect(screen.getByText('Face ID lets us recognize an image of your face to sign you in to this app.\nYou can always change this later in your app settings.')).toBeTruthy()
+    initializeTestInstance(BIOMETRY_TYPE.FACE)
+    expect(screen.getByText('Do you want to allow us to use Face Recognition for sign in?')).toBeTruthy()
+    expect(screen.getByText('Face recognition lets you use facial recognition to sign into this app.\nYou can always change this later in your app settings.')).toBeTruthy()
+    initializeTestInstance(BIOMETRY_TYPE.FINGERPRINT)
+    expect(screen.getByText('Do you want to allow us to use Fingerprint for sign in?')).toBeTruthy()
+    expect(screen.getByText('Fingerprint lets you use your fingerprint to sign into this app.\nYou can always change this later in your app settings.')).toBeTruthy()
+    initializeTestInstance(BIOMETRY_TYPE.IRIS)
+    expect(screen.getByText('Do you want to allow us to use Iris for sign in?')).toBeTruthy()
+    expect(screen.getByText('Iris lets us recognize a video image of your eyes to sign you in to this app.\nYou can always change this later in your app settings.')).toBeTruthy()
   })
 
   describe('on click of the use biometric button', () => {
     it('should call setBiometricsPreference and setDisplayBiometricsPreferenceScreen', async () => {
-      testInstance.findAllByType(VAButton)[0].props.onPress()
+      fireEvent.press(screen.getByText('Turn on Touch ID'))
       expect(setBiometricsPreference).toHaveBeenCalledWith(true)
       expect(setDisplayBiometricsPreferenceScreen).toHaveBeenCalledWith(false)
     })
@@ -66,34 +70,8 @@ context('BiometricsPreferenceScreen', () => {
 
   describe('on click of the skip button button', () => {
     it('should call setDisplayBiometricsPreferenceScreen', async () => {
-      testInstance.findAllByType(VAButton)[1].props.onPress()
+      fireEvent.press(screen.getByText('Skip'))
       expect(setDisplayBiometricsPreferenceScreen).toHaveBeenCalledWith(false)
-    })
-  })
-
-  describe('body text', () => {
-    it('should display the right text for Touch ID', async () => {
-      expect(testInstance.findAllByType(TextView)[1].props.children[0]).toEqual('Touch ID lets you use your fingerprint to sign in to this app.')
-    })
-
-    it('should display the right text for Face ID', async () => {
-      initializeTestInstance(BIOMETRY_TYPE.FACE_ID)
-      expect(testInstance.findAllByType(TextView)[1].props.children[0]).toEqual('Face ID lets us recognize an image of your face to sign you in to this app.')
-    })
-
-    it('should display the right text for Face Recognition', async () => {
-      initializeTestInstance(BIOMETRY_TYPE.FACE)
-      expect(testInstance.findAllByType(TextView)[1].props.children[0]).toEqual('Face recognition lets you use facial recognition to sign into this app.')
-    })
-
-    it('should display the right text for Fingerprint', async () => {
-      initializeTestInstance(BIOMETRY_TYPE.FINGERPRINT)
-      expect(testInstance.findAllByType(TextView)[1].props.children[0]).toEqual('Fingerprint lets you use your fingerprint to sign into this app.')
-    })
-
-    it('should display the right text for Iris', async () => {
-      initializeTestInstance(BIOMETRY_TYPE.IRIS)
-      expect(testInstance.findAllByType(TextView)[1].props.children[0]).toEqual('Iris lets us recognize a video image of your eyes to sign you in to this app.')
     })
   })
 })
