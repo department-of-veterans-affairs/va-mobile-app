4174d9d1cc4f03284976ab7344cad0d2b396a7ae
Jon Bindbeutel <compjon@gmail.com>
2023-10-26 16:02:48 -0400

    update remoteconfig test

--- VAMobile/src/utils/remoteConfig.test.ts
+++ VAMobile/src/utils/remoteConfig.test.ts
@@ -1,5 +1,5 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
-import { overrideRemote, devConfig, featureEnabled, activateRemoteConfig, setDebugConfig, getFeatureToggles } from 'utils/remoteConfig'
+import { overrideRemote, devConfig, featureEnabled, activateRemoteConfig, setDebugConfig, getFeatureToggles, setWaygateDebugConfig } from 'utils/remoteConfig'
 
 const mockGetItem = AsyncStorage.getItem as jest.Mock
 const mockSetItem = AsyncStorage.setItem as jest.Mock
@@ -30,6 +30,177 @@ const mockOverrides = {
   patientCheckInWaygate: true,
 }
 
+const mockWaygateOverrides = {
+  WG_HomeScreen: {
+    enabled: true,
+    errorMsgTitle: undefined,
+    errorMsgBody: undefined,
+    appUpdateButton: false,
+    allowFunction: false,
+    denyAccess: false,
+  },
+  WG_ProfileScreen: {
+    enabled: true,
+    errorMsgTitle: undefined,
+    errorMsgBody: undefined,
+    appUpdateButton: false,
+    allowFunction: false,
+    denyAccess: false,
+  },
+  WG_PersonalInformationScreen: {
+    enabled: true,
+    errorMsgTitle: undefined,
+    errorMsgBody: undefined,
+    appUpdateButton: false,
+    allowFunction: false,
+    denyAccess: false,
+  },
+  WG_HowDoIUpdateScreen: {
+    enabled: true,
+    errorMsgTitle: undefined,
+    errorMsgBody: undefined,
+    appUpdateButton: false,
+    allowFunction: false,
+    denyAccess: false,
+  },
+  WG_PreferredNameScreen: {
+    enabled: true,
+    errorMsgTitle: undefined,
+    errorMsgBody: undefined,
+    appUpdateButton: false,
+    allowFunction: false,
+    denyAccess: false,
+  },
+  WG_GenderIdentityScreen: {
+    enabled: true,
+    errorMsgTitle: undefined,
+    errorMsgBody: undefined,
+    appUpdateButton: false,
+    allowFunction: false,
+    denyAccess: false,
+  },
+  WG_WhatToKnowScreen: {
+    enabled: true,
+    errorMsgTitle: undefined,
+    errorMsgBody: undefined,
+    appUpdateButton: false,
+    allowFunction: false,
+    denyAccess: false,
+  },
+  WG_ContactInformationScreen: {
+    enabled: true,
+    errorMsgTitle: undefined,
+    errorMsgBody: undefined,
+    appUpdateButton: false,
+    allowFunction: false,
+    denyAccess: false,
+  },
+  WG_HowWillYouScreen: {
+    enabled: true,
+    errorMsgTitle: undefined,
+    errorMsgBody: undefined,
+    appUpdateButton: false,
+    allowFunction: false,
+    denyAccess: false,
+  },
+  WG_EditAddressScreen: {
+    enabled: true,
+    errorMsgTitle: undefined,
+    errorMsgBody: undefined,
+    appUpdateButton: false,
+    allowFunction: false,
+    denyAccess: false,
+  },
+  WG_EditPhoneNumberScreen: {
+    enabled: true,
+    errorMsgTitle: undefined,
+    errorMsgBody: undefined,
+    appUpdateButton: false,
+    allowFunction: false,
+    denyAccess: false,
+  },
+  WG_EditEmailScreen: {
+    enabled: true,
+    errorMsgTitle: undefined,
+    errorMsgBody: undefined,
+    appUpdateButton: false,
+    allowFunction: false,
+    denyAccess: false,
+  },
+  WG_MilitaryInformationScreen: {
+    enabled: true,
+    errorMsgTitle: undefined,
+    errorMsgBody: undefined,
+    appUpdateButton: false,
+    allowFunction: false,
+    denyAccess: false,
+  },
+  WG_IncorrectServiceInfoScreen: {
+    enabled: true,
+    errorMsgTitle: undefined,
+    errorMsgBody: undefined,
+    appUpdateButton: false,
+    allowFunction: false,
+    denyAccess: false,
+  },
+  WG_SettingsScreen: {
+    enabled: true,
+    errorMsgTitle: undefined,
+    errorMsgBody: undefined,
+    appUpdateButton: false,
+    allowFunction: false,
+    denyAccess: false,
+  },
+  WG_ManageYourAccountScreen: {
+    enabled: true,
+    errorMsgTitle: undefined,
+    errorMsgBody: undefined,
+    appUpdateButton: false,
+    allowFunction: false,
+    denyAccess: false,
+  },
+  WG_NotificationsSettingsScreen: {
+    enabled: true,
+    errorMsgTitle: undefined,
+    errorMsgBody: undefined,
+    appUpdateButton: false,
+    allowFunction: false,
+    denyAccess: false,
+  },
+  WG_ContactVAScreen: {
+    enabled: true,
+    errorMsgTitle: undefined,
+    errorMsgBody: undefined,
+    appUpdateButton: false,
+    allowFunction: false,
+    denyAccess: false,
+  },
+  WG_VeteransCrisisLineScreen: {
+    enabled: true,
+    errorMsgTitle: undefined,
+    errorMsgBody: undefined,
+    appUpdateButton: false,
+    allowFunction: false,
+    denyAccess: false,
+  },
+  WG_VeteranStatusScreen: {
+    enabled: true,
+    errorMsgTitle: undefined,
+    errorMsgBody: undefined,
+    appUpdateButton: false,
+    allowFunction: false,
+    denyAccess: false,
+  },
+  WG_LoginScreen: {
+    enabled: true,
+    errorMsgTitle: undefined,
+    errorMsgBody: undefined,
+    appUpdateButton: false,
+    allowFunction: false,
+    denyAccess: false,
+  },
+}
+
 describe('activate', () => {
   it('should call setDefaults with devConfig', async () => {
     await activateRemoteConfig()
@@ -43,6 +214,7 @@ describe('activate', () => {
     expect(overrideRemote).toBe(false)
     expect(featureEnabled('testFeature')).toBe(true)
     await setDebugConfig(mockOverrides)
+    await setWaygateDebugConfig(mockWaygateOverrides)
     expect(mockSetItem).toHaveBeenCalledWith('@store_remote_config_overrides', JSON.stringify(mockOverrides))
     expect(overrideRemote).toBe(true)
     expect(featureEnabled('testFeature')).toBe(false)
