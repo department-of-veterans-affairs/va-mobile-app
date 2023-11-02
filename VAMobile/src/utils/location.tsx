a9463d8baf13a5f1ac19897a386a9e89280b4c19
Theo Bentum <theo@adhocteam.us>
2023-11-02 07:50:40 -0700

    Use either address or lat/long for google maps URL (#7177)

--- VAMobile/src/utils/location.tsx
+++ VAMobile/src/utils/location.tsx
@@ -5,19 +5,20 @@ const APPLE_MAPS_BASE_URL = 'https://maps.apple.com/'
 const GOOGLE_MAPS_BASE_URL = 'https://www.google.com/maps/dir/'
 
 export const getDirectionsUrl = (location: AppointmentLocation): string => {
-  const { name, lat, long } = location
-  const destinationString = `${Object.values(location.address || {}).join(' ')}+${name}+${lat},${long}`
+  const { address, name, lat, long } = location
+  const addressString = Object.values(address || {}).join(' ')
+
   if (isIOS()) {
     const queryString = new URLSearchParams({
       // apply type parameter = m (map)
       t: 'm',
-      daddr: destinationString,
+      daddr: `${addressString}+${name}+${lat},${long}`,
     }).toString()
     return `${APPLE_MAPS_BASE_URL}?${queryString}`
   } else {
     const queryString = new URLSearchParams({
       api: '1',
-      destination: destinationString,
+      destination: addressString || `${lat},${long}`,
     }).toString()
     return `${GOOGLE_MAPS_BASE_URL}?${queryString}`
   }
