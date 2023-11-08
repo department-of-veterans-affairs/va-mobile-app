31c2c175617c327ebc73d4f00dcb6700d209db99
Dylan Nienberg <dylannienberg@gmail.com>
2023-11-08 10:40:43 -0600

    updated unit tests

--- VAMobile/src/screens/HomeScreen/ProfileScreen/SettingsScreen/InAppRecruitmentScreen/InAppRecruitmentScreen.test.tsx
+++ VAMobile/src/screens/HomeScreen/ProfileScreen/SettingsScreen/InAppRecruitmentScreen/InAppRecruitmentScreen.test.tsx
@@ -1,8 +1,6 @@
-import 'react-native'
 import React from 'react'
+
 import { fireEvent, screen } from '@testing-library/react-native'
-// Note: test renderer must be required after react-native.
-import 'jest-styled-components'
 import { context, mockNavProps, render } from 'testUtils'
 import InAppRecruitmentScreen from './InAppRecruitmentScreen'
 
@@ -29,18 +27,18 @@ context('CollapsibleMessage', () => {
 
   it('renders screen correctly', () => {
     expect(screen.getByLabelText('Give feedback')).toBeTruthy()
-    expect(screen.getByText('Make this app better for all Veterans')).toBeTruthy()
+    expect(screen.getByRole('header', { name: 'Make this app better for all Veterans' })).toBeTruthy()
     expect(screen.getByText('You can help us improve this app by being part of online feedback sessions.')).toBeTruthy()
     expect(screen.getByText('Register once by completing a 5-minute questionnaire.')).toBeTruthy()
     expect(screen.getByText('Wait to be matched with a session.')).toBeTruthy()
     expect(screen.getByText('Get paid for each session you complete (excludes VA employees).')).toBeTruthy()
-    expect(screen.getByText('Go to questionnaire')).toBeTruthy()
-    expect(screen.getByText('Learn more about the Veteran Usability Project')).toBeTruthy()
+    expect(screen.getByRole('button', { name: 'Go to questionnaire' })).toBeTruthy()
+    expect(screen.getByRole('link', { name: 'Learn more about the Veteran Usability Project' })).toBeTruthy()
     expect(screen.getByText('VA contracts: 36C10B22C0011 & 36C10X18C0061')).toBeTruthy()
   })
 
   it('Goes to the questionnaire when button pressed', () => {
-    fireEvent.press(screen.getByText('Go to questionnaire'))
+    fireEvent.press(screen.getByRole('button', { name: 'Go to questionnaire' }))
     const expectNavArgs = {
         url: 'https://docs.google.com/forms/d/e/1FAIpQLSfRb0OtW34qKm8tGoQwwwDFs8IqwOMCLTde3DeM-ukKOEZBnA/viewform',
         displayTitle: 'va.gov',
@@ -50,7 +48,7 @@ context('CollapsibleMessage', () => {
   })
 
   it('Goes to the Veteran Usability Project when link pressed', () => {
-    fireEvent.press(screen.getByText('Learn more about the Veteran Usability Project'))
+    fireEvent.press(screen.getByRole('link', { name: 'Learn more about the Veteran Usability Project' }))
     expect(mockExternalLinkSpy).toBeCalledWith('https://veteranusability.us/')
   })
 })
