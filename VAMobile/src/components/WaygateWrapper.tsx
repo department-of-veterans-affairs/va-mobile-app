b3ccbde466e67fded29363f4da101e522b4614c1
Jon Bindbeutel <compjon@gmail.com>
2023-11-08 12:47:51 -0500

    updated waygate wrapper

--- VAMobile/src/components/WaygateWrapper.tsx
+++ VAMobile/src/components/WaygateWrapper.tsx
@@ -1,9 +1,13 @@
-import { AlertBox, Box } from 'components'
+import { AlertBox, Box, ClickToCallPhoneNumber } from 'components'
 import { useNavigationState } from '@react-navigation/native'
 import React, { FC } from 'react'
 
+import { NAMESPACE } from 'constants/namespaces'
 import { Waygate, WaygateToggleType, waygateEnabled } from 'utils/waygateConfig'
+import { a11yLabelID } from 'utils/a11yLabel'
+import { displayedTextPhoneNumber } from 'utils/formattingUtils'
 import { useTheme } from 'utils/hooks'
+import { useTranslation } from 'react-i18next'
 
 export type WaygateWrapperProps = {
   /** the waygate name to check for */
@@ -14,11 +18,14 @@ export const WaygateWrapper: FC<WaygateWrapperProps> = ({ children, waygateName 
   const theme = useTheme()
   const waygateStateScreen = 'WG_' + useNavigationState((state) => state.routes[state.routes.length - 1]?.name)
   const waygateScreen = waygateName ? waygateName : waygateStateScreen
+  const { t } = useTranslation(NAMESPACE.COMMON)
 
   const waygateAlertBox = (waygate: Waygate) => {
     return (
       <Box mb={theme.dimensions.condensedMarginBetween}>
-        <AlertBox border="warning" title={waygate.errorMsgTitle} text={waygate.errorMsgBody} />
+        <AlertBox border={waygate.type === 'DenyContent' ? 'error' : 'warning'} title={waygate.errorMsgTitle} text={waygate.errorMsgBody}>
+          <ClickToCallPhoneNumber displayedText={displayedTextPhoneNumber(t('8006982411'))} phone={t('8006982411')} a11yLabel={a11yLabelID(t('8006982411'))} />
+        </AlertBox>
       </Box>
     )
   }
