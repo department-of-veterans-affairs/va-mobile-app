b9f1e8f5de26e9a43334bda34bd0ec26debaf32f
Liz Townsend <liz.townsend@adhocteam.us>
2023-11-14 15:19:17 -0600

    why is prettier dumb? it is what it is

--- VAMobile/e2e/tests/MilitaryInformation.e2e.ts
+++ VAMobile/e2e/tests/MilitaryInformation.e2e.ts
@@ -5,20 +5,20 @@ import { log } from 'console'
 import { setTimeout } from "timers/promises"
 
 export const MilitaryInformationE2eIdConstants = {
-  MILITARY_DATE_TEXT: 'July 13, 1970 – August 31, 1998',
-  SERVICE_INFORMATION_INCORRECT_TITLE_TEXT: 'What if my military service information doesn\'t look right?',
-  SERVICE_INFORMATION_INCORRECT_BODY_LABEL_1: 'Some Veterans have reported seeing military service information in their V-A .gov profiles that doesn’t seem right. When this happens, it’s because there’s an error in the information we’re pulling into V-A .gov from the Defense Enrollment Eligibility Reporting System (D-E-E-R-S).',
-  SERVICE_INFORMATION_INCORRECT_BODY_LABEL_2: 'If the military service information in your profile doesn’t look right, please call the Defense Manpower Data Center (D-M-D-C). They’ll work with you to update your information in D-E-E-R-S.',
-  SERVICE_INFORMATION_INCORRECT_BODY_LABEL_3: 'To reach the D-M-D-C, call Monday through Friday (except federal holidays), 8:00 a.m. to 8:00 p.m. Eastern Time.',
-  SERVICE_NOT_AVAILABLE_PAGE_TITLE_TEXT: 'We can\'t access your military information',
-  SERVICE_NOT_AVAILABLE_PAGE_BODY_LABEL: 'We\'re sorry. We can\'t access your military service records. If you think you should be able to review your service information here, please file a request to change or correct your D-D 2 1 4 or other military records.',
+	MILITARY_DATE_TEXT: 'July 13, 1970 – August 31, 1998',
+	SERVICE_INFORMATION_INCORRECT_TITLE_TEXT: 'What if my military service information doesn\'t look right?',
+	SERVICE_INFORMATION_INCORRECT_BODY_LABEL_1: 'Some Veterans have reported seeing military service information in their V-A .gov profiles that doesn’t seem right. When this happens, it’s because there’s an error in the information we’re pulling into V-A .gov from the Defense Enrollment Eligibility Reporting System (D-E-E-R-S).',
+	SERVICE_INFORMATION_INCORRECT_BODY_LABEL_2: 'If the military service information in your profile doesn’t look right, please call the Defense Manpower Data Center (D-M-D-C). They’ll work with you to update your information in D-E-E-R-S.',
+	SERVICE_INFORMATION_INCORRECT_BODY_LABEL_3: 'To reach the D-M-D-C, call Monday through Friday (except federal holidays), 8:00 a.m. to 8:00 p.m. Eastern Time.',
+	SERVICE_NOT_AVAILABLE_PAGE_TITLE_TEXT: 'We can\'t access your military information',
+	SERVICE_NOT_AVAILABLE_PAGE_BODY_LABEL: 'We\'re sorry. We can\'t access your military service records. If you think you should be able to review your service information here, please file a request to change or correct your D-D 2 1 4 or other military records.',
 }
 
 beforeAll(async () => {
-  await loginToDemoMode()
+	await loginToDemoMode()
 })
 
-export async function verifyMilitaryInfo(militaryBranch) {
+export async function verifyMilitaryInfo(militaryBranch: string) {
 	it('should tap through and verify that ' + militaryBranch + ' is shown on the home, profile, and military information page and that the seal is correct', async () => {
 		//changing the JSON file is currently causing issues only on iOS. Commenting out this code until it can be fixed
 		//await changeMockData('profile.json', ['/v0/military-service-history', 'data', 'attributes', {'serviceHistory': 0}, 'branchOfService'], militaryBranch)
@@ -37,7 +37,7 @@ export async function verifyMilitaryInfo(militaryBranch) {
 	})
 }
 
-describe('Military Information Screen', () => { 
+describe('Military Information Screen', () => {
 	verifyMilitaryInfo('United States Coast Guard')
 	//changing the JSON file is currently causing issues only on iOS. Commenting out this code until it can be fixed
 	/*verifyMilitaryInfo('United States Army')
@@ -53,14 +53,14 @@ describe('Military Information Screen', () => {
 		await expect(element(by.label(MilitaryInformationE2eIdConstants.SERVICE_INFORMATION_INCORRECT_BODY_LABEL_1))).toExist()
 		await expect(element(by.label(MilitaryInformationE2eIdConstants.SERVICE_INFORMATION_INCORRECT_BODY_LABEL_2))).toExist()
 		await expect(element(by.label(MilitaryInformationE2eIdConstants.SERVICE_INFORMATION_INCORRECT_BODY_LABEL_3))).toExist()
-		await expect(element(by.id('incorrectServiceDMDCNumberTestID'))).toExist()
+		await expect(element(by.id('CallVATestID'))).toExist()
 		await element(by.id('IncorrectServiceTestID')).swipe('up')
 		if (device.getPlatform() === 'android') {
-			await element(by.id('incorrectServiceDMDCNumberTestID')).tap()
+			await element(by.id('CallVATestID')).tap()
 			await setTimeout(5000)
 			var tempPath = await device.takeScreenshot('AndroidCallingScreen')
-			await device.launchApp({newInstance: false})
-		} 
+			await device.launchApp({ newInstance: false })
+		}
 	})
 
 	//changing the JSON file is currently causing issues only on iOS. Commenting out this code until it can be fixed
