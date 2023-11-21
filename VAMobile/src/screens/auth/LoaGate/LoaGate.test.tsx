b716bb83f0d4b8fddbcdc6099a16b858ed020f30
Dylan Nienberg <dylannienberg@gmail.com>
2023-11-21 09:49:21 -0600

    finished

--- VAMobile/src/screens/auth/LoaGate/LoaGate.test.tsx
+++ VAMobile/src/screens/auth/LoaGate/LoaGate.test.tsx
@@ -10,10 +10,10 @@ context('LoaGate', () => {
   })
 
   it('initializes correctly', () => {
-    expect(screen.getByText('Sign in')).toBeTruthy()
+    expect(screen.getByRole('header', { name: 'Sign in' })).toBeTruthy()
     expect(screen.getByText("Before we give you access to your VA claim and health care information, we need to make sure you’re you. This helps us protect you from fraud and identity theft.")).toBeTruthy()
     expect(screen.getByText("If you haven’t yet verified your identity, we’ll help you complete the process when you sign in.")).toBeTruthy()
-    expect(screen.getByText("Read more if you haven't yet verified")).toBeTruthy()
+    expect(screen.getByRole('tab', { name: "Read more if you haven't yet verified" })).toBeTruthy()
     expect(screen.getByRole('button', { name: 'Continue to sign in' })).toBeTruthy()
   })
 })
