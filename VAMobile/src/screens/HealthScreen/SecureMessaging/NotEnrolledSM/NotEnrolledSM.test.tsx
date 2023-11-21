ce78a5dfc1935c25255e424a538b93035d9372aa
Dylan Nienberg <87150991+Sparowhawk@users.noreply.github.com>
2023-11-21 12:36:55 -0600

    Update VAMobile/src/screens/HealthScreen/SecureMessaging/NotEnrolledSM/NotEnrolledSM.test.tsx
    Co-authored-by: Theo Bentum <theo@adhocteam.us>

--- VAMobile/src/screens/HealthScreen/SecureMessaging/NotEnrolledSM/NotEnrolledSM.test.tsx
+++ VAMobile/src/screens/HealthScreen/SecureMessaging/NotEnrolledSM/NotEnrolledSM.test.tsx
@@ -21,7 +21,7 @@ context('NotEnrolledSM', () => {
 
   describe('when Learn how to upgrade link is clicked', () => {
     it('should launch external link', () => {
-      fireEvent.press(screen.getByTestId('Learn how to upgrade to a My HealtheVet Premium account'))
+      fireEvent.press(screen.getByRole('link', { name: 'Learn how to upgrade to a My HealtheVet Premium account' }))
       expect(mockExternalLinkSpy).toBeCalledWith('https://www.myhealth.va.gov/web/myhealthevet/upgrading-your-my-healthevet-account-through-in-person-or-online-authentication')
     })
   })
