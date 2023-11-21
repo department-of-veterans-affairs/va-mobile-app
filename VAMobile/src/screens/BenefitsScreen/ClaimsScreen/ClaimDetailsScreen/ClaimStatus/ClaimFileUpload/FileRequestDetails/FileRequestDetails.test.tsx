69f9708a63fcd4d27c24a925f3dca1cbc891e6b0
Dylan Nienberg <87150991+Sparowhawk@users.noreply.github.com>
2023-11-21 13:50:25 -0600

    CU/7059-Dylan-ClaimFileUploadAndTimeline (#7147)
    * finished
    
    * updated
    
    * Update VAMobile/src/screens/BenefitsScreen/ClaimsScreen/ClaimDetailsScreen/ClaimStatus/ClaimFileUpload/AskForClaimDecision/AskForClaimDecision.test.tsx
    
    Co-authored-by: Theo Bentum <theo@adhocteam.us>
    
    * Update VAMobile/src/screens/BenefitsScreen/ClaimsScreen/ClaimDetailsScreen/ClaimStatus/ClaimFileUpload/SelectFile/UploadFile/UploadFile.test.tsx
    
    Co-authored-by: Theo Bentum <theo@adhocteam.us>
    
    * Update VAMobile/src/screens/BenefitsScreen/ClaimsScreen/ClaimDetailsScreen/ClaimStatus/ClaimFileUpload/SelectFile/UploadFile/UploadFile.test.tsx
    
    Co-authored-by: Theo Bentum <theo@adhocteam.us>
    
    * Update VAMobile/src/screens/BenefitsScreen/ClaimsScreen/ClaimDetailsScreen/ClaimStatus/ClaimFileUpload/TakePhotos/UploadOrAddPhotos/UploadOrAddPhotos.test.tsx
    
    Co-authored-by: Theo Bentum <theo@adhocteam.us>
    
    * Update VAMobile/src/screens/BenefitsScreen/ClaimsScreen/ClaimDetailsScreen/ClaimStatus/ClaimFileUpload/TakePhotos/UploadOrAddPhotos/UploadOrAddPhotos.test.tsx
    
    Co-authored-by: Theo Bentum <theo@adhocteam.us>
    
    * Update VAMobile/src/screens/BenefitsScreen/ClaimsScreen/ClaimDetailsScreen/ClaimStatus/ClaimFileUpload/TakePhotos/TakePhotos.test.tsx
    
    Co-authored-by: Theo Bentum <theo@adhocteam.us>
    
    * Update VAMobile/src/screens/BenefitsScreen/ClaimsScreen/ClaimDetailsScreen/ClaimStatus/ClaimFileUpload/SelectFile/UploadFile/UploadFile.test.tsx
    
    Co-authored-by: Theo Bentum <theo@adhocteam.us>
    
    * updated
    
    ---------
    
    Co-authored-by: Theo Bentum <theo@adhocteam.us>

--- VAMobile/src/screens/BenefitsScreen/ClaimsScreen/ClaimDetailsScreen/ClaimStatus/ClaimFileUpload/FileRequestDetails/FileRequestDetails.test.tsx
+++ VAMobile/src/screens/BenefitsScreen/ClaimsScreen/ClaimDetailsScreen/ClaimStatus/ClaimFileUpload/FileRequestDetails/FileRequestDetails.test.tsx
@@ -1,17 +1,11 @@
-import 'react-native'
 import React from 'react'
-// Note: test renderer must be required after react-native.
-import { context, findByTestID, findByTypeWithText, mockNavProps, render, RenderAPI, waitFor, when } from 'testUtils'
+import { screen } from '@testing-library/react-native'
 
+import { context, mockNavProps, render } from 'testUtils'
 import FileRequestDetails from './FileRequestDetails'
 import { ClaimEventData } from 'store/api'
-import { TextView, VAButton } from 'components'
 
 context('FileRequestDetails', () => {
-  let component: RenderAPI
-  let testInstance: any
-  let props: any
-
   let request = {
     type: 'still_need_from_you_list',
     date: '2020-07-16',
@@ -23,37 +17,20 @@ context('FileRequestDetails', () => {
   }
 
   const initializeTestInstance = (request: ClaimEventData) => {
-    props = mockNavProps(undefined, { setOptions: jest.fn() }, { params: { request } })
+    const props = mockNavProps(undefined, { setOptions: jest.fn() }, { params: { request } })
 
-    component = render(<FileRequestDetails {...props} />)
-
-    testInstance = component.UNSAFE_root
+    render(<FileRequestDetails {...props} />)
   }
 
   beforeEach(() => {
     initializeTestInstance(request)
   })
 
-  it('initializes correctly', async () => {
-    expect(component).toBeTruthy()
-  })
-
-  describe("when the request hasn't had files uploaded", () => {
-    it('should display the select a file and take or select photos buttons', async () => {
-      await waitFor(() => {
-        const buttons = testInstance.findAllByType(VAButton)
-        expect(buttons.length).toEqual(2)
-        expect(buttons[0].props.label).toEqual('Select a file')
-        expect(buttons[1].props.label).toEqual('Take or select photos')
-      })
-    })
-
-    it('should display request title and description', async () => {
-      const textViews = testInstance.findAllByType(TextView)
-      await waitFor(() => {
-        expect(textViews[2].props.children).toEqual('Request 1')
-        expect(textViews[4].props.children).toEqual('Need DD214')
-      })
-    })
+  it('initializes correctly', () => {
+    expect(screen.getByText('Requests')).toBeTruthy()
+    expect(screen.getAllByText('Request 1')).toBeTruthy()
+    expect(screen.getByText('Need DD214')).toBeTruthy()
+    expect(screen.getByRole('button', { name: 'Select a file' })).toBeTruthy()
+    expect(screen.getByRole('button', { name: 'Take or select photos' })).toBeTruthy()
   })
 })
