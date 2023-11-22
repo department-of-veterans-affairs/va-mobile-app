fa9ffe15ac0aa859c89ccabd263eb090fb438666
Dylan Nienberg <87150991+Sparowhawk@users.noreply.github.com>
2023-11-22 10:51:57 -0600

    bug/5064-Dylan-FixingBSSVToggleLabel (#7033)
    * finished
    
    * update
    
    * fixed
    
    * Update VAMobile/src/screens/BenefitsScreen/Letters/BenefitSummaryServiceVerification/BenefitSummaryServiceVerification.test.tsx
    
    Co-authored-by: Theo Bentum <theo@adhocteam.us>
    
    * Update VAMobile/src/screens/BenefitsScreen/Letters/BenefitSummaryServiceVerification/BenefitSummaryServiceVerification.test.tsx
    
    Co-authored-by: Theo Bentum <theo@adhocteam.us>
    
    ---------
    
    Co-authored-by: Theo Bentum <theo@adhocteam.us>

--- VAMobile/src/screens/BenefitsScreen/Letters/BenefitSummaryServiceVerification/BenefitSummaryServiceVerification.tsx
+++ VAMobile/src/screens/BenefitsScreen/Letters/BenefitSummaryServiceVerification/BenefitSummaryServiceVerification.tsx
@@ -123,35 +123,22 @@ const BenefitSummaryServiceVerification: FC<BenefitSummaryServiceVerificationPro
     const { monthlyAwardAmount, awardEffectiveDate, serviceConnectedPercentage, hasChapter35Eligibility, hasServiceConnectedDisabilities } =
       letterBeneficiaryData?.benefitInformation || ({} as LetterBenefitInformation)
 
-    if (!!monthlyAwardAmount || !!awardEffectiveDate) {
-      let text = ''
-      if (!!monthlyAwardAmount && !!awardEffectiveDate) {
-        text = t('letters.benefitService.monthlyAwardAndEffectiveDate', {
-          monthlyAwardAmount: roundToHundredthsPlace(monthlyAwardAmount),
-          date: formatDateMMMMDDYYYY(awardEffectiveDate),
-        })
-      } else if (monthlyAwardAmount) {
-        text = t('letters.benefitService.monthlyAward', {
-          monthlyAwardAmount: roundToHundredthsPlace(monthlyAwardAmount),
-        })
-      } else if (awardEffectiveDate) {
-        text = t('letters.benefitService.effectiveDate', {
-          date: formatDateMMMMDDYYYY(awardEffectiveDate),
-        })
-      }
+    const text = t('letters.benefitService.monthlyAwardAndEffectiveDate', {
+      monthlyAwardAmount: roundToHundredthsPlace(monthlyAwardAmount || 0),
+      date: awardEffectiveDate ? formatDateMMMMDDYYYY(awardEffectiveDate) : t('letters.benefitService.effectiveDateInvalid'),
+    })
 
-      toggleListItems.push({
-        text: text,
-        testId: text.replace(',', ''),
-        onPress: (): void => setMonthlyAwardToggle(!monthlyAwardToggle),
-        decorator: ButtonDecoratorType.Switch,
-        decoratorProps: {
-          on: monthlyAwardToggle,
-          a11yHint: t('letters.benefitService.monthlyAwardA11yHint'),
-          testID: 'monthly-award',
-        },
-      })
-    }
+    toggleListItems.push({
+      text: text,
+      testId: text.replace(',', ''),
+      onPress: (): void => setMonthlyAwardToggle(!monthlyAwardToggle),
+      decorator: ButtonDecoratorType.Switch,
+      decoratorProps: {
+        on: monthlyAwardToggle,
+        a11yHint: t('letters.benefitService.monthlyAwardA11yHint'),
+        testID: 'monthly-award',
+      },
+    })
 
     if (serviceConnectedPercentage) {
       const percentText = t('letters.benefitService.combinedServiceConnectingRating', {
