5f3cfb9b21299b4a77cd3530f1c9daf294a5b7e0
Liz Townsend <liz.townsend@adhocteam.us>
2023-11-27 12:12:54 -0600

    update carousel test

--- VAMobile/src/components/Carousel/Carousel.test.tsx
+++ VAMobile/src/components/Carousel/Carousel.test.tsx
@@ -1,31 +1,33 @@
-import 'react-native'
 import React from 'react'
-// Note: test renderer must be required after react-native.
-import 'jest-styled-components'
-import { ReactTestInstance, act } from 'react-test-renderer'
-
-import { context, render, RenderAPI, waitFor } from 'testUtils'
+import { context, render, screen } from 'testUtils'
 import Carousel from './Carousel'
 import { TextView } from '../index'
 
 context('Carousel', () => {
-  let component: RenderAPI
-  let testInstance: ReactTestInstance
-  let t = jest.fn(() => {})
+  let t = jest.fn(() => { })
 
   const TestComponent = () => {
     return <TextView>Test Component</TextView>
   }
 
-  beforeEach(async () => {
-    await waitFor(() => {
-      component = render(<Carousel screenList={[{ name: 'TestComponent', component: TestComponent }]} onCarouselEnd={() => {}} translation={t} />)
-    })
+  const screenList = [
+    {
+      name: 'TestComponent',
+      component: TestComponent,
+      a11yHints: {
+        skipHint: 'skip',
+        doneHint: 'done',
+      }
+    }
+  ]
 
-    testInstance = component.UNSAFE_root
+  beforeEach(async () => {
+    render(<Carousel screenList={screenList} onCarouselEnd={() => { }} translation={t} />)
   })
 
   it('initializes correctly', async () => {
-    expect(component).toBeTruthy()
+    expect(screen.getByText('Test Component')).toBeTruthy()
+    expect(screen.getByAccessibilityHint('skip')).toBeTruthy()
+    expect(screen.getByAccessibilityHint('done')).toBeTruthy()
   })
 })
