1ba0577ad98b8b14556b9cf16e46a900faac4c28
Liz Townsend <liz.townsend@adhocteam.us>
2023-11-27 16:34:06 -0600

    update MessageAlert test

--- VAMobile/src/components/MessageAlert.test.tsx
+++ VAMobile/src/components/MessageAlert.test.tsx
@@ -1,15 +1,8 @@
-import 'react-native'
 import React from 'react'
-// Note: test renderer must be required after react-native.
-import 'jest-styled-components'
-import { ReactTestInstance, act } from 'react-test-renderer'
-
-import { context, findByTypeWithText, findByTypeWithSubstring, render, RenderAPI } from 'testUtils'
-import { AlertBox, MessageAlert, TextView } from 'components'
+import { context, render, screen } from 'testUtils'
+import { AlertBox, MessageAlert } from 'components'
 
 context('MessageAlert', () => {
-  let component: RenderAPI
-  let testInstance: ReactTestInstance
 
   const initializeTestInstance = ({
     hasValidationError,
@@ -21,22 +14,20 @@ context('MessageAlert', () => {
     savingDraft?: boolean
     errorList?: { [key: number]: string }
   }): void => {
-    component = render(<MessageAlert hasValidationError={hasValidationError} saveDraftAttempted={saveDraftAttempted} />)
-
-    testInstance = component.UNSAFE_root
+    render(<MessageAlert hasValidationError={hasValidationError} saveDraftAttempted={saveDraftAttempted} />)
   }
 
   it('displays save draft validation', async () => {
     initializeTestInstance({ hasValidationError: true, saveDraftAttempted: true })
-    expect(testInstance.findAllByType(AlertBox).length).toEqual(1)
-    expect(findByTypeWithText(testInstance, TextView, 'We need more information')).toBeTruthy()
-    expect(findByTypeWithSubstring(testInstance, TextView, 'To save this message, provide this information:')).toBeTruthy()
+    expect(screen.UNSAFE_getAllByType(AlertBox).length).toEqual(1)
+    expect(screen.getByText('We need more information')).toBeTruthy()
+    expect(screen.getByText('To save this message, provide this information:')).toBeTruthy()
   })
 
   it('displays send validation', async () => {
     initializeTestInstance({ hasValidationError: true, saveDraftAttempted: false })
-    expect(testInstance.findAllByType(AlertBox).length).toEqual(1)
-    expect(findByTypeWithText(testInstance, TextView, 'We need more information')).toBeTruthy()
-    expect(findByTypeWithSubstring(testInstance, TextView, 'To send this message, provide this information:')).toBeTruthy()
+    expect(screen.UNSAFE_getAllByType(AlertBox).length).toEqual(1)
+    expect(screen.getByText('We need more information')).toBeTruthy()
+    expect(screen.getByText('To send this message, provide this information:')).toBeTruthy()
   })
 })
