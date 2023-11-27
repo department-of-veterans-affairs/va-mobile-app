af6c96057365040a11241aede4eda5528398699b
Liz Townsend <liz.townsend@adhocteam.us>
2023-11-27 09:31:39 -0600

    refactor FormWrapper test

--- VAMobile/src/components/FormWrapper/FormWrapper.test.tsx
+++ VAMobile/src/components/FormWrapper/FormWrapper.test.tsx
@@ -1,11 +1,5 @@
-import 'react-native'
 import React from 'react'
-import { screen } from '@testing-library/react-native'
-// Note: test renderer must be required after react-native.
-import 'jest-styled-components'
-import { ReactTestInstance, act } from 'react-test-renderer'
-
-import { context, render, RenderAPI, waitFor } from 'testUtils'
+import { context, render, screen } from 'testUtils'
 import FormWrapper, { FieldType, FormFieldType } from './FormWrapper'
 import { VAModalPicker } from 'components'
 import VATextInput from './FormFields/VATextInput'
@@ -13,8 +7,6 @@ import VASelector, { VASelectorProps } from './FormFields/VASelector'
 import Mock = jest.Mock
 
 context('FormWrapper', () => {
-  let component: RenderAPI
-  let testInstance: ReactTestInstance
   let onSaveSpy: any
   let setOnSaveClicked: Mock
   let onSaveClicked: boolean
@@ -25,7 +17,7 @@ context('FormWrapper', () => {
       fieldProps: {
         labelKey: 'editDirectDeposit.routingNumber',
         inputType: 'phone',
-        onChange: () => {},
+        onChange: () => { },
         value: '12345',
         isRequiredField: true,
       },
@@ -36,7 +28,7 @@ context('FormWrapper', () => {
       fieldProps: {
         labelKey: 'editDirectDeposit.accountType',
         selectedValue: 'one',
-        onSelectionChange: () => {},
+        onSelectionChange: () => { },
         pickerOptions: [],
         isRequiredField: true,
       },
@@ -47,7 +39,7 @@ context('FormWrapper', () => {
       fieldProps: {
         labelKey: 'editDirectDeposit.confirm',
         selected: false,
-        onSelectionChange: () => {},
+        onSelectionChange: () => { },
         disabled: false,
         isRequiredField: true,
       },
@@ -63,112 +55,92 @@ context('FormWrapper', () => {
       onSaveClicked = value
     })
 
-    component = render(
+    render(
       <FormWrapper
         fieldsList={fieldsList}
         onSave={onSaveSpy}
-        setFormContainsError={() => {}}
+        setFormContainsError={() => { }}
         resetErrors={resetErrors}
         setOnSaveClicked={setOnSaveClicked}
         onSaveClicked={onSaveClicked}
       />,
     )
-
-    testInstance = component.UNSAFE_root
   }
 
   beforeEach(() => {
     initializeTestInstance()
   })
 
-  it('initializes correctly', async () => {
-    expect(component).toBeTruthy()
-  })
-
   describe('when the picker calls setError with an empty string', () => {
-    it('should set the error to empty string', async () => {
-      await waitFor(() => {
-        let shortenedFieldsList = formFieldsList[1]
-        initializeTestInstance([shortenedFieldsList])
-        testInstance.findByType(VAModalPicker).props.setError('')
-        expect(screen.queryByText('second error message')).toBeFalsy()
-      })
+    it('should set the error to empty string', () => {
+      let shortenedFieldsList = formFieldsList[1]
+      initializeTestInstance([shortenedFieldsList])
+      screen.UNSAFE_getByType(VAModalPicker).props.setError('')
+      expect(screen.queryByText('second error message')).toBeFalsy()
     })
   })
 
   describe('when the textinput calls setError with an empty string', () => {
-    it('should set the error to empty string', async () => {
-      await waitFor(() => {
-        let shortenedFieldsList = formFieldsList[0]
-        initializeTestInstance([shortenedFieldsList])
-        testInstance.findByType(VATextInput).props.setError('')
-        expect(screen.queryByText('first error message')).toBeFalsy()
-      })
+    it('should set the error to empty string', () => {
+      let shortenedFieldsList = formFieldsList[0]
+      initializeTestInstance([shortenedFieldsList])
+      screen.UNSAFE_getByType(VATextInput).props.setError('')
+      expect(screen.queryByText('first error message')).toBeFalsy()
     })
   })
 
   describe('when the checkbox calls setError with an empty string', () => {
-    it('should set the error to empty string', async () => {
-      await waitFor(() => {
-        let shortenedFieldsList = formFieldsList[2]
-        initializeTestInstance([shortenedFieldsList])
-        testInstance.findByType(VASelector).props.setError('')
-        expect(screen.queryByText('third error message')).toBeFalsy()
-      })
+    it('should set the error to empty string', () => {
+      let shortenedFieldsList = formFieldsList[2]
+      initializeTestInstance([shortenedFieldsList])
+      screen.UNSAFE_getByType(VASelector).props.setError('')
+      expect(screen.queryByText('third error message')).toBeFalsy()
     })
   })
 
   describe('when the picker calls setError with no parameter', () => {
-    it('should set the error to the field error message', async () => {
-      await waitFor(() => {
-        let shortenedFieldsList = formFieldsList[1]
-        initializeTestInstance([shortenedFieldsList])
-        testInstance.findByType(VAModalPicker).props.setError()
-        expect(screen.getByText('second error message')).toBeTruthy()
-      })
+    it('should set the error to the field error message', () => {
+      let shortenedFieldsList = formFieldsList[1]
+      initializeTestInstance([shortenedFieldsList])
+      screen.UNSAFE_getByType(VAModalPicker).props.setError()
+      expect(screen.getByText('second error message')).toBeTruthy()
     })
   })
 
   describe('when the textinput calls setError with no parameter', () => {
-    it('should set the error to the field error message', async () => {
-      await waitFor(() => {
-        let shortenedFieldsList = formFieldsList[0]
-        initializeTestInstance([shortenedFieldsList])
-        testInstance.findByType(VATextInput).props.setError()
-        expect(screen.getByText('first error message')).toBeTruthy()
-      })
+    it('should set the error to the field error message', () => {
+      let shortenedFieldsList = formFieldsList[0]
+      initializeTestInstance([shortenedFieldsList])
+      screen.UNSAFE_getByType(VATextInput).props.setError()
+      expect(screen.getByText('first error message')).toBeTruthy()
     })
   })
 
   describe('when the checkbox calls setError with no parameter', () => {
-    it('should set the error to the field error message', async () => {
-      await waitFor(() => {
-        let shortenedFieldsList = formFieldsList[2]
-        initializeTestInstance([shortenedFieldsList])
-        testInstance.findByType(VASelector).props.setError()
-        expect(screen.getByText('third error message')).toBeTruthy()
-      })
+    it('should set the error to the field error message', () => {
+      let shortenedFieldsList = formFieldsList[2]
+      initializeTestInstance([shortenedFieldsList])
+      screen.UNSAFE_getByType(VASelector).props.setError()
+      expect(screen.getByText('third error message')).toBeTruthy()
     })
   })
 
   describe('when resetErrors is true', () => {
-    it('should clear the errors object', async () => {
-      await waitFor(() => {
-        let shortenedFieldsList = formFieldsList[2]
-        initializeTestInstance([shortenedFieldsList])
-        testInstance.findByType(VASelector).props.setError()
-        expect(screen.getByText('third error message')).toBeTruthy()
+    it('should clear the errors object', () => {
+      let shortenedFieldsList = formFieldsList[2]
+      initializeTestInstance([shortenedFieldsList])
+      screen.UNSAFE_getByType(VASelector).props.setError()
+      expect(screen.getByText('third error message')).toBeTruthy()
 
-        initializeTestInstance([shortenedFieldsList], true)
-        expect(screen.getByText('I confirm that this information is correct. (Required)')).toBeTruthy()
-      })
+      initializeTestInstance([shortenedFieldsList], true)
+      expect(screen.getByText('I confirm that this information is correct. (Required)')).toBeTruthy()
     })
   })
 
   describe('when onSaveClicked is true', () => {
     describe('when there are no required fields not filled', () => {
       describe('when validation functions pass', () => {
-        it('should call onSave', async () => {
+        it('should call onSave', () => {
           let updatedList = formFieldsList
           let props = updatedList[2].fieldProps as VASelectorProps
           props.selected = true
