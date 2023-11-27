5732c716bfd1ff36d56f40b36deb1f10fd2165be
Liz Townsend <liz.townsend@adhocteam.us>
2023-11-27 11:49:27 -0600

    update appVersionAndBuild test

--- VAMobile/src/components/AppVersionAndBuild.test.tsx
+++ VAMobile/src/components/AppVersionAndBuild.test.tsx
@@ -1,29 +1,15 @@
-import 'react-native'
 import React from 'react'
-// Note: test renderer must be required after react-native.
-import 'jest-styled-components'
-
-import { context, findByTypeWithSubstring, render, RenderAPI } from 'testUtils'
-import TextView from './TextView'
+import { context, screen, render } from 'testUtils'
 import AppVersionAndBuild from './AppVersionAndBuild'
-import { ReactTestInstance } from 'react-test-renderer'
 
 context('AppVersionAndBuild', () => {
-  let component: RenderAPI
-  let testInstance: ReactTestInstance
   let props: any
 
   beforeEach(() => {
-    component = render(<AppVersionAndBuild {...props} />)
-
-    testInstance = component.UNSAFE_root
-  })
-
-  it('initializes correctly', async () => {
-    expect(component).toBeTruthy()
+    render(<AppVersionAndBuild {...props} />)
   })
 
   it('should display the Version: text', async () => {
-    expect(findByTypeWithSubstring(testInstance, TextView, 'Version:')).toBeTruthy()
+    expect(screen.getByText(/Version:/)).toBeTruthy()
   })
 })
