f71f594eb05ecce2a087754244df691fcce5417d
Liz Townsend <liz.townsend@adhocteam.us>
2023-11-28 11:52:22 -0600

    remove UNSAFE usage, pass text view instead

--- VAMobile/src/components/TextArea.test.tsx
+++ VAMobile/src/components/TextArea.test.tsx
@@ -2,25 +2,26 @@ import React from 'react'
 import Mock = jest.Mock
 import { context, render, screen, fireEvent } from 'testUtils'
 import TextArea from './TextArea'
+import TextView from './TextView'
 
 context('TextArea', () => {
   let onPressSpy: Mock
   beforeEach(() => {
     onPressSpy = jest.fn(() => { })
-    render(<TextArea onPress={onPressSpy} />)
+    render(<TextArea onPress={onPressSpy}><TextView>test text</TextView></TextArea>)
   })
 
   describe('when onPress exists', () => {
     it('should call onPress on press', async () => {
-      fireEvent.press(screen.UNSAFE_getByType(TextArea))
+      fireEvent.press(screen.getByText(/test text/))
       expect(onPressSpy).toBeCalled()
     })
   })
 
   describe('when onPress does not exist', () => {
     it('should not render a TouchableWithoutFeedback', async () => {
-      render(<TextArea />)
-      fireEvent.press(screen.UNSAFE_getByType(TextArea))
+      render(<TextArea><TextView>test text</TextView></TextArea>)
+      fireEvent.press(screen.getByText(/test text/))
       expect(onPressSpy).not.toBeCalled()
     })
   })
