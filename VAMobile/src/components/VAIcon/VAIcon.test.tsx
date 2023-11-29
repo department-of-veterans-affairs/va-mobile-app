d13364a787a18a31e2135871e956ec26f669ade3
Liz Townsend <liz.townsend@adhocteam.us>
2023-11-29 10:30:26 -0600

    remove styling test, remove initial state

--- VAMobile/src/components/VAIcon/VAIcon.test.tsx
+++ VAMobile/src/components/VAIcon/VAIcon.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { context, render, screen } from 'testUtils'
 import VAIcon from './VAIcon'
-import { InitialState } from 'store/slices'
 
 jest.mock('../../utils/hooks', () => {
   let original = jest.requireActual('../../utils/hooks')
@@ -18,22 +17,9 @@ jest.mock('../../utils/hooks', () => {
 context('VAIconTests', () => {
   describe('optional parameters', () => {
     it('should get passed to svg component', () => {
-      render(<VAIcon name={'HomeSelected'} testID={'myId'} height={1} width={2} />, {
-        preloadedState: {
-          ...InitialState,
-          accessibility: {
-            fontScale: 2,
-            isVoiceOverTalkBackRunning: false,
-            isFocus: true,
-          },
-        },
-      })
+      render(<VAIcon name={'HomeSelected'} testID={'myId'} height={1} width={2} />)
 
       expect(screen.getByTestId('myId')).toBeTruthy()
-      expect(screen.UNSAFE_getByProps({
-        height: 3,
-        width: 6,
-      })).toBeTruthy()
     })
   })
 })
