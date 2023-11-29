01cf66e3c307b6cace177a66aea015415f8ed2ca
Liz Townsend <liz.townsend@adhocteam.us>
2023-11-29 14:57:24 -0600

    make CategoryLanding test better

--- VAMobile/src/components/Templates/CategoryLanding.test.tsx
+++ VAMobile/src/components/Templates/CategoryLanding.test.tsx
@@ -1,16 +1,33 @@
 import React from 'react'
-import { context, render, screen } from 'testUtils'
+import { context, render, screen, fireEvent } from 'testUtils'
 import { CategoryLanding } from './CategoryLanding'
 
 context('CategoryLandingTemplate', () => {
+    const onPressSpy = jest.fn()
+
+    const initializeTestInstance = (
+        titleText?: string,
+        headerButton?: any
+    ) => {
+        render(<CategoryLanding title={titleText} headerButton={headerButton} />)
+    }
+
     beforeEach(() => {
-        render(<CategoryLanding title='category' />)
+        initializeTestInstance()
     })
 
-    it('should render the title as a header', () => {
-        expect(screen.getByRole('header', { name: 'category' })).toBeTruthy()
-    })
     it('should render Veterans Crisis Line information', () => {
         expect(screen.getByText(/Veterans Crisis Line/)).toBeTruthy()
+    })
+
+    it('should render the title as a header when passed in', () => {
+        initializeTestInstance('category')
+        expect(screen.getByRole('header', { name: 'category' })).toBeTruthy()
+    })
+
+    it('should render a header button if passed in', () => {
+        initializeTestInstance('', { label: 'test', icon: { name: 'HomeSelected' }, onPress: onPressSpy })
+        fireEvent.press(screen.getByRole('button', { name: 'test' }))
+        expect(onPressSpy).toHaveBeenCalled()
     })
 })
