4449897b71eeb69c2e257b529dc8dd062b15d218
Liz Townsend <liz.townsend@adhocteam.us>
2023-12-05 10:32:55 -0600

    update copy on Notifications screen

--- VAMobile/src/screens/HomeScreen/ProfileScreen/SettingsScreen/NotificationsSettingsScreen/NotificationsSettingsScreen.test.tsx
+++ VAMobile/src/screens/HomeScreen/ProfileScreen/SettingsScreen/NotificationsSettingsScreen/NotificationsSettingsScreen.test.tsx
@@ -36,13 +36,13 @@ jest.mock('store/slices/', () => {
 context('NotificationsSettingsScreen', () => {
   const apptPrefOn: PushPreference = {
     preferenceId: 'appointment_reminders',
-    preferenceName: 'Appointment Reminders',
+    preferenceName: 'Upcoming appointments',
     value: true,
   }
 
   const apptPrefOff: PushPreference = {
     preferenceId: 'appointment_reminders',
-    preferenceName: 'Appointment Reminders',
+    preferenceName: 'Upcoming appointments',
     value: false,
   }
 
@@ -66,19 +66,19 @@ context('NotificationsSettingsScreen', () => {
   describe('appointment reminders switch', () => {
     it('value should be true when pref is set to true', () => {
       renderWithProps(false, true, [apptPrefOn])
-      expect(screen.getByRole('switch', { name: 'Appointment Reminders'}).props.accessibilityState.checked).toEqual(true)
+      expect(screen.getByRole('switch', { name: 'Upcoming appointments' }).props.accessibilityState.checked).toEqual(true)
     })
 
     it('value should be false when pref is set to true', () => {
       renderWithProps(false, true, [apptPrefOff])
-      expect(screen.getByRole('switch', { name: 'Appointment Reminders'}).props.accessibilityState.checked).toEqual(false)
+      expect(screen.getByRole('switch', { name: 'Upcoming appointments' }).props.accessibilityState.checked).toEqual(false)
     })
   })
 
   describe('when system notifications are disabled', () => {
     it('hides the notification switches', () => {
       renderWithProps(false, false, [apptPrefOff])
-      expect(screen.queryByRole('switch', { name: 'Appointment Reminders'})).toBeFalsy()
+      expect(screen.queryByRole('switch', { name: 'Upcoming appointments' })).toBeFalsy()
       expect(screen.getByText("To get notifications from the VA mobile app, you'll need to turn them on in your system settings.")).toBeTruthy()
     })
   })
