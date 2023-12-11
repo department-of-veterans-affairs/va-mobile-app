09f31297d1af21d33a7aaf81bfa08d6d84470c0d
Liz Townsend <liz.townsend@adhocteam.us>
2023-12-11 07:38:29 -0600

    revert changes to file that triggers workflow

--- VAMobile/src/components/MessageAlert.tsx
+++ VAMobile/src/components/MessageAlert.tsx
@@ -48,8 +48,6 @@ const MessageAlert: FC<MessageAlertProps> = ({ hasValidationError, saveDraftAtte
     }
   }
 
-  console.info('Testing 1...2...3...')
-
   const text = saveDraftAttempted ? t('secureMessaging.formMessage.saveDraft.validation.text') : t('secureMessaging.formMessage.sendMessage.validation.text')
 
   return hasValidationError ? (
