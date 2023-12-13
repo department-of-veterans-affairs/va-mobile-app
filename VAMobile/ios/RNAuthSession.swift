42924393d86ac0e906bd80b6a3d47b208c179c2d
Dylan Nienberg <dylannienberg@gmail.com>
2023-12-13 02:26:38 -0600

    fixed

--- VAMobile/ios/RNAuthSession.swift
+++ VAMobile/ios/RNAuthSession.swift
@@ -83,8 +83,8 @@ class RNAuthSession: NSObject, RCTBridgeModule, ASWebAuthenticationPresentationC
  ///   - useWebKit: bool to use webkit or not
  ///   - resolve: React Native Promise resolver.
  /// - Returns: resolves true or false if it was able to clear the users cookies
-@objc(clearCookies:)
- func clearCookies(_ resolve: @escaping RCTPromiseResolveBlock)-> Void {
+ @objc(clearCookies:rejecter:)
+ func clearCookies(_ resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock)-> Void {
      let cookieStorage = HTTPCookieStorage.shared
      for c in cookieStorage.cookies ?? [] {
          cookieStorage.deleteCookie(c)
