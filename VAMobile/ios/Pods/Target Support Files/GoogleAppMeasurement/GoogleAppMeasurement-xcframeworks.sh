0184f9046af87a78878832d9cb7999bf18f57182
Dylan Nienberg <dylannienberg@gmail.com>
2023-12-13 01:40:09 -0600

    test

--- VAMobile/ios/Pods/Target Support Files/GoogleAppMeasurement/GoogleAppMeasurement-xcframeworks.sh
+++ VAMobile/ios/Pods/Target Support Files/GoogleAppMeasurement/GoogleAppMeasurement-xcframeworks.sh
@@ -20,8 +20,11 @@ variant_for_slice()
   "GoogleAppMeasurement.xcframework/ios-arm64_armv7")
     echo ""
     ;;
-  "GoogleAppMeasurement.xcframework/tvos-arm64_x86_64-simulator")
+  "GoogleAppMeasurement.xcframework/ios-arm64_i386_x86_64-simulator")
     echo "simulator"
+    ;;
+  "GoogleAppMeasurement.xcframework/ios-arm64_x86_64-maccatalyst")
+    echo "maccatalyst"
     ;;
   "GoogleAppMeasurement.xcframework/macos-arm64_x86_64")
     echo ""
@@ -29,10 +32,7 @@ variant_for_slice()
   "GoogleAppMeasurement.xcframework/tvos-arm64")
     echo ""
     ;;
-  "GoogleAppMeasurement.xcframework/ios-arm64_x86_64-maccatalyst")
-    echo "maccatalyst"
-    ;;
-  "GoogleAppMeasurement.xcframework/ios-arm64_i386_x86_64-simulator")
+  "GoogleAppMeasurement.xcframework/tvos-arm64_x86_64-simulator")
     echo "simulator"
     ;;
   esac
@@ -44,7 +44,10 @@ archs_for_slice()
   "GoogleAppMeasurement.xcframework/ios-arm64_armv7")
     echo "arm64 armv7"
     ;;
-  "GoogleAppMeasurement.xcframework/tvos-arm64_x86_64-simulator")
+  "GoogleAppMeasurement.xcframework/ios-arm64_i386_x86_64-simulator")
+    echo "arm64 i386 x86_64"
+    ;;
+  "GoogleAppMeasurement.xcframework/ios-arm64_x86_64-maccatalyst")
     echo "arm64 x86_64"
     ;;
   "GoogleAppMeasurement.xcframework/macos-arm64_x86_64")
@@ -53,11 +56,8 @@ archs_for_slice()
   "GoogleAppMeasurement.xcframework/tvos-arm64")
     echo "arm64"
     ;;
-  "GoogleAppMeasurement.xcframework/ios-arm64_x86_64-maccatalyst")
+  "GoogleAppMeasurement.xcframework/tvos-arm64_x86_64-simulator")
     echo "arm64 x86_64"
-    ;;
-  "GoogleAppMeasurement.xcframework/ios-arm64_i386_x86_64-simulator")
-    echo "arm64 i386 x86_64"
     ;;
   esac
 }
@@ -141,5 +141,5 @@ install_xcframework() {
   echo "Copied $source to $destination"
 }
 
-install_xcframework "${PODS_ROOT}/GoogleAppMeasurement/Frameworks/GoogleAppMeasurement.xcframework" "GoogleAppMeasurement/WithoutAdIdSupport" "framework" "ios-arm64_armv7" "ios-arm64_x86_64-maccatalyst" "ios-arm64_i386_x86_64-simulator"
+install_xcframework "${PODS_ROOT}/GoogleAppMeasurement/Frameworks/GoogleAppMeasurement.xcframework" "GoogleAppMeasurement/WithoutAdIdSupport" "framework" "ios-arm64_armv7" "ios-arm64_i386_x86_64-simulator" "ios-arm64_x86_64-maccatalyst"
 
