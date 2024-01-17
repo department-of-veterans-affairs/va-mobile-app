bd361ead17a47c24b9f9b1f9fc4b0a001c42a65c
Dylan Nienberg <dylannienberg@gmail.com>
2024-01-17 13:27:08 -0600

    VAButton and FooterButton are gone

--- VAMobile/src/components/FormWrapper/FormFields/FormAttachments.tsx
+++ VAMobile/src/components/FormWrapper/FormFields/FormAttachments.tsx
@@ -1,3 +1,4 @@
+import { Button, ButtonVariants } from '@department-of-veterans-affairs/mobile-component-library'
 import { Pressable } from 'react-native'
 import { useTranslation } from 'react-i18next'
 import React, { FC, ReactNode } from 'react'
@@ -5,7 +6,7 @@ import React, { FC, ReactNode } from 'react'
 import { ImagePickerResponse } from 'react-native-image-picker/src/types'
 import _ from 'underscore'
 
-import { Box, ButtonTypesConstants, TextView, VAButton, VAButtonProps, VAIcon } from 'components/index'
+import { Box, TextView, VAIcon } from 'components/index'
 import { DocumentPickerResponse } from 'screens/BenefitsScreen/BenefitsStackScreens'
 import { NAMESPACE } from 'constants/namespaces'
 import { getFileDisplay } from 'utils/common'
@@ -14,8 +15,6 @@ import { useTheme } from 'utils/hooks'
 export type FormAttachmentsProps = {
   /** callback called on click of remove link for an attachment */
   removeOnPress?: (attachment: ImagePickerResponse | DocumentPickerResponse) => void
-  /** optional props for large button */
-  largeButtonProps?: Omit<VAButtonProps, 'iconProps' | 'buttonType'>
   /** list of current attachments */
   attachmentsList?: Array<ImagePickerResponse | DocumentPickerResponse>
 }
@@ -80,7 +79,7 @@ const FormAttachments: FC<FormAttachmentsProps> = ({ removeOnPress, largeButtonP
       <Box mt={theme.dimensions.standardMarginBetween} mb={attachmentsDoNotExist || !largeButtonProps ? 0 : theme.dimensions.standardMarginBetween}>
         {renderFileNames()}
       </Box>
-      {!!largeButtonProps && <VAButton {...largeButtonProps} buttonType={ButtonTypesConstants.buttonSecondary} iconProps={{ ...iconProps, fill: 'active', name: 'PaperClip' }} />}
+      {!!largeButtonProps && <Button {...largeButtonProps} buttonType={ButtonVariants.Secondary} />}
     </Box>
   )
 }
