9300001f3a8bcc50530599ad622f95a5e860b1ea
Dylan Nienberg <dylannienberg@gmail.com>
2024-02-02 14:46:38 -0600

    ClaimsDetailsScreen

--- VAMobile/src/screens/BenefitsScreen/ClaimsScreen/ClaimDetailsScreen/ClaimDetailsScreen.test.tsx
+++ VAMobile/src/screens/BenefitsScreen/ClaimsScreen/ClaimDetailsScreen/ClaimDetailsScreen.test.tsx
@@ -1,111 +1,68 @@
 import React from 'react'
 import { fireEvent, screen } from '@testing-library/react-native'
 
-import { context, mockNavProps, render, waitFor, when } from 'testUtils'
+import { QueriesData, context, mockNavProps, render, waitFor, when } from 'testUtils'
 import * as api from 'store/api'
-import { InitialState, initialClaimsAndAppealsState, initialErrorsState } from 'store/slices'
 import ClaimDetailsScreen from './ClaimDetailsScreen'
-import { claim } from '../claimData'
-import { StackNavigationOptions } from '@react-navigation/stack'
+import { claim  as claimData } from '../claimData'
+import { claimsAndAppealsKeys } from 'api/claimsAndAppeals'
 
-jest.mock('@react-navigation/native', () => {
-  let actual = jest.requireActual('@react-navigation/native')
-  return {
-    ...actual,
-    useNavigation: () => ({
-      setOptions: jest.fn(),
-      goBack: jest.fn(),
-    }),
-  }
-})
-
-jest.mock('../../../../api/authorizedServices/getAuthorizedServices', () => {
-  let original = jest.requireActual('../../../../api/authorizedServices/getAuthorizedServices')
-  return {
-    ...original,
-    useAuthorizedServices: jest.fn().mockReturnValue({
-      status: 'success',
-      data: {
-        appeals: true,
-        appointments: true,
-        claims: true,
-        decisionLetters: true,
-        directDepositBenefits: true,
-        directDepositBenefitsUpdate: true,
-        disabilityRating: true,
-        genderIdentity: true,
-        lettersAndDocuments: true,
-        militaryServiceHistory: true,
-        paymentHistory: true,
-        preferredName: true,
-        prescriptions: true,
-        scheduleAppointments: true,
-        secureMessaging: true,
-        userProfileUpdate: true,
-      },
-    }),
-  }
-})
+when(api.get as jest.Mock)
+  .calledWith(`/v0/claim/0`, {}, expect.anything())
+  .mockResolvedValue({
+    data: {
+      ...claimData,
+    },
+  })
 
 context('ClaimDetailsScreen', () => {
   let props: any
-  let navHeaderSpy: any
 
-  const initializeTestInstance = () => {
+  const renderWithData = (claim?: Partial<api.ClaimData>): void => {
+    let queriesData: QueriesData | undefined
+    if (claim) {
+      queriesData = [{
+        queryKey: [claimsAndAppealsKeys.claim, '0'],
+        data: {
+          ...claim
+        }
+      }]
+    }
+
     props = mockNavProps(
       undefined,
       {
         navigate: jest.fn(),
         addListener: jest.fn(),
-        setOptions: (options: Partial<StackNavigationOptions>) => {
-          navHeaderSpy = {
-            back: options.headerLeft ? options.headerLeft({}) : undefined,
-          }
-        },
         goBack: jest.fn(),
       },
       { params: { claimID: '0', claimType: 'ACTIVE' } },
     )
 
-    render(<ClaimDetailsScreen {...props} />, {
-      preloadedState: {
-        ...InitialState,
-        claimsAndAppeals: {
-          ...initialClaimsAndAppealsState,
-          claim: claim,
-        },
-        errors: initialErrorsState,
-      },
-    })
+   render(<ClaimDetailsScreen {...props} />, {queriesData})
   }
 
   describe('when loadingClaim is set to true', () => {
     it('should show loading screen', async () => {
-      initializeTestInstance()
+      renderWithData()
       expect(screen.getByText('Loading your claim details...')).toBeTruthy()
     })
   })
 
   describe('when the selected tab is status', () => {
     it('should display the ClaimStatus component', async () => {
-      await waitFor(() => {
-        when(api.get as jest.Mock)
-          .calledWith(`/v0/claim/0`, {}, expect.anything())
-          .mockResolvedValue({ data: claim })
-        initializeTestInstance()
-      })
+      renderWithData({
+        ...claimData,
+      },)
       expect(screen.getByTestId('Step 1 of 5. completed. Claim received June 6, 2019')).toBeTruthy()
     })
   })
 
   describe('when the selected tab is status', () => {
     it('should display the ClaimDetails component', async () => {
-      await waitFor(() => {
-        when(api.get as jest.Mock)
-          .calledWith(`/v0/claim/0`, {}, expect.anything())
-          .mockResolvedValue({ data: claim })
-        initializeTestInstance()
-      })
+      renderWithData({
+        ...claimData,
+      },)
       fireEvent.press(screen.getByText('Details'))
       fireEvent.press(screen.getByText('Details'))
 
@@ -119,11 +76,9 @@ context('ClaimDetailsScreen', () => {
         .calledWith(`/v0/claim/0`, {}, expect.anything())
         .mockRejectedValue({ networkError: true } as api.APIError)
 
-      await waitFor(() => {
-        initializeTestInstance()
-      })
+      renderWithData()
 
-      expect(screen.getByText("The app can't be loaded.")).toBeTruthy()
+      await waitFor(() =>expect(screen.getByRole('header', { name: "The VA mobile app isn't working right now" })).toBeTruthy())
     })
   })
 })
