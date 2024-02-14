aa5bacbabd82ee4dd370d48560b2fe490d2223b2
Dylan Nienberg <dylannienberg@gmail.com>
2024-02-13 19:00:00 -0600

    unit tests fixed

--- VAMobile/src/screens/PaymentsScreen/PaymentHistory/PaymentDetailsScreen/PaymentDetailsScreen.test.tsx
+++ VAMobile/src/screens/PaymentsScreen/PaymentHistory/PaymentDetailsScreen/PaymentDetailsScreen.test.tsx
@@ -2,66 +2,58 @@ import React from 'react'
 
 import { screen } from '@testing-library/react-native'
 
-import { initialPaymentsState } from 'store/slices'
+import { PaymentsData } from 'api/types'
 import { context, mockNavProps, render } from 'testUtils'
 import { formatDateUtc } from 'utils/formattingUtils'
 
 import PaymentDetailsScreen from './PaymentDetailsScreen'
 
 context('PaymentDetailsScreen', () => {
-  let paymentDate: string
-  let formattedDate: string
+  const paymentDate = '2021-02-01T00:00:00.000-07:00'
+  const formattedDate = formatDateUtc(paymentDate, 'MMMM d, yyyy')
 
-  const initializeTestInstance = (pId: string = '1') => {
+  const payment1: PaymentsData = {
+    id: '1',
+    type: 'paymentHistoryInformation',
+    attributes: {
+      date: paymentDate,
+      amount: '$3,746.20',
+      paymentType: 'Compensation & Pension - Recurring',
+      paymentMethod: 'Direct Deposit',
+      bank: 'BANK OF AMERICA, N.A.',
+      account: '********0567',
+    },
+  }
+
+  const payment2: PaymentsData = {
+    id: '2',
+    type: 'paymentHistoryInformation',
+    attributes: {
+      date: paymentDate,
+      amount: '$3,746.20',
+      paymentType: 'Compensation & Pension - Recurring',
+      paymentMethod: 'Paper Check',
+      bank: null,
+      account: null,
+    },
+  }
+
+  const initializeTestInstance = (payment: PaymentsData) => {
     const props = mockNavProps(
       {},
       {
         goBack: jest.fn(),
         navigate: jest.fn(),
       },
-      { params: { paymentID: pId } },
+      { params: { payment: payment } },
     )
-    paymentDate = '2021-02-01T00:00:00.000-07:00'
-    formattedDate = formatDateUtc(paymentDate, 'MMMM d, yyyy')
 
-    render(<PaymentDetailsScreen {...props} />, {
-      preloadedState: {
-        payments: {
-          ...initialPaymentsState,
-          paymentsById: {
-            '1': {
-              id: '1',
-              type: 'paymentHistoryInformation',
-              attributes: {
-                date: paymentDate,
-                amount: '$3,746.20',
-                paymentType: 'Compensation & Pension - Recurring',
-                paymentMethod: 'Direct Deposit',
-                bank: 'BANK OF AMERICA, N.A.',
-                account: '********0567',
-              },
-            },
-            '2': {
-              id: '2',
-              type: 'paymentHistoryInformation',
-              attributes: {
-                date: paymentDate,
-                amount: '$3,746.20',
-                paymentType: 'Compensation & Pension - Recurring',
-                paymentMethod: 'Paper Check',
-                bank: null,
-                account: null,
-              },
-            },
-          },
-        },
-      },
-    })
+    render(<PaymentDetailsScreen {...props} />)
   }
 
   describe('when showing payment info', () => {
     it('should show payment details information when direct deposit', () => {
-      initializeTestInstance()
+      initializeTestInstance(payment1)
       expect(screen.getByText(formattedDate)).toBeTruthy()
       expect(screen.getByRole('header', { name: 'Compensation & Pension - Recurring' })).toBeTruthy()
       expect(screen.getByText('Amount')).toBeTruthy()
@@ -75,7 +67,7 @@ context('PaymentDetailsScreen', () => {
     })
 
     it('should show payment details information when paper check', () => {
-      initializeTestInstance('2')
+      initializeTestInstance(payment2)
       expect(screen.getByText(formattedDate)).toBeTruthy()
       expect(screen.getByRole('header', { name: 'Compensation & Pension - Recurring' })).toBeTruthy()
       expect(screen.getByText('Amount')).toBeTruthy()
