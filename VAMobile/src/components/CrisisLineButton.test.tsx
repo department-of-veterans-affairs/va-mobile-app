b98095b32019ecde84b3fddf2cb217e687fbe320
Theo Bentum <theo@adhocteam.us>
2024-02-15 20:57:21 -0800

    Create CrisisLineButton component

---
+++ VAMobile/src/components/CrisisLineButton.test.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import { fireEvent, screen } from '@testing-library/react-native'
+
+import { context, render } from 'testUtils'
+
+import CrisisLineButton from './CrisisLineButton'
+
+const mockNavigationSpy = jest.fn()
+jest.mock('utils/hooks', () => {
+  const original = jest.requireActual('utils/hooks')
+  return {
+    ...original,
+    useRouteNavigation: () => mockNavigationSpy,
+  }
+})
+
+context('CrisisLineButton', () => {
+  it('navigates to VeteransCrisisLine on press', () => {
+    render(<CrisisLineButton />)
+    fireEvent.press(screen.getByRole('button', { name: 'Talk to the Veterans Crisis Line now' }))
+    expect(mockNavigationSpy).toHaveBeenCalledWith('VeteransCrisisLine')
+  })
+})
