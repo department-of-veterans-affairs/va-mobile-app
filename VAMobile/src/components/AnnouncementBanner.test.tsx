88347f67b0f5847d7c0de51e4efc750922f81fa1
Chikaodi Adibemma <chika.adibemma@adhocteam.us>
2024-03-11 17:18:40 -0400

    fix test

--- VAMobile/src/components/AnnouncementBanner.test.tsx
+++ VAMobile/src/components/AnnouncementBanner.test.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Linking } from 'react-native'
 
 import { fireEvent, screen } from '@testing-library/react-native'
 
@@ -7,17 +6,27 @@ import { context, render } from 'testUtils'
 
 import AnnouncementBanner from './AnnouncementBanner'
 
+const mockExternalLinkSpy = jest.fn()
+
+jest.mock('utils/hooks', () => {
+  const original = jest.requireActual('utils/hooks')
+  return {
+    ...original,
+    useExternalLink: () => mockExternalLinkSpy,
+  }
+})
+
 context('AnnouncementBanner', () => {
   beforeEach(() => {
-    render(<AnnouncementBanner title={'Learn about PACT Act on VA.gov'} link={'www.va.gov'} />)
+    render(<AnnouncementBanner title={'Learn about PACT Act on VA.gov'} link={'https://www.va.gov'} />)
   })
 
   it('renders title', () => {
-    expect(screen.getByRole('button', { name: 'Learn about PACT Act on VA.gov' })).toBeTruthy()
+    expect(screen.getByRole('link', { name: 'Learn about PACT Act on VA.gov' })).toBeTruthy()
   })
 
   it('navigates to link when tapped', () => {
-    fireEvent.press(screen.getByRole('button', { name: 'Learn about PACT Act on VA.gov' }))
-    expect(Linking.openURL).toBeCalledWith('https://www.va.gov')
+    fireEvent.press(screen.getByRole('link', { name: 'Learn about PACT Act on VA.gov' }))
+    expect(mockExternalLinkSpy).toBeCalledWith('https://www.va.gov')
   })
 })
