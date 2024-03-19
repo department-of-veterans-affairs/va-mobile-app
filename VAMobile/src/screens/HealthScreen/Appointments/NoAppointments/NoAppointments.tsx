b3172b3933535d5f20fa3249772bd979c8bd8ea0
Chris Alexander <chris.alexander@adhocteam.us>
2024-03-19 12:20:25 -0600

    Use Link component in NoAppointments

--- VAMobile/src/screens/HealthScreen/Appointments/NoAppointments/NoAppointments.tsx
+++ VAMobile/src/screens/HealthScreen/Appointments/NoAppointments/NoAppointments.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 
-import { Box, ClickForActionLink, LinkTypeOptionsConstants, TextView } from 'components'
+import { Link } from '@department-of-veterans-affairs/mobile-component-library'
+
+import { Box, TextView } from 'components'
 import { NAMESPACE } from 'constants/namespaces'
 import { a11yLabelVA } from 'utils/a11yLabel'
-import { a11yHintProp, testIdProps } from 'utils/accessibility'
+import { testIdProps } from 'utils/accessibility'
+import { makeLinkAnalytics } from 'utils/analytics'
 import getEnv from 'utils/env'
 import { useTheme } from 'utils/hooks'
 
@@ -38,12 +41,13 @@ export function NoAppointments({ subText, subTextA11yLabel, showVAGovLink = true
         </TextView>
       </Box>
       {showVAGovLink && (
-        <ClickForActionLink
-          {...a11yHintProp(t('mobileBodyLink.a11yHint'))}
-          displayedText={t('noAppointments.visitVA')}
-          numberOrUrlLink={LINK_URL_SCHEDULE_APPOINTMENTS}
-          linkType={LinkTypeOptionsConstants.externalLink}
+        <Link
+          type="url"
+          url={LINK_URL_SCHEDULE_APPOINTMENTS}
+          text={t('noAppointments.visitVA')}
           a11yLabel={a11yLabelVA(t('noAppointments.visitVA'))}
+          a11yHint={t('mobileBodyLink.a11yHint')}
+          analytics={makeLinkAnalytics('https', LINK_URL_SCHEDULE_APPOINTMENTS)}
         />
       )}
     </Box>
