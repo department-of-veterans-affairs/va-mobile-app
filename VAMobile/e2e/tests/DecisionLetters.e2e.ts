851d3405b4c00b9fc5c8330621a7daa97425778a
rbontrager <rachael.bontrager@adhocteam.us>
2024-03-21 12:15:51 -0500

    Remove not needed constants

--- VAMobile/e2e/tests/DecisionLetters.e2e.ts
+++ VAMobile/e2e/tests/DecisionLetters.e2e.ts
@@ -1,41 +1,14 @@
 import { by, device, element, expect, waitFor } from 'detox'
 import { setTimeout } from 'timers/promises'
 
-import {
-  CommonE2eIdConstants,
-  loginToDemoMode,
-  openBenefits,
-  openClaims,
-  openClaimsHistory,
-  resetInAppReview,
-} from './utils'
+import { loginToDemoMode, openBenefits, openClaims, openClaimsHistory, resetInAppReview } from './utils'
 
 export const ClaimsE2eIdConstants = {
-  CLAIM_1_ID: 'Claim for compensation updated on December 05, 2021 Submitted December 05, 2021',
-  CLAIM_2_ID: 'Claim for compensation updated on December 04, 2021 Submitted December 04, 2021',
-  CLAIM_3_ID: 'Claim for compensation updated on July 20, 2021 Submitted July 20, 2021',
-  CLAIM_4_ID: 'Claim for compensation updated on May 05, 2021 Submitted January 01, 2021',
-  CLAIM_5_ID: 'Claim for compensation updated on May 04, 2021 Submitted January 01, 2021',
-  CLAIM_6_ID: 'Claim for dependency updated on July 30, 2016 Submitted January 01, 2016',
   CLOSED_CLAIM_DECISION_LETTER_ID:
     'Claim for compensation updated on April 09, 2021 Submitted January 01, 2021 Decision letter ready',
-  CLAIM_1_STATUS_STEP_1_ID: 'Step 1 of 5. completed. Claim received July 20, 2021',
-  CLAIM_1_STATUS_STEP_2_ID: 'Step 2 of 5. current. Initial review July 20, 2021',
-  CLAIM_1_STATUS_STEP_3_ID: 'Step 3 of 5.  Evidence gathering, review, and decision',
-  CLAIM_1_STATUS_STEP_4_ID: 'Step 4 of 5.  Preparation for notification',
-  CLAIM_1_STATUS_STEP_5_ID: 'Step 5 of 5.  Complete',
   GET_CLAIMS_LETTER_BUTTON_ID: 'getClaimLettersTestID',
   DECISION_CLAIM_LETTER_1_ID: 'March 11, 2023 letter Notification Letter (e.g. VA 20-8993, VA 21-0290, PCGL)',
   DECISION_CLAIM_LETTER_2_ID: 'September 21, 2022 letter Decision Rating Letter',
-  FILE_REQUEST_BUTTON_ID: 'Review file requests',
-  TAKE_OR_SELECT_PHOTOS_CAMERA_OPTION_TEXT: device.getPlatform() === 'ios' ? 'Camera' : 'Camera ',
-  TAKE_OR_SELECT_PHOTOS_PHOTO_GALLERY_OPTION_TEXT: device.getPlatform() === 'ios' ? 'Photo Gallery' : 'Photo gallery ',
-  SELECT_A_FILE_FILE_FOLDER_OPTION_TEXT: device.getPlatform() === 'ios' ? 'File Folder' : 'File folder ',
-  SELECT_A_FILE_TEXT: 'Select a file',
-  TAKE_OR_SELECT_PHOTOS_TEXT: 'Take or select photos',
-  ACCEPTED_FILE_TYPES_TEXT: 'PDF (unlocked), GIF, JPEG, JPG, BMP, TXT',
-  MAXIMUM_FILE_SIZE_LABEL: '50 megabytes',
-  CLAIMS_DETAILS_SCREEN_ID: 'ClaimDetailsScreen',
   CLAIMS_HISTORY_TEXT: 'Claims history',
 }
 
