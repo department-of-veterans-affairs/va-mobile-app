961022b2bffd153a64d81ce2d87439d8aa3f1ca4
Dylan Nienberg <87150991+Sparowhawk@users.noreply.github.com>
2024-03-26 09:55:16 -0500

    CU/6876-Dylan-LetterReactQueryMigration (#8088)
    * beginning of the end
    
    * all but unit tests updated
    
    * unit tests fixed
    
    * updated letters
    
    * refetch letter
    
    * updated
    
    * Update ResourceBundle-AccessibilityResources-React-Core-Info.plist

--- VAMobile/src/store/index.ts
+++ VAMobile/src/store/index.ts
@@ -7,7 +7,6 @@ import authReducer from 'store/slices/authSlice'
 import claimsAndAppealsReducer from 'store/slices/claimsAndAppealsSlice'
 import demoReducer from 'store/slices/demoSlice'
 import errorReducer from 'store/slices/errorSlice'
-import lettersReducer from 'store/slices/lettersSlice'
 import notificationReducer from 'store/slices/notificationSlice'
 import secureMessagingReducer from 'store/slices/secureMessagingSlice'
 import settingsReducer from 'store/slices/settingsSlice'
@@ -23,7 +22,6 @@ const store = configureStore({
     analytics: analyticsReducer,
     appointments: appointmentsReducer,
     claimsAndAppeals: claimsAndAppealsReducer,
-    letters: lettersReducer,
     notifications: notificationReducer,
     secureMessaging: secureMessagingReducer,
     snackBar: snackbarReducer,
