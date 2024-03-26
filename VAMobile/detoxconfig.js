059500cd54c6dea1f1ac3a293c4624ced196ccf0
rbontrager <rachael.bontrager@adhocteam.us>
2024-03-26 13:16:54 -0500

    Update detoxconfig.js

--- VAMobile/detoxconfig.js
+++ VAMobile/detoxconfig.js
@@ -5,17 +5,22 @@ module.exports = {
   ...tsjPreset,
   preset: 'react-native',
   maxWorkers: 1,
-  testEnvironment: "./e2e/environment",
-  setupFilesAfterEnv: ["./e2e/setup.ts"],
+  testEnvironment: './e2e/environment',
+  setupFilesAfterEnv: ['./e2e/setup.ts'],
   verbose: true,
-  reporters: ['detox/runners/jest/reporter', [ 'jest-junit', {
-    outputDirectory: '<rootDir>/e2e/test_reports',
-    outputName: 'e2e-junit.xml',
-  } ]],
-  globalSetup: 'detox/runners/jest/globalSetup',
+  reporters: [
+    'detox/runners/jest/reporter',
+    [
+      'jest-junit',
+      {
+        outputDirectory: '<rootDir>/e2e/test_reports',
+        outputName: 'e2e-junit.xml',
+      },
+    ],
+  ],
   globalTeardown: 'detox/runners/jest/globalTeardown',
   testTimeout: 400000,
-  testRegex: "\\.e2e\\.ts$",
+  testRegex: '\\.e2e\\.ts$',
   transform: {
     ...tsjPreset.transform,
     '^.+\\.tsx?$': [
@@ -28,7 +33,6 @@ module.exports = {
   },
   transformIgnorePatterns: ['jest-runner', '/node_modules/(?!native-base)/'],
   moduleNameMapper: {
-    '@env': '<rootDir>/env/test.env.ts'
+    '@env': '<rootDir>/env/test.env.ts',
   },
-};
-
+}
