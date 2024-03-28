22ab55c94ca723076841757bdc858f2e2e930bf5
Dylan Nienberg <dylannienberg@gmail.com>
2024-03-28 11:27:00 -0500

    finished

--- VAMobile/src/screens/HealthScreen/Appointments/AppointmentDetailsCommon/TypeOfCare.test.tsx
+++ VAMobile/src/screens/HealthScreen/Appointments/AppointmentDetailsCommon/TypeOfCare.test.tsx
@@ -8,21 +8,15 @@ import { defaultAppointmentAttributes } from 'utils/tests/appointments'
 import TypeOfCare from './TypeOfCare'
 
 context('TypeOfCare', () => {
-  const initializeTestInstance = (phoneOnly = false, typeOfCare?: string, healthcareService?: string): void => {
+  const initializeTestInstance = (phoneOnly = false, typeOfCare?: string): void => {
     const props = {
       ...defaultAppointmentAttributes,
-      healthcareService: healthcareService,
       phoneOnly: phoneOnly,
       typeOfCare: typeOfCare,
     }
 
     render(<TypeOfCare attributes={props} />)
   }
-
-  it('When a appointment with no type of care noted it should render correctly', () => {
-    initializeTestInstance(true, undefined, 'phone consult')
-    expect(screen.getByRole('header', { name: 'phone consult' })).toBeTruthy()
-  })
 
   it('When a appointment with no type of care or healthcare service noted it should render correctly', () => {
     initializeTestInstance(true)
