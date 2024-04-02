24d259f759d370b4f92d38d018738307c3df1f3c
Dylan Nienberg <dylannienberg@gmail.com>
2024-04-02 13:45:40 -0500

    updated immunization and payments

--- VAMobile/src/screens/HealthScreen/Vaccines/VaccineList/VaccineListScreen.tsx
+++ VAMobile/src/screens/HealthScreen/Vaccines/VaccineList/VaccineListScreen.tsx
@@ -18,11 +18,12 @@ import {
   TextLine,
 } from 'components'
 import { NAMESPACE } from 'constants/namespaces'
+import { DowntimeFeatureTypeConstants } from 'store/api'
 import { ScreenIDTypesConstants } from 'store/api/types/Screens'
 import { a11yLabelVA } from 'utils/a11yLabel'
 import { getA11yLabelText } from 'utils/common'
 import { formatDateMMMMDDYYYY } from 'utils/formattingUtils'
-import { useRouteNavigation, useTheme } from 'utils/hooks'
+import { useDowntime, useRouteNavigation, useTheme } from 'utils/hooks'
 import { screenContentAllowed } from 'utils/waygateConfig'
 
 import { HealthStackParamList } from '../../HealthStackScreens'
@@ -35,11 +36,12 @@ type VaccineListScreenProps = StackScreenProps<HealthStackParamList, 'VaccineLis
  */
 function VaccineListScreen({ navigation }: VaccineListScreenProps) {
   const [page, setPage] = useState(1)
+  const vaccinesInDowntime = useDowntime(DowntimeFeatureTypeConstants.immunizations)
   const {
     data: vaccines,
     isLoading: loading,
     isError: vaccineError,
-  } = useVaccines(page, { enabled: screenContentAllowed('WG_VaccineList') })
+  } = useVaccines(page, { enabled: screenContentAllowed('WG_VaccineList') && !vaccinesInDowntime })
   const theme = useTheme()
   const { t } = useTranslation(NAMESPACE.COMMON)
   const navigateTo = useRouteNavigation()
@@ -87,7 +89,7 @@ function VaccineListScreen({ navigation }: VaccineListScreenProps) {
     )
   }
 
-  if (vaccineError) {
+  if (vaccineError || vaccinesInDowntime) {
     return (
       <FeatureLandingTemplate
         backLabel={t('health.title')}
