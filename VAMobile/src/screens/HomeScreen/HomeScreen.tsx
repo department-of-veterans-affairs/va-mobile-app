abf9288d789c97f1661231bed8e2f71b13f522ab
Theo Bentum <theo@adhocteam.us>
2024-04-25 07:57:09 -0700

    feature/8038-theo-BenefitsScreenEdgeCases (#8402)
    * Add fonts and styles
    
    * Add translations
    
    * Add error and edge cases
    
    * Update logic
    
    * Update logic for showing count
    
    * Add type
    
    * Update text
    
    * Handle nonfatal errors
    
    * Add translation
    
    * Update claimsAndAppeals query logic
    
    * Remove AF logic
    
    * Easiest fix ever?
    
    * Added Error AccessibilityLabel
    
    * ExclamationCircleEdits
    
    * oops
    
    * Update icon alignment
    
    ---------
    
    Co-authored-by: Jon Bindbeutel <compjon@gmail.com>

--- VAMobile/src/screens/HomeScreen/HomeScreen.tsx
+++ VAMobile/src/screens/HomeScreen/HomeScreen.tsx
@@ -331,12 +331,13 @@ export function HomeScreen({}: HomeScreenProps) {
               mb={theme.dimensions.standardMarginBetween}
               accessible={true}
               accessibilityRole={'text'}
-              accessibilityLabel={t('aboutYou.error') + t('aboutYou.noInformation')}>
+              accessibilityLabel={t('errorIcon') + t('aboutYou.noInformation')}>
               <VAIcon
                 accessible={false}
                 importantForAccessibility="no"
                 name={'ExclamationCircle'}
                 fill={theme.colors.icon.homeScreenError}
+                preventScaling={true}
               />
               <TextView
                 ml={theme.dimensions.condensedMarginBetween}
