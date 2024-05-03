917868f7f8768190e8027a85185773235205077c
Dylan Nienberg <dylannienberg@gmail.com>
2024-05-03 16:59:48 -0500

    finished appt location for va in person and pending va appts

--- VAMobile/src/screens/HealthScreen/Appointments/AppointmentTypeComponents/SharedComponents/AppointmentLocation.tsx
+++ VAMobile/src/screens/HealthScreen/Appointments/AppointmentTypeComponents/SharedComponents/AppointmentLocation.tsx
@@ -1,45 +1,180 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 
+import { TFunction } from 'i18next'
+
 import { AppointmentAttributes } from 'api/types'
-import { Box, TextView } from 'components'
+import { Box, ClickToCallPhoneNumber, LinkWithAnalytics, TextView } from 'components'
 import { NAMESPACE } from 'constants/namespaces'
+import { VATheme } from 'styles/theme'
+import { a11yLabelVA } from 'utils/a11yLabel'
 import {
   AppointmentDetailsScreenType,
   AppointmentDetailsSubType,
   AppointmentDetailsSubTypeConstants,
+  AppointmentDetailsTypeConstants,
 } from 'utils/appointments'
+import getEnv from 'utils/env'
 import { useTheme } from 'utils/hooks'
+
+const { WEBVIEW_URL_FACILITY_LOCATOR } = getEnv()
 
 type AppointmentLocationProps = {
   attributes: AppointmentAttributes
   subType: AppointmentDetailsSubType
   type: AppointmentDetailsScreenType
 }
+const locationHeading = (subType: AppointmentDetailsSubType, type: AppointmentDetailsScreenType, t: TFunction) => {
+  switch (subType) {
+    case AppointmentDetailsSubTypeConstants.CanceledAndPending:
+    case AppointmentDetailsSubTypeConstants.Pending:
+      switch (type) {
+        case AppointmentDetailsTypeConstants.CommunityCare:
+          return undefined
+        default:
+          return t('appointments.canceled.whoCanceled.facility')
+      }
+    default:
+      switch (type) {
+        case AppointmentDetailsTypeConstants.InPersonVA:
+          return t('appointments.location.title')
+        default:
+          undefined
+      }
+  }
+}
+
+const getLocationNameAddressDirectionsPhone = (attributes: AppointmentAttributes, t: TFunction, theme: VATheme) => {
+  const { location } = attributes
+
+  const hasFullAddress = Boolean(
+    location?.address?.street && location?.address?.city && location?.address?.state && location?.address?.zipCode,
+  )
+  const hasLatLong = Boolean(location?.lat && location?.long)
+  const hasDirectionLink = hasFullAddress || hasLatLong
+  const hasPhone = Boolean(location?.phone?.number)
+  const locationName = location?.name
+  let missingBodyText
+
+  if (locationName && hasDirectionLink && !hasPhone) {
+    missingBodyText = t('appointments.inPersonVA.missingAddress.hasDirections.noPhone')
+  } else if (locationName && hasDirectionLink && hasPhone) {
+    missingBodyText = t('appointments.inPersonVA.missingAddress.hasDirections.noAddressOnly')
+  } else if (!locationName && hasDirectionLink && !hasPhone) {
+    missingBodyText = t('appointments.inPersonVA.missingAddress.hasDirections.noAnything')
+  } else if (locationName && !hasDirectionLink && !hasPhone) {
+    missingBodyText = t('appointments.inPersonVA.missingAddress.noDirections.noPhone')
+  } else if (locationName && !hasDirectionLink && hasPhone) {
+    missingBodyText = t('appointments.inPersonVA.missingAddress.noDirections.noAddressOnly')
+  } else if (!locationName && !hasDirectionLink && !hasPhone) {
+    missingBodyText = t('appointments.inPersonVA.missingAddress.noDirections.noAnything')
+  }
+
+  const locationData = { ...location, latitude: location.lat!, longitude: location.long! }
+
+  return (
+    <>
+      <TextView variant="MobileBody" selectable={true} mb={theme.dimensions.standardMarginBetween}>
+        {locationName}
+      </TextView>
+      {hasFullAddress ? (
+        <Box>
+          <TextView variant="MobileBody" selectable={true} mb={theme.dimensions.standardMarginBetween}>
+            {location.address?.street}
+          </TextView>
+          <TextView variant="MobileBody" selectable={true} mb={theme.dimensions.standardMarginBetween}>
+            {`${location.address?.city}, ${location.address?.state} ${location.address?.zipCode}`}
+          </TextView>
+        </Box>
+      ) : (
+        <TextView
+          variant={'MobileBody'}
+          selectable={true}
+          accessibilityLabel={a11yLabelVA(missingBodyText || '')}
+          mb={theme.dimensions.standardMarginBetween}>
+          {missingBodyText}
+        </TextView>
+      )}
+      {hasDirectionLink && (
+        <LinkWithAnalytics
+          type="directions"
+          locationData={locationData}
+          text={t('directions')}
+          a11yLabel={t('directions')}
+          a11yHint={t('directions.a11yHint')}
+          testID="directionsTestID"
+        />
+      )}
+      {hasPhone && <ClickToCallPhoneNumber phone={location?.phone} />}
+      {!hasFullAddress && !hasPhone && (
+        <LinkWithAnalytics
+          type="url"
+          url={WEBVIEW_URL_FACILITY_LOCATOR}
+          text={t('appointments.inPersonVA.missingAddress.goToVALink')}
+          a11yLabel={a11yLabelVA(t('appointments.inPersonVA.missingAddress.goToVALink'))}
+          a11yHint={t('upcomingAppointmentDetails.findYourVAFacility.a11yHint')}
+        />
+      )}
+    </>
+  )
+}
+
+const getClinicInfo = (
+  attributes: AppointmentAttributes,
+  subType: AppointmentDetailsSubType,
+  type: AppointmentDetailsScreenType,
+  t: TFunction,
+  theme: VATheme,
+) => {
+  const { friendlyLocationName, physicalLocation } = attributes
+
+  switch (subType) {
+    case AppointmentDetailsSubTypeConstants.CanceledAndPending:
+    case AppointmentDetailsSubTypeConstants.Pending:
+      return undefined
+  }
+
+  switch (type) {
+    case AppointmentDetailsTypeConstants.InPersonVA:
+      return (
+        <Box>
+          <TextView variant="MobileBody" my={theme.dimensions.standardMarginBetween}>
+            {t('appointments.clinic', {
+              clinicName:
+                friendlyLocationName && friendlyLocationName.length > 1
+                  ? friendlyLocationName
+                  : t('appointments.notAvailable'),
+            })}
+          </TextView>
+          <TextView variant="MobileBody" mb={theme.dimensions.standardMarginBetween}>
+            {t('appointments.physicalLocation', {
+              physicalLocation:
+                physicalLocation && physicalLocation.length > 1 ? physicalLocation : t('appointments.notAvailable'),
+            })}
+          </TextView>
+        </Box>
+      )
+    default:
+      return undefined
+  }
+}
 
 function AppointmentLocation({ attributes, subType, type }: AppointmentLocationProps) {
   const theme = useTheme()
   const { t } = useTranslation(NAMESPACE.COMMON)
-  const { location } = attributes
+  const heading = locationHeading(subType, type, t)
 
-  // let heading
-  // switch (subType) {
-  //   case AppointmentDetailsSubTypeConstants.CanceledAndPending:
-  //   case AppointmentDetailsSubTypeConstants.Pending:
-  //     return <></>
-  //   default:
-  //     heading = t('appointments.provider.title')
-  // }
-  // return (
-  //   <Box>
-  //     <TextView variant="MobileBodyBold" accessibilityRole="header" mb={theme.dimensions.standardMarginBetween}>
-  //       {heading}
-  //     </TextView>
-  //     <TextView variant="MobileBody" paragraphSpacing={true}>
-  //       {healthcareProvider}
-  //     </TextView>
-  //   </Box>
-  // )
+  if (heading) {
+    return (
+      <Box>
+        <TextView variant="MobileBodyBold" accessibilityRole="header" mb={theme.dimensions.standardMarginBetween}>
+          {heading}
+        </TextView>
+        {getLocationNameAddressDirectionsPhone(attributes, t, theme)}
+        {getClinicInfo(attributes, subType, type, t, theme)}
+      </Box>
+    )
+  }
 
   return <></>
 }
