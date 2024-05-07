3057dc101e181ca235634a8ca21c297a9fe12ce6
rbontrager <rachael.bontrager@adhocteam.us>
2024-05-07 08:58:33 -0500

    Update ContactInformation.e2e.ts

--- VAMobile/e2e/tests/ContactInformation.e2e.ts
+++ VAMobile/e2e/tests/ContactInformation.e2e.ts
@@ -431,9 +431,11 @@ describe(':ios: Contact Info Screen', () => {
   validateAddresses(ContactInfoE2eIdConstants.HOME_ADDRESS_ID, 'Home')
   removeContactInfoFeature('Home address 3101 N Fort Valley Rd, 2 Flagstaff, AZ, 86001', 'home address')
   validatePhoneNumbers(ContactInfoE2eIdConstants.HOME_PHONE_ID, 'Home')
+  removeContactInfoFeature(ContactInfoE2eIdConstants.HOME_PHONE_ID, 'home phone')
   validatePhoneNumbers(ContactInfoE2eIdConstants.WORK_PHONE_ID, 'Work')
   removeContactInfoFeature(ContactInfoE2eIdConstants.WORK_PHONE_ID, 'work phone')
   validatePhoneNumbers(ContactInfoE2eIdConstants.MOBILE_PHONE_ID, 'Mobile')
+  removeContactInfoFeature(ContactInfoE2eIdConstants.MOBILE_PHONE_ID, 'mobile phone')
 
   it('should open the email address', async () => {
     await resetInAppReview()
@@ -482,10 +484,8 @@ describe(':ios: Contact Info Screen', () => {
     await element(by.id(ContactInfoE2eIdConstants.CONTACT_INFO_PAGE_ID)).scrollTo('top')
   })
 
-  removeContactInfoFeature(ContactInfoE2eIdConstants.HOME_PHONE_ID, 'home phone')
-  removeContactInfoFeature(ContactInfoE2eIdConstants.MOBILE_PHONE_ID, 'mobile phone')
   removeContactInfoFeature(ContactInfoE2eIdConstants.EMAIL_ADDRESS_ID, 'email address')
 
   verifyNonUSorMilitaryAddresses(ContactInfoE2eIdConstants.HOME_ADDRESS_ID, 'Home')
-  verifyNonUSorMilitaryAddresses('Mailing address 3101 N Fort Valley Rd, 2 Flagstaff, AZ, 86001', 'Mailing')
+  verifyNonUSorMilitaryAddresses('Mailing address 3101 N Fort Valley Rd, Flagstaff, AZ, 86001', 'Mailing')
 })
