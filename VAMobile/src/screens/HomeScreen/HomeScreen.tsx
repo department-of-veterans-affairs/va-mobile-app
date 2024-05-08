d045821fce7201f02f4dbe690296c8be32b1e147
Theo Bentum <theo@adhocteam.us>
2024-05-08 15:06:15 -0700

    Fix downtime constant

--- VAMobile/src/screens/HomeScreen/HomeScreen.tsx
+++ VAMobile/src/screens/HomeScreen/HomeScreen.tsx
@@ -80,7 +80,7 @@ export function HomeScreen({}: HomeScreenProps) {
   const { data: userAuthorizedServices } = useAuthorizedServices()
   const appointmentsInDowntime = useDowntime(DowntimeFeatureTypeConstants.appointments)
   const claimsInDowntime = useDowntime(DowntimeFeatureTypeConstants.claims)
-  const appealsInDowntime = useDowntime(DowntimeFeatureTypeConstants.claims)
+  const appealsInDowntime = useDowntime(DowntimeFeatureTypeConstants.appeals)
   const rxInDowntime = useDowntime(DowntimeFeatureTypeConstants.rx)
   const smInDowntime = useDowntime(DowntimeFeatureTypeConstants.secureMessaging)
 
