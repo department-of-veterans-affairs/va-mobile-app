cf8518194ddf63edb4faf081f06c8ab8e58e8d92
Theo Bentum <theo@adhocteam.us>
2024-05-16 06:11:59 -0700

    Merge develop into feature-xl/HomeScreenPersonalization2 (5/15) (#8647)
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Tim Wright <459581+timwright12@users.noreply.github.com>
    Co-authored-by: Tim Wright <timwright12@gmail.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Dylan Nienberg <87150991+Sparowhawk@users.noreply.github.com>
    Co-authored-by: Adam Kasmier <116006847+bischoffa@users.noreply.github.com>
    Co-authored-by: Therese <94404065+TKDickson@users.noreply.github.com>
    Co-authored-by: Rachael Bontrager <rachael.bontrager@adhocteam.us>
    Co-authored-by: Dylan Nienberg <dylannienberg@gmail.com>
    Co-authored-by: VA Automation Bot <va-mobileapp@adhocteam.us>

--- VAMobile/e2e/tests/Messages.e2e.ts
+++ VAMobile/e2e/tests/Messages.e2e.ts
@@ -53,7 +53,6 @@ export const MessagesE2eIdConstants = {
   EDIT_DRAFT_CANCEL_ID: 'editDraftCancelTestID',
   EDIT_DRAFT_CANCEL_DELETE_TEXT: device.getPlatform() === 'ios' ? 'Delete Changes' : 'Delete Changes ',
   EDIT_DRAFT_CANCEL_SAVE_TEXT: device.getPlatform() === 'ios' ? 'Save Changes' : 'Save Changes ',
-  OPEN_URL_TEXT: 'Leave',
 }
 
 const tapItems = async (items: string, type: string) => {
@@ -383,7 +382,7 @@ describe('Messages Screen', () => {
     await device.launchApp({ newInstance: false })
 
     await element(by.text('Start a confidential chat')).tap()
-    await element(by.text('Leave')).tap()
+    await element(by.text(CommonE2eIdConstants.LEAVING_APP_LEAVE_TEXT)).tap()
     await setTimeout(5000)
     await device.takeScreenshot('messagesHelpChat')
     await device.launchApp({ newInstance: false })
