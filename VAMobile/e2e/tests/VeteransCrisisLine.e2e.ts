58a09d623663241c9911b1ea81ca70212b84258b
Theo Bentum <theo@adhocteam.us>
2024-05-16 06:11:59 -0700

    Merge develop into feature-xl/HomeScreenPersonalization2 (5/15) (#8647)
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Tim Wright <459581+timwright12@users.noreply.github.com>
    Co-authored-by: Tim Wright <timwright12@gmail.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Dylan Nienberg <87150991+Sparowhawk@users.noreply.github.com>
    Co-authored-by: Adam Kasmier <116006847+bischoffa@users.noreply.github.com>
    Co-authored-by: Therese <94404065+TKDickson@users.noreply.github.com>
    Co-authored-by: Rachael Bontrager <rachael.bontrager@adhocteam.us>
    Co-authored-by: Dylan Nienberg <dylannienberg@gmail.com>
    Co-authored-by: VA Automation Bot <va-mobileapp@adhocteam.us>

--- VAMobile/e2e/tests/VeteransCrisisLine.e2e.ts
+++ VAMobile/e2e/tests/VeteransCrisisLine.e2e.ts
@@ -1,7 +1,9 @@
+import { Commands } from 'react-native-webview/lib/RNCWebViewNativeComponent'
+
 import { by, device, element, expect } from 'detox'
 import { setTimeout } from 'timers/promises'
 
-import { loginToDemoMode, openVeteransCrisisLine } from './utils'
+import { CommonE2eIdConstants, loginToDemoMode, openVeteransCrisisLine } from './utils'
 
 export const VCLConstants = {
   HEADING_TEXT: 'Veterans Crisis Line',
@@ -53,7 +55,7 @@ describe('Veterans Crisis Line', () => {
 
   it('should open chat link', async () => {
     await element(by.text(VCLConstants.CHAT_LINK_TEXT)).tap()
-    await element(by.text('Leave')).tap()
+    await element(by.text(CommonE2eIdConstants.LEAVING_APP_LEAVE_TEXT)).tap()
     await setTimeout(5000)
     await device.takeScreenshot('CrisisLineChat')
     await device.launchApp({ newInstance: false })
@@ -61,7 +63,7 @@ describe('Veterans Crisis Line', () => {
 
   it('should open website link', async () => {
     await element(by.text(VCLConstants.VCL_SITE_LINK_TEXT)).tap()
-    await element(by.text('Leave')).tap()
+    await element(by.text(CommonE2eIdConstants.LEAVING_APP_LEAVE_TEXT)).tap()
     await setTimeout(5000)
     await device.takeScreenshot('VCLWebsite')
     await device.launchApp({ newInstance: false })
