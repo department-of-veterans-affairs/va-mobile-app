8f7ae3718a2c2b4561e8d4ad7d664ec9af68036f
Chris Alexander <chris.alexander@adhocteam.us>
2024-06-03 10:09:22 -0600

    Remove preferred modality (pending CC requests)

--- VAMobile/src/screens/HealthScreen/Appointments/AppointmentTypeComponents/CommunityCareAppointment.tsx
+++ VAMobile/src/screens/HealthScreen/Appointments/AppointmentTypeComponents/CommunityCareAppointment.tsx
@@ -13,7 +13,6 @@ import {
   AppointmentDetailsModality,
   AppointmentLocation,
   AppointmentPersonalContactInfo,
-  AppointmentPreferredModality,
   AppointmentProvider,
   AppointmentReasonAndComment,
   AppointmentTypeOfCare,
