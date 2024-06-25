46dc0cc806aa3e091654bf1107da586286266b45
Dylan Nienberg <87150991+Sparowhawk@users.noreply.github.com>
2024-06-25 15:52:20 -0500

    feature/8790-Dylan-CleanUpUnusedApptCode (#8868)
    Co-authored-by: Chris Alexander <chris.alexander@adhocteam.us>

--- VAMobile/src/screens/HealthScreen/Appointments/PastAppointments/PastAppointmentDetails.tsx
+++ VAMobile/src/screens/HealthScreen/Appointments/PastAppointments/PastAppointmentDetails.tsx
@@ -4,13 +4,10 @@ import { useTranslation } from 'react-i18next'
 import { StackScreenProps } from '@react-navigation/stack/lib/typescript/src/types'
 
 import { AppointmentAttributes, AppointmentData, AppointmentStatusConstants, AppointmentTypeConstants } from 'api/types'
-import { Box, ClickToCallPhoneNumber, FeatureLandingTemplate, LinkWithAnalytics, TextArea, TextView } from 'components'
+import { FeatureLandingTemplate } from 'components'
 import { Events, UserAnalytics } from 'constants/analytics'
 import { NAMESPACE } from 'constants/namespaces'
-import { a11yLabelVA } from 'utils/a11yLabel'
 import { logAnalyticsEvent, setAnalyticsUserProperty } from 'utils/analytics'
-import getEnv from 'utils/env'
-import { useTheme } from 'utils/hooks'
 import { registerReviewEvent } from 'utils/inAppReviews'
 
 import {
@@ -20,18 +17,6 @@ import {
   isAPendingAppointment,
 } from '../../../../utils/appointments'
 import { HealthStackParamList } from '../../HealthStackScreens'
-import {
-  AppointmentAddressAndNumber,
-  AppointmentAlert,
-  AppointmentReason,
-  AppointmentTypeAndDate,
-  ContactInformation,
-  PreferredAppointmentType,
-  PreferredDateAndTime,
-  ProviderName,
-  TypeOfCare,
-} from '../AppointmentDetailsCommon'
-import ClinicNameAndPhysicalLocation from '../AppointmentDetailsCommon/ClinicNameAndPhysicalLocation'
 import {
   ClaimExamAppointment,
   CommunityCareAppointment,
@@ -44,17 +29,13 @@ import {
 } from '../AppointmentTypeComponents'
 
 type PastAppointmentDetailsProps = StackScreenProps<HealthStackParamList, 'PastAppointmentDetails'>
-const { LINK_URL_VA_SCHEDULING } = getEnv()
 
 function PastAppointmentDetails({ route, navigation }: PastAppointmentDetailsProps) {
   const { appointment } = route.params
-
-  const theme = useTheme()
   const { t } = useTranslation(NAMESPACE.COMMON)
 
   const { attributes } = (appointment || {}) as AppointmentData
-  const { appointmentType, status, phoneOnly, location, serviceCategoryName } =
-    attributes || ({} as AppointmentAttributes)
+  const { appointmentType, status, phoneOnly, serviceCategoryName } = attributes || ({} as AppointmentAttributes)
   const appointmentIsCanceled = status === AppointmentStatusConstants.CANCELLED
   const pendingAppointment = isAPendingAppointment(attributes)
 
@@ -73,63 +54,6 @@ function PastAppointmentDetails({ route, navigation }: PastAppointmentDetailsPro
       registerReviewEvent()
     }
   }, [appointment, pendingAppointment, attributes])
-
-  const appointmentTypeAndDateIsLastItem =
-    appointmentType === AppointmentTypeConstants.VA_VIDEO_CONNECT_GFE ||
-    appointmentType === AppointmentTypeConstants.VA_VIDEO_CONNECT_HOME ||
-    appointmentIsCanceled
-
-  function renderScheduleAnotherAppointment() {
-    if (pendingAppointment) {
-      return <></>
-    }
-
-    if (
-      phoneOnly ||
-      (appointmentType === AppointmentTypeConstants.VA && serviceCategoryName !== 'COMPENSATION & PENSION')
-    ) {
-      const title = appointmentIsCanceled ? t('appointments.reschedule.title') : t('appointments.schedule.title')
-      const body = appointmentIsCanceled ? t('appointments.reschedule.body') : t('appointments.schedule.body')
-
-      return (
-        <Box mt={theme.dimensions.condensedMarginBetween}>
-          <TextArea>
-            <TextView
-              variant="MobileBodyBold"
-              accessibilityRole="header"
-              accessibilityLabel={a11yLabelVA(title)}
-              mb={theme.dimensions.condensedMarginBetween}>
-              {title}
-            </TextView>
-            <TextView variant="MobileBody" accessibilityLabel={a11yLabelVA(body)} paragraphSpacing={true}>
-              {body}
-            </TextView>
-            {location?.phone && location.phone.areaCode && location.phone.number ? (
-              <ClickToCallPhoneNumber phone={location.phone} />
-            ) : undefined}
-            <LinkWithAnalytics
-              type="url"
-              url={LINK_URL_VA_SCHEDULING}
-              text={t('appointments.vaSchedule')}
-              a11yLabel={a11yLabelVA(t('appointments.vaSchedule'))}
-            />
-          </TextArea>
-        </Box>
-      )
-    }
-
-    return (
-      <Box mt={theme.dimensions.condensedMarginBetween}>
-        <TextArea>
-          <TextView
-            variant="MobileBody"
-            accessibilityLabel={a11yLabelVA(t('pastAppointmentDetails.toScheduleAnotherAppointment'))}>
-            {t('pastAppointmentDetails.toScheduleAnotherAppointment')}
-          </TextView>
-        </TextArea>
-      </Box>
-    )
-  }
 
   const isInPersonVAAppointment =
     appointmentType === AppointmentTypeConstants.VA && serviceCategoryName !== 'COMPENSATION & PENSION'
@@ -209,25 +133,7 @@ function PastAppointmentDetails({ route, navigation }: PastAppointmentDetailsPro
           goBack={navigation.goBack}
         />
       ) : (
-        <Box mb={theme.dimensions.contentMarginBottom}>
-          <AppointmentAlert attributes={attributes} />
-          <TextArea>
-            <Box mb={appointmentTypeAndDateIsLastItem ? 0 : theme.dimensions.standardMarginBetween}>
-              <AppointmentTypeAndDate attributes={attributes} isPastAppointment={true} />
-            </Box>
-            <TypeOfCare attributes={attributes} />
-            <ProviderName attributes={attributes} />
-            <ClinicNameAndPhysicalLocation attributes={attributes} />
-            <AppointmentAddressAndNumber attributes={attributes} isPastAppointment={true} />
-
-            <PreferredDateAndTime attributes={attributes} />
-            <PreferredAppointmentType attributes={attributes} />
-            <AppointmentReason attributes={attributes} />
-            <ContactInformation attributes={attributes} />
-          </TextArea>
-
-          {renderScheduleAnotherAppointment()}
-        </Box>
+        <></>
       )}
     </FeatureLandingTemplate>
   )
