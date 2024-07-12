3b0a6c279c5912585f70e011bb747b54bd592284
Jon Bindbeutel <compjon@gmail.com>
2024-07-12 09:50:39 -0400

    stepping backwards to revamp

--- VAMobile/src/components/VALogo/VALogo.tsx
+++ VAMobile/src/components/VALogo/VALogo.tsx
@@ -10,8 +10,15 @@ export const VALogo = () => {
     width: 254,
     height: 57,
   }
-
-  return <Image style={logoProps} source={theme.mode === 'dark' ? { uri: 'VAOnDark' } : { uri: 'VAOnLight' }} />
+  if (theme.mode === 'dark') {
+    return (
+      <Image style={logoProps} source={require('@department-of-veterans-affairs/mobile-assets/VALogo/VAOnDark.png')} />
+    )
+  } else {
+    return (
+      <Image style={logoProps} source={require('@department-of-veterans-affairs/mobile-assets/VALogo/VAOnLight.png')} />
+    )
+  }
 }
 
 export default VALogo
