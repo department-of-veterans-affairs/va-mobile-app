120a8d62d94144323245c152e42387d50d440716
Dylan Nienberg <dylannienberg@gmail.com>
2024-07-18 11:30:05 -0500

    fixed linting error

--- VAMobile/src/screens/BenefitsScreen/ClaimsScreen/NeedHelpData/NeedHelpData.tsx
+++ VAMobile/src/screens/BenefitsScreen/ClaimsScreen/NeedHelpData/NeedHelpData.tsx
@@ -46,7 +46,11 @@ function NeedHelpData({ isAppeal }: NeedHelpDataProps) {
       <TextView variant="MobileBodyBold" accessibilityRole="header" accessible={true}>
         {t('claimDetails.needHelp')}
       </TextView>
-      <TextView variant="MobileBody" mb={theme.dimensions.standardMarginBetween} accessible={true} accessibilityLabel={t('claimDetails.callVA.a11yLabel')}>
+      <TextView
+        variant="MobileBody"
+        mb={theme.dimensions.standardMarginBetween}
+        accessible={true}
+        accessibilityLabel={t('claimDetails.callVA.a11yLabel')}>
         {t('claimDetails.callVA')}
       </TextView>
       <ClickToCallPhoneNumber phone={displayedTextPhoneNumber(t('8008271000'))} />
