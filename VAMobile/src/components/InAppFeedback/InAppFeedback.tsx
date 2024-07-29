e82a92b5ffdbdbe8f2f5a41424405109b48a283f
Jon Bindbeutel <compjon@gmail.com>
2024-07-29 14:26:33 -0400

    spike/inAppFeedbackPOC

---
+++ VAMobile/src/components/InAppFeedback/InAppFeedback.tsx
@@ -0,0 +1,73 @@
+import React, { useEffect } from 'react'
+import { useTranslation } from 'react-i18next'
+
+import { LinkProps } from '@department-of-veterans-affairs/mobile-component-library/src/components/Link/Link'
+
+import { useFacilitiesInfo } from 'api/facilities/getFacilitiesInfo'
+import { Facility } from 'api/types/FacilityData'
+import { Box, CollapsibleAlert, LinkWithAnalytics, TextView } from 'components'
+import { Events } from 'constants/analytics'
+import { NAMESPACE } from 'constants/namespaces'
+import { a11yLabelVA } from 'utils/a11yLabel'
+import { logAnalyticsEvent } from 'utils/analytics'
+import getEnv from 'utils/env'
+import { useTheme } from 'utils/hooks'
+
+const { LINK_URL_GO_TO_PATIENT_PORTAL } = getEnv()
+
+function InAppFeedback() {
+  const { t } = useTranslation(NAMESPACE.COMMON)
+  const theme = useTheme()
+
+  function accordionContent() {
+    const body = cernerFacilities.map((facility: Facility) => {
+      return (
+        <TextView
+          variant="MobileBodyBold"
+          key={facility.id}
+          mb={theme.dimensions.standardMarginBetween}
+          selectable={true}
+          accessibilityLabel={`${facility.name} (${a11yLabelVA(t('cernerAlert.nowUsing'))})`}>
+          {facility.name}
+          <TextView variant="MobileBody">{` (${t('cernerAlert.nowUsing')})`}</TextView>
+        </TextView>
+      )
+    })
+
+    const linkProps: LinkProps = {
+      type: 'url',
+      url: LINK_URL_GO_TO_PATIENT_PORTAL,
+      text: t('goToMyVAHealth'),
+      a11yLabel: a11yLabelVA(t('goToMyVAHealth')),
+      testID: 'goToMyVAHealthTestID',
+    }
+
+    return (
+      <Box mt={theme.paragraphSpacing.spacing20FontSize}>
+        <TextView variant="MobileBody" paragraphSpacing={true}>
+          {t('cernerAlert.ourRecordsShow')}
+        </TextView>
+        {body}
+        <TextView
+          variant="MobileBody"
+          paragraphSpacing={true}
+          accessibilityLabel={a11yLabelVA(t('cernerAlert.footer'))}>
+          {t('cernerAlert.footer')}
+        </TextView>
+        <LinkWithAnalytics {...linkProps} />
+      </Box>
+    )
+  }
+
+  return (
+    <CollapsibleAlert
+      border="warning"
+      headerText={headerText}
+      body={accordionContent()}
+      a11yLabel={headerA11yLabel}
+      onExpand={() => logAnalyticsEvent(Events.vama_cerner_alert_exp())}
+    />
+  )
+}
+
+export default CernerAlert
