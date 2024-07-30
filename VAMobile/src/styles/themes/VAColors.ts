c4ca032fbea525a6095c8a22c788b8497397e0b0
Theo Bentum <theo@adhocteam.us>
2024-07-29 19:02:03 -0700

    Replace remaining colors with component-library colors

--- VAMobile/src/styles/themes/VAColors.ts
+++ VAMobile/src/styles/themes/VAColors.ts
@@ -3,10 +3,6 @@
  */
 export default {
   transparent: 'transparent',
-  coolBlueLighter: '#8ba6ca',
   blackWith30PercentOpacity: '#0000004D',
   whiteWith70PercentOpacity: '#ffffffb3',
-  darkmodeBorder: '#4C4C4C',
-  darkmodeGrayWarmDark: '#292929',
-  primaryDarker2: '#2864AF',
 }
