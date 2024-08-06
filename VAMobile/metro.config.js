b32bdde62d10576d098601e35d063704878d3e58
Theo Bentum <theo@adhocteam.us>
2024-08-06 16:34:25 -0700

    Test default config

--- VAMobile/metro.config.js
+++ VAMobile/metro.config.js
@@ -1,8 +1,8 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config')
-const path = require('path')
+// const path = require('path')
 
 const defaultConfig = getDefaultConfig(__dirname)
-const { assetExts, sourceExts } = defaultConfig.resolver
+// const { assetExts, sourceExts } = defaultConfig.resolver
 
 /**
  * Metro configuration
@@ -11,14 +11,14 @@ const { assetExts, sourceExts } = defaultConfig.resolver
  * @type {import('metro-config').MetroConfig}
  */
 const config = {
-  transformer: {
-    babelTransformerPath: require.resolve('react-native-svg-transformer'),
-  },
-  resolver: {
-    assetExts: assetExts.filter((ext) => ext !== 'svg'),
-    sourceExts: [...sourceExts, 'svg', 'png'],
-  },
-  watchFolders: [path.resolve(__dirname, 'node_modules/@department-of-veterans-affairs/mobile-assets')],
+  // transformer: {
+  //   babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  // },
+  // resolver: {
+  //   assetExts: assetExts.filter((ext) => ext !== 'svg'),
+  //   sourceExts: [...sourceExts, 'svg', 'png'],
+  // },
+  // watchFolders: [path.resolve(__dirname, 'node_modules/@department-of-veterans-affairs/mobile-assets')],
 }
 
 module.exports = mergeConfig(defaultConfig, config)
