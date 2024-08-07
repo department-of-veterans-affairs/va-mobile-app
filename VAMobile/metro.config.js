b5aeb227b211430d826294ce87dd1a0eeaa6d793
Theo Bentum <theo@adhocteam.us>
2024-08-06 20:34:32 -0700

    Disable inlineRequires

--- VAMobile/metro.config.js
+++ VAMobile/metro.config.js
@@ -2,7 +2,7 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config')
 const path = require('path')
 
 const defaultConfig = getDefaultConfig(__dirname)
-// const { assetExts, sourceExts } = defaultConfig.resolver
+const { assetExts, sourceExts } = defaultConfig.resolver
 
 /**
  * Metro configuration
@@ -11,14 +11,20 @@ const defaultConfig = getDefaultConfig(__dirname)
  * @type {import('metro-config').MetroConfig}
  */
 const config = {
-  // transformer: {
-  //   babelTransformerPath: require.resolve('react-native-svg-transformer'),
-  // },
-  // resolver: {
-  //   assetExts: assetExts.filter((ext) => ext !== 'svg'),
-  //   sourceExts: [...sourceExts, 'svg', 'png'],
-  // },
-  watchFolders: [path.resolve(__dirname, 'node_modules/@department-of-veterans-affairs/mobile-assets')],
+  transformer: {
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false, // Disable inline requires
+      },
+    }),
+  },
+  resolver: {
+    assetExts: assetExts.filter((ext) => ext !== 'svg'),
+    sourceExts: [...sourceExts, 'svg'],
+  },
+  watchFolders: [path.resolve(__dirname, 'node_modules/@department-of-veterans-affairs')],
 }
 
 module.exports = mergeConfig(defaultConfig, config)
