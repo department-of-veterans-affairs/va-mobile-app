f39882acbae680d7cf39a8fa4dc5fdfa10f5fdb4
Jon Bindbeutel <compjon@gmail.com>
2024-08-13 11:03:31 -0400

    feature/7158-Binny-InAppReviewFeatureFlagForDetox

--- VAMobile/src/utils/inAppReviews.tsx
+++ VAMobile/src/utils/inAppReviews.tsx
@@ -6,6 +6,7 @@ import { Events } from 'constants/analytics'
 
 import { logAnalyticsEvent } from './analytics'
 import { getVersionName } from './deviceData'
+import { featureEnabled } from './remoteConfig'
 import { requestReview } from './rnReviews'
 
 export const STORAGE_REVIEW_EVENT_KEY = '@review_event'
@@ -15,20 +16,22 @@ export const IN_APP_REVIEW_INTERVAL_DAYS = 122
 export const IN_APP_REVIEW_ACTIONS_THRESHOLD = 7
 
 export const registerReviewEvent = async (): Promise<void> => {
-  const prev = await AsyncStorage.getItem(STORAGE_REVIEW_EVENT_KEY)
-  const total = prev ? parseInt(prev, 10) + 1 : 1
-  await AsyncStorage.setItem(STORAGE_REVIEW_EVENT_KEY, `${total}`)
-  const versionName = await getVersionName()
-  if (total >= IN_APP_REVIEW_ACTIONS_THRESHOLD) {
-    const lastReview = await AsyncStorage.getItem(STORAGE_LAST_REVIEW_PROMPT_DATE_MILLIS)
-    if (!lastReview) {
-      await callReviewAPI(versionName)
-    } else {
-      const days = DateTime.fromMillis(parseInt(lastReview, 10)).diffNow('days').days
-      const lastReviewVersion = await AsyncStorage.getItem(STORAGE_LAST_REVIEW_VERSION)
-
-      if (days > IN_APP_REVIEW_INTERVAL_DAYS && lastReviewVersion !== versionName) {
+  if (featureEnabled('inAppReview')) {
+    const prev = await AsyncStorage.getItem(STORAGE_REVIEW_EVENT_KEY)
+    const total = prev ? parseInt(prev, 10) + 1 : 1
+    await AsyncStorage.setItem(STORAGE_REVIEW_EVENT_KEY, `${total}`)
+    const versionName = await getVersionName()
+    if (total >= IN_APP_REVIEW_ACTIONS_THRESHOLD) {
+      const lastReview = await AsyncStorage.getItem(STORAGE_LAST_REVIEW_PROMPT_DATE_MILLIS)
+      if (!lastReview) {
         await callReviewAPI(versionName)
+      } else {
+        const days = DateTime.fromMillis(parseInt(lastReview, 10)).diffNow('days').days
+        const lastReviewVersion = await AsyncStorage.getItem(STORAGE_LAST_REVIEW_VERSION)
+
+        if (days > IN_APP_REVIEW_INTERVAL_DAYS && lastReviewVersion !== versionName) {
+          await callReviewAPI(versionName)
+        }
       }
     }
   }
