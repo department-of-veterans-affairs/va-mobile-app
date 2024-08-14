877d26c2d1c48d1d5c874419dd1fa1e4142d9382
Jon Bindbeutel <compjon@gmail.com>
2024-08-14 10:52:30 -0400

    chris suggestion

--- VAMobile/src/utils/inAppReviews.tsx
+++ VAMobile/src/utils/inAppReviews.tsx
@@ -16,22 +16,21 @@ export const IN_APP_REVIEW_INTERVAL_DAYS = 122
 export const IN_APP_REVIEW_ACTIONS_THRESHOLD = 7
 
 export const registerReviewEvent = async (): Promise<void> => {
-  if (featureEnabled('inAppReview')) {
-    const prev = await AsyncStorage.getItem(STORAGE_REVIEW_EVENT_KEY)
-    const total = prev ? parseInt(prev, 10) + 1 : 1
-    await AsyncStorage.setItem(STORAGE_REVIEW_EVENT_KEY, `${total}`)
-    const versionName = await getVersionName()
-    if (total >= IN_APP_REVIEW_ACTIONS_THRESHOLD) {
-      const lastReview = await AsyncStorage.getItem(STORAGE_LAST_REVIEW_PROMPT_DATE_MILLIS)
-      if (!lastReview) {
-        await callReviewAPI(versionName)
-      } else {
-        const days = DateTime.fromMillis(parseInt(lastReview, 10)).diffNow('days').days
-        const lastReviewVersion = await AsyncStorage.getItem(STORAGE_LAST_REVIEW_VERSION)
+  if (!featureEnabled('inAppReview')) return
+  const prev = await AsyncStorage.getItem(STORAGE_REVIEW_EVENT_KEY)
+  const total = prev ? parseInt(prev, 10) + 1 : 1
+  await AsyncStorage.setItem(STORAGE_REVIEW_EVENT_KEY, `${total}`)
+  const versionName = await getVersionName()
+  if (total >= IN_APP_REVIEW_ACTIONS_THRESHOLD) {
+    const lastReview = await AsyncStorage.getItem(STORAGE_LAST_REVIEW_PROMPT_DATE_MILLIS)
+    if (!lastReview) {
+      await callReviewAPI(versionName)
+    } else {
+      const days = DateTime.fromMillis(parseInt(lastReview, 10)).diffNow('days').days
+      const lastReviewVersion = await AsyncStorage.getItem(STORAGE_LAST_REVIEW_VERSION)
 
-        if (days > IN_APP_REVIEW_INTERVAL_DAYS && lastReviewVersion !== versionName) {
-          await callReviewAPI(versionName)
-        }
+      if (days > IN_APP_REVIEW_INTERVAL_DAYS && lastReviewVersion !== versionName) {
+        await callReviewAPI(versionName)
       }
     }
   }
