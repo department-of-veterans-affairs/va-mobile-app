ee9aeeb5748590fb533bd66f5751802ba0625ded
Theo Bentum <theo@adhocteam.us>
2024-08-19 21:42:04 -0700

    Add logic to WebViewScreen for SSO authentication

--- VAMobile/src/screens/WebviewScreen/WebviewScreen.tsx
+++ VAMobile/src/screens/WebviewScreen/WebviewScreen.tsx
@@ -9,14 +9,19 @@ import { Box, BoxProps, LoadingComponent } from 'components'
 import { BackButton } from 'components/BackButton'
 import { BackButtonLabelConstants } from 'constants/backButtonLabels'
 import { NAMESPACE } from 'constants/namespaces'
+import * as api from 'store/api'
 import { a11yLabelVA } from 'utils/a11yLabel'
 import { testIdProps } from 'utils/accessibility'
+import { logNonFatalErrorToFirebase } from 'utils/analytics'
+import getEnv from 'utils/env'
 import { useTheme } from 'utils/hooks'
 import { isIOS } from 'utils/platform'
 
 import WebviewControlButton from './WebviewControlButton'
 import WebviewControls, { WebviewControlsProps } from './WebviewControls'
 import WebviewTitle from './WebviewTitle'
+
+const { AUTH_SIS_TOKEN_EXCHANGE_URL } = getEnv()
 
 type ReloadButtonProps = {
   reloadPressed: () => void
@@ -78,10 +83,16 @@ export type WebviewStackParams = {
     displayTitle: string
     /** Text to appear with a lock icon in the header */
     loadingMessage?: string
+    /** Use SSO to authenticate webview */
+    useSSO?: boolean
   }
 }
 
 type WebviewScreenProps = StackScreenProps<WebviewStackParams, 'Webview'>
+
+type Cookies = {
+  [key: string]: string
+}
 
 /**
  * Screen for displaying web content within the app. Provides basic navigation and controls
@@ -93,8 +104,10 @@ function WebviewScreen({ navigation, route }: WebviewScreenProps) {
   const [canGoBack, setCanGoBack] = useState(false)
   const [canGoForward, setCanGoForward] = useState(false)
   const [currentUrl, setCurrentUrl] = useState('')
+  const [loading, setLoading] = useState(true)
+  const [headers, setHeaders] = useState<Cookies>()
 
-  const { url, displayTitle, loadingMessage } = route.params
+  const { url, displayTitle, loadingMessage, useSSO } = route.params
 
   const onReloadPressed = (): void => {
     webviewRef?.current.reload()
@@ -115,6 +128,42 @@ function WebviewScreen({ navigation, route }: WebviewScreenProps) {
       headerRight: () => <ReloadButton reloadPressed={onReloadPressed} />,
     })
   })
+
+  useEffect(() => {
+    const fetchSSOCookies = async () => {
+      try {
+        const response = await fetch(AUTH_SIS_TOKEN_EXCHANGE_URL, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+          body: new URLSearchParams({
+            grant_type: 'urn:ietf:params:oauth:grant-type:token-exchange',
+            subject_token: api.getAccessToken() || '',
+            subject_token_type: 'urn:ietf:params:oauth:token-type:access_token',
+            actor_token: api.getDeviceSecret() || '',
+            actor_token_type: 'urn:x-oath:params:oauth:token-type:device-secret',
+            client_id: 'vaweb',
+          }).toString(),
+        })
+
+        const cookieHeaders = response.headers.get('Set-Cookie')
+        const cookiesObj = cookieHeaders?.split(';').reduce((cookies: Cookies, cookie) => {
+          const [key, value] = cookie.split('=')
+          cookies[key.trim()] = value ? value.trim() : ''
+          return cookies
+        }, {})
+
+        setHeaders(cookiesObj)
+      } catch (error) {
+        logNonFatalErrorToFirebase(error, `Error fetching SSO cookies: ${error}`)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    useSSO && fetchSSOCookies()
+  }, [])
 
   const backPressed = (): void => {
     webviewRef?.current.goBack()
@@ -156,7 +205,9 @@ function WebviewScreen({ navigation, route }: WebviewScreenProps) {
     bottom: 0,
   }
 
-  return (
+  return useSSO && loading ? (
+    <WebviewLoading loadingMessage={loadingMessage} />
+  ) : (
     <Box {...mainViewBoxProps} {...testIdProps('Webview-page', true)}>
       <StatusBar
         translucent
@@ -166,7 +217,7 @@ function WebviewScreen({ navigation, route }: WebviewScreenProps) {
       <WebView
         startInLoadingState
         renderLoading={(): ReactElement => <WebviewLoading loadingMessage={loadingMessage} />}
-        source={{ uri: url }}
+        source={{ uri: url, headers }}
         injectedJavaScript={INJECTED_JAVASCRIPT}
         ref={webviewRef}
         // onMessage is required to be present for injected javascript to work on iOS
