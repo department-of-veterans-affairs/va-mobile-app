db808c6a6f8af85fc0807b70adf0c0fe27003769
Chris Alexander <chris.alexander@adhocteam.us>
2024-09-19 10:26:35 -0600

    Use ClickForActionLinkDeprecated when flag is enabled

--- VAMobile/src/components/AttachmentLink.tsx
+++ VAMobile/src/components/AttachmentLink.tsx
@@ -1,6 +1,14 @@
 import React, { FC } from 'react'
+import { AccessibilityProps, Pressable, PressableProps } from 'react-native'
 
+import { a11yHintProp } from 'utils/accessibility'
+import { useTheme } from 'utils/hooks'
+import { featureEnabled } from 'utils/remoteConfig'
+
+import Box from './Box'
 import LinkWithAnalytics from './LinkWithAnalytics'
+import TextView from './TextView'
+import VAIcon from './VAIcon'
 
 export type AttachmentLinkProps = {
   /** Name of link/attachment */
@@ -31,15 +39,43 @@ const AttachmentLink: FC<AttachmentLinkProps> = ({
   const text = [name, formattedSize].join(' ').trim()
   const textA11y = [name, formattedSizeA11y].join(' ').trim()
 
-  return (
-    <LinkWithAnalytics
-      type="attachment"
-      text={text}
-      onPress={onPress}
-      a11yLabel={textA11y}
-      a11yHint={a11yHint}
-      a11yValue={a11yValue}
-    />
-  )
+  const theme = useTheme()
+  if (featureEnabled('useOldLinkComponent')) {
+    const pressableProps: PressableProps = {
+      onPress: onPress,
+      accessibilityRole: 'button',
+      accessible: true,
+    }
+
+    const a11yProps: AccessibilityProps = {
+      accessibilityLabel: textA11y,
+      ...a11yHintProp(a11yHint || ''),
+      accessibilityValue: a11yValue ? { text: a11yValue } : {},
+    }
+
+    return (
+      <Pressable {...a11yProps} {...pressableProps}>
+        <Box flexDirection={'row'} mr={theme.dimensions.gutter}>
+          <Box mt={theme.dimensions.attachmentIconTopMargin} mr={theme.dimensions.textIconMargin}>
+            <VAIcon name="PaperClip" width={16} height={16} fill={'link'} />
+          </Box>
+          <TextView mr={theme.dimensions.textIconMargin} variant={'MobileBodyLink'}>
+            {text}
+          </TextView>
+        </Box>
+      </Pressable>
+    )
+  } else {
+    return (
+      <LinkWithAnalytics
+        type="attachment"
+        text={text}
+        onPress={onPress}
+        a11yLabel={textA11y}
+        a11yHint={a11yHint}
+        a11yValue={a11yValue}
+      />
+    )
+  }
 }
 export default AttachmentLink
