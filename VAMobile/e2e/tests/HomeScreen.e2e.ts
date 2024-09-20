ef62b56f2d55809db6956efe06ad4477fd1ed7ff
rbontrager <rachael.bontrager@adhocteam.us>
2024-09-20 14:24:02 -0500

    Update HomeScreen.e2e.ts

--- VAMobile/e2e/tests/HomeScreen.e2e.ts
+++ VAMobile/e2e/tests/HomeScreen.e2e.ts
@@ -125,6 +125,10 @@ describe('Home Screen', () => {
 
   it('taps home then jumps to claims from claims button', async () => {
     await element(by.text(CommonE2eIdConstants.HOME_TAB_BUTTON_TEXT)).tap()
+    await waitFor(element(by.text(HomeE2eIdConstants.CLAIMS_BUTTON_SUBTEXT_TEXT)))
+      .toBeVisible()
+      .whileElement(by.id('homeScreenID'))
+      .scroll(200, 'down')
     await element(by.text(HomeE2eIdConstants.CLAIMS_BUTTON_SUBTEXT_TEXT)).tap()
     await expect(element(by.text(CommonE2eIdConstants.CLAIMS_HISTORY_BUTTON_TEXT))).toExist()
   })
