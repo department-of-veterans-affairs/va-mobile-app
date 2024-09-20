54c96708449104982803ba2acdb0a69dda358b8b
Theo Bentum <theo@adhocteam.us>
2024-09-19 20:53:34 -0700

    Create MilitaryBranchLogo component

---
+++ VAMobile/src/components/MilitaryBranchLogo.tsx
@@ -0,0 +1,72 @@
+import React, { FC } from 'react'
+import { Image, ImageStyle, StyleProp } from 'react-native'
+
+import { BranchOfService, BranchesOfServiceConstants } from 'api/types'
+
+export type MilitaryBranchLogoProps = {
+  branch: BranchOfService
+  width: number
+  height: number
+}
+
+export const MilitaryBranchLogo: FC<MilitaryBranchLogoProps> = ({ branch, width, height }) => {
+  const style: StyleProp<ImageStyle> = {
+    width,
+    height,
+  }
+
+  switch (branch) {
+    case BranchesOfServiceConstants.AirForce:
+      return (
+        <Image
+          testID="United States Air Force"
+          style={style}
+          source={require('@department-of-veterans-affairs/mobile-assets/serviceEmblems/vic-air-force-coat-of-arms.png')}
+        />
+      )
+    case BranchesOfServiceConstants.Army:
+      return (
+        <Image
+          testID="United States Army"
+          style={style}
+          source={require('@department-of-veterans-affairs/mobile-assets/serviceEmblems/vic-army-symbol.png')}
+        />
+      )
+    case BranchesOfServiceConstants.CoastGuard:
+      return (
+        <Image
+          testID="United States Coast Guard"
+          style={style}
+          source={require('@department-of-veterans-affairs/mobile-assets/serviceEmblems/vic-cg-emblem.png')}
+        />
+      )
+    case BranchesOfServiceConstants.MarineCorps:
+      return (
+        <Image
+          testID="United States Marine Corps"
+          style={style}
+          source={require('@department-of-veterans-affairs/mobile-assets/serviceEmblems/vic-usmc-emblem.png')}
+        />
+      )
+    case BranchesOfServiceConstants.Navy:
+      return (
+        <Image
+          testID="United States Navy"
+          style={style}
+          source={require('@department-of-veterans-affairs/mobile-assets/serviceEmblems/vic-navy-emblem.png')}
+        />
+      )
+    case BranchesOfServiceConstants.SpaceForce:
+      return (
+        <Image
+          testID="United States Space Force"
+          style={style}
+          source={require('@department-of-veterans-affairs/mobile-assets/serviceEmblems/vic-space-force-logo.png')}
+        />
+      )
+    default:
+      return null
+  }
+}
+
+export default MilitaryBranchLogo
