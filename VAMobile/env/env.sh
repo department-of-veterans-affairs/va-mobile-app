7128ae03ae0528e71cf023eab44d1a2e5be41ceb
Theo Bentum <theo@adhocteam.us>
2024-09-24 01:49:36 -0700

    Set link prefixes dynamically

--- VAMobile/env/env.sh
+++ VAMobile/env/env.sh
@@ -52,7 +52,10 @@ fi
 echo "DEMO_PASSWORD=${DEMO_PASSWORD}" >> .env
 
 # set website URLs
+echo "LINK_URL_APPLY_FOR_HEALTH_CARE=https://${WEBSITE_PREFIX}va.gov/health-care/apply/application/introduction" >> .env
+echo "LINK_URL_CLAIM_APPEAL_STATUS=https://${WEBSITE_PREFIX}va.gov/claim-or-appeal-status" >> .env
 echo "LINK_URL_SCHEDULE_APPOINTMENTS=https://${WEBSITE_PREFIX}va.gov/health-care/schedule-view-va-appointments" >> .env
+echo "LINK_URL_VA_NOTIFICATIONS=https://${WEBSITE_PREFIX}va.gov/profile/notifications" >> .env
 
 # Get all vars that are the same across environments
 while read p; do
