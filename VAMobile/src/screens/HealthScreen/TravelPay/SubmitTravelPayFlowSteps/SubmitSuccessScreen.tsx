5cb58907e4c0b561333fd6c370314379245378ea
Liz Townsend <liz.townsend@adhocteam.us>
2024-09-27 15:11:44 -0500

    add content to success screen

--- VAMobile/src/screens/HealthScreen/TravelPay/SubmitTravelPayFlowSteps/SubmitSuccessScreen.tsx
+++ VAMobile/src/screens/HealthScreen/TravelPay/SubmitTravelPayFlowSteps/SubmitSuccessScreen.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 
 import { Box, TextView } from 'components'
+import { NAMESPACE } from 'constants/namespaces'
 import { useOrientation, useTheme } from 'utils/hooks'
 
 function SubmitSuccessScreen() {
+  const { t } = useTranslation(NAMESPACE.COMMON)
+
   const theme = useTheme()
   const isPortrait = useOrientation()
 
@@ -11,7 +15,21 @@ function SubmitSuccessScreen() {
     <Box
       mb={theme.dimensions.contentMarginBottom}
       mx={isPortrait ? theme.dimensions.gutter : theme.dimensions.headerHeight}>
-      <TextView>Submission success screen</TextView>
+      <TextView variant="BitterBoldHeading" accessibilityRole="header">
+        {t('travelPay.success.title')}
+      </TextView>
+      <TextView variant="MobileBody" mt={theme.dimensions.standardMarginBetween}>
+        {t('travelPay.success.text', { claimId: '12345' })}
+      </TextView>
+      <TextView variant="MobileBodyBold" mt={theme.dimensions.standardMarginBetween}>
+        {t('travelPay.success.nextTitle')}
+      </TextView>
+      <TextView variant="MobileBody" mt={theme.dimensions.standardMarginBetween}>
+        {t('travelPay.success.nextText')}
+      </TextView>
+      <TextView variant="MobileBody" mt={theme.dimensions.standardMarginBetween}>
+        {t('travelPay.success.nextText2')}
+      </TextView>
     </Box>
   )
 }
