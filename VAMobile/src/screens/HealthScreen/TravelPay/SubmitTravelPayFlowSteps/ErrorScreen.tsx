95f5035bd43f03372a0d0fdb32f7238da6062619
Liz Townsend <liz.townsend@adhocteam.us>
2024-09-27 15:44:19 -0500

    add content to error screen

--- VAMobile/src/screens/HealthScreen/TravelPay/SubmitTravelPayFlowSteps/ErrorScreen.tsx
+++ VAMobile/src/screens/HealthScreen/TravelPay/SubmitTravelPayFlowSteps/ErrorScreen.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 
 import { StackScreenProps } from '@react-navigation/stack'
 
 import { Box, TextView, VAScrollView } from 'components'
+import { NAMESPACE } from 'constants/namespaces'
 import { useOrientation, useTheme } from 'utils/hooks'
 
 import { SubmitTravelPayFlowModalStackParamList } from '../SubmitMileageTravelPayScreen'
@@ -11,6 +13,8 @@ import { FileOnlineComponent, TravelPayHelp } from './components'
 type ErrorScreenProps = StackScreenProps<SubmitTravelPayFlowModalStackParamList, 'ErrorScreen'>
 
 function ErrorScreen({ navigation, route }: ErrorScreenProps) {
+  const { t } = useTranslation(NAMESPACE.COMMON)
+
   const theme = useTheme()
   const isPortrait = useOrientation()
 
@@ -19,8 +23,13 @@ function ErrorScreen({ navigation, route }: ErrorScreenProps) {
       <Box
         mb={theme.dimensions.contentMarginBottom}
         mx={isPortrait ? theme.dimensions.gutter : theme.dimensions.headerHeight}>
-        <TextView>Error screen</TextView>
-        <TextView>{route.params.error}</TextView>
+        <TextView variant="BitterBoldHeading" accessibilityRole="header">
+          {t('travelPay.error.title')}
+        </TextView>
+        <TextView variant="MobileBody" mt={theme.dimensions.standardMarginBetween}>
+          {t('travelPay.error.text')}
+        </TextView>
+        {/* <TextView>{route.params.error}</TextView> */}
         <FileOnlineComponent />
         <TravelPayHelp />
       </Box>
