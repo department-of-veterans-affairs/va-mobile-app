192f7777109b55c38e320e3ed41176f906aed4a0
rbontrager <rachael.bontrager@adhocteam.us>
2024-09-30 13:05:45 -0500

    bug/9746-rachael-detox-fix-nightly-build-errors

--- VAMobile/e2e/setup.ts
+++ VAMobile/e2e/setup.ts
@@ -1,5 +1,7 @@
 import { device } from 'detox'
 
 beforeAll(async () => {
+  device.disableSynchronization()
   await device.launchApp({ newInstance: true, permissions: { notifications: 'YES' } })
+  device.enableSynchronization()
 })
