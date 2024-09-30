064ebaca39a1df16ccd503ebf07094feca35d531
rbontrager <rachael.bontrager@adhocteam.us>
2024-09-30 13:14:53 -0500

    Update setup.ts

--- VAMobile/e2e/setup.ts
+++ VAMobile/e2e/setup.ts
@@ -1,7 +1,5 @@
 import { device } from 'detox'
 
 beforeAll(async () => {
-  device.disableSynchronization()
   await device.launchApp({ newInstance: true, permissions: { notifications: 'YES' } })
-  device.enableSynchronization()
 })
