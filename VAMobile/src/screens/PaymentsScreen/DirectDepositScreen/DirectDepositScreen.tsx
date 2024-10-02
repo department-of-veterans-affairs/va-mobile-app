60075b0e1d8e6985293cfb96291fc1c584f26f6e
rbontrager <rachael.bontrager@adhocteam.us>
2024-10-02 13:55:55 -0500

    Update DirectDepositScreen.tsx

--- VAMobile/src/screens/PaymentsScreen/DirectDepositScreen/DirectDepositScreen.tsx
+++ VAMobile/src/screens/PaymentsScreen/DirectDepositScreen/DirectDepositScreen.tsx
@@ -96,7 +96,7 @@ function DirectDepositScreen({ navigation }: DirectDepositScreenProps) {
       backLabel={t('payments.title')}
       backLabelOnPress={navigation.goBack}
       title={t('directDeposit.title')}
-      testID="DirectDepositEditAccount1"
+      testID="DirectDepositEditAccount"
       backLabelTestID="directDepositPageBackID">
       {loading ? (
         <LoadingComponent text={t('directDeposit.loading')} />
