daf424c7c2c27db8ce417bfa672c26d83e1a1dc1
rbontrager <rachael.bontrager@adhocteam.us>
2024-10-02 12:30:19 -0500

    Update PushNotifications.e2e.ts

--- VAMobile/e2e/tests/PushNotifications.e2e.ts
+++ VAMobile/e2e/tests/PushNotifications.e2e.ts
@@ -5,7 +5,7 @@ import { backButton, loginToDemoMode } from './utils'
 const PushNotificationsConstants = {
   HEALTH_SCREEN_TITLE: 'Health',
   MESSAGE_COMPOSE_BUTTON_TEXT: 'Start new message',
-  REVIEW_MESSAGE_SCREEN_TITLE: 'Review message1',
+  REVIEW_MESSAGE_SCREEN_TITLE: 'Review message',
 }
 
 const mockNotification = {
