0cf241ace09e142913bf58b0d7d55a5733bac3bb
Dylan Nienberg <dylannienberg@gmail.com>
2024-10-04 10:45:45 -0500

    fix detox

--- VAMobile/src/api/notifications/getPushPreferences.tsx
+++ VAMobile/src/api/notifications/getPushPreferences.tsx
@@ -3,6 +3,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 import { useQuery } from '@tanstack/react-query'
 
 import { GetPushPrefsResponse, LoadPushPreferencesData } from 'api/types'
+import store from 'store'
 import { get } from 'store/api'
 
 import { notificationKeys } from './queryKeys'
@@ -13,9 +14,12 @@ import { DEVICE_ENDPOINT_SID } from './registerDevice'
  */
 const getPushPreferences = async (): Promise<LoadPushPreferencesData | undefined> => {
   const endpoint_sid = await AsyncStorage.getItem(DEVICE_ENDPOINT_SID)
+  const demoMode = store.getState().demo.demoMode
   let response
   if (endpoint_sid) {
     response = await get<GetPushPrefsResponse>(`/v0/push/prefs/${endpoint_sid}`)
+  } else if (demoMode) {
+    response = await get<GetPushPrefsResponse>(`/v0/push/prefs/`)
   }
   return {
     preferences: response?.data.attributes.preferences || [],
