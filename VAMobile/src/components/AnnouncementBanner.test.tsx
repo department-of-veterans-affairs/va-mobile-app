c1e3cd92e39c5c5ccaf0b0e86e52268b74314585
Theo Bentum <theo@adhocteam.us>
2024-10-07 20:53:59 -0700

    Add WebView unit test for AnnouncementBanner

--- VAMobile/src/components/AnnouncementBanner.test.tsx
+++ VAMobile/src/components/AnnouncementBanner.test.tsx
@@ -7,12 +7,14 @@ import { context, render } from 'testUtils'
 import AnnouncementBanner from './AnnouncementBanner'
 
 const mockExternalLinkSpy = jest.fn()
+const mockNavigationSpy = jest.fn()
 
 jest.mock('utils/hooks', () => {
   const original = jest.requireActual('utils/hooks')
   return {
     ...original,
     useExternalLink: () => mockExternalLinkSpy,
+    useRouteNavigation: () => mockNavigationSpy,
   }
 })
 
@@ -28,5 +30,25 @@ context('AnnouncementBanner', () => {
   it('navigates to link when tapped', () => {
     fireEvent.press(screen.getByRole('link', { name: 'Learn about PACT Act on VA.gov' }))
     expect(mockExternalLinkSpy).toBeCalledWith('https://www.va.gov')
+  })
+
+  it("opens WebView when 'useWebView' is true", () => {
+    const loadingMessage = 'Loading webview opened via announcement banner'
+
+    render(
+      <AnnouncementBanner
+        title={'Learn about PACT Act on VA.gov'}
+        link={'https://www.va.gov'}
+        useWebView={true}
+        webViewLoadingMessage={loadingMessage}
+      />,
+    )
+    fireEvent.press(screen.getByRole('link', { name: 'Learn about PACT Act on VA.gov' }))
+    expect(mockNavigationSpy).toHaveBeenCalledWith('Webview', {
+      url: 'https://www.va.gov',
+      displayTitle: 'va.gov',
+      loadingMessage,
+      useSSO: true,
+    })
   })
 })
