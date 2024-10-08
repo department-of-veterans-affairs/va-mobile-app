dbcd7b417d1e19c09a4e7fbe6d7f45ed05ef46d2
Charnelle <145595240+IsraelleHub@users.noreply.github.com>
2024-10-08 13:20:27 -0500

    bypasscheck

--- VAMobile/release_branch.sh
+++ VAMobile/release_branch.sh
@@ -20,7 +20,10 @@ Help() {
   echo "4. Checks out and pulls latest develop branch"
   echo "5. Creates a new release branch with the correct name and pushes it up to the origin"
   echo
-  echo "Syntax: ./release_branch.sh [-h]"
+  echo "Syntax: ./release_branch.sh [-h] [--bypass-date-check]"
+  echo "Options:"
+  echo "-h     Print this help."
+  echo "--bypass-date-check   Bypass the date check."
 }
 
 
@@ -39,6 +42,7 @@ increment_version() {
 
 
 #### Process the options
+bypass_date_check=false  # Initialize variable
 
 while [[ $# -gt 0 ]]
 do
@@ -61,7 +65,7 @@ do
 done
 
 # First release branch was 08-04-2021. check and see that we are at TWO WEEK interval (14 days) unless the --bypass-date-check flag is passed
-if $bypass_date_check || [[ $[$((($(date +%s)-$(date +%s --date "2021-08-04"))/(3600*24)))%14] == 0 ]]
+if [[ "$bypass_date_check" == true ]] || [[ $[$((($(date +%s)-$(date +%s --date "2021-08-04"))/(3600*24)))%14] == 0 ]]
 then
 
   echo "Checking out and pulling latest from main branch"
