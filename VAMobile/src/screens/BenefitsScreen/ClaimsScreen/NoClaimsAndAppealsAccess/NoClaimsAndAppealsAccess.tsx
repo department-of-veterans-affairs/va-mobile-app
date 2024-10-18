12e1e3b47c20dcfa895dce26f05d867f0ed2362d
rbontrager <rachael.bontrager@adhocteam.us>
2024-10-18 12:05:51 -0500

    Remove stuff

--- VAMobile/src/screens/BenefitsScreen/ClaimsScreen/NoClaimsAndAppealsAccess/NoClaimsAndAppealsAccess.tsx
+++ VAMobile/src/screens/BenefitsScreen/ClaimsScreen/NoClaimsAndAppealsAccess/NoClaimsAndAppealsAccess.tsx
@@ -7,8 +7,6 @@ import { a11yLabelVA } from 'utils/a11yLabel'
 import { displayedTextPhoneNumber } from 'utils/formattingUtils'
 import { useTheme } from 'utils/hooks'
 
-let detoxTestsToRun = ['Claims', 'Appeals']
-
 function NoClaimsAndAppealsAccess() {
   const theme = useTheme()
   const { t } = useTranslation(NAMESPACE.COMMON)
