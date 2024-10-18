12e161b534bda2e1b4c6829c61a846975ef1daff
Dylan Nienberg <dylannienberg@gmail.com>
2024-10-18 10:05:54 -0500

    updates

--- VAMobile/src/components/PhotoAdd.tsx
+++ VAMobile/src/components/PhotoAdd.tsx
@@ -2,11 +2,13 @@ import React, { FC } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Pressable, PressableProps } from 'react-native'
 
+import { Icon } from '@department-of-veterans-affairs/mobile-component-library'
+
 import { NAMESPACE } from 'constants/namespaces'
 import { useTheme } from 'utils/hooks'
 
 import TextView, { TextViewProps } from './TextView'
-import { Box, BoxProps, VAIcon } from './index'
+import { Box, BoxProps } from './index'
 
 type PhotoAddProps = {
   /** width of the component */
@@ -49,14 +51,7 @@ const PhotoAdd: FC<PhotoAddProps> = ({ width, height, onPress }) => {
   return (
     <Pressable {...pressableProps}>
       <Box {...boxProps}>
-        <VAIcon
-          name={'Add'}
-          width={32}
-          height={32}
-          fill={themeColor.icon.photoAdd}
-          fill2={themeColor.icon.transparent}
-          maxWidth={70}
-        />
+        <Icon name={'AddCircle'} width={40} height={40} fill={themeColor.icon.photoAdd} maxWidth={70} />
       </Box>
       <Box width={width} flexDirection="row">
         <TextView {...textProps}>{t('fileUpload.addPhoto')}</TextView>
