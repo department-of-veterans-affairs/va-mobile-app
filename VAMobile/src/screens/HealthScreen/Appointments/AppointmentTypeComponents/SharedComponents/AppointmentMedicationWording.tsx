ba441e5d7e5670b6f7228917ffc12bbcef7d4af3
Juan Castillo <juan.cast@agile6.com>
2024-10-23 10:54:07 -0700

    feat: 9763 Update Medication Wording | VA Video

--- VAMobile/src/screens/HealthScreen/Appointments/AppointmentTypeComponents/SharedComponents/AppointmentMedicationWording.tsx
+++ VAMobile/src/screens/HealthScreen/Appointments/AppointmentTypeComponents/SharedComponents/AppointmentMedicationWording.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 
-import { Box, LinkWithAnalytics, TextView } from 'components'
+import { TFunction } from 'i18next'
+
+import { Box, LinkWithAnalytics, TextView, VABulletList } from 'components'
 import { NAMESPACE } from 'constants/namespaces'
 import {
   AppointmentDetailsScreenType,
@@ -12,6 +14,15 @@ import {
 import getEnv from 'utils/env'
 import { useRouteNavigation, useTheme } from 'utils/hooks'
 
+const getWebViewLink = (onPress: () => void, t: TFunction) => (
+  <LinkWithAnalytics
+    type="custom"
+    icon={{ name: 'Launch' }}
+    onPress={onPress}
+    text={t('appointmentsTab.medicationWording.whatToBringLink')}
+  />
+)
+
 type AppointmentMedicationWordingProps = {
   subType: AppointmentDetailsSubType
   type: AppointmentDetailsScreenType
@@ -20,42 +31,72 @@ type AppointmentMedicationWordingProps = {
 const { WEBVIEW_URL_WHAT_TO_BRING_TO_APPOINTMENTS } = getEnv()
 
 function AppointmentMedicationWording({ subType, type }: AppointmentMedicationWordingProps) {
-  const theme = useTheme()
   const { t } = useTranslation(NAMESPACE.COMMON)
   const navigateTo = useRouteNavigation()
+  const body = t('appointmentsTab.medicationWording.default.body')
+  const theme = useTheme()
+
+  const openWebviewLink = () => {
+    navigateTo('Webview', {
+      url: WEBVIEW_URL_WHAT_TO_BRING_TO_APPOINTMENTS,
+      displayTitle: t('webview.vagov'),
+      loadingMessage: t('loading.vaWebsite'),
+    })
+  }
+
+  const webViewLink = getWebViewLink(openWebviewLink, t)
+
+  const getContent = () => {
+    switch (type) {
+      case AppointmentDetailsTypeConstants.InPersonVA:
+      case AppointmentDetailsTypeConstants.Phone:
+      case AppointmentDetailsTypeConstants.CommunityCare:
+      case AppointmentDetailsTypeConstants.VideoVA:
+        return (
+          <>
+            <TextView variant="MobileBody">{body}</TextView>
+            {webViewLink}
+          </>
+        )
+      case AppointmentDetailsTypeConstants.VideoAtlas:
+      case AppointmentDetailsTypeConstants.VideoGFE:
+      case AppointmentDetailsTypeConstants.VideoHome:
+        return (
+          <>
+            <VABulletList listOfText={[body]} />
+            {webViewLink}
+            <VABulletList listOfText={[t('appointmentsTab.medicationWording.bullet2')]} />
+            <LinkWithAnalytics
+              type="custom"
+              testID="prepareForVideoVisitTestID"
+              text={t('appointmentsTab.medicationWording.howToSetUpDevice')}
+              onPress={() => {
+                navigateTo('PrepareForVideoVisit')
+              }}
+            />
+          </>
+        )
+      default:
+        return null
+    }
+  }
 
   switch (subType) {
     case AppointmentDetailsSubTypeConstants.Upcoming:
     case AppointmentDetailsSubTypeConstants.Canceled:
-      switch (type) {
-        case AppointmentDetailsTypeConstants.InPersonVA:
-        case AppointmentDetailsTypeConstants.Phone:
-        case AppointmentDetailsTypeConstants.CommunityCare:
-        case AppointmentDetailsTypeConstants.VideoVA:
-          const body = t('appointmentsTab.medicationWording.default.body')
-          return (
-            <Box mb={theme.dimensions.standardMarginBetween}>
-              <TextView variant="MobileBodyBold" accessibilityRole="header">
-                {t('appointmentsTab.medicationWording.title')}
-              </TextView>
-              <TextView variant="MobileBody">{body}</TextView>
-              <LinkWithAnalytics
-                type="custom"
-                icon={{ name: 'Launch' }}
-                onPress={() => {
-                  navigateTo('Webview', {
-                    url: WEBVIEW_URL_WHAT_TO_BRING_TO_APPOINTMENTS,
-                    displayTitle: t('webview.vagov'),
-                    loadingMessage: t('loading'),
-                  })
-                }}
-                text={t('appointmentsTab.medicationWording.whatToBringLink')}
-              />
-            </Box>
-          )
-        default:
-          return <></>
+      const content = getContent()
+      if (!content) {
+        return <></>
       }
+
+      return (
+        <Box mb={theme.dimensions.standardMarginBetween}>
+          <TextView variant="MobileBodyBold" accessibilityRole="header">
+            {t('appointmentsTab.medicationWording.title')}
+          </TextView>
+          {content}
+        </Box>
+      )
     default:
       return <></>
   }
