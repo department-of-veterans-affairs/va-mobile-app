29f769c7034ae250eedff8776d23ebedb6d171df
Chris Alexander <chris.alexander@adhocteam.us>
2024-10-31 13:45:17 -0600

    Add test for MultiStepSubtask template

--- VAMobile/src/components/Templates/MultiStepSubtask.test.tsx
+++ VAMobile/src/components/Templates/MultiStepSubtask.test.tsx
@@ -1,1 +1,32 @@
-// TODO: Need to write tests for this component
+import React from 'react'
+
+import { createStackNavigator } from '@react-navigation/stack'
+
+import TextView from 'components/TextView'
+import { context, render, screen } from 'testUtils'
+
+import MultiStepSubtask from './MultiStepSubtask'
+
+context('MultiStepSubtask', () => {
+  type TestStackParams = {
+    TestScreen: undefined
+  }
+
+  const TestStackNavigator = createStackNavigator<TestStackParams>()
+
+  const TestScreen = () => {
+    return <TextView>This is the screen</TextView>
+  }
+
+  beforeEach(() => {
+    render(
+      <MultiStepSubtask<TestStackParams> stackNavigator={TestStackNavigator}>
+        <TestStackNavigator.Screen name="TestScreen" component={TestScreen} />
+      </MultiStepSubtask>,
+    )
+  })
+
+  it('renders child screen inside provided StackNavigator', () => {
+    expect(screen.getByText('This is the screen')).toBeTruthy()
+  })
+})
