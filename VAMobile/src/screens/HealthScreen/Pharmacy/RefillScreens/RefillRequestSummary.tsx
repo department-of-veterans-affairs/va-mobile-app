09e6ecc2cef8ddb4ad11ffc7043ba7762a709fba
rbontrager <rachael.bontrager@adhocteam.us>
2024-11-04 09:29:51 -0600

    Update RefillRequestSummary.tsx

--- VAMobile/src/screens/HealthScreen/Pharmacy/RefillScreens/RefillRequestSummary.tsx
+++ VAMobile/src/screens/HealthScreen/Pharmacy/RefillScreens/RefillRequestSummary.tsx
@@ -227,7 +227,7 @@ function RefillRequestSummary({ navigation, route }: RefillRequestSummaryProps) 
           onNavToHistory()
         }}
         title={t('refillRequest')}
-        leftButtonTestID="prescriptionsBackTestID">
+        leftButtonTestID="prescriptionsBackTestID1">
         {showLoadingScreenRequestRefillsRetry ? (
           <LoadingComponent text={t('prescriptions.refill.send', { count: 1 })} />
         ) : (
