37d624782718361899b66b87db8d2e19e1304930
Dylan Nienberg <dylannienberg@gmail.com>
2024-11-06 10:21:07 -0600

    more unit test fixes

--- VAMobile/src/screens/auth/RequestNotifications/RequestNotificationsScreen.test.tsx
+++ VAMobile/src/screens/auth/RequestNotifications/RequestNotificationsScreen.test.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
 
-import { fireEvent, screen } from '@testing-library/react-native'
+import { screen } from '@testing-library/react-native'
 import { t } from 'i18next'
 
-import { InitialState, setNotificationsPreferenceScreen, setRequestNotifications } from 'store/slices'
 import { context, render } from 'testUtils'
 
 import RequestNotificationsScreen from './RequestNotificationsScreen'
@@ -21,20 +20,5 @@ context('BiometricsPreferenceScreen', () => {
     expect(screen.getByRole('header', { name: t('requestNotifications.stayUpdated') })).toBeTruthy()
     expect(screen.getByText(t('requestNotifications.getNotified'))).toBeTruthy()
     expect(screen.getByText(t('requestNotifications.youCanChange'))).toBeTruthy()
-  })
-
-  describe('on click of the turn on notifications button', () => {
-    it('should call setNotificationsPreferenceScreen and setRequestNotifications', () => {
-      fireEvent.press(screen.getByRole('button', { name: t('requestNotifications.turnOn') }))
-      expect(setNotificationsPreferenceScreen).toHaveBeenCalledWith(false)
-      expect(setRequestNotifications).toHaveBeenCalledWith(true)
-    })
-  })
-
-  describe('on click of skip notifications for now button', () => {
-    it('should call setNotificationsPreferenceScreen', () => {
-      fireEvent.press(screen.getByRole('button', { name: t('requestNotifications.turnOn') }))
-      expect(setNotificationsPreferenceScreen).toHaveBeenCalledWith(false)
-    })
   })
 })
