9f8cc6bed0d040b60a255d753d62b083e9e867c2
Dylan Nienberg <dylannienberg@gmail.com>
2024-11-06 12:12:09 -0600

    more linting

--- VAMobile/src/api/authorizedServices/getAuthorizedServices.tsx
+++ VAMobile/src/api/authorizedServices/getAuthorizedServices.tsx
@@ -1,9 +1,12 @@
+import { useSelector } from 'react-redux'
+
 import { useQuery } from '@tanstack/react-query'
 import { has } from 'underscore'
 
-import { useAuthSettings } from 'api/auth/getAuthSettings'
 import { AuthorizedServicesPayload, UserAuthorizedServicesData } from 'api/types'
+import { RootState } from 'store'
 import { get } from 'store/api'
+import { AuthState } from 'store/slices'
 
 import { authorizedServicesKeys } from './queryKeys'
 
@@ -19,12 +22,12 @@ export const getAuthorizedServices = async (): Promise<UserAuthorizedServicesDat
  * Returns a query for user demographics
  */
 export const useAuthorizedServices = (options?: { enabled?: boolean }) => {
-  const authSettingsQuery = useAuthSettings()
+  const { loggedIn } = useSelector<RootState, AuthState>((state) => state.auth)
   const queryEnabled = options && has(options, 'enabled') ? options.enabled : true
 
   return useQuery({
     ...options,
-    enabled: !!(authSettingsQuery.data?.loggedIn && queryEnabled),
+    enabled: !!(loggedIn && queryEnabled),
     queryKey: authorizedServicesKeys.authorizedServices,
     queryFn: () => getAuthorizedServices(),
     meta: {
