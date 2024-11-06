aa11e2addfd94a3b8e4f3ea073e034c4f9524565
Dylan Nienberg <dylannienberg@gmail.com>
2024-11-06 10:39:53 -0600

    unit test test

--- VAMobile/src/screens/auth/RequestNotifications/RequestNotificationsScreen.test.tsx
+++ VAMobile/src/screens/auth/RequestNotifications/RequestNotificationsScreen.test.tsx
@@ -1,15 +1,25 @@
 import React from 'react'
 
-import { screen } from '@testing-library/react-native'
+import { fireEvent, screen } from '@testing-library/react-native'
 import { t } from 'i18next'
 
+import NotificationManager, { useNotificationContext } from 'components/NotificationManager'
 import { context, render } from 'testUtils'
 
 import RequestNotificationsScreen from './RequestNotificationsScreen'
 
 context('BiometricsPreferenceScreen', () => {
+  const setRequestNotifications = jest.fn()
+  const setNotificationsPreferenceScreen = jest.fn()
   const initializeTestInstance = () => {
-    render(<RequestNotificationsScreen />)
+    const notificationContext = useNotificationContext()
+    notificationContext.setRequestNotifications = setRequestNotifications
+    notificationContext.setRequestNotificationPreferenceScreen = setNotificationsPreferenceScreen
+    render(
+      <NotificationManager {...notificationContext}>
+        <RequestNotificationsScreen />
+      </NotificationManager>,
+    )
   }
 
   beforeEach(() => {
@@ -20,5 +30,20 @@ context('BiometricsPreferenceScreen', () => {
     expect(screen.getByRole('header', { name: t('requestNotifications.stayUpdated') })).toBeTruthy()
     expect(screen.getByText(t('requestNotifications.getNotified'))).toBeTruthy()
     expect(screen.getByText(t('requestNotifications.youCanChange'))).toBeTruthy()
+  })
+
+  describe('on click of the turn on notifications button', () => {
+    it('should call setNotificationsPreferenceScreen and setRequestNotifications', () => {
+      fireEvent.press(screen.getByRole('button', { name: t('requestNotifications.turnOn') }))
+      expect(setNotificationsPreferenceScreen).toHaveBeenCalledWith(false)
+      expect(setRequestNotifications).toHaveBeenCalledWith(true)
+    })
+  })
+
+  describe('on click of skip notifications for now button', () => {
+    it('should call setNotificationsPreferenceScreen', () => {
+      fireEvent.press(screen.getByRole('button', { name: t('requestNotifications.turnOn') }))
+      expect(setNotificationsPreferenceScreen).toHaveBeenCalledWith(false)
+    })
   })
 })
