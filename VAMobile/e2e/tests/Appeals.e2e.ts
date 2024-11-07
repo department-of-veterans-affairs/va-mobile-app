8c0fc0929406029f112519589b96e9c3e07e1c14
rbontrager <rachael.bontrager@adhocteam.us>
2024-11-07 09:37:59 -0600

    Trying new logic

--- VAMobile/e2e/tests/Appeals.e2e.ts
+++ VAMobile/e2e/tests/Appeals.e2e.ts
@@ -1,13 +1,7 @@
 import { by, device, element, expect, waitFor } from 'detox'
 import { setTimeout } from 'timers/promises'
 
-import {
-  CommonE2eIdConstants,
-  loginToDemoMode,
-  openBenefits,
-  openClaims,
-  openClaimsHistory,
-} from '../../../../e2e/tests/utils'
+import { CommonE2eIdConstants, loginToDemoMode, openBenefits, openClaims, openClaimsHistory } from './utils'
 
 export const AppealsIdConstants = {
   APPEAL_1_ID: 'Disability compensation appeal Received June 12, 2008 Moved to this step on November 22, 2011',
