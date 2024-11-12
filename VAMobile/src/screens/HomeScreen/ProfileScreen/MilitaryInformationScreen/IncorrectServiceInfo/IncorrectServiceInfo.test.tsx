cf7df1b7b21150734b44b059e3b0312be4978e39
ATeal <arteal89@gmail.com>
2024-11-12 09:47:54 -0500

    chore: update military info screen tests with i18n

--- VAMobile/src/screens/HomeScreen/ProfileScreen/MilitaryInformationScreen/IncorrectServiceInfo/IncorrectServiceInfo.test.tsx
+++ VAMobile/src/screens/HomeScreen/ProfileScreen/MilitaryInformationScreen/IncorrectServiceInfo/IncorrectServiceInfo.test.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 import { Linking } from 'react-native'
 
 import { fireEvent, screen } from '@testing-library/react-native'
+import { t } from 'i18next'
 
 import { context, mockNavProps, render } from 'testUtils'
+import { displayedTextPhoneNumber } from 'utils/formattingUtils'
 
 import IncorrectServiceInfo from './index'
 
@@ -14,29 +16,16 @@ context('IncorrectServiceInfo', () => {
   })
 
   it('initializes correctly', () => {
-    expect(
-      screen.getByRole('header', { name: "What if my military service information doesn't look right?" }),
-    ).toBeTruthy()
-    expect(
-      screen.getByText(
-        'Some Veterans have reported seeing military service information in their VA.gov profiles that doesn’t seem right. When this happens, it’s because there’s an error in the information we’re pulling into VA.gov from the Defense Enrollment Eligibility Reporting System (DEERS).',
-      ),
-    ).toBeTruthy()
-    expect(
-      screen.getByText(
-        'If the military service information in your profile doesn’t look right, please call the Defense Manpower Data Center (DMDC). They’ll work with you to update your information in DEERS.',
-      ),
-    ).toBeTruthy()
-    expect(
-      screen.getByText(
-        'To reach the DMDC, call Monday through Friday (except federal holidays), 8:00 a.m. to 8:00 p.m. ET.',
-      ),
-    ).toBeTruthy()
-    expect(screen.getByRole('link', { name: '800-538-9552' })).toBeTruthy()
+    expect(screen.getByRole('header', { name: t('militaryInformation.incorrectServiceInfo') })).toBeTruthy()
+    expect(screen.getByText(t('militaryInformation.incorrectServiceInfo.body.1'))).toBeTruthy()
+    expect(screen.getByText(t('militaryInformation.incorrectServiceInfo.body.2'))).toBeTruthy()
+    expect(screen.getByText(t('militaryInformation.incorrectServiceInfo.body.3'))).toBeTruthy()
+    expect(screen.getByRole('link', { name: displayedTextPhoneNumber(t('8005389552')) })).toBeTruthy()
   })
 
   it('should call DMDC on press', () => {
-    fireEvent.press(screen.getByRole('link', { name: '800-538-9552' }))
-    expect(Linking.openURL).toHaveBeenCalledWith('tel:8005389552')
+    const i18nPhoneNumber = t('8005389552')
+    fireEvent.press(screen.getByRole('link', { name: displayedTextPhoneNumber(i18nPhoneNumber) }))
+    expect(Linking.openURL).toHaveBeenCalledWith(`tel:${i18nPhoneNumber}`)
   })
 })
