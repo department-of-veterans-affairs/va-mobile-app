b4b2bd8f2d93e8e9d1b324eb6c390a8487adfadf
Matthew Guest - Work <matthewguest-wilcore@Matthews-MacBook-Pro.local>
2024-11-21 13:16:12 -0800

    chore: updated PaymentHistory screens with i18n

--- VAMobile/src/screens/PaymentsScreen/PaymentHistory/PaymentMissingSceen/PaymentMissingScreen.test.tsx
+++ VAMobile/src/screens/PaymentsScreen/PaymentHistory/PaymentMissingSceen/PaymentMissingScreen.test.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 import { Linking } from 'react-native'
 
 import { fireEvent, screen } from '@testing-library/react-native'
+import { t } from 'i18next'
 
 import { context, mockNavProps, render } from 'testUtils'
+import { displayedTextPhoneNumber } from 'utils/formattingUtils'
 
 import PaymentMissing from './PaymentMissingScreen'
 
@@ -14,24 +16,20 @@ context('PaymentMissing', () => {
   })
 
   it('initializes correctly', () => {
-    expect(screen.getByRole('header', { name: "What if I'm missing a payment?" })).toBeTruthy()
-    expect(
-      screen.getByText(
-        'VA pays benefits on the first day of the month for the previous month. Please wait at least 3 business days (Monday through Friday) before reporting non-receipt of a payment.',
-      ),
-    ).toBeTruthy()
+    expect(screen.getByRole('header', { name: t('payments.ifIAmMissingPayemt') })).toBeTruthy()
+    expect(screen.getByText(t('payments.missingOrNoPayments.body.1'))).toBeTruthy()
   })
 
   describe('when the help phone number link is clicked', () => {
     it('should call Linking open url with the parameter tel:8008271000', () => {
-      fireEvent.press(screen.getByRole('link', { name: '800-827-1000' }))
+      fireEvent.press(screen.getByRole('link', { name: displayedTextPhoneNumber(t('8008271000')) }))
       expect(Linking.openURL).toBeCalledWith('tel:8008271000')
     })
   })
 
   describe('when the call TTY phone link is clicked', () => {
     it('should call Linking open url with the parameter tel:711', () => {
-      fireEvent.press(screen.getByRole('link', { name: 'TTY: 711' }))
+      fireEvent.press(screen.getByRole('link', { name: t('contactVA.tty.displayText') }))
       expect(Linking.openURL).toBeCalledWith('tel:711')
     })
   })
