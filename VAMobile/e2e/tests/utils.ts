518b120579c6fa6507224e9dd7d2fbd3b0263651
Rachael Bontrager <rachael.bontrager@adhocteam.us>
2024-12-02 13:37:32 -0600

    bug/10222-rachael-detox-fix-nightly-errors (#10244)

--- VAMobile/e2e/tests/utils.ts
+++ VAMobile/e2e/tests/utils.ts
@@ -154,6 +154,10 @@ export async function loginToDemoMode(skipOnboarding = true, pushNotifications?:
       .toExist()
       .withTimeout(60000)
   }
+  await waitFor(element(by.id(CommonE2eIdConstants.VA_LOGO_ICON_ID)))
+    .toBeVisible()
+    .whileElement(by.id('Login-page'))
+    .scroll(100, 'down')
   await element(by.id(CommonE2eIdConstants.VA_LOGO_ICON_ID)).multiTap(7)
 
   if (DEMO_PASSWORD !== undefined) {
@@ -163,6 +167,10 @@ export async function loginToDemoMode(skipOnboarding = true, pushNotifications?:
   await element(by.id(CommonE2eIdConstants.DEMO_MODE_INPUT_ID)).tapReturnKey()
   await element(by.id(CommonE2eIdConstants.DEMO_BTN_ID)).multiTap(2)
 
+  await waitFor(element(by.id(CommonE2eIdConstants.SIGN_IN_BTN_ID)))
+    .toBeVisible()
+    .whileElement(by.id('Login-page'))
+    .scroll(100, 'down')
   await element(by.id(CommonE2eIdConstants.SIGN_IN_BTN_ID)).tap()
 
   if (skipOnboarding === true) {
