b4fecb4558532e9469c851eef78cd255691aa56c
Jon Bindbeutel <compjon@gmail.com>
2024-12-03 15:17:03 -0500

    last analytic edit

--- VAMobile/src/utils/inAppReviews.tsx
+++ VAMobile/src/utils/inAppReviews.tsx
@@ -43,7 +43,6 @@ export const useReviewEvent = (screenView?: boolean, feedbackScreen?: string): (
             DateTime.fromMillis(parseInt(lastFeedback, 10)).diffNow('days').days > IN_APP_FEEDBACK_INTERVAL_DAYS) ||
           !lastFeedback
         ) {
-          logAnalyticsEvent(Events.vama_feedback_page_ent(feedbackScreen))
           inAppFeedback(feedbackScreen)
           await AsyncStorage.setItem(feedbackKey, '0')
           await AsyncStorage.setItem(STORAGE_LAST_FEEDBACK_PROMPT_DATE_MILLIS, `${DateTime.now().millisecond}`)
