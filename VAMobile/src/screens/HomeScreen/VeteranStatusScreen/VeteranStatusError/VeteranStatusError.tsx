95458db655e05fe020a47d0951b277d56879c244
Kim McCaskill <187308860+kimmccaskill-oddball@users.noreply.github.com>
2025-03-11 12:10:51 -0600

    Shorten HeaderBanner
    Add catch-all for VeteranStatusCard errors
    
    Add other two errors to VeteranStatusCard

--- VAMobile/src/screens/HomeScreen/VeteranStatusScreen/VeteranStatusError/VeteranStatusError.tsx
+++ VAMobile/src/screens/HomeScreen/VeteranStatusScreen/VeteranStatusError/VeteranStatusError.tsx
@@ -1,21 +1,49 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 
-import { AlertWithHaptics, ClickToCallPhoneNumber, LargePanel } from 'components'
+import { useVeteranStatus } from 'api/veteranStatus'
+import { AlertWithHaptics, ClickToCallPhoneNumber, LargePanel, TextView } from 'components'
 import { NAMESPACE } from 'constants/namespaces'
 import { a11yLabelID, a11yLabelVA } from 'utils/a11yLabel'
 import { displayedTextPhoneNumber } from 'utils/formattingUtils'
 
 const VeternStatusError = () => {
+  const { data: veteranStatus } = useVeteranStatus()
   const { t } = useTranslation(NAMESPACE.COMMON)
+  const notConfirmedReason = veteranStatus?.data?.attributes.notConfirmedReason
 
-  return (
-    <LargePanel
-      title={t('veteranStatus.title')}
-      rightButtonText={t('close')}
-      dividerMarginBypass={true}
-      testID="veteranStatusTestID"
-      rightButtonTestID="veteranStatusCloseID">
+  const renderError = () => {
+    if (notConfirmedReason === 'NOT_TITLE_38') {
+      return (
+        <AlertWithHaptics
+          variant="warning"
+          header={t('veteranStatus.error.notTitle38.title')}
+          headerA11yLabel={a11yLabelVA(t('veteranStatus.error.notTitle38.title'))}
+          description={t('veteranStatus.error.notTitle38.body1')}
+          descriptionA11yLabel={a11yLabelVA(t('veteranStatus.error.notTitle38.body1'))}>
+          <TextView accessibilityLabel={t('veteranStatus.error.notTitle38.body2')}>
+            {t('veteranStatus.error.notTitle38.body2')}
+          </TextView>
+          <ClickToCallPhoneNumber
+            a11yLabel={a11yLabelID(t('8005389552'))}
+            displayedText={displayedTextPhoneNumber(t('8005389552'))}
+            phone={t('8005389552')}
+          />
+        </AlertWithHaptics>
+      )
+    }
+    if (notConfirmedReason === 'ERROR') {
+      return (
+        <AlertWithHaptics
+          variant="error"
+          header={t('error.somethingWentWrong')}
+          headerA11yLabel={a11yLabelVA(t('error.somethingWentWrong'))}
+          description={t('veteranStatus.error.generic')}
+          descriptionA11yLabel={a11yLabelVA(t('veteranStatus.error.generic'))}
+        />
+      )
+    }
+    return (
       <AlertWithHaptics
         variant="warning"
         header={t('veteranStatus.error.catchAll.title')}
@@ -28,6 +56,17 @@ const VeternStatusError = () => {
           phone={t('8005389552')}
         />
       </AlertWithHaptics>
+    )
+  }
+
+  return (
+    <LargePanel
+      title={t('veteranStatus.title')}
+      rightButtonText={t('close')}
+      dividerMarginBypass={true}
+      testID="veteranStatusTestID"
+      rightButtonTestID="veteranStatusCloseID">
+      {renderError()}
     </LargePanel>
   )
 }
