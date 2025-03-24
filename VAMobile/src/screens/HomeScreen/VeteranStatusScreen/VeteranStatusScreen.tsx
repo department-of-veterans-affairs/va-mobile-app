99622e64b72c735eaacda56c8f6b45095dd095b9
Matthew Guest - Work <matthewguest-wilcore@Matthews-MacBook-Pro.local>
2025-03-24 16:45:19 -0700

    Updated VeteranStatusScreen to use the new card design. Still WIP.

--- VAMobile/src/screens/HomeScreen/VeteranStatusScreen/VeteranStatusScreen.tsx
+++ VAMobile/src/screens/HomeScreen/VeteranStatusScreen/VeteranStatusScreen.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
+import { Platform } from 'react-native'
 
 import { StackScreenProps } from '@react-navigation/stack'
-
-import { colors } from '@department-of-veterans-affairs/mobile-tokens'
-import { map } from 'underscore'
 
 import { useAuthorizedServices } from 'api/authorizedServices/getAuthorizedServices'
 import { useDisabilityRating } from 'api/disabilityRating'
@@ -15,15 +13,12 @@ import { useVeteranStatus } from 'api/veteranStatus'
 import {
   AlertWithHaptics,
   BackgroundVariant,
-  BorderColorVariant,
   Box,
-  BoxProps,
   ClickToCallPhoneNumber,
   ClickToCallPhoneNumberDeprecated,
   LargePanel,
   MilitaryBranchEmblem,
   TextView,
-  VALogo,
 } from 'components'
 import { NAMESPACE } from 'constants/namespaces'
 import { HomeStackParamList } from 'screens/HomeScreen/HomeStackScreens'
@@ -34,6 +29,20 @@ import { useReviewEvent } from 'utils/inAppReviews'
 import { displayedTextPhoneNumber } from '../../../utils/formattingUtils'
 
 // import PhotoUpload from 'components/PhotoUpload'
+
+const LANDSCAPE_PADDING = 144
+const PORTRAIT_PADDING = 18
+
+const EMBLEM_SIZE_LANDSCAPE = 82
+const EMBLEM_SIZE_PORTRAIT = 50
+
+const TOP_STRIP_HEIGHT_LANDSCAPE = 70
+const TOP_STRIP_HEIGHT_PORTRAIT = 50
+
+const EMBLEM_TOP_LANDSCAPE = 40
+const EMBLEM_TOP_PORTRAIT = 42
+
+const EMBLEM_OFFSET_FROM_CARD_RIGHT = 26
 
 type VeteranStatusScreenProps = StackScreenProps<HomeStackParamList, 'VeteranStatus'>
 
@@ -52,6 +61,7 @@ function VeteranStatusScreen({ navigation }: VeteranStatusScreenProps) {
   const theme = useTheme()
   const { t } = useTranslation(NAMESPACE.COMMON)
   const isPortrait = useOrientation()
+  const isLandscape = !isPortrait
   const ratingPercent = ratingData?.combinedDisabilityRating
   const ratingIsDefined = ratingPercent !== undefined && ratingPercent !== null
   const combinedPercentText = ratingIsDefined
@@ -63,8 +73,15 @@ function VeteranStatusScreen({ navigation }: VeteranStatusScreenProps) {
     registerReviewEvent()
   })
 
-  const getPeriodOfService: React.ReactNode = map(serviceHistory, (service: ServiceData) => {
-    const branchOfService = t('militaryInformation.branch', { branch: service.branchOfService })
+  const getLatestPeriodOfService = (): React.ReactNode => {
+    if (!serviceHistory || serviceHistory.length === 0) {
+      return null
+    }
+    const service = serviceHistory[0]
+    const branchOfService = t('militaryInformation.branch', {
+      branch: service.branchOfService,
+    })
+
     return (
       <Box>
         <Box display="flex" flexDirection="row" alignItems="center" mt={theme.dimensions.condensedMarginBetween}>
@@ -72,18 +89,22 @@ function VeteranStatusScreen({ navigation }: VeteranStatusScreenProps) {
             {branchOfService}
           </TextView>
         </Box>
+
         <Box>
           <TextView
             variant="HelperText"
             color="primaryContrast"
             mb={theme.dimensions.condensedMarginBetween}
             testID="veteranStatusMilitaryServiceTestID">
-            {t('militaryInformation.history', { begin: service.formattedBeginDate, end: service.formattedEndDate })}
+            {t('militaryInformation.history', {
+              begin: service.formattedBeginDate,
+              end: service.formattedEndDate,
+            })}
           </TextView>
         </Box>
       </Box>
     )
-  })
+  }
 
   const getError = () => {
     const notConfirmedReason = veteranStatus!.data.attributes.notConfirmedReason
@@ -134,23 +155,20 @@ function VeteranStatusScreen({ navigation }: VeteranStatusScreenProps) {
     )
   }
 
-  const boxProps: BoxProps = {
-    minHeight: 81,
-    borderRadius: 6,
-    pt: theme.dimensions.cardPadding,
-    pb: theme.dimensions.cardPadding,
-    backgroundColor: theme.colors.background.veteranStatus as BackgroundVariant,
-    borderTopWidth: theme.dimensions.borderWidth,
-    borderColor: theme.colors.border.veteranStatus as BorderColorVariant,
-    borderStyle: 'solid',
-  }
+  const horizontalPadding = isPortrait ? PORTRAIT_PADDING : LANDSCAPE_PADDING
+  const emblemSize = isPortrait ? EMBLEM_SIZE_PORTRAIT : EMBLEM_SIZE_LANDSCAPE
+  const topStripHeight = isPortrait ? TOP_STRIP_HEIGHT_PORTRAIT : TOP_STRIP_HEIGHT_LANDSCAPE
+  const emblemTopOffset = isPortrait ? EMBLEM_TOP_PORTRAIT : EMBLEM_TOP_LANDSCAPE
+
+  const emblemRightOffset = horizontalPadding + EMBLEM_OFFSET_FROM_CARD_RIGHT
+  const clampedEmblemRight = emblemRightOffset < 0 ? 16 : emblemRightOffset
 
   return (
     <LargePanel
       title={t('veteranStatus.title')}
       rightButtonText={t('close')}
       dividerMarginBypass={true}
-      removeInsets={!showError}
+      removeInsets={false}
       testID="veteranStatusTestID"
       rightButtonTestID="veteranStatusCloseID">
       {showError ? (
@@ -158,94 +176,137 @@ function VeteranStatusScreen({ navigation }: VeteranStatusScreenProps) {
       ) : (
         <>
           <Box
-            mx={isPortrait ? theme.dimensions.gutter : theme.dimensions.headerHeight}
-            alignItems="center"
-            mt={theme.dimensions.standardMarginBetween}>
-            <VALogo variant="dark" testID="VeteranStatusCardVAIcon" />
-            {/* <Box my={theme.dimensions.standardMarginBetween}>
-        //TODO: Put back PhotoUpload later after concerns have been met
-          <PhotoUpload width={100} height={100} />
-        </Box> */}
-            <Box my={theme.dimensions.formMarginBetween}>
-              <TextView
-                textTransform="capitalize"
-                mb={theme.dimensions.textIconMargin}
-                variant="BitterBoldHeading"
-                color="primaryContrast"
-                testID="veteranStatusFullNameTestID"
-                accessibilityRole="header">
-                {personalInfo?.fullName}
+            position="relative"
+            width="100%"
+            px={horizontalPadding}
+            pt={20}
+            borderRadius={15}
+            style={{
+              shadowColor: '#000000',
+              shadowOffset: { width: 0, height: 3.75 },
+              shadowOpacity: 0.25,
+              shadowRadius: 3.75,
+              ...Platform.select({
+                android: {
+                  elevation: 4,
+                },
+              }),
+            }}>
+            <Box
+              accessibilityRole="header"
+              alignItems="flex-start"
+              justifyContent="center"
+              backgroundColor={theme.colors.background.carousel as BackgroundVariant}
+              height={topStripHeight}
+              borderRadiusTop={15}
+              style={{
+                paddingTop: 16,
+                paddingLeft: 16,
+                paddingBottom: 8,
+                paddingRight: 64,
+              }}>
+              {/* VeteranStatusCardHeaderBold */}
+              <TextView color="primaryContrast" variant="MobileBodyBold">
+                Veteran Status Card
               </TextView>
-              {accessToMilitaryInfo && (
-                <Box display="flex" flexDirection="row" flexWrap="wrap">
-                  <MilitaryBranchEmblem
-                    testID="veteranStatusCardBranchEmblem"
-                    branch={branch}
-                    width={34}
-                    height={34}
-                    variant="dark"
-                  />
-                  <TextView ml={10} variant="MobileBody" color="primaryContrast" testID="veteranStatusBranchTestID">
-                    {branch}
+            </Box>
+
+            <Box
+              backgroundColor={theme.colors.background.veteranStatus as BackgroundVariant}
+              borderRadiusBottom={15}
+              px={isPortrait ? 18 : 0}
+              pl={isPortrait ? 0 : 18}>
+              <Box pt={12}>
+                <TextView color="primaryContrast" variant="MobileBodyBold">
+                  Name
+                </TextView>
+                <TextView color="primaryContrast" variant="MobileBody">
+                  {personalInfo?.fullName}
+                </TextView>
+              </Box>
+
+              <Box mt={8}>
+                <TextView color="primaryContrast" variant="MobileBodyBold">
+                  {'Latest period of service'}
+                </TextView>
+                <TextView color="primaryContrast" variant="MobileBody">
+                  {getLatestPeriodOfService()}
+                </TextView>
+              </Box>
+
+              <Box flexDirection={isLandscape ? 'row' : 'column'}>
+                <Box flex={isLandscape ? 1 : undefined} mr={isLandscape ? 8 : 0}>
+                  <TextView color="primaryContrast" variant="MobileBodyBold">
+                    {t('veteranStatus.dodIdNumber')}
+                  </TextView>
+                  <TextView color="primaryContrast" variant="MobileBody">
+                    {personalInfo?.edipi}
                   </TextView>
                 </Box>
-              )}
+
+                <Box flex={isLandscape ? 1 : undefined} mt={isLandscape ? 0 : 8}>
+                  <TextView color="primaryContrast" variant="MobileBodyBold">
+                    {t('disabilityRating.title')}
+                  </TextView>
+                  <TextView color="primaryContrast" variant="MobileBody">
+                    {combinedPercentText}
+                  </TextView>
+                </Box>
+              </Box>
+
+              <Box mt={16} pb={16}>
+                <TextView color="primaryContrast" variant="VeteranStatusProof">
+                  {t('veteranStatus.noBenefitsEntitled')}
+                </TextView>
+              </Box>
+            </Box>
+
+            <Box
+              position="absolute"
+              top={emblemTopOffset}
+              right={clampedEmblemRight}
+              width={emblemSize}
+              height={emblemSize}
+              borderRadius={emblemSize / 2}
+              overflow="hidden">
+              <MilitaryBranchEmblem
+                testID="veteranStatusCardBranchEmblem"
+                branch={branch}
+                width={emblemSize}
+                height={emblemSize}
+                variant="dark"
+              />
             </Box>
           </Box>
-          <Box mx={isPortrait ? theme.dimensions.gutter : theme.dimensions.headerHeight}>
-            {ratingIsDefined && (
-              <Box {...boxProps}>
-                <TextView variant="MobileBodyBold" color="primaryContrast" accessibilityRole="header">
-                  {t('disabilityRating.title')}
-                </TextView>
-                <TextView variant="MobileBody" color="primaryContrast" testID="veteranStatusDisabilityRatingTestID">
-                  {combinedPercentText}
-                </TextView>
-              </Box>
-            )}
-            <Box {...boxProps} borderBottomWidth={personalInfo?.edipi ? 0 : theme.dimensions.borderWidth}>
-              <TextView variant="MobileBodyBold" color="primaryContrast" accessibilityRole="header">
-                {t('veteranStatus.periodOfService')}
-              </TextView>
-              {getPeriodOfService}
-            </Box>
-            {personalInfo?.edipi && (
-              <Box {...boxProps} borderBottomWidth={theme.dimensions.borderWidth}>
-                <TextView variant="MobileBodyBold" color="primaryContrast" accessibilityRole="header">
-                  {t('veteranStatus.dodIdNumber')}
-                </TextView>
-                <TextView variant="MobileBody" color="primaryContrast" testID="veteranStatusDODTestID">
-                  {personalInfo?.edipi}
-                </TextView>
-              </Box>
-            )}
-            <Box my={theme.dimensions.formMarginBetween}>
-              <TextView variant="MobileBody" color="primaryContrast" mb={theme.dimensions.formMarginBetween}>
-                {t('veteranStatus.uniformedServices')}
-              </TextView>
-              <TextView variant="MobileBodyBold" color="primaryContrast" accessibilityRole="header">
-                {t('veteranStatus.fixAnError')}
-              </TextView>
-              <TextView variant="MobileBody" color="primaryContrast" mb={theme.dimensions.condensedMarginBetween}>
-                {t('veteranStatus.fixAnError.2')}
-              </TextView>
-              <ClickToCallPhoneNumberDeprecated
-                phone={t('8008271000')}
-                displayedText={displayedTextPhoneNumber(t('8008271000'))}
-                colorOverride={'veteranStatus'}
-                iconColorOverride={colors.vadsColorWhite}
-              />
-              <TextView variant="MobileBody" color="primaryContrast" my={theme.dimensions.condensedMarginBetween}>
-                {t('veteranStatus.fixAnError.3')}
-              </TextView>
-              <ClickToCallPhoneNumberDeprecated
-                phone={t('8005389552')}
-                displayedText={displayedTextPhoneNumber(t('8005389552'))}
-                colorOverride={'veteranStatus'}
-                iconColorOverride={colors.vadsColorWhite}
-                ttyBypass={true}
-              />
-            </Box>
+          <Box my={theme.dimensions.formMarginBetween} px={horizontalPadding}>
+            <TextView variant="MobileBodyBold" color="primary" accessibilityRole="header" mb={12}>
+              {t('veteranStatus.about')}
+            </TextView>
+            <TextView variant="MobileBody" color="bodyText" mb={theme.dimensions.formMarginBetween}>
+              {t('veteranStatus.uniformedServices')}
+            </TextView>
+            <TextView variant="MobileBodyBold" color="primary" accessibilityRole="header" mb={12}>
+              {t('veteranStatus.fixAnError')}
+            </TextView>
+            <TextView variant="MobileBody" color="bodyText" mb={theme.dimensions.condensedMarginBetween}>
+              {t('veteranStatus.fixAnError.2')}
+            </TextView>
+            <ClickToCallPhoneNumberDeprecated
+              phone={t('8008271000')}
+              displayedText={displayedTextPhoneNumber(t('8008271000'))}
+              colorOverride={'link'}
+              iconColorOverride={theme.colors.icon.link}
+            />
+            <TextView variant="MobileBody" color="bodyText" my={theme.dimensions.condensedMarginBetween}>
+              {t('veteranStatus.fixAnError.3')}
+            </TextView>
+            <ClickToCallPhoneNumberDeprecated
+              phone={t('8005389552')}
+              displayedText={displayedTextPhoneNumber(t('8005389552'))}
+              colorOverride={'link'}
+              iconColorOverride={theme.colors.icon.link}
+              ttyBypass={true}
+            />
           </Box>
         </>
       )}
