fcf0c5ecd3743f28e9ed9ab0ce189fdce12d1500
Jon Bindbeutel <compjon@gmail.com>
2025-04-08 16:03:00 -0400

    feature/10678-Binny-VSC-Error-Analytics (#10679)

--- VAMobile/src/constants/analytics.ts
+++ VAMobile/src/constants/analytics.ts
@@ -1217,6 +1217,14 @@ export const Events = {
       },
     }
   },
+  vama_vsc_error_shown: (errorType?: string): Event => {
+    return {
+      name: 'vama_vsc_error_shown',
+      params: {
+        errorType,
+      },
+    }
+  },
   vama_webview_fail: (error: string): Event => {
     return {
       name: 'vama_webview_fail',
