f257359834fe9116049a3a008c479e88b5f852ea
AdryienH <104521177+AdryienH@users.noreply.github.com>
2025-04-10 12:18:30 -0700

    chore: Update Benefits Disability Rating screen unit tests to use i18n translations

--- VAMobile/src/screens/BenefitsScreen/DisabilityRatingsScreen/NoDisabilityRatings/NoDisabilityRatings.test.tsx
+++ VAMobile/src/screens/BenefitsScreen/DisabilityRatingsScreen/NoDisabilityRatings/NoDisabilityRatings.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { screen } from '@testing-library/react-native'
+import { t } from 'i18next'
 
 import { context, render } from 'testUtils'
 
@@ -9,13 +10,7 @@ import NoDisabilityRatings from './NoDisabilityRatings'
 context('NoDisabilityRatings', () => {
   it('should render text fields correctly', () => {
     render(<NoDisabilityRatings />)
-    expect(
-      screen.getByRole('header', { name: 'You do not have a VA combined disability rating on record.' }),
-    ).toBeTruthy()
-    expect(
-      screen.getByText(
-        "This tool doesn't include ratings for any disability claims that are still in process. You can check the status of pending claims with the claims status tool.",
-      ),
-    ).toBeTruthy()
+    expect(screen.getByRole('header', { name: t('disabilityRating.noDisabilityRatings.title') })).toBeTruthy()
+    expect(screen.getByText(t('disabilityRating.noDisabilityRatings.body'))).toBeTruthy()
   })
 })
