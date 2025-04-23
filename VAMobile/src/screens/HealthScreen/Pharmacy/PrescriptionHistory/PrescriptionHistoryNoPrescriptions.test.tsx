339955f25c7e99b8e33470faeadc5d1923fdfa78
AdryienH <adryien@themostudio.com>
2025-04-23 12:22:36 -0700

    Update Pharmacy unit tests to use i18n translations

--- VAMobile/src/screens/HealthScreen/Pharmacy/PrescriptionHistory/PrescriptionHistoryNoPrescriptions.test.tsx
+++ VAMobile/src/screens/HealthScreen/Pharmacy/PrescriptionHistory/PrescriptionHistoryNoPrescriptions.test.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
 import { screen } from '@testing-library/react-native'
+import { t } from 'i18next'
 
 import { context, render } from 'testUtils'
+import { displayedTextPhoneNumber } from 'utils/formattingUtils'
 
 import PrescriptionHistoryNoPrescriptions from './PrescriptionHistoryNoPrescriptions'
 
@@ -19,19 +21,15 @@ jest.mock('utils/hooks', () => {
 context('PrescriptionHistoryNoPrescriptions', () => {
   it('initializes correctly', () => {
     render(<PrescriptionHistoryNoPrescriptions />)
-    expect(screen.getByText("We can't find any VA prescriptions for you")).toBeTruthy()
-    expect(screen.getByText('Your VA prescription list in the app won’t include:')).toBeTruthy()
-    expect(screen.getByText('New prescriptions not yet processed by a VA pharmacy')).toBeTruthy()
-    expect(screen.getByText('Prescriptions filled at non-VA pharmacies')).toBeTruthy()
-    expect(screen.getByText('Prescriptions that are inactive for more than 180 days')).toBeTruthy()
-    expect(screen.getByText('Medications administered at a clinic or ER')).toBeTruthy()
-    expect(screen.getByText('Self-entered medications')).toBeTruthy()
-    expect(
-      screen.getByText(
-        'If you think this is an error, or if you have questions about your VA prescriptions, please call the My HealtheVet help desk. We’re here Monday through Friday, 8:00 AM to 8:00 PM ET.',
-      ),
-    ).toBeTruthy()
-    expect(screen.getByRole('link', { name: '877-327-0022' })).toBeTruthy()
-    expect(screen.getByRole('link', { name: 'TTY: 711' })).toBeTruthy()
+    expect(screen.getByText(t('prescriptions.notFound.title'))).toBeTruthy()
+    expect(screen.getByText(t('prescriptions.notFound.yourVA'))).toBeTruthy()
+    expect(screen.getByText(t('prescription.help.item1'))).toBeTruthy()
+    expect(screen.getByText(t('prescription.help.item2'))).toBeTruthy()
+    expect(screen.getByText(t('prescription.help.item3'))).toBeTruthy()
+    expect(screen.getByText(t('prescription.help.item4'))).toBeTruthy()
+    expect(screen.getByText(t('prescription.help.item5'))).toBeTruthy()
+    expect(screen.getByText(t('prescriptions.notFound.bullets.ifYouThink'))).toBeTruthy()
+    expect(screen.getByRole('link', { name: displayedTextPhoneNumber(t('8773270022')) })).toBeTruthy()
+    expect(screen.getByRole('link', { name: t('contactVA.tty.displayText') })).toBeTruthy()
   })
 })
