311c36b7d3422f656cdd0ddb05d12b5d2e165bf9
Tony Doan <tonydoan@Tony-MiniM4Pro-2025.local>
2025-05-06 15:03:27 -0700

    fix scroll on android

--- VAMobile/src/components/RadioGroupModal.test.tsx
+++ VAMobile/src/components/RadioGroupModal.test.tsx
@@ -34,6 +34,7 @@ context('RadioGroupModal', () => {
   })
 
   it('renders modal header and group title', () => {
+    jest.advanceTimersByTime(50)
     expect(screen.getByRole('header', { name: 'Modal header' })).toBeTruthy()
     expect(screen.getAllByRole('header', { name: 'Group 1 title' })[0]).toBeTruthy()
   })
@@ -51,6 +52,7 @@ context('RadioGroupModal', () => {
   })
 
   it('renders options with roles and a11yLabels', () => {
+    jest.advanceTimersByTime(50)
     expect(screen.getByRole('radio', { name: 'Option One' })).toBeTruthy()
     expect(screen.getByLabelText('Option One option 1 of 2')).toBeTruthy()
     expect(screen.getByRole('radio', { name: 'Option Two' })).toBeTruthy()
@@ -58,6 +60,7 @@ context('RadioGroupModal', () => {
   })
 
   it('calls onSetOption when option is pressed', () => {
+    jest.advanceTimersByTime(50)
     fireEvent.press(screen.getByRole('radio', { name: 'Option One' }))
     expect(onSetSpy).toHaveBeenCalledWith('option-one')
     fireEvent.press(screen.getByRole('radio', { name: 'Option Two' }))
