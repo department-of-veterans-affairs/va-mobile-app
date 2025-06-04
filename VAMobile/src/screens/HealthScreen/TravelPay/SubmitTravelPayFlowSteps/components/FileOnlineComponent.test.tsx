e34f05cdb4dab1ad51c24ecc14ed14d07969f368
Juan Castillo <juan.cast@agile6.com>
2025-06-04 12:05:14 -0700

    Update tests

--- VAMobile/src/screens/HealthScreen/TravelPay/SubmitTravelPayFlowSteps/components/FileOnlineComponent.test.tsx
+++ VAMobile/src/screens/HealthScreen/TravelPay/SubmitTravelPayFlowSteps/components/FileOnlineComponent.test.tsx
@@ -1,16 +1,36 @@
 import React from 'react'
 
-import { screen } from '@testing-library/react-native'
+import { fireEvent, screen } from '@testing-library/react-native'
 import { t } from 'i18next'
 
-import { context, mockNavProps, render } from 'testUtils'
+import { context, render } from 'testUtils'
+import getEnv from 'utils/env'
 
 import FileOnlineComponent from './FileOnlineComponent'
 
+const { LINK_URL_TRAVEL_PAY_FILE_CLAIM_BTSSS } = getEnv()
+
+const mockNavigationSpy = jest.fn()
+jest.mock('utils/hooks', () => {
+  const original = jest.requireActual('utils/hooks')
+  return {
+    ...original,
+    useRouteNavigation: () => {
+      return mockNavigationSpy
+    },
+  }
+})
+
 context('FileOnlineComponent', () => {
+  const btsssAnalyticsOnPress = jest.fn()
+  const vaFormAnalyticsOnPress = jest.fn()
   const initializeTestInstance = () => {
-    const props = mockNavProps()
-    render(<FileOnlineComponent {...props} />)
+    render(
+      <FileOnlineComponent
+        btsssAnalyticsOnPress={btsssAnalyticsOnPress}
+        vaFormAnalyticsOnPress={vaFormAnalyticsOnPress}
+      />,
+    )
   }
   it('should initialize correctly', () => {
     initializeTestInstance()
@@ -21,5 +41,24 @@ context('FileOnlineComponent', () => {
     expect(screen.getByText(t('travelPay.otherWaysToFile.method2'))).toBeTruthy()
     expect(screen.getByTestId('fileOnlineBTSSSLink')).toBeTruthy()
     expect(screen.getByTestId('fileOnlineVAFormLink')).toBeTruthy()
+  })
+
+  it('should call analytics on press', () => {
+    initializeTestInstance()
+    fireEvent.press(screen.getByTestId('fileOnlineBTSSSLink'))
+    expect(btsssAnalyticsOnPress).toHaveBeenCalled()
+    fireEvent.press(screen.getByTestId('fileOnlineVAFormLink'))
+    expect(vaFormAnalyticsOnPress).toHaveBeenCalled()
+  })
+
+  it('should navigate to webview for btsss', () => {
+    initializeTestInstance()
+    fireEvent.press(screen.getByTestId('fileOnlineBTSSSLink'))
+    expect(mockNavigationSpy).toHaveBeenCalledWith('Webview', {
+      url: LINK_URL_TRAVEL_PAY_FILE_CLAIM_BTSSS,
+      displayTitle: t('travelPay.webview.fileForTravelPay.title'),
+      loadingMessage: t('loading.vaWebsite'),
+      useSSO: true,
+    })
   })
 })
