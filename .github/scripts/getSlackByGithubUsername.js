a42f053a58b5be063ef00fbf1c83a6d102dc9783
Tim Wright <timwright12@gmail.com>
2025-06-16 10:10:24 -0400

    user lookip

---
+++ .github/scripts/getSlackByGithubUsername.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+const githubUsername = process.env.GITHUB_USERNAME;
+
+if (!githubUsername) {
+  console.error('Missing GITHUB_USERNAME env variable.');
+  process.exit(1);
+}
+
+const users = JSON.parse(fs.readFileSync('./github-users.json', 'utf8'));
+const user = users[githubUsername];
+
+if (!user) {
+  console.error(`No Slack user found for GitHub username: ${githubUsername}`);
+  process.exit(1);
+}
+
+console.log(`::set-output name=slack_username::${user.slack}`);
