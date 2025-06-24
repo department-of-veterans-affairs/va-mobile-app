ce55bb390c4f9617b6404ff796eae8710fee4923
Juan Castillo <juan.cast@agile6.com>
2025-06-24 07:24:32 -0700

    Revert changes

--- VAMobile/e2e/tests/AppointmentsExpanded.e2e.ts
+++ VAMobile/e2e/tests/AppointmentsExpanded.e2e.ts
@@ -1177,6 +1177,6 @@ beforeAll(async () => {
 })
 
 describe(':ios: Appointments Screen Expansion', () => {
-  // apppointmentVerification()
+  apppointmentVerification()
   apppointmentVerification(true)
 })
