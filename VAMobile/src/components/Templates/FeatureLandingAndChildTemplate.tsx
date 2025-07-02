b3a2eb17627aa805df5347b2e555518eb4d1975e
Matthew Sweeney <matthew@digitalfoundry.com>
2025-07-02 13:45:04 -0700

    lint fixes

--- VAMobile/src/components/Templates/FeatureLandingAndChildTemplate.tsx
+++ VAMobile/src/components/Templates/FeatureLandingAndChildTemplate.tsx
@@ -5,12 +5,10 @@ import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 import { useIsScreenReaderEnabled } from '@department-of-veterans-affairs/mobile-component-library'
 
-import { HeaderButton, TextView, TextViewProps, WaygateWrapper } from 'components'
+import { HeaderBanner, HeaderBannerProps, HeaderButton, TextView, TextViewProps, WaygateWrapper } from 'components'
 import VAScrollView, { VAScrollViewProps } from 'components/VAScrollView'
 import { NAMESPACE } from 'constants/namespaces'
 import { useTheme } from 'utils/hooks'
-
-import HeaderBanner, { HeaderBannerProps } from './HeaderBanner'
 
 /* To use these templates:
 1. Wrap the screen content you want in <FeatureLandingTemplate> </FeatureLandingTemplate> or <ChildTemplate> </ChildTemplate> and
