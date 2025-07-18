74aec99e7c3d9b1399d96f68a15998d0a783431b
Josh Bergman <199831110+jbergman-oddball@users.noreply.github.com>
2025-07-18 11:19:47 -0400

    some debugging updates

--- .github/scripts/app-store-images/generate_images.sh
+++ .github/scripts/app-store-images/generate_images.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
-# set -x
+set -x
 set -e
 # This Script will generate the app store images so we are able to do them automatically instead of manually creating.
 
 # This is needed for local runs
 # cd ../../../VAMobile/
 
-echo "Installing Packages.."
-yarn install
-
-echo "Bundling  and building.."
-yarn e2e:ios-build
-yarn e2e:android-build
+# echo "Bundling  and building.."
+# yarn e2e:ios-build
+# yarn e2e:android-build
 
 echo "Gahtering Android screens.."
 yarn jest:clear && detox test -c android --cleanup ./screenshot.e2e.ts
@@ -22,10 +19,8 @@ yarn jest:clear && detox test -c ios --cleanup ./screenshot.e2e.ts
 echo "Gathering iPad screens.."
 yarn jest:clear && detox test -c ipad --cleanup ./screenshot.e2e.ts
 
-# echo "Current dir is"
-# pwd
 echo "navigating to script directory.."
-cd ../.github/scripts/app-store-images
+cd ./.github/scripts/app-store-images
 
 
 echo "Moving Images.."
