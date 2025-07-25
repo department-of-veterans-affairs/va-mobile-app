233b01f89c77fd2717a03e96201fda3ec282adfd
asiisii <ashish.malla@agile6.com>
2025-07-25 11:42:00 -0600

    fix e2e test failing for android

--- VAMobile/e2e/tests/HomeScreen.e2e.ts
+++ VAMobile/e2e/tests/HomeScreen.e2e.ts
@@ -52,6 +52,7 @@ describe('Home Screen', () => {
     await device.uninstallApp()
     await device.installApp()
     await device.launchApp({ newInstance: true, permissions: { notifications: 'YES' } })
+    await toggleRemoteConfigFlag(CommonE2eIdConstants.TRAVEL_PAY_STATUS_LIST_FLAG_TEXT)
     await toggleRemoteConfigFlag(CommonE2eIdConstants.TRAVEL_PAY_CONFIG_FLAG_TEXT)
     await loginToDemoMode()
   })
@@ -96,7 +97,6 @@ describe('Home Screen', () => {
     await expect(element(by.id(CommonE2eIdConstants.CLAIMS_LANDING_BUTTON_ID))).toExist()
     await expect(element(by.id(CommonE2eIdConstants.LETTERS_LANDING_BUTTON_ID))).toExist()
   })
-
   it('health tab tap: verify the health screen tab items', async () => {
     await element(by.text(CommonE2eIdConstants.HEALTH_TAB_BUTTON_TEXT)).tap()
     await expect(element(by.id(CommonE2eIdConstants.APPOINTMENTS_BUTTON_ID))).toExist()
