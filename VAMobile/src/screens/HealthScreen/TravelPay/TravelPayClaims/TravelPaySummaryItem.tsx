d121dbdcfad330b3b1357a3eeb6534f53cf656ae
Samuel Cohen <s.cohen@theta-llc.com>
2025-08-05 23:30:14 -0400

    placeholder travel pay summary files

---
+++ VAMobile/src/screens/HealthScreen/TravelPay/TravelPayClaims/TravelPaySummaryItem.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default function TravelPaySummaryItem() {
+  return (
+    <View>
+      <Text>TravelPaySummaryItem</Text>
+    </View>
+  )
+}
