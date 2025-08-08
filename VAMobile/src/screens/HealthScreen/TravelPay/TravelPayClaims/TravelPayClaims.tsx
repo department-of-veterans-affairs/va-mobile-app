16294978e30a0fcc2d626dd01c6fa1faecbb86a3
Samuel Cohen <s.cohen@theta-llc.com>
2025-08-07 21:11:45 -0400

    starting travel pay claim summary list view

--- VAMobile/src/screens/HealthScreen/TravelPay/TravelPayClaims/TravelPayClaims.tsx
+++ VAMobile/src/screens/HealthScreen/TravelPay/TravelPayClaims/TravelPayClaims.tsx
@@ -1,25 +1,38 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { useTranslation } from 'react-i18next'
+import { ScrollView } from 'react-native'
 
 import { StackScreenProps } from '@react-navigation/stack'
 
-import { FeatureLandingTemplate } from 'components'
+import { Box, FeatureLandingTemplate } from 'components'
+import { VAScrollViewProps } from 'components/VAScrollView'
 import { NAMESPACE } from 'constants/namespaces'
 import { HealthStackParamList } from 'screens/HealthScreen/HealthStackScreens'
 import { TravelPayClaimsFilters } from 'screens/HealthScreen/TravelPay/TravelPayClaims/TravelPayClaimsFilters'
+import TravelPaySummaryList from 'screens/HealthScreen/TravelPay/TravelPayClaims/TravelPaySummaryList'
 
 type TravelPayClaimsProps = StackScreenProps<HealthStackParamList, 'TravelPayClaims'>
 
 export function TravelPayClaims({ navigation }: TravelPayClaimsProps) {
   const { t } = useTranslation(NAMESPACE.COMMON)
 
+  const scrollViewRef = useRef<ScrollView | null>(null)
+  const scrollViewProps: VAScrollViewProps = {
+    scrollViewRef: scrollViewRef,
+  }
+
   return (
     <FeatureLandingTemplate
       backLabel={t('health.title')}
       backLabelOnPress={navigation.goBack}
       title={t('travelPay.statusList.title')}
-      testID="travelPayClaimsTestID">
+      testID="travelPayClaimsTestID"
+      scrollViewProps={scrollViewProps}>
       <TravelPayClaimsFilters />
+
+      <Box flex={1}>
+        <TravelPaySummaryList scrollViewRef={scrollViewRef} />
+      </Box>
     </FeatureLandingTemplate>
   )
 }
--- VAMobile/src/screens/HealthScreen/TravelPay/TravelPayClaims/TravelPaySummaryList.tsx
+++ VAMobile/src/screens/HealthScreen/TravelPay/TravelPayClaims/TravelPaySummaryList.tsx
@@ -1,10 +1,112 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { RefObject, useEffect, useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import { ScrollView } from 'react-native'
 
-export default function TravelPaySummaryList() {
+import { DateTime } from 'luxon'
+
+import { useTravelPayClaims } from 'api/travelPay'
+import { TravelPayClaimData } from 'api/types'
+import {
+  Box,
+  DefaultList,
+  DefaultListItemObj,
+  LoadingComponent,
+  Pagination,
+  PaginationProps,
+  TextLine,
+} from 'components'
+import { NAMESPACE } from 'constants/namespaces'
+import {
+  capitalizeFirstLetter,
+  getFormattedDateOrTimeWithFormatOption,
+  getFormattedTimeNoTimeZone,
+} from 'utils/formattingUtils'
+import { useTheme } from 'utils/hooks'
+
+type TravelPaySummaryListProps = {
+  scrollViewRef: RefObject<ScrollView>
+}
+
+export default function TravelPaySummaryList({ scrollViewRef }: TravelPaySummaryListProps) {
+  const { t } = useTranslation(NAMESPACE.COMMON)
+  const theme = useTheme()
+
+  const [page, setPage] = useState(1)
+  const { data: summariesPayload, isLoading: isLoadingSummaries } = useTravelPayClaims({ startDate: '', endDate: '' })
+  const [summariesToShow, setSummariesToShow] = useState<Array<TravelPayClaimData>>([])
+
+  const summaries = summariesPayload?.data
+
+  const metadata = summariesPayload?.meta
+  const { perPage, totalEntries } = {
+    perPage: 10,
+    totalEntries: metadata?.totalRecordCount || 0,
+  }
+
+  useEffect(() => {
+    const summaryList = summaries?.slice((page - 1) * perPage, page * perPage)
+    setSummariesToShow(summaryList || [])
+  }, [summaries, page, perPage])
+
+  const getListItemVals = (): Array<DefaultListItemObj> => {
+    const listItems: Array<DefaultListItemObj> = []
+    summariesToShow?.forEach((summary) => {
+      const { attributes } = summary
+      const { appointmentDateTime, claimStatus } = attributes
+
+      const textLines: Array<TextLine> = []
+
+      const dateString = getFormattedDateOrTimeWithFormatOption(appointmentDateTime, DateTime.DATE_FULL, undefined, {
+        weekday: 'long',
+      }) // TODO: SC - timezone?
+      const timeString = getFormattedTimeNoTimeZone(appointmentDateTime) // TODO: SC - formatting
+      textLines.push({
+        text: t('travelPay.statusList.appointmentDateLine1', { date: dateString }),
+        variant: 'MobileBodyBold',
+      })
+      textLines.push({
+        text: t('travelPay.statusList.appointmentDateLine2', { time: timeString }),
+        variant: 'MobileBodyBold',
+      })
+      textLines.push({ text: t('travelPay.statusList.claimStatus', { status: capitalizeFirstLetter(claimStatus) }) })
+
+      const a11yValue = `test-a11y`
+
+      listItems.push({
+        textLines,
+        a11yValue,
+        onPress: () => {},
+        testId: 'test',
+      })
+    })
+
+    return listItems
+  }
+
+  if (isLoadingSummaries) {
+    return <LoadingComponent text={t('travelPay.statusList.loadingClaims')} />
+  }
+
+  const paginationProps: PaginationProps = {
+    onNext: () => {
+      setPage(page + 1)
+      scrollViewRef.current?.scrollTo({ x: 0, y: 0, animated: false })
+    },
+    onPrev: () => {
+      setPage(page - 1)
+      scrollViewRef.current?.scrollTo({ x: 0, y: 0, animated: false })
+    },
+    totalEntries: totalEntries,
+    pageSize: perPage,
+    page,
+  }
+
   return (
-    <View>
-      <Text>TravelPaySummaryList</Text>
-    </View>
+    <Box>
+      <DefaultList items={getListItemVals()} />
+      <Box flex={1} mt={theme.dimensions.paginationTopPadding} mx={theme.dimensions.gutter}>
+        <Pagination {...paginationProps} />
+      </Box>
+    </Box>
   )
 }
