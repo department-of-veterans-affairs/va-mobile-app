93c7f2def58d6fbf1cb8261d4c49780ee5814a0f
Juan Castillo <juan.cast@agile6.com>
2025-08-18 14:35:18 -0700

    Remove webview analytics

--- VAMobile/src/screens/HealthScreen/TravelPay/SubmitTravelPayFlowSteps/components/FileOnBTSSSLink.tsx
+++ VAMobile/src/screens/HealthScreen/TravelPay/SubmitTravelPayFlowSteps/components/FileOnBTSSSLink.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 
 import { LinkWithAnalytics } from 'components'
-import { Events } from 'constants/analytics'
-import { logAnalyticsEvent } from 'utils/analytics'
 import getEnv from 'utils/env'
 
 const { LINK_URL_GO_TO_BTSSS } = getEnv()
@@ -13,15 +11,5 @@ type FileOnBTSSSLinkProps = {
 }
 
 export default function FileOnBTSSSLink({ text, testID }: FileOnBTSSSLinkProps) {
-  return (
-    <LinkWithAnalytics
-      type="url"
-      text={text}
-      testID={testID}
-      url={LINK_URL_GO_TO_BTSSS}
-      analyticsOnPress={() => {
-        logAnalyticsEvent(Events.vama_webview(LINK_URL_GO_TO_BTSSS))
-      }}
-    />
-  )
+  return <LinkWithAnalytics type="url" text={text} testID={testID} url={LINK_URL_GO_TO_BTSSS} />
 }
