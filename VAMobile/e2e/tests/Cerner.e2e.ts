8cefadb16014290e4233cd1d3a5d2111e8de409b
Geoff Winner <geoff.winner@agile6.com>
2025-09-03 18:29:09 -0700

    TICKET-118319 ::  Remove e2e tests for cernere2e file that were related to the cerner alert component and the healthhelp drawer

--- VAMobile/e2e/tests/Cerner.e2e.ts
+++ VAMobile/e2e/tests/Cerner.e2e.ts
@@ -1,14 +1,10 @@
 import { by, device, element, expect, waitFor } from 'detox'
-import { setTimeout } from 'timers/promises'
 
 import {
   CommonE2eIdConstants,
   loginToDemoMode,
-  openAppointments,
   openDeveloperScreen,
-  openHealth,
   openMessages,
-  openPayments,
   openProfile,
   openSettings,
 } from './utils'
@@ -65,63 +61,6 @@ describe(':android: Cerner Notice', () => {
       .whileElement(by.id(CommonE2eIdConstants.HOME_SCREEN_SCROLL_ID))
       .scroll(200, 'down')
     await expect(element(by.text(CernerIdConstants.CERNER_HOME_SUBTEXT_TEXT))).toExist()
-  })
-
-  it('should automatically display cerner panel on health screen', async () => {
-    await openHealth()
-    await expect(element(by.text(CernerIdConstants.CERNER_PANEL_MULTI_ALL_TEXT))).toExist()
-  })
-
-  it('should have correct cerner-only multiple facilities content', async () => {
-    await expect(element(by.text(CernerIdConstants.CERNER_PANEL_MULTI_ALL_TEXT))).toExist()
-    await expect(element(by.text(CernerIdConstants.CERNER_PANEL_MANAGE_MULTI_TEXT))).toExist()
-    await expect(element(by.id(CernerIdConstants.GO_TO_VA_HEALTH_LINK_ID))).toExist()
-  })
-
-  it('should close panel and show cerner subtext on health screen', async () => {
-    await element(by.text('Close')).tap()
-    await element(by.id(CernerIdConstants.HEALTH_CATEGORY_ID)).scrollTo('bottom')
-    await expect(element(by.text(CernerIdConstants.CERNER_HEALTH_HELP_SUBTEXT_TEXT))).toExist()
-    await expect(element(by.text(CernerIdConstants.CERNER_HEALTH_HELP_LINK_TEXT))).toExist()
-  })
-
-  it('should not automatically reopen cerner panel on health screen', async () => {
-    await openPayments()
-    await openHealth()
-    await expect(element(by.text(CernerIdConstants.CERNER_PANEL_MULTI_ALL_TEXT))).not.toExist()
-  })
-
-  it('appointments: verify the cerner notification is present and collapsed', async () => {
-    await element(by.id(CernerIdConstants.HEALTH_CATEGORY_ID)).scrollTo('top')
-    await openAppointments()
-    await expect(element(by.id(CernerIdConstants.CERNER_NOTE_HEADING_ID))).toExist()
-    await expect(element(by.text(CernerIdConstants.CERNER_NOTE_RECORDS_SHOW_TEXT))).not.toExist()
-  })
-
-  it('should match the cerner notice design', async () => {
-    await element(by.id(CernerIdConstants.CERNER_NOTE_HEADING_ID)).tap()
-    await waitFor(element(by.id(CernerIdConstants.GO_TO_VA_HEALTH_LINK_ID)))
-      .toBeVisible()
-      .whileElement(by.id(CommonE2eIdConstants.APPOINTMENTS_SCROLL_ID))
-      .scroll(200, 'down')
-    await expect(element(by.text(CernerIdConstants.CERNER_NOTE_FACILITY_TEXT))).toExist()
-    await expect(element(by.text(CommonE2eIdConstants.CHEYENNE_FACILITY_TEXT))).toExist()
-    await expect(element(by.id(CernerIdConstants.GO_TO_VA_HEALTH_LINK_ID))).toExist()
-  })
-
-  it('verify the correct webpage My Health link is opened', async () => {
-    await element(by.id(CernerIdConstants.GO_TO_VA_HEALTH_LINK_ID)).tap()
-    await element(by.text(CommonE2eIdConstants.LEAVING_APP_LEAVE_TEXT)).tap()
-    await setTimeout(5000)
-    await device.takeScreenshot('cernerVAHealthLink')
-    await device.launchApp({ newInstance: false })
-  })
-
-  it('should tap on the cerner notification and verify the alert closes', async () => {
-    await element(by.id(CommonE2eIdConstants.APPOINTMENTS_SCROLL_ID)).swipe('down')
-    await element(by.text(CernerIdConstants.CERNER_NOTE_HEADING_TEXT)).tap()
-    await expect(element(by.text(CernerIdConstants.CERNER_NOTE_FACILITY_TEXT))).not.toExist()
-    await expect(element(by.text(CommonE2eIdConstants.CHEYENNE_FACILITY_TEXT))).not.toExist()
   })
 
   it('tap on messages and verify the cerner notification is present and collapsed', async () => {
