7426ea39be233e169580a2a3be2b75d76e27f0ff
AdryienH <adryien@themostudio.com>
2025-09-09 17:17:10 -0700

    Fix onDateChange and wire up min and max date

--- VAMobile/android/app/src/main/java/gov/va/mobileapp/native_modules/DatePickerViewManager.kt
+++ VAMobile/android/app/src/main/java/gov/va/mobileapp/native_modules/DatePickerViewManager.kt
@@ -1,47 +1,64 @@
 package gov.va.mobileapp.native_modules
 
-import android.content.Context
-import android.util.Log
 import androidx.compose.ui.platform.ComposeView
 import com.facebook.react.bridge.Arguments
 import com.facebook.react.uimanager.SimpleViewManager
 import com.facebook.react.uimanager.ThemedReactContext
 import com.facebook.react.uimanager.annotations.ReactProp
-import com.facebook.react.uimanager.events.RCTEventEmitter
 import android.view.ViewGroup.LayoutParams
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SelectableDates
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.*
 import androidx.compose.runtime.collectAsState
-import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.StateFlow
 import java.time.Instant
 import com.facebook.react.uimanager.events.Event
 import com.facebook.react.bridge.WritableMap
 import com.facebook.react.uimanager.UIManagerHelper
 import com.facebook.react.common.MapBuilder
+import java.time.LocalDate
+import java.time.ZoneOffset
 
 class DatePickerViewManager : SimpleViewManager<ComposeView>() {
     override fun getName() = "RNDatePicker"
 
     // Flow that always holds the latest date from JS
     private val dateFlow = MutableStateFlow<Long?>(null)
+    private val minimumDateFlow = MutableStateFlow<Long?>(null)
+    private val maximumDateFlow = MutableStateFlow<Long?>(null)
 
     @OptIn(ExperimentalMaterial3Api::class)
     override fun createViewInstance(reactContext: ThemedReactContext): ComposeView {
-        Log.i("Look here", "hiiiiiiiiiiiii")
         return ComposeView(reactContext).apply {
             // Make sure it respects the parent size
             layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
 
             setContent {
-                val date by dateFlow.collectAsState() // observe JS updates
+                // 1. Observe date prop updates
+                val date by dateFlow.collectAsState()
+                val minimumDate by minimumDateFlow.collectAsState()
+                val maximumDate by maximumDateFlow.collectAsState()
 
+                // 2. Hook into Compose’s DatePicker state
                 val datePickerState = rememberDatePickerState(
-                    initialSelectedDateMillis = date
+                    initialSelectedDateMillis = date,
+                    selectableDates = object : SelectableDates {
+                        override fun isSelectableDate(utcTimeMillis: Long): Boolean {
+                            return (minimumDate == null || utcTimeMillis >= minimumDate!!)
+                                    && (maximumDate == null || utcTimeMillis <= maximumDate!!)
+                        }
+
+                        fun getYear(dateInMillis: Long): Int =
+                            getDateFromMillis(dateInMillis).year
+
+                        override fun isSelectableYear(year: Int): Boolean {
+                            return (minimumDate == null ||  year >= getYear(minimumDate!!))
+                                    && (maximumDate == null || year <= getYear(maximumDate!!))
+                        }
+                    }
                 )
 
                 // 3. Whenever user changes date, send event back to JS
@@ -60,24 +77,24 @@ class DatePickerViewManager : SimpleViewManager<ComposeView>() {
     @OptIn(ExperimentalMaterial3Api::class)
     @ReactProp(name = "date")
     fun setDate(view: ComposeView, date: String?) {
-        Log.i("RNDatePicker", "Default is $date")
-        val dateInMillis = date?.let {
-            try {
-                Instant.parse(it).toEpochMilli()
-            } catch (e: Exception) {
-                null
-            }
-        }
-        dateFlow.value = dateInMillis
+        dateFlow.value = parseDate(date)
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @ReactProp(name = "minimumDate")
+    fun setMinimumDate(view: ComposeView, minimumDate: String?) {
+        minimumDateFlow.value = parseDate(minimumDate)
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @ReactProp(name = "maximumDate")
+    fun setMaximumDate(view: ComposeView, maximumDate: String?) {
+        maximumDateFlow.value = parseDate(maximumDate)
     }
 
     private fun sendDateChangeEvent(context: ThemedReactContext, viewId: Int, millis: Long) {
-        Log.d("RNDatePicker", "User picked $millis")
-        val isoDate = java.time.Instant.ofEpochMilli(millis).toString()
+        val isoDate = getDateFromMillis(millis).toString()
         val eventDispatcher = UIManagerHelper.getEventDispatcherForReactTag(context, viewId)
-
-        Log.d("test", eventDispatcher.toString())
-
         eventDispatcher?.dispatchEvent(DateChangeEvent(viewId, isoDate))
     }
 
@@ -85,6 +102,22 @@ class DatePickerViewManager : SimpleViewManager<ComposeView>() {
         DateChangeEvent.EVENT_NAME to MapBuilder.of("registrationName", "onDateChange")
     )
 
+    private fun parseDate(dateString: String?): Long? {
+        val dateInMillis = dateString?.let {
+            try {
+                LocalDate.parse(it) // parse "YYYY-MM-DD"
+                    .atStartOfDay(ZoneOffset.UTC)
+                    .toInstant()
+                    .toEpochMilli()
+            } catch (e: Exception) {
+                null
+            }
+        }
+       return dateInMillis
+    }
+
+    private fun getDateFromMillis(dateInMillis: Long): LocalDate =
+        Instant.ofEpochMilli(dateInMillis).atZone(ZoneOffset.UTC).toLocalDate()
 
 }
 
