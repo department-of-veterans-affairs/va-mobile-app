82e0c8759db5df3f9265677458829550328cd394
Jayson Perkins <jayson.perkins@adhocteam.us>
2025-09-12 11:14:35 -0400

    update cerner test

--- VAMobile/e2e/tests/Cerner.e2e.ts
+++ VAMobile/e2e/tests/Cerner.e2e.ts
@@ -3,6 +3,7 @@ import { setTimeout } from 'timers/promises'
 
 import {
   CommonE2eIdConstants,
+  changeDemoModeUser,
   loginToDemoMode,
   openDeveloperScreen,
   openHealth,
@@ -27,6 +28,7 @@ export const CernerIdConstants = {
     "Some care teams use My VA Health. Information from that health portal isn't included here.",
   CERNER_HEALTH_HELP_LINK_TEXT: 'Check if your care team uses My VA Health',
   MESSAGES_ID: 'messagesTestID',
+  DESIRED_DEMO_MODE_USER_ID: 'Dennis Madison option 4 of 4',
 }
 
 beforeAll(async () => {
@@ -54,6 +56,7 @@ beforeAll(async () => {
 
     //navigates to correct screen with cerner on
     await loginToDemoMode()
+    await changeDemoModeUser(CernerIdConstants.DESIRED_DEMO_MODE_USER_ID)
   }
 })
 
