485482f11928809c9c551d1f80ea5715a9d41228
AdryienH <adryien@themostudio.com>
2025-09-23 15:23:00 -0700

    Initial upgrade to 0.77.3

--- VAMobile/ios/VAMobile/AppDelegate.h
+++
@@ -1,9 +0,0 @@
-#import <React/RCTBridgeDelegate.h>
-#import <RCTAppDelegate.h>
-#import <UIKit/UIKit.h>
-
-@interface AppDelegate : RCTAppDelegate <UIApplicationDelegate, RCTBridgeDelegate>
-
-@property (nonatomic, strong) UIWindow *window;
-
-@end
