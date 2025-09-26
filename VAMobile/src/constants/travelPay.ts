b90040d517250517a693be81c41c7e9342352c03
Samuel Cohen <s.cohen@theta-llc.com>
2025-09-26 10:36:44 -0700

    travel claims entry points, appt detail native entry

--- VAMobile/src/constants/travelPay.ts
+++ VAMobile/src/constants/travelPay.ts
@@ -2,16 +2,18 @@ export type TravelPayError = 'noAddress' | 'unsupportedType' | 'error'
 
 export type TravelPayPartialSuccessStatus = 'Incomplete' | 'Saved'
 
-export type TravelClaimsScreenEntryType = 'Health' | 'Claims' | 'Payments'
+export type TravelClaimsScreenEntryType = 'Health' | 'Claims' | 'Payments' | 'AppointmentDetail'
 
 export const TravelClaimsScreenEntry: {
   Health: TravelClaimsScreenEntryType
   Claims: TravelClaimsScreenEntryType
   Payments: TravelClaimsScreenEntryType
+  AppointmentDetail: TravelClaimsScreenEntryType
 } = {
   Health: 'Health',
   Claims: 'Claims',
   Payments: 'Payments',
+  AppointmentDetail: 'AppointmentDetail',
 }
 
 /**
