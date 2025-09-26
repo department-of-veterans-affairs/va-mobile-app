2d01d8fd4f2691ca40cd6fedb58bd864aae08024
Samuel Cohen <s.cohen@theta-llc.com>
2025-09-25 20:48:50 -0700

    travel claims entry point tests

---
+++ VAMobile/src/screens/BenefitsScreen/ClaimsScreen/ClaimsScreen.test.tsx
@@ -0,0 +1,79 @@
+import React from 'react'
+
+import { fireEvent, screen } from '@testing-library/react-native'
+
+import { TravelClaimsScreenEntry } from 'constants/travelPay'
+import ClaimsScreen from 'screens/BenefitsScreen/ClaimsScreen'
+import { context, mockNavProps, render, when } from 'testUtils'
+import { featureEnabled } from 'utils/remoteConfig'
+
+const mockNavigationSpy = jest.fn()
+jest.mock('utils/hooks', () => {
+  const original = jest.requireActual('utils/hooks')
+
+  return {
+    ...original,
+    useRouteNavigation: () => mockNavigationSpy,
+  }
+})
+
+jest.mock('utils/remoteConfig', () => ({
+  featureEnabled: jest.fn(),
+}))
+
+context('ClaimsScreen', () => {
+  const initializeTestInstance = () => {
+    const props = mockNavProps(undefined, { navigate: mockNavigationSpy })
+
+    render(<ClaimsScreen {...props} />)
+  }
+
+  it('navigates to Claims history when the button is pressed', () => {
+    initializeTestInstance()
+
+    fireEvent.press(screen.getByTestId('toClaimsHistoryID'))
+    expect(mockNavigationSpy).toHaveBeenCalledWith('ClaimsHistoryScreen')
+  })
+
+  it('navigates to Claims letters when the button is pressed', () => {
+    initializeTestInstance()
+
+    fireEvent.press(screen.getByTestId('toClaimLettersID'))
+    expect(mockNavigationSpy).toHaveBeenCalledWith('ClaimLettersScreen')
+  })
+
+  describe('Travel Claims button', () => {
+    it('is not displayed if feature toggle is disabled', () => {
+      when(featureEnabled as jest.Mock)
+        .calledWith('travelPayStatusList')
+        .mockReturnValue(false)
+
+      initializeTestInstance()
+
+      expect(screen.queryByTestId('toTravelPayClaimsID')).toBeFalsy()
+    })
+
+    it('is displayed if feature toggle is enabled', () => {
+      when(featureEnabled as jest.Mock)
+        .calledWith('travelPayStatusList')
+        .mockReturnValue(true)
+
+      initializeTestInstance()
+
+      expect(screen.getByTestId('toTravelPayClaimsID')).toBeTruthy()
+    })
+
+    it('navigates to Travel Claims screen when pressed', () => {
+      when(featureEnabled as jest.Mock)
+        .calledWith('travelPayStatusList')
+        .mockReturnValue(true)
+      initializeTestInstance()
+
+      fireEvent.press(screen.getByTestId('toTravelPayClaimsID'))
+
+      expect(mockNavigationSpy).toHaveBeenCalledWith('TravelPayClaims', {
+        from: TravelClaimsScreenEntry.Claims,
+      })
+    })
+  })
+})
