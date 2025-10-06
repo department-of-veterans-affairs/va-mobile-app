653bcfb0bcd34eaf43aeae6cf4578e1ddb7e76de
Adrian Rollett <101649+acrollet@users.noreply.github.com>
2025-10-06 11:42:58 -0600

    Remove LOCAL_API_CHANGES.md and local-api-example.js; update authSlice.ts for local mocked authentication

--- VAMobile/src/store/slices/authSlice.ts
+++ VAMobile/src/store/slices/authSlice.ts
@@ -771,25 +771,14 @@ export const startWebLogin = (): AppThunk => async (dispatch) => {
   // TODO: modify code challenge and state based on
   // what will be used in LoginSuccess.js for the token exchange.
   // The code challenge is a SHA256 hash of the code verifier string.
-  
-  // For local development with Mocked Authentication, we need client_id=vamock-mobile
-  const paramData: { [key: string]: string } = {
+  const params = new URLSearchParams({
     code_challenge_method: 'S256',
     code_challenge: 'tDKCgVeM7b8X2Mw7ahEeSPPFxr7TGPc25IV5ex0PvHI',
     application: 'vamobile',
     oauth: 'true',
-  }
-  
-  // Add client_id for local environment to enable Mocked Authentication
-  if (ENVIRONMENT === 'local') {
-    paramData.client_id = 'vamock-mobile'
-  }
-  
-  const params = new URLSearchParams(paramData).toString()
-  console.log(params)
+  }).toString()
 
   const url = `${AUTH_SIS_ENDPOINT}?${params}`
-  console.log(url)
   dispatch(dispatchShowWebLogin(url))
 }
 
