cbfad4b108b5711cdd66e95e7958dcefeb3e9147
Matthew Guest - Work <matthewguest-wilcore@Matthews-MacBook-Pro.local>
2025-10-14 11:47:30 -0700

    Implemented the Dispute Copay Screen

--- VAMobile/src/screens/PaymentsScreen/Copays/DisputeCopay/DisputeCopayScreen.tsx
+++ VAMobile/src/screens/PaymentsScreen/Copays/DisputeCopay/DisputeCopayScreen.tsx
@@ -1,17 +1,24 @@
 import React from 'react'
-import { useTranslation } from 'react-i18next'
+import { Trans, useTranslation } from 'react-i18next'
 
 import { useNavigationState } from '@react-navigation/native'
 import { StackScreenProps } from '@react-navigation/stack'
 
-import { FeatureLandingTemplate } from 'components'
+import { Box, FeatureLandingTemplate, LinkWithAnalytics, TextArea, TextView } from 'components'
 import { NAMESPACE } from 'constants/namespaces'
 import { PaymentsStackParamList } from 'screens/PaymentsScreen/PaymentsStackScreens'
+import getEnv from 'utils/env'
+import { useTheme } from 'utils/hooks'
 
 type DisputeCopayScreenProps = StackScreenProps<PaymentsStackParamList, 'DisputeCopay'>
 
+const { LINK_URL_DISPUTE_COPAY } = getEnv()
+
 function DisputeCopayScreen({ navigation }: DisputeCopayScreenProps) {
   const { t } = useTranslation(NAMESPACE.COMMON)
+  const theme = useTheme()
+  const { contentMarginBottom, condensedMarginBetween } = theme.dimensions
+
   const prevScreen = useNavigationState((state) => state.routes[state.routes.length - 2]?.name)
   const backLabel = prevScreen === 'CopayDetails' ? t('copays.details.title') : t('copays.title')
 
@@ -21,8 +28,28 @@ function DisputeCopayScreen({ navigation }: DisputeCopayScreenProps) {
       backLabelOnPress={navigation.goBack}
       title={t('copays.disputeCopay.title')}
       testID="disputeCopayTestID"
-      backLabelTestID="disputeCopayBackTestID"
-    />
+      backLabelTestID="disputeCopayBackTestID">
+      <Box mb={contentMarginBottom}>
+        <TextArea>
+          <Trans
+            i18nKey="copays.disputeCopay.copy"
+            components={{
+              p: <TextView my={condensedMarginBetween} variant="MobileBody" />,
+              bold: <TextView variant="MobileBodyBold" />,
+            }}
+          />
+
+          <Box mt={condensedMarginBetween}>
+            <LinkWithAnalytics
+              type="url"
+              url={LINK_URL_DISPUTE_COPAY}
+              text={t('copays.disputeCopay.link')}
+              testID="learn-dispute-copay-link"
+            />
+          </Box>
+        </TextArea>
+      </Box>
+    </FeatureLandingTemplate>
   )
 }
 
