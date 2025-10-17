5b7ae235bce6bad21803552759a795db63b92e7c
Liz Townsend <liz.townsend@adhocteam.us>
2025-10-17 11:24:18 -0500

    update allergies types, update api call

--- VAMobile/src/api/allergies/getAllergies.tsx
+++ VAMobile/src/api/allergies/getAllergies.tsx
@@ -1,15 +1,27 @@
 import { useQuery } from '@tanstack/react-query'
 
+import { allergyKeys } from 'api/allergies/queryKeys'
+import { useAuthorizedServices } from 'api/authorizedServices/getAuthorizedServices'
 import { AllergyListPayload } from 'api/types'
 import { LARGE_PAGE_SIZE } from 'constants/common'
 import { get } from 'store/api'
 
-import { allergyKeys } from './queryKeys'
-
 /**
  * Fetch user Allergies
  */
-const getAllergies = (): Promise<AllergyListPayload | undefined> => {
+const getAllergies = ({
+  getOHAllergies = false,
+}: {
+  getOHAllergies?: boolean
+}): Promise<AllergyListPayload | undefined> => {
+  if (getOHAllergies) {
+    return get<AllergyListPayload>('/v1/health/allergy-intolerances', {
+      'page[number]': '1',
+      'page[size]': LARGE_PAGE_SIZE.toString(),
+      sort: 'date',
+      useCache: 'false',
+    })
+  }
   return get<AllergyListPayload>('/v0/health/allergy-intolerances', {
     'page[number]': '1',
     'page[size]': LARGE_PAGE_SIZE.toString(),
@@ -22,10 +34,12 @@ const getAllergies = (): Promise<AllergyListPayload | undefined> => {
  * Returns a query for user Allergies
  */
 export const useAllergies = (options?: { enabled?: boolean }) => {
+  const { data: authorizedServices } = useAuthorizedServices()
+
   return useQuery({
     ...options,
     queryKey: [allergyKeys.allergies],
-    queryFn: () => getAllergies(),
+    queryFn: () => getAllergies({ getOHAllergies: authorizedServices?.allergiesOracleHealthEnabled }),
     meta: {
       errorName: 'getAllergies: Service error',
     },
