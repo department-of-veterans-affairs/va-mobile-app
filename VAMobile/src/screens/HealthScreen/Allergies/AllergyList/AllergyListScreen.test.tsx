1fc02111a8e6feba86d64659ac23834b26873532
Liz Townsend <liz.townsend@adhocteam.us>
2025-10-20 15:05:26 -0500

    add flags to tests; add tests for flags

--- VAMobile/src/screens/HealthScreen/Allergies/AllergyList/AllergyListScreen.test.tsx
+++ VAMobile/src/screens/HealthScreen/Allergies/AllergyList/AllergyListScreen.test.tsx
@@ -7,104 +7,106 @@ import { authorizedServicesKeys } from 'api/authorizedServices/queryKeys'
 import AllergyListScreen from 'screens/HealthScreen/Allergies/AllergyList/AllergyListScreen'
 import * as api from 'store/api'
 import { context, mockNavProps, render, when } from 'testUtils'
+import { featureEnabled } from 'utils/remoteConfig'
+
+jest.mock('utils/remoteConfig')
 
 context('AllergyListScreen', () => {
-  context('with allergies V0', () => {
-    const allergyData = [
-      {
-        id: '4-1abLZzsevfVnWK',
-        type: 'allergy_intolerance',
-        attributes: {
-          resourceType: 'AllergyIntolerance',
-          type: 'allergy',
-          clinicalStatus: {
-            coding: [
-              {
-                system: 'http://terminology.hl7.org/CodeSystem/allergyintolerance-clinical',
-                code: 'active',
-              },
-            ],
-          },
-          category: ['medication'],
-          code: {
-            coding: [
-              {
-                system: 'http://hl7.org/fhir/ndfrt',
-                code: 'N0000008048',
-                display: 'Sulfonamides',
-              },
-            ],
+  const allergyData = [
+    {
+      id: '4-1abLZzsevfVnWK',
+      type: 'allergy_intolerance',
+      attributes: {
+        resourceType: 'AllergyIntolerance',
+        type: 'allergy',
+        clinicalStatus: {
+          coding: [
+            {
+              system: 'http://terminology.hl7.org/CodeSystem/allergyintolerance-clinical',
+              code: 'active',
+            },
+          ],
+        },
+        category: ['medication'],
+        code: {
+          coding: [
+            {
+              system: 'http://hl7.org/fhir/ndfrt',
+              code: 'N0000008048',
+              display: 'Sulfonamides',
+            },
+          ],
+          text: 'Sulfonamides',
+        },
+        recordedDate: '2019-03-12T16:30:00Z',
+        patient: {
+          reference: 'https://sandbox-api.va.gov/services/fhir/v0/r4/Patient/1013956965V299908',
+          display: 'DAMASO SUPNICK',
+        },
+        notes: [
+          {
+            authorReference: {
+              reference: 'https://sandbox-api.va.gov/services/fhir/v0/r4/Practitioner/4-Nn79MgdlF9vV',
+              display: 'Dr. Alicia629 Ureña88 MD',
+            },
+            time: '2019-03-12T16:30:00Z',
             text: 'Sulfonamides',
           },
-          recordedDate: '2019-03-12T16:30:00Z',
-          patient: {
-            reference: 'https://sandbox-api.va.gov/services/fhir/v0/r4/Patient/1013956965V299908',
-            display: 'DAMASO SUPNICK',
-          },
-          notes: [
+        ],
+        recorder: {
+          reference: 'https://sandbox-api.va.gov/services/fhir/v0/r4/Practitioner/4-Nn79MgdlF9vV',
+          display: 'Dr. Alicia629 Ureña88 MD',
+        },
+        reactions: [],
+      },
+    },
+    {
+      id: '4-1wYbwqxtod74iS',
+      type: 'allergy_intolerance',
+      attributes: {
+        resourceType: 'AllergyIntolerance',
+        type: 'allergy',
+        clinicalStatus: {
+          coding: [
             {
-              authorReference: {
-                reference: 'https://sandbox-api.va.gov/services/fhir/v0/r4/Practitioner/4-Nn79MgdlF9vV',
-                display: 'Dr. Alicia629 Ureña88 MD',
-              },
-              time: '2019-03-12T16:30:00Z',
-              text: 'Sulfonamides',
+              system: 'http://terminology.hl7.org/CodeSystem/allergyintolerance-clinical',
+              code: 'active',
             },
           ],
-          recorder: {
-            reference: 'https://sandbox-api.va.gov/services/fhir/v0/r4/Practitioner/4-Nn79MgdlF9vV',
-            display: 'Dr. Alicia629 Ureña88 MD',
-          },
-          reactions: [],
         },
-      },
-      {
-        id: '4-1wYbwqxtod74iS',
-        type: 'allergy_intolerance',
-        attributes: {
-          resourceType: 'AllergyIntolerance',
-          type: 'allergy',
-          clinicalStatus: {
-            coding: [
+        category: ['medication'],
+        code: {
+          coding: [],
+          text: 'penicillins',
+        },
+        recordedDate: '2023-01-10T18:26:28Z',
+        patient: {
+          reference: 'https://sandbox-api.va.gov/services/fhir/v0/r4/Patient/1013956965V299908',
+          display: 'SUPNICK, DAMASO',
+        },
+        notes: [],
+        recorder: {
+          reference: 'https://sandbox-api.va.gov/services/fhir/v0/r4/Practitioner/4-X3BWnhAtrFa0Ko0R',
+          display: 'Nurse2, VA-HBPC',
+        },
+        reactions: [
+          {
+            substance: {
+              coding: [],
+              text: null,
+            },
+            manifestation: [
               {
-                system: 'http://terminology.hl7.org/CodeSystem/allergyintolerance-clinical',
-                code: 'active',
+                coding: [],
+                text: 'Urticaria (Hives)',
               },
             ],
           },
-          category: ['medication'],
-          code: {
-            coding: [],
-            text: 'penicillins',
-          },
-          recordedDate: '2023-01-10T18:26:28Z',
-          patient: {
-            reference: 'https://sandbox-api.va.gov/services/fhir/v0/r4/Patient/1013956965V299908',
-            display: 'SUPNICK, DAMASO',
-          },
-          notes: [],
-          recorder: {
-            reference: 'https://sandbox-api.va.gov/services/fhir/v0/r4/Practitioner/4-X3BWnhAtrFa0Ko0R',
-            display: 'Nurse2, VA-HBPC',
-          },
-          reactions: [
-            {
-              substance: {
-                coding: [],
-                text: null,
-              },
-              manifestation: [
-                {
-                  coding: [],
-                  text: 'Urticaria (Hives)',
-                },
-              ],
-            },
-          ],
-        },
+        ],
       },
-    ]
-
+    },
+  ]
+  context('with allergies V0', () => {
     const initializeTestInstance = () => {
       render(<AllergyListScreen {...mockNavProps()} />)
     }
@@ -207,13 +209,15 @@ context('AllergyListScreen', () => {
       },
     ]
 
-    const initializeTestInstance = () => {
+    const initializeTestInstance = ({ flagEnabled = true, serviceEnabled = true }) => {
+      when(featureEnabled).calledWith('allergiesOracleHealthApiEnabled').mockReturnValue(flagEnabled)
+
       render(<AllergyListScreen {...mockNavProps()} />, {
         queriesData: [
           {
             queryKey: authorizedServicesKeys.authorizedServices,
             data: {
-              allergiesOracleHealthEnabled: true,
+              allergiesOracleHealthEnabled: serviceEnabled,
             },
           },
         ],
@@ -224,10 +228,27 @@ context('AllergyListScreen', () => {
       when(api.get as jest.Mock)
         .calledWith('/v1/health/allergy-intolerances', expect.anything())
         .mockResolvedValue({ data: allergyDataV1 })
-
-      initializeTestInstance()
+      initializeTestInstance({})
       await waitFor(() => expect(screen.getByText('ASPIRIN allergy')).toBeTruthy())
       await waitFor(() => expect(screen.getByText('Penicillin allergy')).toBeTruthy())
+    })
+
+    it('initializes with v0 endpoint when flag is disabled even if services is authorized', async () => {
+      when(api.get as jest.Mock)
+        .calledWith('/v0/health/allergy-intolerances', expect.anything())
+        .mockResolvedValue({ data: allergyData })
+      initializeTestInstance({ flagEnabled: false })
+      await waitFor(() => expect(screen.getByText('Sulfonamides allergy')).toBeTruthy())
+      await waitFor(() => expect(screen.getByText('Penicillins allergy')).toBeTruthy())
+    })
+
+    it('initializes with v0 endpoint when flag is enabled if services are not authorized', async () => {
+      when(api.get as jest.Mock)
+        .calledWith('/v0/health/allergy-intolerances', expect.anything())
+        .mockResolvedValue({ data: allergyData })
+      initializeTestInstance({ serviceEnabled: false })
+      await waitFor(() => expect(screen.getByText('Sulfonamides allergy')).toBeTruthy())
+      await waitFor(() => expect(screen.getByText('Penicillins allergy')).toBeTruthy())
     })
 
     describe('when loading is set to true with v1 endpoint', () => {
@@ -235,7 +256,7 @@ context('AllergyListScreen', () => {
         when(api.get as jest.Mock)
           .calledWith('/v1/health/allergy-intolerances', expect.anything())
           .mockResolvedValue({ data: allergyDataV1 })
-        initializeTestInstance()
+        initializeTestInstance({})
         expect(screen.getByText('Loading your allergy record...')).toBeTruthy()
       })
     })
@@ -246,7 +267,7 @@ context('AllergyListScreen', () => {
           .calledWith('/v1/health/allergy-intolerances', expect.anything())
           .mockResolvedValue({ data: [] })
 
-        initializeTestInstance()
+        initializeTestInstance({})
         await waitFor(() =>
           expect(
             screen.getByRole('heading', { name: "We couldn't find information about your VA allergies" }),
@@ -277,7 +298,7 @@ context('AllergyListScreen', () => {
           .calledWith('/v1/health/allergy-intolerances', expect.anything())
           .mockRejectedValue({ networkError: true } as api.APIError)
 
-        initializeTestInstance()
+        initializeTestInstance({})
         await waitFor(() => expect(screen.getByRole('header', { name: "The app can't be loaded." })).toBeTruthy())
       })
     })
