c718abc443f6ed76299c349df287d6ecca19d7d8
Matthew Sweeney <matthew@digitalfoundry.com>
2025-12-04 15:48:10 -0800

    Memoized whats new text

--- VAMobile/src/components/WhatsNew.tsx
+++ VAMobile/src/components/WhatsNew.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useMemo, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
 import { AlertWithHaptics, Box, LinkWithAnalytics, TextView, VABulletList, VABulletListText } from 'components'
@@ -32,88 +32,104 @@ export const WhatsNew = () => {
   }, [])
 
   // TODO: refactor this to not use a while loop
-  const getBullets = (featureStringBase: string) => {
-    const bullets: VABulletListText[] = []
+  const getBullets = useCallback(
+    (featureStringBase: string) => {
+      const bullets: VABulletListText[] = []
 
-    while (1) {
-      const bulletKey = `${featureStringBase}.bullet.${bullets.length + 1}`
-      //@ts-ignore
-      const text = t(bulletKey)
-      //@ts-ignore
-      const a11yLabel = t(`${bulletKey}.a11yLabel`)
+      while (1) {
+        const bulletKey = `${featureStringBase}.bullet.${bullets.length + 1}`
+        //@ts-ignore
+        const text = t(bulletKey)
+        //@ts-ignore
+        const a11yLabel = t(`${bulletKey}.a11yLabel`)
 
-      if (text.startsWith(featureStringBase) || !text || bullets.length > 20) {
-        return bullets
-      } else {
-        bullets.push({
-          text,
-          a11yLabel: a11yLabel.startsWith(featureStringBase) ? undefined : a11yLabel,
-        })
+        if (text.startsWith(featureStringBase) || !text || bullets.length > 20) {
+          return bullets
+        } else {
+          bullets.push({
+            text,
+            a11yLabel: a11yLabel.startsWith(featureStringBase) ? undefined : a11yLabel,
+          })
+        }
       }
+    },
+    [t],
+  )
+
+  const { whatsNewDisplay, featuresDisplayed } = useMemo(() => {
+    const display: React.ReactNode[] = []
+    const features: string[] = []
+
+    if (whatsNewItems.length) {
+      whatsNewItems.forEach((newFeature, idx) => {
+        // Do not show features that do not have their flag enabled
+        const shouldDisplayFeature = !newFeature.featureFlag || featureEnabled(newFeature.featureFlag)
+        // Check if the feature has already been skipped by dismissing a whats new including it
+        const featureSkipped = skippedFeatures?.includes(newFeature.featureName)
+
+        if (!shouldDisplayFeature || featureSkipped) {
+          return
+        } else {
+          features.push(newFeature.featureName)
+        }
+
+        // The base string of the feature will be the key for the main description text.
+        const featureStringBase = `whatsNew.bodyCopy.${newFeature.featureName}`
+        // Attempt to translate the base string
+        //@ts-ignore
+        const labelValue = t(`${featureStringBase}.a11yLabel`)
+        /**
+         * If there is no translation string found, translation will return the key used. Match this to know if there
+         * is an a11y label for this string, if not we will skip it
+         */
+        const bodyA11yLabel = labelValue.startsWith(featureStringBase) ? undefined : labelValue
+
+        //@ts-ignore
+        const body = t(featureStringBase)
+        const bullets = getBullets(featureStringBase) || []
+
+        // get URL if it exists
+        const linkKey = `${featureStringBase}.link.url`
+        const linkUrl = t(linkKey)
+        const linkTextKey = `${featureStringBase}.link.text`
+        const linkText = t(linkTextKey)
+        // Check if we have a link to show
+        const showLink = !linkUrl.startsWith(featureStringBase)
+
+        const topPadding = idx === 0 ? 0 : theme.dimensions.standardMarginBetween
+
+        display.push(
+          <Box pt={topPadding}>
+            {/* eslint-disable-next-line react-native-a11y/has-accessibility-hint */}
+            <TextView accessibilityLabel={bodyA11yLabel} pb={theme.dimensions.tinyMarginBetween}>
+              {body}
+            </TextView>
+            {bullets.length && <VABulletList listOfText={bullets} />}
+            {showLink && (
+              <LinkWithAnalytics
+                type="url"
+                url={linkUrl}
+                text={linkText}
+                a11yHint={`${linkText} ${t('mobileBodyLink.a11yHint')}`}
+              />
+            )}
+          </Box>,
+        )
+      })
     }
-  }
 
-  const whatsNewDisplay: React.ReactNode[] = []
-  const featuresDisplayed: string[] = []
-
-  // TODO: memoize this
-  if (whatsNewItems.length) {
-    whatsNewItems.forEach((newFeature, idx) => {
-      // Do not show features that do not have their flag enabled
-      const shouldDisplayFeature = !newFeature.featureFlag || featureEnabled(newFeature.featureFlag)
-      // Check if the feature has already been skipped by dismissing a whats new including it
-      const featureSkipped = skippedFeatures?.includes(newFeature.featureName)
-
-      if (!shouldDisplayFeature || featureSkipped) {
-        return
-      } else {
-        featuresDisplayed.push(newFeature.featureName)
-      }
-
-      // The base string of the feature will be the key for the main description text.
-      const featureStringBase = `whatsNew.bodyCopy.${newFeature.featureName}`
-      // Attempt to translate the base string
-      //@ts-ignore
-      const labelValue = t(`${featureStringBase}.a11yLabel`)
-      /**
-       * If there is no translation string found, translation will return the key used. Match this to know if there
-       * is an a11y label for this string, if not we will skip it
-       */
-      const bodyA11yLabel = labelValue.startsWith(featureStringBase) ? undefined : labelValue
-
-      //@ts-ignore
-      const body = t(featureStringBase)
-      const bullets = getBullets(featureStringBase) || []
-
-      // get URL if it exists
-      const linkKey = `${featureStringBase}.link.url`
-      const linkUrl = t(linkKey)
-      const linkTextKey = `${featureStringBase}.link.text`
-      const linkText = t(linkTextKey)
-      // Check if we have a link to show
-      const showLink = !linkUrl.startsWith(featureStringBase)
-
-      const topPadding = idx === 0 ? 0 : theme.dimensions.standardMarginBetween
-
-      whatsNewDisplay.push(
-        <Box pt={topPadding}>
-          {/* eslint-disable-next-line react-native-a11y/has-accessibility-hint */}
-          <TextView accessibilityLabel={bodyA11yLabel} pb={theme.dimensions.tinyMarginBetween}>
-            {body}
-          </TextView>
-          {bullets.length && <VABulletList listOfText={bullets} />}
-          {showLink && (
-            <LinkWithAnalytics
-              type="url"
-              url={linkUrl}
-              text={linkText}
-              a11yHint={`${linkText} ${t('mobileBodyLink.a11yHint')}`}
-            />
-          )}
-        </Box>,
-      )
-    })
-  }
+    return {
+      whatsNewDisplay: display,
+      featuresDisplayed: features,
+    }
+  }, [
+    getBullets,
+    skippedFeatures,
+    t,
+    theme.dimensions.standardMarginBetween,
+    theme.dimensions.tinyMarginBetween,
+    whatsNewItems,
+  ])
 
   const expandCollapsible = () => logAnalyticsEvent(Events.vama_whatsnew_more())
   const closeCollapsible = () => logAnalyticsEvent(Events.vama_whatsnew_close())
