ed0304333cd32c4ade6fd1d8a3dfcd0619ff02a0
John Marchi <johnmarchi@John-M-MBP-M1M-2021.local>
2025-12-04 16:31:41 -0800

    adding broken test

--- VAMobile/src/components/WhatsNew.test.tsx
+++ VAMobile/src/components/WhatsNew.test.tsx
@@ -3,12 +3,14 @@ import React from 'react'
 import { fireEvent, screen, waitFor } from '@testing-library/react-native'
 import { t } from 'i18next'
 
-import WhatsNew from 'components/WhatsNew'
+import WhatsNew, { getWhatsNewConfig } from 'components/WhatsNew'
 import { InitialState } from 'store/slices'
 import { context, render, when } from 'testUtils'
-import { getLocalVersion, getVersionSkipped, setVersionSkipped } from 'utils/homeScreenAlerts'
+import { FeatureToggleType, featureEnabled } from 'utils/remoteConfig'
 
+const mockGetConfig = jest.fn()
 const mockT = jest.fn()
+
 jest.mock('react-i18next', () => {
   const original = jest.requireActual('react-i18next')
   return {
@@ -21,17 +23,42 @@ jest.mock('react-i18next', () => {
   }
 })
 
-const NEWS_LIST = {
-  SSO_TEXT: `Single sign on for VA has been updated`,
-  SSO_TEXT_A11Y: `Single sign on for V-A has been updated`,
-  BULLET1: 'Prescriptions SS to webview',
-  BULLET2: 'Medication SSO to webview',
+jest.mock('components/WhatsNew', () => {
+  const original = jest.requireActual('components/WhatsNew')
+
+  return {
+    ...original,
+    getWhatsNewConfig: mockGetConfig,
+  }
+})
+
+const featureConfigs = {
+  empty: [],
+  oneFeatureNoFlag: [
+    {
+      featureName: 'testFeatureNoFlag',
+    },
+  ],
+  twoFeaturesOneFlag: [
+    {
+      featureName: 'testFeature',
+      featureFlag: 'featureflag1',
+    },
+    {
+      featureName: 'testFeatureNoFlag',
+    },
+  ],
 }
 
 context('WhatsNew', () => {
-  const initializeTestInstance = (localVersion = '0.0', skippedVersion = '0.0') => {
-    when(getLocalVersion).mockReturnValue(Promise.resolve(localVersion))
-    when(getVersionSkipped).mockReturnValue(Promise.resolve(skippedVersion))
+  const initializeTestInstance = (featureName: string, featureFlag?: string, flagEnabled?: boolean) => {
+    when(mockGetConfig).calledWith().mockReturnValue(featureConfigs[featureName])
+
+    if (featureFlag) {
+      when(featureEnabled)
+        .calledWith(featureFlag as FeatureToggleType)
+        .mockReturnValue(flagEnabled)
+    }
 
     // The component uses translation keys to hide and show its element, therefore we need to mock out translation to get it to render
     when(mockT)
@@ -39,15 +66,15 @@ context('WhatsNew', () => {
       .mockReturnValue(t('whatsNew.title'))
       .calledWith('whatsNew.dismissMessage')
       .mockReturnValue(t('whatsNew.dismissMessage'))
-      .calledWith(`whatsNew.bodyCopy.${localVersion}`)
-      .mockReturnValue(NEWS_LIST.SSO_TEXT)
-      .calledWith(`whatsNew.bodyCopy.${localVersion}.a11yLabel`)
-      .mockReturnValue(NEWS_LIST.SSO_TEXT_A11Y)
+      .calledWith(`whatsNew.bodyCopy.${featureName}`)
+      .mockReturnValue(`${featureName} main text`)
+      .calledWith(`whatsNew.bodyCopy.${featureName}.a11yLabel`)
+      .mockReturnValue(`${featureName} a11y label`)
       .defaultReturnValue('')
-      .calledWith(`whatsNew.bodyCopy.${localVersion}.bullet.1`)
-      .mockReturnValue(NEWS_LIST.BULLET1)
-      .calledWith(`whatsNew.bodyCopy.${localVersion}.bullet.2`)
-      .mockReturnValue(NEWS_LIST.BULLET2)
+      .calledWith(`whatsNew.bodyCopy.${featureName}.bullet.1`)
+      .mockReturnValue(`${featureName} bullet 1`)
+      .calledWith(`whatsNew.bodyCopy.${featureName}.bullet.2`)
+      .mockReturnValue(`${featureName} bullet 2`)
 
     render(<WhatsNew />, {
       preloadedState: {
@@ -59,34 +86,34 @@ context('WhatsNew', () => {
     })
   }
 
-  it('should not render when version is skipped', async () => {
-    initializeTestInstance('1.1', '1.1')
+  it('should not render when no features are listed', async () => {
+    initializeTestInstance('empty')
     await waitFor(() => expect(screen.queryByText(t('whatsNew.title'))).toBeFalsy())
   })
 
-  it('should render when version is not skipped', async () => {
-    initializeTestInstance('1.1', '1.0')
-    await waitFor(async () => {
-      const title = await screen.findByText(t('whatsNew.title'))
-      return expect(title).toBeTruthy()
-    })
-  })
+  // it('should render a feature that has not been skipped', async () => {
+  //   initializeTestInstance('oneFeatureNoFlag')
+  //   await waitFor(async () => {
+  //     const title = await screen.findByText(t('whatsNew.title'))
+  //     return expect(title).toBeTruthy()
+  //   })
+  // })
 
-  it('should render details when expanded ', async () => {
-    initializeTestInstance('1.1', '1.0')
-    await waitFor(() => fireEvent.press(screen.getByRole('tab', { name: t('whatsNew.title') })))
-    expect(screen.getByText(NEWS_LIST.SSO_TEXT)).toBeTruthy()
-    expect(screen.getByText(NEWS_LIST.BULLET1)).toBeTruthy()
-    expect(screen.getByText(NEWS_LIST.BULLET2)).toBeTruthy()
-  })
-
-  it('should set the next skip version when dismissed ', async () => {
-    initializeTestInstance('1.1', '1.0')
-    expect(setVersionSkipped).not.toBeCalled()
-
-    await waitFor(() => fireEvent.press(screen.getByRole('tab', { name: t('whatsNew.title') })))
-    await waitFor(() => fireEvent.press(screen.getByRole('button', { name: t('whatsNew.dismissMessage') })))
-
-    expect(setVersionSkipped).toBeCalledWith(undefined, '1.1')
-  })
+  // it('should render details when expanded ', async () => {
+  //   initializeTestInstance('1.1', '1.0')
+  //   await waitFor(() => fireEvent.press(screen.getByRole('tab', { name: t('whatsNew.title') })))
+  //   expect(screen.getByText(NEWS_LIST.SSO_TEXT)).toBeTruthy()
+  //   expect(screen.getByText(NEWS_LIST.BULLET1)).toBeTruthy()
+  //   expect(screen.getByText(NEWS_LIST.BULLET2)).toBeTruthy()
+  // })
+  //
+  // it('should set the next skip version when dismissed ', async () => {
+  //   initializeTestInstance('1.1', '1.0')
+  //   expect(setVersionSkipped).not.toBeCalled()
+  //
+  //   await waitFor(() => fireEvent.press(screen.getByRole('tab', { name: t('whatsNew.title') })))
+  //   await waitFor(() => fireEvent.press(screen.getByRole('button', { name: t('whatsNew.dismissMessage') })))
+  //
+  //   expect(setVersionSkipped).toBeCalledWith(undefined, '1.1')
+  // })
 })
