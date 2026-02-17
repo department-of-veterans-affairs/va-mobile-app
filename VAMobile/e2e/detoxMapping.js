7b5bf0e12d20004628ed29bed4e6e35445e9add1
John Marchi <johnmarchi@John-M-MBP-M1M-2021.local>
2026-02-17 08:53:46 -0800

    alphabatizing

--- VAMobile/e2e/detoxMapping.js
+++ VAMobile/e2e/detoxMapping.js
@@ -11,7 +11,6 @@ const fullTestRun = [
   'AppointmentsTestTime',
   'AvailabilityFramework',
   'Claims',
-  'ReviewClaimDetails',
   'ContactInformation',
   'DecisionLetters',
   'DirectDeposit',
