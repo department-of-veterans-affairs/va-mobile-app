0e1d8f468aad057ce0119b81fdd690e05cdd71e8
Narin Ratana <narin@themostudio.com>
2026-02-20 18:55:53 -0800

    Rorder conditional checks for feature ID lookups and add a try/catch block for more robust element visibility and scrolling.

--- VAMobile/e2e/tests/NavigationShared.ts
+++ VAMobile/e2e/tests/NavigationShared.ts
@@ -136,8 +136,8 @@ export const navigateToPage = async (key, navigationDicValue) => {
   }
   const navigationArray = navigationDicValue
   if (typeof navigationArray[1] === 'string') {
-    if (navigationArray[1] in featureID) {
-      scrollID = featureID[navigationArray[1]]
+    if (key in featureID) {
+      scrollID = featureID[key]
       await waitFor(element(by.id(scrollID)))
         .toExist()
         .withTimeout(5000)
@@ -145,8 +145,8 @@ export const navigateToPage = async (key, navigationDicValue) => {
         .toBeVisible()
         .whileElement(by.id(scrollID))
         .scroll(50, 'down')
-    } else if (key in featureID) {
-      scrollID = featureID[key]
+    } else if (navigationArray[1] in featureID) {
+      scrollID = featureID[navigationArray[1]]
       await waitFor(element(by.id(scrollID)))
         .toExist()
         .withTimeout(5000)
@@ -177,9 +177,7 @@ export const navigateToPage = async (key, navigationDicValue) => {
           .toBeVisible()
           .whileElement(by.id(scrollID))
           .scroll(50, 'down')
-      }
-
-      if (subNavigationArray[k] in featureID) {
+      } else if (subNavigationArray[k] in featureID) {
         scrollID = featureID[subNavigationArray[k]]
         await waitFor(element(by.id(scrollID)))
           .toExist()
@@ -197,10 +195,14 @@ export const navigateToPage = async (key, navigationDicValue) => {
       await waitFor(element(by.text('AMLODIPINE BESYLATE 10MG TAB')))
         .toBeVisible()
         .withTimeout(10000)
-      await waitFor(element(by.text('Get prescription details')).atIndex(0))
-        .toBeVisible()
-        .whileElement(by.id(CommonE2eIdConstants.PRESCRIPTION_HISTORY_SCROLL_ID))
-        .scroll(100, 'down')
+      try {
+        await expect(element(by.text('Get prescription details')).atIndex(0)).toBeVisible()
+      } catch (e) {
+        await waitFor(element(by.text('Get prescription details')).atIndex(0))
+          .toBeVisible()
+          .whileElement(by.id(CommonE2eIdConstants.PRESCRIPTION_HISTORY_SCROLL_ID))
+          .scroll(100, 'down')
+      }
       await element(by.text('Get prescription details')).atIndex(0).tap()
       return
     } else if (subNavigationArray.slice(-1)[0] === 'Received June 12, 2008') {
