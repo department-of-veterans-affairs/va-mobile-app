bd23cff7e8e2700025b4c24ab9f52ae9e6c67c05
Eli Mellen <3342274+eli-oat@users.noreply.github.com>
2026-03-12 11:57:46 -0400

    Quick fix to address comment left by copilot, now there is a single source of truth.

--- VAMobile/src/components/FloatingButton.tsx
+++ VAMobile/src/components/FloatingButton.tsx
@@ -1,12 +1,11 @@
 import React, { FC } from 'react'
-import { ViewStyle, useWindowDimensions } from 'react-native'
+import { ViewStyle } from 'react-native'
 
-import { Button, ButtonProps, useIsScreenReaderEnabled } from '@department-of-veterans-affairs/mobile-component-library'
+import { Button, ButtonProps } from '@department-of-veterans-affairs/mobile-component-library'
 import { colors } from '@department-of-veterans-affairs/mobile-tokens'
 
 import { Box } from 'components'
-import { FAB_INLINE_FONT_SCALE_THRESHOLD } from 'constants/common'
-import { useTheme } from 'utils/hooks'
+import { useInlineFab, useTheme } from 'utils/hooks'
 
 /**
  *  Signifies the props that need to be passed in to {@link FloatingButton}
@@ -22,12 +21,7 @@ export type FloatingButtonProps = {
  */
 const FloatingButton: FC<FloatingButtonProps> = ({ isHidden, ...buttonProps }: FloatingButtonProps) => {
   const theme = useTheme()
-  const screenReaderEnabled = useIsScreenReaderEnabled()
-  const fontScale = useWindowDimensions().fontScale
-
-  // Render in-place if screen reader enabled OR font scale is in a clearly large-text range.
-  // useWindowDimensions().fontScale is reactive to iOS Dynamic Type changes.
-  const shouldRenderInPlace = screenReaderEnabled || fontScale >= FAB_INLINE_FONT_SCALE_THRESHOLD
+  const shouldRenderInPlace = useInlineFab()
 
   const floatingButtonStyle: ViewStyle = {
     position: 'absolute',
